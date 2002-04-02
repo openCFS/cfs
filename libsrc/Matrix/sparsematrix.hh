7e6d0fc3f5183a5e130fd6ff4505da3089620c3a
elena <elena@cfs-dev.mdmt.tuwien.ac.at>
2002-04-02 11:59:04 +0000

    new: complete class SparseMatrix

--- libsrc/Matrix/sparsematrix.hh
+++ libsrc/Matrix/sparsematrix.hh
@@ -10,14 +10,14 @@ namespace CoupledField
 template<class TYPE>
 struct ElemSparseMatrix
 {
-//! number of column
-Integer column;
+  //! number of column
+  Integer column;
 
-//! value
-TYPE value;
+  //! value
+  TYPE value;
 
-//! pointer to next element
-ElemSparseMatrix<TYPE> * next;
+  //! pointer to next element
+  ElemSparseMatrix<TYPE> * next;
 };
 
 template<class TYPE>
@@ -40,7 +40,6 @@ public:
   Integer GetRow() const { return numrows;}
   /// Return number of colomns
   Integer GetCol() const {return numcol;}
-
   //// access to elements
   TYPE & operator()(Integer row,Integer col);
 
@@ -70,26 +69,26 @@ public:
   ///
   Integer operator!=      (const SparseMatrix<TYPE> &) const;
 
-template<class S>
-friend std::ostream & operator<< (std::ostream& , const SparseMatrix<S>&);
+  template<class S>
+  friend std::ostream & operator<< (std::ostream& , const SparseMatrix<S>&);
 
 private:
- //! number of rows
- Integer numrows;
- //! number of column
- Integer numcol;
- //! a pointer to a pointer to Elements
- ElemSparseMatrix<TYPE> ** ptRow; 
+  //! number of rows
+  Integer numrows;
+  //! number of column
+  Integer numcol;
+  //! a pointer to a pointer to Elements
+  ElemSparseMatrix<TYPE> ** ptRow; 
 
- //! Auxialary function for copy matrix 
- void Copy(const SparseMatrix<TYPE> & x);
- //! Auxialary function for inserting element
- TYPE & InsertElement(ElemSparseMatrix<TYPE> * ptElem,const Integer row, const Integer col, const Integer first);
- //! Auxialary function for deleting element
- void DeleteElement(const Integer i, const Integer j);
+  //! Auxialary function for copy matrix 
+  void Copy(const SparseMatrix<TYPE> & x);
+  //! Auxialary function for inserting element
+  TYPE & InsertElement(ElemSparseMatrix<TYPE> * ptElem,const Integer row, const Integer col, const Integer first);
+  //! Auxialary function for deleting element
+  void DeleteElement(const Integer i, const Integer j);
 
 public:
- void test();
+  void test();
 
 };
 
@@ -100,12 +99,13 @@ std::ostream & operator << ( std::ostream & , const SparseMatrix<TYPE> &);
 template<class TYPE>
 inline SparseMatrix<TYPE>::~SparseMatrix()
 {
- if (ptRow) delete [] ptRow;
+  if (ptRow) delete [] ptRow;
 }
 
 #ifdef __GNUC__
 template class SparseMatrix<Double>;
 #endif
+
 } // end of namespace
 
 #endif
