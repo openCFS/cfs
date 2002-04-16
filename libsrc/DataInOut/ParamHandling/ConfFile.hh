b1f6ffb5b47baf20c622af327bc24737ec6f572d
Elena Zhelezina <elena@svn>
2002-04-16 12:39:44 +0000

    fix: small changes concerned template in class ConfFile

--- libsrc/DataInOut/ParamHandling/ConfFile.hh
+++ libsrc/DataInOut/ParamHandling/ConfFile.hh
@@ -15,14 +15,9 @@ public:
   //! deconstructor
   ~ConfFile(); 
 
-  //! get from conf-file Double value  
-  void get(const std::string keyword, Double & val);
- 
-  //! get from conf-file Integer value
-  void get(const std::string keyword, Integer & val);
-
-  //! get from conf-file string value
-  void get(const std::string keyword, std::string & val);
+  //! get from conf-file TypeVal(string, integer, double) value
+  template<class TypeVal>
+  void get(const std::string keyword, TypeVal & val);
 
 protected:
 
