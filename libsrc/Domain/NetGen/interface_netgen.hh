f0f7d972ee5bcb0efc35eee67220773748c91ac9
elena <elena@cfs-dev.mdmt.tuwien.ac.at>
2002-05-02 17:34:15 +0000

    new: interface with netgen

---
+++ libsrc/Domain/NetGen/interface_netgen.hh
@@ -0,0 +1,83 @@
+#ifndef FILE_INTERFACE_NetGen_2002
+#define FILE_INTERFACE_NetGen_2002
+
+#include "filetype.hh"
+#include "grid.hh"
+
+namespace CoupledField
+{
+ 
+/// Interface to library NetGen
+template<class Dim>
+class InterfaceNetGen: public Grid<Dim>
+{
+public:
+  /// Constructor with parameter - pointer to FileType for reading initial grid
+  InterfaceNetGen(FileType * const aptFileType);
+
+  /// Deconstructor
+  virtual ~InterfaceNetGen(){;}
+  
+   /// Uniform subdivision of domain
+  virtual void SubdivideUniform(const Integer level);
+
+  /// Get coordinates of all nodes which belong to element
+  virtual void GetCoordOfNodesElem(const Integer numElem, const Integer numlevel, const Integer numnodes, Dim * ptCoordElem); 
+
+   /// Get coordinates of node with global number inode
+   virtual void GetCoordinateNode(const Integer inode, const Integer numlevel, Dim & rfPoint);
+
+   /// Get connection of element
+  virtual void GetConnection(Integer * result, const Integer level, 
+           const Integer numElem, const Integer numnodesPerElem);
+
+  /// Return maximum number of nodes
+  virtual Integer GetMaxnumnodes(const Integer numlevel);
+
+  /// Return maximum number of elements 
+  virtual Integer GetMaxnumElem(const Integer numlevel);
+
+  /// Return num of nodes per element i
+  virtual Integer GetNumNodesPerElem(const Integer iElem, const Integer level);
+
+  /// Print coordinates of grid in out
+  virtual void PrintCoordinate(const Integer level, std::ostream * out) const;
+
+  /// Put information about grid
+  void Read();
+
+  //! Put global numbers of nodes with boundary condition in Vector
+  void GetNodesBoundaryCondition(Vector<Integer> & nodesDirBC, const Integer level);
+
+private:
+
+  //! 
+  FileType * ptFileType;
+      
+  //! 
+  Mesh mesh;
+
+  //! if we do subdivision, then this variable is TRUE
+  Boolean DoesGridSubdivide;
+};
+
+template<class Dim>
+inline InterfaceNetGen<Dim>::InterfaceNetGen(FileType * aptFileType)
+: Grid<Dim>(aptFileType)
+{
+#ifdef TRACE
+ (*trace) << "Entering InterfaceNetGen<Dim>::InterfaceNetGen<Dim>" << std::endl;
+#endif
+
+  ptFileType=aptFileType;
+  DoesGridSubdivide=FALSE;
+
+}
+
+#ifdef __GNUC__
+//template class InterfaceNetGen<Point3D>;
+template class InterfaceNetGen<Point2D>;
+#endif
+
+} // end of namespace
+#endif // 
