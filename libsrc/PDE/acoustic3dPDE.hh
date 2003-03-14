542ec8a109cd59d0265cb9bfcc87254fbd28291c
Manfred Kaltenbacher <manfred.kaltenbacher@tuwien.ac.at>
2003-03-14 13:50:55 +0000

    *** empty log message ***

---
+++ libsrc/PDE/acoustic3dPDE.hh
@@ -0,0 +1,190 @@
+#ifndef FILE_ACOUSTIC3DPDE_2003
+#define FILE_ACOUSTIC3DPDE_2003
+
+#include "basepde.hh"
+
+namespace CoupledField
+{
+
+  //! Class for acoustic equation
+  /*! 
+    This class is derived from class BasePDE. It is used for solving acoustic equation on one time step.  We set rules for assembling global system matrix according to weak form of PDE, define right hand side and set boundary conditions. Then we cause one of methods of LinSystem for solving linear system. On the last step we calculate first and second derivatives of the solution.
+  */
+
+class Acoustic3dPDE: virtual public BasePDE
+{
+public:
+
+  //  WriteResultsUnverg * ptOutput;
+
+  //!
+  Acoustic3dPDE(AbstractAlgebraicSys * aptalgsys, Grid * , Material * , TimeFunc * ,FileType * , WriteResults * );
+
+  //!
+  virtual ~Acoustic3dPDE();
+
+  //!
+  void SpecifySolver(Integer &solvertype, Integer &precondtype, Double &eps, Double &dampiter, Integer &maxnumit, 
+		     Integer &numeqcoarse, Double &coarsealpha);
+
+  //!
+  void SpecifyMatrices(Integer &matrixtype, Integer *matrixsystype, Integer &graphtype, Integer &numdofpernode, Integer &numdirichlets, Integer &numconstraints);
+
+  //!
+  void SetMatrixFactors();
+
+  //!
+  void SetupMatrices(const Integer level);
+
+    //!
+  void SetBCs(BCs * ptBCs, const Integer level, const Integer update, const Double atime);
+
+  //!
+  void ComputeRHS(const Double atime, BCs * ptBCs=NULL);
+
+  //!
+  void preComputeRHS();
+
+	//! recovery solution for SPR
+  void RecoverySol(Vector<Double> & result);
+
+  //!
+  void ComputeRHS4RecoverySol();
+
+  //! calculation derivates of solution
+  void CalculationDerivativesSol(const Boolean Recalc);
+  void CalcDerSol();
+  
+  //! create pointer to class for time error estimation
+  virtual TimeErrorEstimator * CreatePtTimeError();  
+
+  //! Calculation of energy norm
+  virtual Double CalcEnergyNorm();
+
+  //!
+  void SolveStepStatic(BCs * ptBCs ,const Integer level);
+
+  //!
+  void SolveStepTrans(BCs * ptBCs ,const Integer kstep, const Double steptime, const Integer level, const Boolean updatesysmat);
+
+  //!
+   void SolveStepTransNewMesh(BCs * ptBCs, const Integer kstep, const Double asteptime, const Integer level);
+
+  //!
+  void RestoreSol();
+
+  //!
+  void SaveSolAsPrevStep();
+
+  //!
+   void WriteResultsInFile();
+
+  //!
+  virtual const Vector<Double> & getS() const { return sol_;}
+
+  //!
+  virtual const Vector<Double> & getS1() const { return sol_der1_;}
+
+  //!
+  virtual const Vector<Double> & getS1old() const { return sol_der1_old_;}
+
+  //!
+  virtual const Vector<Double> & getS2() const { return sol_der2_;}
+
+  //!
+  virtual const Vector<Double> & getS2old() const { return sol_der2_old_;}
+
+  //!
+  virtual Integer getSize() const { return size_;}
+
+  //!
+  Double getBeta() const { return beta_;}
+  Double getGamma() const { return gamma_;}
+
+  //! We use this function for time-error estimation
+  void CalcThirdDerivateFromEquation(Vector<Double> & result);
+
+private:
+  //!
+  Integer dofspernode_;
+
+  //!
+  Grid * ptgrid_;
+
+  //! Calculation parameters for Newmark method
+  virtual void CalcParameters(const Double dt);
+
+  //!
+  void CalcCoeff(Vector<Double> & coeffmass, Vector<Double> & coeffstiff);
+
+  //!
+  Double a0_,a1_,a2_,a3_,a4_,a5_,a6_,a7_;
+
+  //! Integration parameters
+  Double alpha_,gamma_, beta_;
+
+  //! store solution, 1st derivative , 2nd derivative solution
+  Vector<Double> sol_, sol_der1_, sol_der2_, sol_old_, sol_der1_old_, sol_der2_old_;  
+
+  Vector<Double> s_oldold_, s_d1_oldold_, s_d2_oldold_;
+
+  //! Last time on which we have calculated solution
+  Double lasttimecalc_;
+
+  //! Number of last timestep on which we have calculated our solution
+  Integer laststepcalc_;
+
+  //! size of solution and etc.
+  Integer size_;
+
+  //! list of surfaces, on which we have force
+  std::vector<std::string> rhs_surfaces_;
+
+  //! function for RHS
+  Integer arg_rhs_;
+//  pfn1var ptRHSFnc_;
+  std::vector<Double> directionFnc_;
+
+  //! Indicator: is there RHS function
+  Boolean SetRHSFnc;
+
+  //! pointer to class of error estimators
+  SpaceErrorEstimator<3> * ptError_;
+
+};
+
+// class PutElemMatInAlgSys
+// {
+// public:
+
+//   PutElemMatInAlgSys(AbstractAlgebraicSys * aptalgsys, Grid * aptgrid, const Double acoeffm, const Double acoeffs, const Integer as_sysid, const Integer alevel)
+//   { sysid_=as_sysid; ptalgsys_=aptalgsys; ptgrid_=aptgrid;
+//     coeffm_=acoeffm; coeffs_=acoeffs; level_=alevel;
+//     matrix_stiff_=2;  matrix_mass_=5; }
+
+//   ~PutElemMatInAlgSys() {}
+
+//   // method
+//   void operator() (Elem t);
+
+// private:
+
+//      //!
+//   Grid * ptgrid_;
+
+//      //!
+//   AbstractAlgebraicSys * ptalgsys_;
+
+//      //!
+//   Integer sysid_, level_;
+
+//      //!
+//   Double coeffm_, coeffs_;
+
+//   //!
+//   Integer matrix_stiff_, matrix_mass_;
+
+// };
+
+} // end of namespace
+#endif
---
+++ libsrc/PDE/pdes_header.hh
@@ -0,0 +1,10 @@
+#ifndef FILE_PDES_HEADER_2003
+#define FILE_PDES_HEADER__2003
+
+#include "acoustic2dPDE.hh"
+#include "acoustic3dPDE.hh"
+#include "therm2dPDE.hh"
+#include "elecst2dPDE.hh"
+#include "elecst3dPDE.hh"
+
+#endif
