6d2ab56c5ae2a722fd4f41ad8003a4416c1bc595
Manfred Kaltenbacher <manfred.kaltenbacher@tuwien.ac.at>
2003-03-27 15:06:14 +0000

    *** empty log message ***

---
+++ libsrc/PDE/mech2d.cc
@@ -0,0 +1,402 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <math.h>
+
+#include "mech2d.hh"
+#include "actimeerror.hh"
+#include "outUnverg.hh"
+#include "outGMV.hh"
+#include "forms_header.hh"
+#include "spaceerror.hh" 
+
+
+
+namespace CoupledField
+{
+
+
+Mech2dPDE::Mech2dPDE(AbstractAlgebraicSys * ptalgsys, Grid * aptgrid, Material * ptMaterial, TimeFunc * aptTimeFunc, FileType * aptFileType, WriteResults * aptOut)
+:BasePDE(ptalgsys,ptMaterial,aptFileType,aptOut,aptTimeFunc)
+{
+#ifdef TRACE
+  (*trace) << "entering Mech2dPDE::Mech2dPDE " << std::endl;
+#endif
+
+  dofspernode_=2;
+  ptgrid_=aptgrid; 
+
+  laststepcalc_=0;
+  size_=ptgrid_->GetMaxnumnodes(0);
+
+  sol_.Resize(size_*dofspernode_);
+  sol_.Init(0);
+
+  sol_der1_.Resize(size_*dofspernode_);
+  sol_der1_.Init(0);
+
+  sol_der2_.Resize(size_*dofspernode_);
+  sol_der2_.Init(0);
+
+  //  conf->get("alpha_NM",alpha_,"Mech2d"); 
+  //  conf->get("beta_NM",beta_,"Mech2d"); 
+  //  conf->get("gamma_NM",gamma_,"Mech2d");
+
+  conf->getsubdompde(subdoms_,"Mech2d");
+  ReadBCs("Mech2d");
+
+}
+
+void Mech2dPDE::SpecifySolver(Integer &solvertype, Integer &precondtype, Double &eps, Double &dampiter, Integer &maxnumit, Integer &numeqcoarse, Double &coarsealpha)
+{
+#ifdef TRACE
+  (*trace) << "entering Mech2dPDE::SpecifySolver" << std::endl;
+#endif
+
+  conf->get("eps",eps,"Mech2d"); // relative accuracy in the precond. energy
+  conf->get("dampiter",dampiter,"Mech2d"); // damping parameter for Jacobi, SSOR
+  conf->get("maxnumit",maxnumit,"Mech2d"); // max number of iterations
+  conf->get("solvertype",solvertype,"Mech2d"); // Richardson or CG
+  conf->get("precondtype", precondtype, "Mech2d"); // ID or MG
+  conf->get("numeqcoarse",numeqcoarse,"Mech2d"); // number of equation for coarsing
+  conf->get("coarsealpha",coarsealpha,"Mech2d"); // coarsing parameter for AMG
+}
+
+
+void Mech2dPDE::SetMatrixFactors()
+{
+#ifdef TRACE
+  (*trace) << "entering Mech2dPDE::SetMatrixFactors" << std::endl;
+#endif
+ 
+  matrix_factor_[0] = 1.0;  // stiffness
+  matrix_factor_[1] = 0.0;  // damping
+  matrix_factor_[2] = 0.0;  //convection
+  matrix_factor_[3] = 0.0; //mass
+}
+
+void Mech2dPDE::SpecifyMatrices(Integer &matrixtype, Integer * matrixsystype, Integer &graphtype, Integer &numdofpernode, Integer &numdirichlets,
+Integer &numconstraints)
+{
+#ifdef TRACE
+  (*trace) << "entering Mech2dPDE::SpecifyMatrices" << std::endl;
+#endif
+
+  matrixtype = RBLOCK;
+
+  matrixsystype[0] = 1;   // memory for the system matrix
+  matrixsystype[1] = 2;   // memory for the stiffness matrix
+  matrixsystype[2] = 0;   // memory for the damping matrix
+  matrixsystype[3] = 0;   // memory for the convection matrix
+  matrixsystype[4] = 5;   // memory for the mass matrix
+
+  graphtype = NODEGRAPH;
+
+  numdofpernode  = dofspernode_;
+
+  //not used; just for interface
+  numdirichlets  = 1;
+  numconstraints = 0;
+}
+
+void Mech2dPDE::SetupMatrices(const Integer level, BCs * ptBCs)
+{
+#ifdef TRACE
+  (*trace) << "entering Mech2dPDE::SetupMatrices" << std::endl;
+#endif
+
+  Matrix<Double> elemmat;
+  Point<2> * ptCoord; 
+
+  BaseElem * ptEl;
+
+  //get material tensor 
+  Vector<Double> coeffm, coeffst;
+  //  CalcCoeff(coeffm, coeffst);
+
+  Vector<Integer> connecth;
+  std::vector<Elem*> elemssd;
+
+  Integer i, j;
+  for (i=0; i<subdoms_.size(); i++)
+    {
+      ptgrid_->GetElemSD(elemssd,subdoms_[i],level);
+
+      for (j=0; j< elemssd.size(); j++)
+	{
+	  ptEl=elemssd[j]->ptElem;
+
+	  BaseForm<2> * bilinear_stiff = new LaplaceInt<2>(ptEl,1);
+	  BaseForm<2> * bilinear_mass  = new MassInt<2>(ptEl,dofspernode_);
+
+	  Integer ii;
+	  Integer elsize=(elemssd[j]->connect).size();
+	  connecth.Resize(elsize);
+	  for (ii=0; ii<elsize; ii++)
+	    connecth[ii]=(elemssd[j]->connect)[ii];
+  
+	  ptCoord=new Point<2>[connecth.size()];
+	  ptgrid_->GetCoordNodesElem(connecth,ptCoord,level); 
+
+	  // stiffness part
+	  bilinear_stiff->CalcElemMatrix(ptCoord, elemmat);
+
+          Integer blocksize = ptEl->GetNumNodes()*dofspernode_;
+
+          Matrix<Double> blockelemmat(blocksize,blocksize);
+	  blockelemmat.Init();
+	  Integer off = connecth.size();
+          for (Integer kr=0;kr<off;kr++)
+	    for (Integer kc=0;kc<off;kc++)
+	      {
+		blockelemmat[kr*2][kc*2] = elemmat[kr][kc];
+		blockelemmat[kr*2+1][kc*2+1] = elemmat[kr][kc];
+	      }
+          
+	  // multiply with material
+	  //	  elemmat*=coeffst[i];
+
+#ifdef DEBUG
+	  (*debug) << "Connection array  " << std::endl;
+	  (*debug)  << connecth  << std::endl;
+
+	  (*debug) << "Stiffnessmatrix, ElementNumber  " <<   j << std::endl;
+	  (*debug) << blockelemmat << std::endl;
+
+
+#endif     
+
+	  ptalgsys_->PutElemMatAlgSys(blockelemmat.getinarray(), connecth.get(), connecth.size(), as_sysid_, 
+				      as_sysid_, SYSTEM);
+
+	  // mass part
+	  bilinear_mass->CalcElemMatrix(ptCoord, elemmat);
+
+	  //	  elemmat*=coeffm[i];
+
+	  blockelemmat.Init();
+          for (Integer kr=0;kr<off;kr++)
+	    for (Integer kc=0;kc<off;kc++)
+	      {
+		blockelemmat[kr][kc] = elemmat[kr][kc];
+		blockelemmat[kr+off][kc+off] = elemmat[kr][kc];
+	      }
+
+#ifdef DEBUG
+	  (*debug) << "Massmatrix, ElementNumber  " << j << std::endl;
+
+	  (*debug) <<blockelemmat << std::endl;
+#endif
+      
+	  //	  ptalgsys_->PutElemMatAlgSys(blockelemmat.getinarray(), connecth.get(), connecth.size(), as_sysid_, 
+	  //				      as_sysid_,MASS);
+
+  
+	  delete bilinear_stiff;
+	  delete bilinear_mass;
+	  delete [] ptCoord;
+	}
+    }
+#ifdef TRACE
+  (*trace) << "Leaving Mech2dPDE::SetupMatrices" << std::endl;
+#endif
+}
+
+void Mech2dPDE::SetBCs(BCs * ptBCs, const Integer level, const Integer update, const Double atime)
+{
+#ifdef TRACE
+  (*trace) << "entering Mech2dPDE::SetBCs" << std::endl;
+#endif
+
+  Integer node,dof;
+  Double val;
+
+  std::string dof_ux = "ux";
+  std::string dof_uy = "uy";
+
+  // set dirichlet boundary conditions
+  Integer i,j=0;
+  std::list<Integer> nodes;
+  Integer sizebc=bcs_hd_.size();
+
+  // homogeneous boundary conditions
+  val = 0;
+  for (i=0; i< bcs_hd_.size(); i++)
+    {
+      std::string doftype = bcs_hd_[i]; 
+      if (doftype.substr(0,2)==dof_ux)
+	dof = 1;
+      else 
+	if (doftype.substr(0,2)==dof_uy)
+	dof = 2;
+      else
+	Error("Unknown dof-type in homog. BC; substring is:must start with ux or uy!!",__FILE__,__LINE__);
+      
+#ifdef DEBUG
+      (*debug) << " Homog. Dirichlet BC" << std::endl;
+#endif
+      nodes=ptBCs->GetNodesLevel(bcs_hd_[i]);
+   
+      for (std::list<Integer>::const_iterator p=nodes.begin(); p!=nodes.end(); p++, j++)
+	{
+	  node=*p;
+	  if (update==1)
+	    {
+#ifdef DEBUG
+	      (*debug) << " node: " << node << " dof:" << dof << " val: " << val << std::endl;
+#endif
+      
+      ptalgsys_->SetBCDirichletUpdate(i+1, node, val, dof, as_sysid_, as_sysid_, SYSTEM);
+
+	    }
+	  else
+	    { 
+#ifdef DEBUG
+	      (*debug) << " node: " << node << " dof:" << dof << " val: " << val << std::endl;
+#endif
+
+      ptalgsys_->SetBCDirichlet(j+1, node, val, dof, as_sysid_, as_sysid_, SYSTEM);
+
+	    }
+	}  
+    }
+
+  //inhomogeneous boundary conditions
+  for (i=0; i<bcs_id_.size(); i++)
+    {
+      std::string doftype = bcs_hd_[i]; 
+      if (doftype.substr(0,2)==dof_ux)
+	dof = 1;
+      else 
+	if (doftype.substr(0,2)==dof_uy)
+	dof = 2;
+	else
+	  Error("Unknown dof-type in inhomog. BC; substring is:must start with ux or uy!!",__FILE__,__LINE__);
+      
+#ifdef DEBUG
+      (*debug) << " Inhomog. Dirichlet BC" << std::endl;
+#endif
+
+      nodes=ptBCs->GetNodesLevel(bcs_id_[i], level);
+
+      val=val_id_[i];
+
+      for (std::list<Integer>::const_iterator p=nodes.begin(); p!=nodes.end(); p++, j++)
+	{
+	  node=*p;
+          if (update==1)
+            {
+
+#ifdef DEBUG
+	      (*debug) << " node: " << node << " dof:" << dof << " val: " << val << std::endl;
+#endif
+
+              ptalgsys_->SetBCDirichletUpdate(j+1, node, val, dof, as_sysid_, as_sysid_, SYSTEM);
+            }
+          else
+            {
+
+#ifdef DEBUG
+	      (*debug) << " node: " << node << " dof:" << dof << " val: " << val << std::endl;
+#endif
+              ptalgsys_->SetBCDirichlet(j+1, node, val, dof, as_sysid_,as_sysid_, SYSTEM);
+            }
+	}
+    }
+
+#ifdef TRACE
+  (*trace) << "leaving Mech2dPDE::SetBCs" << std::endl;
+#endif
+}
+
+
+void Mech2dPDE::SolveStepStatic(BCs * ptBCs, Integer level)
+{
+#ifdef TRACE
+  (*trace) << "entering Mech2dPDE::SolveStepStatic" << std::endl;
+#endif
+
+  Integer update = 0;
+  Integer job = 1;
+
+  SetupMatrices(level);
+  SetBCs(ptBCs,level,update,0);
+  //  ptalgsys_->ComputeSysMatrix(as_sysid_,as_sysid_,matrix_factor_);
+  ptalgsys_->ComputePrecond(job,as_sysid_);
+  ptalgsys_->SolveAlgSys(as_sysid_);
+
+  // get solution
+  Double * ptsol;
+  ptsol = ptalgsys_->GetSolution(as_sysid_);
+
+  // save solution
+  Vector<Double> transsol(ptgrid_->GetMaxnumnodes(level)*dofspernode_, ptsol);
+  sol_=transsol;
+}
+
+void Mech2dPDE::SolveStepTrans(BCs * ptBCs, const Integer kstep, const Double asteptime, const Integer level, const Boolean reset)
+{
+#ifdef TRACE
+  (*trace) << "entering Mech2dPDE::SolveStepTrans" << std::endl;
+#endif
+
+}
+
+void Mech2dPDE::WriteResultsInFile()
+{
+#ifdef TRACE
+  (*trace) << "entering Mech2dPDE::WriteResultsInFile" << std::endl;
+#endif
+
+  if (OutFile_->IsGMV())
+    {
+      OutFile_->WriteSolution(sol_,laststepcalc_,lasttimecalc_,"disp");
+      //      OutFile_->WriteSolution(sol_der1_,laststepcalc_,lasttimecalc_,"vp_1der");
+      //      OutFile_->WriteSolution(sol_der2_,laststepcalc_,lasttimecalc_,"vp_2der");
+    }
+  else
+    {
+      OutFile_->WriteSolution(sol_,laststepcalc_,lasttimecalc_,"disp");
+      //      OutFile_->WriteSolution(sol_der1_,laststepcalc_,lasttimecalc_,"fluid potential, 1st deriv., ");
+      //      OutFile_->WriteSolution(sol_der2_,laststepcalc_,lasttimecalc_,"fluid potential, 2nd deriv., ");
+    }
+  
+}
+
+void Mech2dPDE :: CalcParameters(const Double dt)
+{
+#ifdef TRACE
+  (*trace) << "entering Mech2dPDE::CalcParameters" << std::endl;
+#endif
+
+  a2_=1.0/(beta_*dt);
+  a0_=a2_*(1/dt);
+  a1_=gamma_*a2_;
+  a3_=0.5/(beta_)-1.0;
+  a4_=gamma_/beta_-1.0;
+  a5_=0.5*dt*(a4_-1.0);
+  a6_=dt*(1-gamma_);
+  a7_=gamma_*dt;
+}
+
+void Mech2dPDE::CalcCoeff(Vector<Double> & coeffmass, Vector<Double> & coeffstiff)
+{
+  if (!MatFile_) Error("You didn't specialize material file. Check your config-file.");
+
+  coeffmass.Resize(subdoms_.size());
+  coeffstiff.Resize(subdoms_.size());
+ 
+  Integer i,matnum;
+  for (i=0; i<subdoms_.size(); i++)
+    {
+      conf->get(subdoms_[i],matnum,"list_subdomains");
+
+      // read density and compress with material number matnum
+      Double density, compress;
+      MatFile_->ReadDensityAndCompressity(density,compress,matnum,"fluid");
+
+      coeffmass[i]  = density*density/compress;
+      coeffstiff[i] = density;
+    }
+}
+}
---
+++ libsrc/PDE/mech2d.hh
@@ -0,0 +1,146 @@
+#ifndef FILE_MECHANIC2DPDE_2001
+#define FILE_MECHANIC2DPDE_2001
+
+#include "basepde.hh"
+
+ 
+namespace CoupledField
+{
+
+  //! Class for mechanic equation 2D
+  /*! 
+    This class is derived from class BasePDE. It is used for solving acoustic equation on one time step.  We set rules for assembling global system matrix according to weak form of PDE, define right hand side and set boundary conditions. Then we cause one of methods of LinSystem for solving linear system. On the last step we calculate first and second derivatives of the solution.
+  */
+
+  class Mech2dPDE: public BasePDE
+  {
+  public:
+    
+    //!  Constructor. here we read integration parameters
+    /*!
+      \param  aptalgsys pointer to class Algebraic system
+      \param aGrid pointer to grid
+      \param aMatFile pointer to class Material. material data.
+      \param aInFile pointer to class FileType. input data.
+      \param aOutFile  pointer to class WriteResults. output data.
+      \param aTimeFunc pointer to class TimeFunc
+    */
+    Mech2dPDE(AbstractAlgebraicSys * aptalgsys, Grid * aGrid , Material * aMatFile, TimeFunc * aTimeFunc ,FileType * aInFile, WriteResults * aOutFile );
+
+    //!  Deconstructor
+    virtual ~Mech2dPDE(){;}
+
+    //! specify type of solver for algebraic system. it is read from config-file
+    /*!
+      \param solvertype  Richardson or CG
+      \param precondtype ID or MG
+      \param eps relative accuracy in the precond. energy
+      \param dampiter damping parameter for Jacobi, SSOR
+      \param maxnumit max number of iterations
+       \param numeqcoarse number of equation for coarsing
+      \param coarsealpha coarsing parameter for AMG
+    */
+    virtual void SpecifySolver(Integer &solvertype, Integer &precondtype, Double &eps, Double &dampiter, Integer &maxnumit, 
+			       Integer &numeqcoarse, Double &coarsealpha);
+
+    //! specify type of system matrix for AlgebraicSystem
+    /*!
+      \param matrixtype out: 0..NOCLASS, 1..RSPARSE, 2..CSPARSE, 3..RBLOCK, 4.. CBLOCK = 0,
+      5..RFULL, 6..CFULL, 7..MIXED
+      \param matrixsystype out:define need we memory for different types of element-matrix or not                     
+      \param graphtype out: type of graph
+      \param numdofpernode out: number of dof per node
+      \param numdirichlets out:number of nodes for dirichlets conditions
+      \param numconstraints out:number of nodes for constraints conditions
+    */
+    virtual void SpecifyMatrices(Integer &matrixtype, Integer *matrixsystype, Integer &graphtype, Integer &numdofpernode, Integer &numdirichlets, Integer &numconstraints);
+
+    //! set information for algebraic system about PDE. set matrix factors
+    virtual void SetMatrixFactors();
+
+    //! specify type of system matrix for AlgebraicSystem
+    /*!
+      \param level (input) level of Grid
+    */
+    virtual void SetupMatrices(const Integer level, BCs * ptBCs=NULL);
+
+    //! set boundary condition
+    /*!
+      \param ptBCs pointer to boundary condition
+      \param level level of grid
+      \param update indicator: do we update boundary condition in algebraic system ot set new
+      \param atime time of calculation
+    */
+    virtual void SetBCs(BCs * ptBCs, const Integer level, const Integer update, const Double atime);
+
+    /*!
+      \param ptBCs pointer to class with data about boundary condition
+      \param level level of grid
+    */
+    virtual void SolveStepStatic(BCs * ptBCs ,const Integer level);
+
+    //! solve one step for transient problem 
+    /*!
+      \param ptBCs pointer to class with data about boundary condition
+      \param kstep number of calculating step
+      \param steptime time of calculation
+      \param level level of grid
+      \param updatesysmat indicator: need we to update algebraic system. it is used for adaptive procedure in space
+    */
+    virtual void SolveStepTrans(BCs * ptBCs ,const Integer kstep, const Double steptime, const Integer level, const Boolean updatesysmat);
+
+    //! write results in file
+    virtual void WriteResultsInFile();
+
+
+    //! return pointer to vector with solution
+    virtual const Vector<Double> & getS() const { return sol_;};
+
+  protected:
+
+    //!
+    Integer dofspernode_;
+
+    //!
+    Grid * ptgrid_;
+
+    //! Calculation parameters for Newmark method
+    virtual void CalcParameters(const Double dt);
+
+    //!  calculation of coefficient.
+    /*!
+      \param coeffmass coefficient before mass matrix
+      \param coeffstiff coefficient before stiffness matrix
+    */
+    void CalcCoeff(Vector<Double> & coeffmass, Vector<Double> & coeffstiff);
+
+    //! coefficients from Newmark method
+    Double a0_,a1_,a2_,a3_,a4_,a5_,a6_,a7_;
+
+    //! Integration parameters
+    Double alpha_,gamma_, beta_;
+
+    //! store solution, 1st derivative , 2nd derivative solution
+    Vector<Double> sol_, sol_der1_, sol_der2_, sol_old_, sol_der1_old_, sol_der2_old_;  
+
+    //! Last time on which we have calculated solution
+    Double lasttimecalc_;
+
+    //! Number of last timestep on which we have calculated our solution
+    Integer laststepcalc_;
+
+    //! size of solution and etc.
+    Integer size_;
+
+    //! function for RHS
+    Integer arg_rhs_;
+    pfn1var ptRHSFnc_;
+    std::vector<Double> directionFnc_;
+
+    //! Indicator: is there RHS function
+    Boolean SetRHSFnc;
+
+  };
+
+} // end of namespace
+#endif
