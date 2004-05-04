e1ac68e030d41e54666a990c67a3272b0e21614e
Markus Mohr <marcus.mohr@geophysik.uni-muenchen.de>
2004-05-04 12:27:15 +0000

    - Added documentation - Changed names to fit to coding rules (hopefully) - Added a macro TRACE_INDENT which can be used to alter the depth of   indentation in the trace log.

--- libsrc/Utils/tracing.cc
+++ libsrc/Utils/tracing.cc
@@ -5,8 +5,8 @@ namespace CoupledField{
   // Definition and Initialization of static variables of class Debugger
   // has to be outside class scope
 #ifdef TRACE
-  fcn_trace* Debugger::foo_ = NULL;
-  int Debugger::fcn_trace_depth_ = 0;
+  FcnTrace* Debugger::foo_ = NULL;
+  unsigned int Debugger::fcnTraceDepth_ = 0;
 #endif
 }
 
--- libsrc/Utils/tracing.hh
+++ libsrc/Utils/tracing.hh
@@ -5,104 +5,149 @@
 
 #include <iostream>
 
-// namespace OutInfo{
-//   extern std::ostream *trace;
-//}
+
+//! Macro for specifying depth of indentation in function trace log
+#define TRACE_INDENT "  "
+
 
 namespace CoupledField {
 
-//! function tracing is written to this file stream
+  //! Function tracing is written to this file stream
   extern std::ostream *trace;
-//using OutInfo::trace;
 
-//! this class is a list entry for function tracing
-  class fcn_trace{
+
+  // ========================================================================
+  // FCN_TRACE CLASS
+  // ========================================================================
+
+  //! Class for generating a doubly linked list of called functions
+
+  //! This class performs the actual logging of function trace information
+  //! to the trace stream. Each object stores a pointer to its predecessor
+  //! (i.e. the function which called this one) and its successor (i.e. the
+  //! function it called itself). The latter information is dynamic and
+  //! changes, once a called subfunction has terminated.
+  class FcnTrace{
+
   public:
-    fcn_trace(char *name,int depth){
-      this->name_=name;
-      fcn_depth_=depth;
+
+    //! Constructor
+    FcnTrace( char *name, int depth ){
+      this->name_ = name;
+      fcn_depth_ = depth;
     
     if (fcn_depth_<=TRACE){
 	for (int i=0;i<fcn_depth_;i++)
-	  (*trace) << "\t";
+	  (*trace) << TRACE_INDENT;
 	(*trace) << "entering function " << name_ << std::endl;
-      }//if
-    }//fcn_trace
-  
-    ~fcn_trace(){
+      }
+    }
+
+    //! Default destructor
+
+    //! The default destructor is responsible for issuing a "leaving
+    //! function" message to the trace stream object.
+    ~FcnTrace(){
       if (fcn_depth_<=TRACE){
 	for (int i=0;i<fcn_depth_;i++)
-	  (*trace) << "\t";
+	  (*trace) << TRACE_INDENT;
 	(*trace) << "leaving function " << name_ << std::endl;
-      }//if
+      }
       fcn_depth_=0;
       name_=NULL;
-    }//~
-  public:
+    }
+
+    FcnTrace *caller_; //!< Link to FcnTrace object for predecessor
+    FcnTrace *called_; //!< Link to FcnTrace object for successor
+
+  private:
     int fcn_depth_;
-
-    fcn_trace *caller_, *called_;
-
-  protected:
     char *name_;
+
   };
 
-//! this class is static. It controls function tracing
-//! if TRACE is defined
+
+  // ========================================================================
+  // DEBUGGER CLASS
+  // ========================================================================
+
+  //! Central class object for fuction tracing.
+
+  //! This static class is taking care of generating function trace
+  //! information. If will only exist, if the TRACE macro is defined
+  //! during compilation.
   class Debugger {
 
   public:
+
+    //! Init method
     static void Init(){
-      fcn_trace_depth_=0;
+      fcnTraceDepth_ = 0;
       foo_ = NULL;
     }
 
-    //!enter a function
+    //! Handle startup of a function.
+
+    //! This method performs all things needed for tracing at the startup
+    //! of a method.
     static void EnterFcn(char *name){
-      if (!foo_){
-	foo_=new fcn_trace(name,fcn_trace_depth_++);
-	foo_->caller_=NULL;
-	foo_->called_=NULL;
+      if ( foo_ == NULL ){
+	foo_ = new FcnTrace( name, fcnTraceDepth_++ );
+	foo_->caller_ = NULL;
+	foo_->called_ = NULL;
       }
       else{
-	foo_->called_=new fcn_trace(name,fcn_trace_depth_++);
-	foo_->called_->caller_=foo_;
-	foo_=foo_->called_;
+	foo_->called_ = new FcnTrace( name, fcnTraceDepth_++ );
+	foo_->called_->caller_ = foo_;
+	foo_ = foo_->called_;
       }
     }
-    //!leave a function
-    static void LeaveFcn(){
-      fcn_trace *tmp;
-      if (foo_){
-	tmp=foo_->caller_;
-	delete foo_;
-	foo_=tmp;
-	fcn_trace_depth_--;
-      }//if
-    }//leave
 
-    //!write a complete call trace
+    //! Handle termination of a function.
+
+    //! This method performs all things needed for tracing when a function
+    //! is left.
+    static void LeaveFcn(){
+      FcnTrace *tmp;
+      if (foo_){
+	tmp = foo_->caller_;
+	delete foo_;
+	foo_ = tmp;
+	fcnTraceDepth_--;
+      }
+    }
+
+    //! This method writes a complete call trace
     static void Dump(){
       while(foo_) LeaveFcn();
     }
 
-  protected:
-    static fcn_trace *foo_;
-    static int fcn_trace_depth_;
+  private:
+    static FcnTrace *foo_;
+    static unsigned int fcnTraceDepth_;
   };
 
 
-//! when an instance of this class is generated,
-//! it tells the static Debugger that a function has been entered.
-//! upon destruction, it tells the Debugger that a function has been left
-  class FcnObj {
-public:
+  // ========================================================================
+  // FCNOBJ CLASS
+  // ========================================================================
 
-  FcnObj(char *name){
-  Debugger::EnterFcn(name);
-  }
-  
-  ~FcnObj(){Debugger::LeaveFcn();}
+  //! When the TRACE macro is defined and the ENTER_FCN macro is called in
+  //! a function, then a local object of this class will be instantiated.
+  //! Its constructor and destructor trigger function tracing via the
+  //! static Debugger class.
+  class FcnObj {
+  public:
+
+    //! Constructor
+    FcnObj(char *name){
+      Debugger::EnterFcn(name);
+    }
+
+    //! Destructor
+    ~FcnObj(){
+      Debugger::LeaveFcn();
+    }
 
   };
 
