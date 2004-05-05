587f7d4dc36afc5d75ab6b674e8eaedc8068986e
Markus Mohr <marcus.mohr@geophysik.uni-muenchen.de>
2004-05-05 08:22:45 +0000

    - Added ENTER_FCN macros - Added a consistency check which compares the problem dimension in the   geometry file to that in the parameter file.

--- libsrc/Domain/domain.cc
+++ libsrc/Domain/domain.cc
@@ -31,139 +31,150 @@
 #include "Utils/vector.hh"
 #include "PDE/basePDE.hh"
 
-namespace CoupledField
-{
+namespace CoupledField {
 
-Domain:: Domain(FileType * const aptFileType, WriteResults * ptOut, TimeFunc * aptTimeFunc) 
-{
-#ifdef TRACE
-  (*trace) << "entering Domain::Domain" << std::endl;
-#endif
+  // **************
+  //   Construtor
+  // **************
+  Domain:: Domain(FileType * const aptFileType, WriteResults * ptOut,
+		  TimeFunc * aptTimeFunc) {
+
+    ENTER_FCN( "Domain::Domain" );
   
-  numlevel_ = 0;
+    numlevel_ = 0;
 
- InFile_     = aptFileType; 
- OutFile_    = ptOut;
- ptTimeFunc_ = aptTimeFunc;
+    InFile_     = aptFileType; 
+    OutFile_    = ptOut;
+    ptTimeFunc_ = aptTimeFunc;
 
- // Integer i;
+    // Integer i;
  
-  // read type of output results from conf-file
+    // read type of output results from conf-file
 #ifndef XMLPARAMS
- std::string libmesh="cfsgrid";
- conf->ifget("mesh_library",libmesh);
+    std::string libmesh="cfsgrid";
+    conf->ifget("mesh_library",libmesh);
 #else
- std::string libmesh;
- params->Get( "mesh_library", libmesh );
+    std::string libmesh;
+    params->Get( "mesh_library", libmesh );
 #endif
 
- Integer dim=InFile_->ReadDim();
+    Integer dim=InFile_->ReadDim();
 
- // initialize pointer to grid 
- if (dim==2) {
+    // initialize pointer to grid 
+    if (dim==2) {
 
-  if (libmesh =="cfsgrid") 
-      ptgrid_=new GridInterfaceCFS<2>(InFile_);
+      if (libmesh =="cfsgrid") {
+	ptgrid_=new GridInterfaceCFS<2>(InFile_);
+      }
 
 #ifdef ADAPTGRID
-  else
-    if (libmesh== "adaptgrid")
-      ptgrid_=new InterfaceAdaptGrid<2>(InFile_);
+      else if (libmesh== "adaptgrid") {
+	ptgrid_=new InterfaceAdaptGrid<2>(InFile_);
+      }
 #endif
 
-    else
-      {
+      else {
 	std::string errmsg = "Type of mesh_library should be one of ";
 	errmsg += "'cfsgrid' or 'adaptgrid', but is '" + libmesh + "'";
 	Info->Error( errmsg, __FILE__, __LINE__ );
       }
- }
+    }
 
- if (dim==3) 
-   {
-     if (libmesh =="cfsgrid") 
-       ptgrid_=new GridInterfaceCFS<3>(InFile_);
+    if (dim==3) {
+      if (libmesh =="cfsgrid") {
+	ptgrid_=new GridInterfaceCFS<3>(InFile_);
+      }
 
 #ifdef ADAPTGRID
-     else
-       if (libmesh== "adaptgrid")
-	 ptgrid_=new InterfaceAdaptGrid<3>(InFile_);
+      else if (libmesh== "adaptgrid") {
+	ptgrid_=new InterfaceAdaptGrid<3>(InFile_);
+      }
 #endif
-       else
-	 Error("Unknown type of mesh_library in conf-file",__FILE__,__LINE__);
-   }
+      else {
+	Error("Unknown type of mesh_library in conf-file",__FILE__,__LINE__);
+      }
+    }
 
- //read in the mesh information
- ptgrid_->Read();
+    //read in the mesh information
+    ptgrid_->Read();
  
- if (PrintGridOnly)
-   {
-     PrintGrid(0);
-     exit(0);
-   }
+    if (PrintGridOnly) {
+      PrintGrid(0);
+      exit(0);
+    }
 
- // allocate an object with an information about boundary condition
- ptBCs_=new BCs(InFile_);
+    // allocate an object with an information about boundary condition
+    ptBCs_=new BCs(InFile_);
 
- //read restraints information
- ptBCs_->ReadBCs();
+    //read restraints information
+    ptBCs_->ReadBCs();
 
- InitPDEs();
+    InitPDEs();
 
- // Initialize Coupled PDEs
- InitCoupledPDE();
-
+    // Initialize Coupled PDEs
+    InitCoupledPDE();
  
- //set the algebraic systems and read material data
- for (int i=0;i< numpde_;i++)
-   ptpde_[i]->SetAlgSys(i);
-}
+    // Set the algebraic systems and read material data
+    for (int i=0;i< numpde_;i++)
+      ptpde_[i]->SetAlgSys(i);
+
+  }
 
 
+  // **********************
+  //   Default destructor
+  // **********************
+  Domain::~Domain() {
+    ENTER_FCN( "Domain::~Domain" );
+    if (ptgrid_) delete ptgrid_;
+    if (ptBCs_) delete ptBCs_;
+  }
 
-Domain :: ~Domain()
-{
-#ifdef TRACE
-  (*trace) << "entering Domain::~Domain" << std::endl;
-#endif
 
-  if (ptgrid_) delete ptgrid_;
-  if (ptBCs_) delete ptBCs_;
-}
+  // **************************
+  //   Initialisation of PDEs
+  // **************************
+  void Domain::InitPDEs() {
 
-void Domain :: InitPDEs()
-{
-#ifdef TRACE
-  (*trace) << "entering Domain::InitPDEs" << std::endl;
-#endif
+    ENTER_FCN( "Domain::InitPDEs" );
 
-  // get numbers of PDEs in domain
-  std::vector<std::string> pdes;
+    // get numbers of PDEs in domain
+    std::vector<std::string> pdes;
 #ifndef XMLPARAMS
-  conf->getliststr("list_pdes",pdes);
+    conf->getliststr("list_pdes",pdes);
 #else
-  params->GetPDEList( pdes );
+    params->GetPDEList( pdes );
 #endif
 
-  numpde_=pdes.size();
-  ptpde_.resize(numpde_);
+    numpde_ = pdes.size();
+    ptpde_.resize(numpde_);
 
 
-  //allocate all specific PDEs
-  //  if (!ptalgsys_) Error("You try to allocate object BasePDE with null pointer to AlgSys");
+    // Read dimension from mesh file and perform a consistency check
+    Integer dim = InFile_->ReadDim();
+#ifdef XMLPARAMS
+    std::string probGeo;
+    params->Get( "type", probGeo, "geometry" );
+    if ( !(probGeo == "3d"          && dim == 3 ||
+	   probGeo == "axi"         && dim == 2 ||
+	   probGeo == "plainStrain" && dim == 2 ) ) {
+      Info->Error( "Dimensions in parameter file and geometry file do not fit",
+		   __FILE__, __LINE__ );
+    }
+#endif
+    
+    // Allocate all specific PDEs
+    // if (!ptalgsys_) Error("You try to allocate object BasePDE with null
+    // pointer to AlgSys");
 
-  Integer dim=InFile_->ReadDim();
 
-  for (int i=0;i< pdes.size();i++)
-    {
+    for (int i=0;i< pdes.size();i++) {
       if (pdes[i] == "electrostatic") 
 	ptpde_[i]=new ElecPDE(ptgrid_,ptBCs_,ptTimeFunc_,InFile_,OutFile_);
 
-      else if (pdes[i] == "mechanic") {
-	// Info->Error( "Got this far", __FILE__, __LINE__ );
-
+      else if (pdes[i] == "mechanic")
 	ptpde_[i]=new MechPDE(ptgrid_,ptBCs_,ptTimeFunc_,InFile_,OutFile_);
-      }
+
       else if (pdes[i] == "acoustic")
   	ptpde_[i]=new AcousticPDE(ptgrid_,ptBCs_,ptTimeFunc_,InFile_,OutFile_);
 
@@ -202,85 +213,62 @@ void Domain :: InitPDEs()
 } // end of InitPDE()
 
 
-void Domain::InitCoupledPDE()
-{
-#ifdef TRACE
-  (*trace) << "entering Domain::InitCoupledPDE" << std::endl;
-#endif
+  void Domain::InitCoupledPDE() {
+    ENTER_FCN( "Domain::InitCoupledPDE" );
   
-  // check if more than one PDEs are defined
-  if (numpde_ <= 1)
-    {
+    // check if more than one PDEs are defined
+    if (numpde_ <= 1) {
       ptcoupledpde_ = 0;
       return;
     }
 
-
-  // Initialize the definiton of coupled PDEs
-  CoupledPDEDef * CouplingDef = new CoupledPDEDef(ptgrid_, ptBCs_);
-  CouplingDef->CreateCoupling(orderedpdes_,couplings_, ptpde_);
+    // Initialize the definiton of coupled PDEs
+    CoupledPDEDef * CouplingDef = new CoupledPDEDef(ptgrid_, ptBCs_);
+    CouplingDef->CreateCoupling(orderedpdes_,couplings_, ptpde_);
   
-  // create new Coupled PDE
-  ptcoupledpde_ = new IterCoupledPDE(orderedpdes_,couplings_,ptgrid_, ptBCs_,InFile_,OutFile_);
-  ptcoupledpde_->InitCoupling(numlevel_);
+    // create new Coupled PDE
+    ptcoupledpde_ = new IterCoupledPDE(orderedpdes_,couplings_,ptgrid_,
+				       ptBCs_,InFile_,OutFile_);
+    ptcoupledpde_->InitCoupling(numlevel_);
 
-  delete CouplingDef;
-
-}
-
+    delete CouplingDef;
+  }
 
 
-void Domain :: PrintGrid(const Integer level)
-{
-#ifdef TRACE
-  (*trace) << "entering Domain::PrintGrid" << std::endl;
-#endif
+  void Domain::PrintGrid(const Integer level) {
+    ENTER_FCN( "Domain::PrintGrid" );
+    OutFile_->Init(ptgrid_);
+    OutFile_->WriteGrid(level);
+  }
 
- OutFile_->Init(ptgrid_);
- OutFile_->WriteGrid(level);
-}
 
-void Domain :: SetSubdomains()
-{
-#ifdef TRACE
-  (*trace) << "entering Domain::SetSubdomains" << std::endl;
-#endif
- ;
-}
+  void Domain::SetSubdomains() {
+    ENTER_FCN( "Domain::SetSubdomains" );
+  }
 
-void Domain::Update(const Integer level)
-{
-#ifdef TRACE
-  (*trace) << "entering Domain::Update" << std::endl;
-#endif
+
+  void Domain::Update(const Integer level) {
+    ENTER_FCN( "Domain::Update" );
  
-  ptBCs_->Update(ptgrid_);
+    ptBCs_->Update(ptgrid_);
 
-  // Init AlgSystem
-  UpdateAlgSys(level);
-   
-}
+    // Init AlgSystem
+    UpdateAlgSys(level);
+  }
 
-void Domain::UpdateAlgSys(const Integer level)
-{
-#ifdef TRACE
-  (*trace) << "entering Domain::UpdateAlgSys" << std::endl;
-#endif
 
-  numlevel_ ++;
+  void Domain::UpdateAlgSys(const Integer level) {
+    ENTER_FCN( "Domain::UpdateAlgSys"  );
+
+    numlevel_ ++;
  
-  //set the algebraic systems and read material data
-  for (int i=0;i< numpde_;i++)
-    {
-      ptpde_[i]->DeleteAlgSys(i);
-      ptpde_[i]->Reset();
-      ptpde_[i]->SetAlgSys(i); 
-    } 
-  
-#ifdef TRACE
-  (*trace) << " leaving Domain::UpdateAlgSys " << std::endl;
-#endif
-}
-
+    //set the algebraic systems and read material data
+    for (int i=0;i< numpde_;i++)
+      {
+	ptpde_[i]->DeleteAlgSys(i);
+	ptpde_[i]->Reset();
+	ptpde_[i]->SetAlgSys(i); 
+      }
+  }
 
 }
