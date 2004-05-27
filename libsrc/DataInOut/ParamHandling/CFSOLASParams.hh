171d9553478976ec311559a150d0d1405b14e9ce
mmohr <mmohr@cfs-dev.mdmt.tuwien.ac.at>
2004-05-27 14:09:06 +0000

    Module for inserting parameters from the CFS parameter handling object into the olasParams object.

---
+++ libsrc/DataInOut/ParamHandling/CFSOLASParams.hh
@@ -0,0 +1,38 @@
+#ifndef FILE_CFSOLASPARAMS
+#define FILE_CFSOLASPARAMS
+
+namespace CoupledField
+{
+
+  // Forward declaration of classes
+  class BaseParamHandler;
+  class OLAS_Params;
+
+  //! Class for passing steering parameters from CFSü++ to OLAS
+  class CFSOLASParams {
+
+  public:
+
+    //! Set parameters for solver for linear system
+
+    //! This routine queries the BaseParamHandler object for the parameters
+    //! belonging to the solver for the linear system associated to the
+    //! PDE specified via the pdename input parameter and inserts them into
+    //! The olasParams object.
+    static void SetSolverParams( std::string pdename, BaseParamHandler *cfs,
+				 OLAS_Params *olas );
+
+    //! Set parameters for preconditioner for linear system
+
+    //! This routine queries the BaseParamHandler object for the parameters
+    //! belonging to the preconditionerr for the linear system associated to
+    //! the PDE specified via the pdename input parameter and inserts them into
+    //! The olasParams object.
+    static void SetPrecondParams( std::string pdename, BaseParamHandler *cfs,
+				  OLAS_Params *olas );
+
+  };
+
+}
+
+#endif
