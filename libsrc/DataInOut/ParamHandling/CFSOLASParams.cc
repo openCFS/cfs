82079188847898d7e5297ac7594167c91391f047
Markus Mohr <marcus.mohr@geophysik.uni-muenchen.de>
2004-05-27 14:09:06 +0000

    Module for inserting parameters from the CFS parameter handling object into the olasParams object.

---
+++ libsrc/DataInOut/ParamHandling/CFSOLASParams.cc
@@ -0,0 +1,26 @@
+#include "General/environment.hh"
+#include "CFSOLASParams.hh"
+
+namespace CoupledField {
+
+  // *******************
+  //   SetSolverParams
+  // *******************
+  void CFSOLASParams::SetSolverParams( std::string pdename,
+				       BaseParamHandler *cfs,
+				       OLAS_Params *olas ) {
+
+    ENTER_FCN( "CFSOLASParams::SetSolverParams" );
+  }
+
+  // ********************
+  //   SetPrecondParams
+  // ********************
+  void CFSOLASParams::SetPrecondParams( std::string pdename,
+					BaseParamHandler *cfs,
+					OLAS_Params *olas ) {
+
+    ENTER_FCN( "CFSOLASParams::SetPrecondParams" );
+  }
+
+}
