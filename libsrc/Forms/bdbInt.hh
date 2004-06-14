eeea16347f93d57aa57acff1e43c218286c36d12
mmohr <mmohr@cfs-dev.mdmt.tuwien.ac.at>
2004-06-14 15:02:54 +0000

    Formatted module accorsing to new rules.

--- libsrc/Forms/bdbInt.hh
+++ libsrc/Forms/bdbInt.hh
@@ -3,50 +3,53 @@
 
 #include "baseForm.hh" 
 
-namespace CoupledField
-{
+namespace CoupledField {
 
-  /// abstract class for calculation of bdb forms
-class BDBInt : public BaseForm
-{
+  //! abstract class for calculation of bdb forms
+  class BDBInt : public BaseForm {
 
-public:
-  /// Constructor with pointer to BaseElem
-  BDBInt(BaseFE * aptelem, MaterialData & matData);
+  public:
 
-  /// Constructor with pointer to BaseElem
-  BDBInt(MaterialData & matData);
+    //! Constructor with pointer to BaseElem
+    BDBInt(BaseFE *aptelem, MaterialData &matData);
 
+    //! Constructor with pointer to BaseElem
+    BDBInt(MaterialData &matData);
 
-  /// Destructor
-  virtual ~BDBInt();
+    //! Destructor
+    virtual ~BDBInt();
 
+    //! Function for calculation bdb matrix 
+    virtual void CalcElementMatrix(Matrix<Double> &ptCoord, Matrix<Double> & );
 
-  /// Function for calculation bdb matrix 
-  virtual void CalcElementMatrix(Matrix<Double>& ptCoord, Matrix<Double> &);
+  protected:
 
+    //! returns B - matrix for BDB
+    virtual void calcBMat( Matrix<Double> &bMat, Integer ip,
+			   Matrix<Double> &ptCoord ) = 0;
 
-protected:
-  /// returns B - matrix for BDB
-  virtual void calcBMat(Matrix<Double> & bMat, Integer ip, Matrix<Double> & ptCoord)=0;
+    //! returns D - matrix for BDB
+    virtual void calcDMat( Matrix<Double> &dMat ) {
+      Error( "BDBInt::calcDMat(Matrix<Double>&) not correctly overwritten!",
+	     __FILE__, __LINE__);
+    };
 
-  /// returns D - matrix for BDB
-  virtual void calcDMat(Matrix<Double> & dMat)
-  {Error("BDBInt::calcDMat(Matrix<Double>&) not correct overwritten !",__FILE__,__LINE__);};
+    //! returns D - matrix for BDB, changes in every integration point
+    virtual void calcDMat( Matrix<Double> &dMat, Integer ip,
+			   Matrix<Double> &ptCoord ) {
+      Error( "BDBInt::calcDMat(Matrix<Double>&, int, Matrix<Double>&) not correct overwritten!",__FILE__,__LINE__);
+    };
 
-  /// returns D - matrix for BDB, changes in every integration point
-  virtual void calcDMat(Matrix<Double> & dMat, Integer ip, Matrix<Double> & ptCoord)
-  {Error("BDBInt::calcDMat(Matrix<Double>&, int, Matrix<Double>&) not correct overwritten !",__FILE__,__LINE__);};
+    //! returns dimension of D matrix
+    virtual Integer getDimD()=0;
 
-  /// returns dimension of D matrix
-  virtual Integer getDimD()=0;
+    //! returns nr. of degrees of freedom
+    virtual Integer getNrDofs()=0;
 
-  /// returns nr. of degrees of freedom
-  virtual Integer getNrDofs()=0;
-
-  /// for nonlinear calculations, the d-matrix has to be updated in every integration point
-  Integer updateDMatInEveryIP_;
-};
+    //! for nonlinear calculations, the d-matrix has to be updated in every
+    //! integration point
+    Integer updateDMatInEveryIP_;
+  };
 
 }
 
