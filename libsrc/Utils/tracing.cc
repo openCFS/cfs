556b309b42ed392a99bc28f040eb36e72f25f9db
Markus Mohr <marcus.mohr@geophysik.uni-muenchen.de>
2004-07-06 16:45:50 +0000

    In preparation for a runtime setting of the depth of function tracing, the relevant classes were changed in the following way: - FcnTraceListElem class now has a private member limit_, which is used to   determine whether a message should be generated or not. The member is   initialised via the new limit parameter in the constructor. - FcnTraceHandler class now has a static member fcnTraceDepthLimit_ which   is passed to the constructor of FcnTraceListElem. - fcnTraceDepthlimit_ is currently still set to the value of the TRACE macro   in tracing.cc

--- libsrc/Utils/tracing.cc
+++ libsrc/Utils/tracing.cc
@@ -2,11 +2,12 @@
 
 namespace OutInfo{
 
-  // Definition and Initialization of static variables of class Debugger
-  // has to be outside class scope
+  // Definition and Initialization of static variables of class FcnTraceHandler
+  // has to be outside of class scope
 #ifdef TRACE
   FcnTraceListElem* FcnTraceHandler::foo_ = NULL;
   unsigned int FcnTraceHandler::fcnTraceDepth_ = 0;
+  unsigned int FcnTraceHandler::fcnTraceDepthLimit_ = TRACE;
 #endif
 }
 
