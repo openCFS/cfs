1bee7d216f59f712e888fdf1a0a364ca56185979
Markus Mohr <marcus.mohr@geophysik.uni-muenchen.de>
2004-07-07 08:54:48 +0000

    Added a more detailed description of the class. Note that the description might not be 100% correct, since I did not implement this class.

--- libsrc/PDE/scalarnodeEQN.hh
+++ libsrc/PDE/scalarnodeEQN.hh
@@ -3,65 +3,70 @@
 
 #include "nodeEQN.hh"
 
-namespace CoupledField
-{
+namespace CoupledField {
 
-//! Euation handling class for scalar valued
-//! PDEs.
+  //! This class is used to perform the handling of equation numbers in the
+  //! case of scalar PDEs, i.e. PDEs which only have one degree of freedom per
+  //! node. In this case every unknown receives a unique equation number.
+  //! Unknowns whose value is fixed by homogeneous Dirichlet boundary
+  //! conditions are eliminated. Unknowns given by inhomogeneous Dirichlet
+  //! boundary conditions are treated with the penalty approach. The matrix
+  //! representing the linear system associated with the discretised PDE will
+  //! have scalar double or complex entries.
+  class ScalarNodeEQN : public NodeEQN {
 
-class ScalarNodeEQN : public NodeEQN
-{
-public:
+  public:
   
-  //! Constructor
-  ScalarNodeEQN(Grid * aptGrid, 
-		BCs * aptBCs,
-		StdVector<std::string>& asubdoms, 
-		Integer actlevel, 
-		Integer dofsPerNode);
+    //! Constructor
+    ScalarNodeEQN( Grid * aptGrid, 
+		   BCs * aptBCs,
+		   StdVector<std::string>& asubdoms, 
+		   Integer actlevel, 
+		   Integer dofsPerNode );
   
-  //! Destructor
-  virtual ~ScalarNodeEQN();
+    //! Destructor
+    virtual ~ScalarNodeEQN();
   
-  //! Calculate the mapping after Dirichlet and
-  //! constraint nodes were set
-  void CalcMapping();
+    //! Calculate the mapping after Dirichlet and
+    //! constraint nodes were set
+    void CalcMapping();
   
-  //! Print the mapping nodes <-> EQNs
-  void Print(std::ostream & out) const;
+    //! Print the mapping nodes <-> EQNs
+    void Print(std::ostream & out) const;
   
-  // -----------------------------------------------------------------------
-  // Functions for mapping node numbers <-> EQN numbers
-  // -----------------------------------------------------------------------
+    // -----------------------------------------------------------------------
+    // Functions for mapping node numbers <-> EQN numbers
+    // -----------------------------------------------------------------------
   
-   //! Map vector of equation numbers to 
-  //! positions in global solution vector
-  void EQN2SolVectorPos(const StdVector<Integer> &eqnNr, 
-			StdVector<Integer> &pos) const;
+    //! Map vector of equation numbers to 
+    //! positions in global solution vector
+    void EQN2SolVectorPos(const StdVector<Integer> &eqnNr, 
+			  StdVector<Integer> &pos) const;
 
-  //! Map node number and dof to according equation number
-  void Node2EQN(const Integer nodeNr, 
-		const Integer dof,
-		Integer & eqnNr,
-		Integer & eqnDof) const;
+    //! Map node number and dof to according equation number
+    void Node2EQN(const Integer nodeNr, 
+		  const Integer dof,
+		  Integer & eqnNr,
+		  Integer & eqnDof) const;
 
-  //! Map node number to according equation number(s)
-  void Node2EQN(const Integer nodeNr, StdVector<Integer> &eqns) const;
+    //! Map node number to according equation number(s)
+    void Node2EQN(const Integer nodeNr, StdVector<Integer> &eqns) const;
   
-  //! Map vector of node numbers to according
-  //! vector of equiation numbers
-  void Node2EQN(const StdVector<Integer> &nodeNr,
-		StdVector<Integer> &eqnNr) const;
+    //! Map vector of node numbers to according
+    //! vector of equiation numbers
+    void Node2EQN(const StdVector<Integer> &nodeNr,
+		  StdVector<Integer> &eqnNr) const;
 
-private:
+  private:
 
-  //! Mapping global node number->EQN
-  StdVector<Integer> pdeNode2EQN_;
-  
-  
-};
+    //! Default constructor is disallowed
+    ScalarNodeEQN() {};
 
-  
+    //! Mapping global node number->EQN
+    StdVector<Integer> pdeNode2EQN_;
+
+  };
+
 }  // end of namespace
 
 #endif // FILE_SCALARNODEEQN
