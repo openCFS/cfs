20e1f828391bfdcb002f16cf8d87e28cbabc5a6a
mmohr <mmohr@cfs-dev.mdmt.tuwien.ac.at>
2004-07-08 15:52:38 +0000

    Commented out the 'ptdriver = new piezoParamIdent(domain);' part. There is no such class in the trunk at the moment!

--- main/cfs.cc
+++ main/cfs.cc
@@ -187,10 +187,11 @@ int main(int argc, char *argv[])
       ptdriver = new TransientDriver(domain);
       break;
     case HARMONIC:
-      if (analysis == "paramIdent")
-        ptdriver = new piezoParamIdent(domain);  // calls Driver for parameter identification, using harmonic analysis
-      else
-        ptdriver = new HarmonicDriver(domain);
+      // if (analysis == "paramIdent")
+      // calls Driver for parameter identification, using harmonic analysis
+      // ptdriver = new piezoParamIdent(domain);
+      // else
+      ptdriver = new HarmonicDriver(domain);
       break;
     case MULTI_SEQUENCE:
       ptdriver = new MultiSequenceDriver(domain);
