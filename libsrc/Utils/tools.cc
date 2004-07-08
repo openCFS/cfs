36765672997ac13b1e25f39f149637c6050f43f0
Markus Mohr <marcus.mohr@geophysik.uni-muenchen.de>
2004-07-08 08:54:58 +0000

    - Warning() now like Error() just calls the respective method of the WriteInfo   class. - Improved documentation and formatting.

--- libsrc/Utils/tools.cc
+++ libsrc/Utils/tools.cc
@@ -11,251 +11,231 @@
 
 
 namespace CoupledField {
-  
-  
-
-template<Integer dim>
-void PrintPoint(Point<dim> point, std::ostream * out) 
- {
-   Integer i;
-   for(i=0; i<dim; i++) 
-  (*out)<< "   " << point[i];
- }
-
-#ifdef __GNUC__
-template void PrintPoint(Point<2>, std::ostream *);
-template void PrintPoint(Point<3>, std::ostream *);
-#endif
  
-void Error(const Char * Text, const Char * const filename,
-                      const Integer numline)
-{
-  Info->Error(Text, filename, numline);
-}
+  // =======================
+  //   Issue Error Message
+  // =======================
+  void Error( const Char * Text, const Char * const filename,
+	      const Integer numline ) {
+    Info->Error( Text, filename, numline );
+  }
 
-void Warning(const Char *Text, const Char * const filename,
-                      const Integer numline)
-{
- std::cerr << "\033[31mWARNING:\033[0m " << Text;
+  // =========================
+  //   Issue Warning Message
+  // =========================
+  void Warning( const Char *Text, const Char * const filename,
+		const Integer numline ) {
+    Info->Warning( Text, filename, numline );
+    std::cerr << "\033[31mWARNING:\033[0m " << Text;
+  }
 
-#ifdef DEBUG
- if (filename) { std::cerr <<"( " << filename <<" ";
-                 if (numline) std::cerr << numline;
-                 std::cerr << ")";}
-#endif
+  // =========================================
+  //   Split a string into a list of strings
+  // =========================================
+  void SplitStringList( std::string list, StdVector<std::string> &strVec,
+			Char delimiter ) {
 
- std::cerr << std::endl;
-}
-
-
-
-
-void SplitStringList(std::string list, 
-		     StdVector<std::string> & strVec,
-		     Char delimiter)
-
-{
-  Integer lastDelim = 0;
-  strVec.Clear();
-  Integer i=0;
+    Integer lastDelim = 0;
+    strVec.Clear();
+    Integer i=0;
   
-  // ignore all leading spaces
-  while (i<list.length() && list[i] == ' ')
-    {
+    // ignore all leading spaces
+    while ( i < list.length() && list[i] == ' ' ) {
       i++;
       lastDelim++;
     }
 
-  // get the n-1 entries of the list
-  std::cerr << "list = " << list << std::endl;
-  for (i=0; i<list.length(); i++)
-    if (list[i] == delimiter)
-      {
-	strVec.Push_back(std::string(list,lastDelim, i-lastDelim));
-
+    // get the n-1 entries of the list
+    for ( i = 0; i < list.length(); i++ ) {
+      if ( list[i] == delimiter ) {
+	strVec.Push_back( std::string( list, lastDelim, i-lastDelim ));
 	i++;
+
 	// ignore next spaces
-	while (i<list.length() && list[i] == ' ')
-	    i++;
+	while ( i < list.length() && list[i] == ' ' ) {
+	  i++;
+	}
+
 	lastDelim = i;
       }
+    }
 
-  // get the n-th entry
-  i=lastDelim;
-  while (i<list.length() && list[i] != ' ')
-    i++;
-  strVec.Push_back(std::string(list,lastDelim, i-lastDelim));
+    // get the n-th entry
+    i = lastDelim;
+    while ( i < list.length() && list[i] != ' ' ) {
+      i++;
+    }
 
-}
+    strVec.Push_back( std::string( list, lastDelim, i-lastDelim ));
 
-template<Integer dim>
-Point<dim> & Point<dim>::operator+(const Point<dim>&t)
-{
-  Integer i;
-  for (i=0; i<dim; i++)
-    p[i]+=t.p[i];	
-  return *this;
-}
-
-template<Integer dim>
-Point<dim> & Point<dim>::operator=(const Point<dim>&t)
-{
-  Integer i;
-  for (i=0; i<dim; i++)
-    p[i]=t.p[i];
-  return *this;
-}
-
-template<Integer dim>
-Point<dim> & Point<dim>::operator-(const Point<dim>&t)
-{
-  Integer i;
-  for (i=0; i<dim; i++)
-    p[i]-=t.p[i];	
-  return *this;
-}
-
-Double Sin(const Double x)
-{
-  return sin(x);
-}
-
-Double Cos(const Double x)
-{
-  return cos(x);
-}
-
-pfn1var FncReader(const std::string namefnc)
-{
-  if (namefnc == "sin") return Sin;
-  else {
-    if (namefnc == "cos") return Cos;
-    else Error("This function is not predefined in CFS++");
   }
-}
-
-template<Integer dim>
-Double dist(Point<dim> a,Point<dim> b)
-{
-  Double preSqrt=0;
-  Integer i;
-  for (i=0; i<dim; i++)
-    preSqrt+=sqr(a[i]-b[i]);
- return sqrt(preSqrt);
-}
 
 
-Double dist_Mat(Matrix<Double> a)
-{
-  Double preSqrt=0;
-  Integer i;
-  Integer k=a.GetSizeRow();
-  // std::cout<<"tools.cc:size of matrix: "<<k<<std::endl;
-  for (i=0; i<k; i++)
-    preSqrt+=sqr(a[i][0]-a[i][1]);
- return sqrt(preSqrt);
-}
+  template<Integer dim>
+  Point<dim> & Point<dim>::operator+(const Point<dim>&t) {
+    Integer i;
+    for (i=0; i<dim; i++)
+      p[i]+=t.p[i];	
+    return *this;
+  }
+
+
+  template<Integer dim>
+  void PrintPoint(Point<dim> point, std::ostream * out) {
+    Integer i;
+    for(i=0; i<dim; i++) 
+      (*out)<< "   " << point[i];
+  }
 
 #ifdef __GNUC__
-template Double dist(Point<2>,Point<2>);
-template Double dist(Point<3>,Point<3>);
+  template void PrintPoint(Point<2>, std::ostream *);
+  template void PrintPoint(Point<3>, std::ostream *);
+#endif
+
+  template<Integer dim>
+  Point<dim> & Point<dim>::operator=(const Point<dim>&t) {
+    Integer i;
+    for (i=0; i<dim; i++)
+      p[i]=t.p[i];
+    return *this;
+  }
+
+  template<Integer dim>
+  Point<dim> & Point<dim>::operator-(const Point<dim>&t) {
+    Integer i;
+    for (i=0; i<dim; i++)
+      p[i]-=t.p[i];	
+    return *this;
+  }
+
+  Double Sin(const Double x) {
+    return sin(x);
+  }
+
+  Double Cos(const Double x) {
+    return cos(x);
+  }
+
+  pfn1var FncReader(const std::string namefnc) {
+    if (namefnc == "sin") return Sin;
+    else {
+      if (namefnc == "cos") return Cos;
+      else Error("This function is not predefined in CFS++");
+    }
+  }
+
+  template<Integer dim>
+  Double dist(Point<dim> a,Point<dim> b) {
+    Double preSqrt=0;
+    Integer i;
+    for (i=0; i<dim; i++)
+      preSqrt+=sqr(a[i]-b[i]);
+    return sqrt(preSqrt);
+  }
+
+  Double dist_Mat(Matrix<Double> a) {
+    Double preSqrt=0;
+    Integer i;
+    Integer k=a.GetSizeRow();
+    // std::cout<<"tools.cc:size of matrix: "<<k<<std::endl;
+    for (i=0; i<k; i++)
+      preSqrt+=sqr(a[i][0]-a[i][1]);
+    return sqrt(preSqrt);
+  }
+
+#ifdef __GNUC__
+  template Double dist(Point<2>,Point<2>);
+  template Double dist(Point<3>,Point<3>);
 #endif
 
 
-/// a-->b
-void calcNormal2Line(Vector<Double> & normal,Point<2> a,Point<2> b)
-{
-  Double distance=dist(a,b);
-  normal[0]=(b[1]-a[1])/distance;
-  normal[1]=(a[0]-b[0])/distance;
-}
+  /// a-->b
+  void calcNormal2Line(Vector<Double> & normal,Point<2> a,Point<2> b) {
+    Double distance=dist(a,b);
+    normal[0]=(b[1]-a[1])/distance;
+    normal[1]=(a[0]-b[0])/distance;
+  }
 
-/// a-->b
-void calcNormal2Line_Mat(Vector<Double> & normal,Matrix<Double> a)
-{
-  Double distance=dist_Mat(a);
-  // std::cout<<"distance :"<<distance<<std::endl;
+  /// a-->b
+  void calcNormal2Line_Mat(Vector<Double> & normal,Matrix<Double> a)
+  {
+    Double distance=dist_Mat(a);
+    // std::cout<<"distance :"<<distance<<std::endl;
   
-  normal[0]=(a[1][1]-a[1][0])/distance;
-  normal[1]=(a[0][1]-a[0][0])/distance;
-}
+    normal[0]=(a[1][1]-a[1][0])/distance;
+    normal[1]=(a[0][1]-a[0][0])/distance;
+  }
 
 
-/// a-->b-->c. no fix orientation.
-void calcNormal2Surface(Vector<Double> & normal,Point<3> a,Point<3> b, Point<3> c)
-{
-  Point<3> t,s;
-  Double L2_normal; 
-  s=(a-b);
-  t=(c-b);
-  normal[0]=t[1]*s[2]-t[2]*s[1];
-  normal[1]=t[2]*s[0]-t[0]*s[2];  
-  normal[2]=t[0]*s[1]-t[1]*s[0];
-  L2_normal=sqrt(sqr(normal[0])+sqr(normal[1])+sqr(normal[2]));
-  normal[0]=normal[0]/L2_normal;
-  normal[1]=normal[1]/L2_normal;
-  normal[2]=normal[2]/L2_normal;  
-}
+  /// a-->b-->c. no fix orientation.
+  void calcNormal2Surface(Vector<Double> & normal,Point<3> a,Point<3> b,
+			  Point<3> c) {
+    Point<3> t,s;
+    Double L2_normal; 
+    s=(a-b);
+    t=(c-b);
+    normal[0]=t[1]*s[2]-t[2]*s[1];
+    normal[1]=t[2]*s[0]-t[0]*s[2];  
+    normal[2]=t[0]*s[1]-t[1]*s[0];
+    L2_normal=sqrt(sqr(normal[0])+sqr(normal[1])+sqr(normal[2]));
+    normal[0]=normal[0]/L2_normal;
+    normal[1]=normal[1]/L2_normal;
+    normal[2]=normal[2]/L2_normal;  
+  }
 
-/// a-->b-->c. no fix orientation.
-void calcNormal2Surface_Mat(Vector<Double> & normal,Matrix<Double> ptCoord)
-{
-  Point<3> t,s,a,b,c;
+  /// a-->b-->c. no fix orientation.
+  void calcNormal2Surface_Mat(Vector<Double> & normal,Matrix<Double> ptCoord){
+    Point<3> t,s,a,b,c;
 
-  for (Integer i=0;i<3;i++)
-    {
+    for (Integer i=0;i<3;i++) {
       a[i]=ptCoord[i][0];
       b[i]=ptCoord[i][1];      
       c[i]=ptCoord[i][2];
     }
   
-  Double L2_normal; 
-  s=(a-b);
-  t=(c-b);
-  normal[0]=t[1]*s[2]-t[2]*s[1];
-  normal[1]=t[2]*s[0]-t[0]*s[2];  
-  normal[2]=t[0]*s[1]-t[1]*s[0];
-  L2_normal=sqrt(sqr(normal[0])+sqr(normal[1])+sqr(normal[2]));
-  normal[0]=normal[0]/L2_normal;
-  normal[1]=normal[1]/L2_normal;
-  normal[2]=normal[2]/L2_normal;  
-}
+    Double L2_normal; 
+    s=(a-b);
+    t=(c-b);
+    normal[0]=t[1]*s[2]-t[2]*s[1];
+    normal[1]=t[2]*s[0]-t[0]*s[2];  
+    normal[2]=t[0]*s[1]-t[1]*s[0];
+    L2_normal=sqrt(sqr(normal[0])+sqr(normal[1])+sqr(normal[2]));
+    normal[0]=normal[0]/L2_normal;
+    normal[1]=normal[1]/L2_normal;
+    normal[2]=normal[2]/L2_normal;  
+  }
 
-char * c_string(const std::string & s)
-{
-   char * p = new char[s.length()+1];
-   s.copy(p, std::string::npos);
-   p[s.length()]=0;
-   return p;
-}
+  char * c_string(const std::string & s) {
+    char * p = new char[s.length()+1];
+    s.copy(p, std::string::npos);
+    p[s.length()]=0;
+    return p;
+  }
 
+  Integer defineRefinements(const Double tolElem, const Double tolTotal,
+			    const Integer noOfChilds){
+    Double tmp=log(tolElem/tolTotal)/log(noOfChilds);
+    return (Integer)tmp + 1;
+  }
 
-Integer defineRefinements(const Double tolElem, const Double tolTotal, const Integer noOfChilds)
-{
-  Double tmp=log(tolElem/tolTotal)/log(noOfChilds);
-  return (Integer)tmp + 1;
-}
+  Double CalcArea(Elem * ptE, Grid * ptgrid, const Integer level){
+    Double         area = 0;
+    BaseFE         * ptelem = ptE->ptElem;
+    const StdVector<Integer> & connect = ptE->connect;
+    Matrix<Double> ptCoord;
+    Integer        nrIntPnts = ptelem->GetNumIntPoints();
+    const Vector<Double> & intWeights = ptelem->GetIntWeights();  
+    Integer        i;
+    Double         jacDet;
 
-Double CalcArea(Elem * ptE, Grid * ptgrid, const Integer level)
-{
-  Double         area = 0;
-  BaseFE         * ptelem = ptE->ptElem;
-  const StdVector<Integer> & connect = ptE->connect;
-  Matrix<Double> ptCoord;
-  Integer        nrIntPnts = ptelem->GetNumIntPoints();
-  const Vector<Double> & intWeights = ptelem->GetIntWeights();  
-  Integer        i;
-  Double         jacDet;
+    ptgrid->GetCoordNodesElemMat(connect,ptCoord,level);
 
-  ptgrid->GetCoordNodesElemMat(connect,ptCoord,level);
-
-  for (i=0; i<nrIntPnts; i++)
-    {
+    for (i=0; i<nrIntPnts; i++) {
       jacDet = ptelem->CalcJacobianDetAtIp(i+1, ptCoord);
       area +=jacDet*intWeights[i];
-    } 
+    }
 
-  return area;
-}
+    return area;
+  }
 
 }// namespace CoupledField
