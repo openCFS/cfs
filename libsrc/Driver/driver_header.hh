ba066cabf12fd47019773f71c039fc81ea4d033e
Tom Lahmer <tom.lahmer@uni-weimar.de>
2004-07-09 09:52:56 +0000

    Constructor of class piezoParamIdent is now derived from class SingleDriver

--- libsrc/Driver/driver_header.hh
+++ libsrc/Driver/driver_header.hh
@@ -8,7 +8,9 @@
 #include <Driver/staticdriver.hh>
 #include <Driver/staticAdaptSpaceDriver.hh>
 #include <Driver/harmonicAdapt.hh>
+#include <Driver/piezoParamIdent.hh>
 #include <Driver/multiSequenceDriver.hh>
+
 //#include <Driver/transientAdaptSpaceDriver.hh>
 
 #endif
