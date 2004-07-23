61d97bcd90eeeb8b051365bae1322cb7a5f30d62
Manfred Kaltenbacher <manfred.kaltenbacher@tuwien.ac.at>
2004-07-23 15:16:31 +0000

    second order hex-elements (currently not working)

---
+++ libsrc/Elements/3D/hexa2FE.hh
@@ -0,0 +1,77 @@
+#ifndef FILE_HEXA2FE_2004
+#define FILE_HEXA2FE_2004
+
+#include "hexaFE.hh"
+
+namespace CoupledField
+{
+
+//! Class with  description of hexahedral element
+
+class Hexa2FE : public HexaFE
+{
+public:
+   //! Constructor with type of integration rule
+   Hexa2FE();
+
+   //! Deconstructor
+   virtual ~Hexa2FE();
+
+protected:
+   //! Define variables of this class
+   virtual void Init();
+
+  //! Set local corner coordinates
+  virtual void SetCornerCoords();
+
+
+  //! calculates the shape functions at an arbitrary local point
+  /*!
+    \param Shape (output) Vector of shape fnc values \f$ (N_{1},\cdots\,N_{NumNodes})^T \f$
+    \param LCoord (input) Local coordinates of evalutation point 
+  */
+  virtual void CalcShapeFnc(Vector<Double> & LShape, 
+			    const Vector<Double> & LCoord);
+
+
+  
+  //! calculates the local derivatives of shape functions at an arbitrary local point
+  /*!
+    \param LDeriv (output) Matrix with local derivatives of all shape functions
+    \f[ \left( \begin{array}{ccc} N_{1,d\xi} & N_{1,d\eta} & \cdots \\
+                                  N_{2,d\xi} & N_{2,d\eta} & \cdots \\
+                                  \cdots     & \cdots      & \cdots \end{array}\right) \f]
+    \param LCoord (input) Local coordinates of evalutation point 
+  */
+  virtual void CalcLocalDerivShapeFnc(Matrix<Double> & LDeriv, 
+				      const Vector<Double> & LCoord);
+
+
+  //! Calculates corresponding volume point of neighbouring surface
+
+  //! For a given surface element and a neighbouring volume element this
+  //! mehtod calculates the local volume-coordinates out of the given
+  //! local surface-coordinates, which have one less dimension.
+  //! This can be used to get the corrsponding volume coordinates of 
+  //! the integration points of a surface. Therefore it calculates 
+  //! on which side of the volume element the surface elemente lies
+  //! and creates the according volume point.
+  /*!
+    \param surfConnect (input) Node numbers of surface element
+    \param volConnect (input) Node numbers of colume element
+    \param surfIntPoint (input) Surface integration point, which gets mapped
+    onto the volume element
+    \param volIntPoint (output) Corresponding volume integration point
+  */
+  void GetLocalIntPoints4Surface(const StdVector<Integer> & surfConnect,
+				 const StdVector<Integer> & volConnect,
+				 const Vector<Double> & surfIntPoint,
+				 Vector<Double> & volIntPoint);
+
+
+private:
+   
+};
+
+}
+#endif //
