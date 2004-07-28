a1f483000230c69a908d7eb871bf9967a3c80c10
Oliver Weitzel <oweitzel@svn>
2004-07-28 09:29:42 +0000

    Faster implementation of dbLineData::Set() with c-strings New implementation of WriteResultsDatabase::Update()

--- libsrc/DataInOut/Database/dbLineData.cc
+++ libsrc/DataInOut/Database/dbLineData.cc
@@ -6,33 +6,95 @@ namespace CoupledField
 
 dbLineData::dbLineData(const std::string &tname)
 {
+  ENTER_FCN("dbLineData::dbLineData");
   SetTableName(tname);
+  fields 	= new char[STRINGBLOCKSIZE];
+  strcpy(fields,"");
+  fieldlength	= STRINGBLOCKSIZE;
+  values 	= new char[STRINGBLOCKSIZE];
+  strcpy(values,"");
+  valuelength	= STRINGBLOCKSIZE;
 }
 
-std::string dbLineData::GetFieldName(int i)
+dbLineData::~dbLineData()
 {
-  return field[i];
+  ENTER_FCN("dbLineData::~dbLineData");
+  delete[] fields;
+  delete[] values;
 }
 
-std::string dbLineData::GetValue(int i)
+char *dbLineData::GetFieldNames()
 {
-  return value[i];
+  ENTER_IFCN("dbLineData::GetFieldNames");
+  return fields;
 }
 
-int dbLineData::Size()
+char *dbLineData::GetValues()
+{
+  ENTER_IFCN("dbLineData::GetValues");
+  return values;
+}
+
+/*int dbLineData::Size()
 {
   return field.size();
-}
+}*/
 
 void dbLineData::SetTableName(const std::string &tname)
 {
+  ENTER_IFCN("dbLineData::SetTableName");
   tablename = tname;
 }
 
 void dbLineData::Clear()
 {
-  value.clear();
-  field.clear();
+  ENTER_FCN("dbLineData::Clear");
+  strcpy(values,"");
+  strcpy(fields,"");
+//  value.clear();
+//  field.clear();
 }
+
+template<> void dbLineData::toString(const char *val, char *dst)
+{
+  ENTER_IFCN("dbLineData::toString(char*,char*)");
+  if (strlen(val)>ELEMENTBLOCKSIZE)
+  {
+    delete dst;
+    dst = new char[strlen(val)+1];
+  }
+  strcpy(dst,val);
+}
+
+template<> void dbLineData::toString(std::string val, char *dst)
+{
+  ENTER_IFCN("dbLineData::toString(std::string,char*)");
+  Integer vallen=strlen(val.c_str());
+  if (vallen>ELEMENTBLOCKSIZE)
+  {
+    delete dst;
+    dst = new char[vallen+1];
+  }
+  strcpy(dst,val.c_str());
+}
+
+template<> void dbLineData::toString(Double val, char *dst)
+{
+  ENTER_IFCN("dbLineData::toString(Double,char*)");
+  sprintf(dst,"%e",val);
+}
+
+template<> void dbLineData::toString(float val, char *dst)
+{
+  ENTER_IFCN("dbLineData::toString(float,char*)");
+  sprintf(dst,"%e",val);
+}
+
+template<> void dbLineData::toString(Integer val, char *dst)
+{
+  ENTER_IFCN("dbLineData::toString(Integer,char*)");
+  sprintf(dst,"%d",val);
+}
+
 
 } // end of namespace
--- libsrc/DataInOut/Database/dbLineData.hh
+++ libsrc/DataInOut/Database/dbLineData.hh
@@ -4,6 +4,10 @@
 
 #include <vector>
 #include <string>
+#include "General/environment.hh"
+
+#define STRINGBLOCKSIZE	 1024
+#define ELEMENTBLOCKSIZE   32
 
 
 namespace CoupledField
@@ -18,31 +22,64 @@ public:
   */
   dbLineData(const std::string &tname);
 
+  //! Default destructor
+  ~dbLineData();
+
   //! Set field to value
   /*!
     \param fieldname Field name
     \param val Value
+    \param quoted If value is sent to database quoted or unquoted
   */
-  template<class T> int Set (const char *fieldname, const T val)
+  template<class T> int Set (const char *fieldname, 
+                             const T val, 
+                             Boolean quoted=TRUE)
   {
-    field.push_back(std::string(fieldname));
-    std::stringstream v;
-    v<<"'"<<val<<"'";
-    value.push_back(v.str());
+    ENTER_IFCN("dbLineData::Set"); 
+    Integer fnamelength = strlen(fieldname);
+    Integer curFLength  = strlen(fields);
+    if (curFLength+fnamelength>=fieldlength)	// Allocate new memory
+    {
+#ifdef DEBUG
+    (*debug)<<"dbLineData::Set : Allocate new memory"<<std::endl;
+#endif
+      char *newFields = new char[2*fieldlength];
+      strcpy(newFields,fields);
+      delete[] fields;
+      fields = newFields;
+    }
+#ifdef DEBUG2
+    (*debug)<<"dbLineData::Set : Fields="<<fields<<" (before) "<<std::endl;
+#endif
+    if (curFLength>0)
+      strcat(fields,",");
+    strcat(fields,fieldname); 
+#ifdef DEBUG2
+    (*debug)<<"dbLineData::Set : Fields="<<fields<<std::endl;
+#endif
 
-  };
-
-  //! Set field to value and do not quote value. Use it only if you give functions in 'val'
-  /*!
-    \param fieldname Field name
-    \param val Value
-  */
-  template<class T> int SetUnquoted (const char *fieldname, const T val)
-  {
-    field.push_back(std::string(fieldname));
-    std::stringstream v;
-    v<<val;
-    value.push_back(v.str());
+    char *valstr = new char[ELEMENTBLOCKSIZE];
+    toString(val,valstr);
+    Integer vallength = strlen(valstr);
+    Integer curVLength= strlen(values);
+    if (curVLength+vallength>=valuelength)	// Allocate new memory
+    {
+      char *newValues = new char[2*valuelength];
+      strcpy(newValues,values);
+      delete[] values;
+      values = newValues;
+    }
+    if (curVLength>0)
+      strcat(values,",");
+    if (quoted)
+      strcat(values,"'");
+    strcat(values,valstr);
+    delete[] valstr;
+    if (quoted)
+      strcat(values,"'");
+#ifdef DEBUG2
+    (*debug)<<"dbLineData::Set : Values="<<values<<std::endl;
+#endif
 
   };
 
@@ -62,20 +99,42 @@ public:
   /*!
     \param idx Index
   */
-  std::string GetFieldName(int idx);
+  char *GetFieldNames();
 
   //! Get value (as string)
   /*!
     \param idx Index
   */
-  std::string GetValue (int idx);
+  char *GetValues ();
 
-private:
   //! Field names
-  std::vector<std::string> field;
+//  std::vector<std::string> field;
+  char *fields;
 
   //! Values
-  std::vector<std::string> value;
+//  std::vector<std::string> value;
+  char *values;
+
+  template<class T> void toString(const T val, char *dst)
+  {
+    ENTER_IFCN("dbLineData::toString(class T, char*)");
+    std::stringstream sstream;
+    sstream<<val;
+    strcpy(dst,sstream.str().c_str());
+  };
+
+
+  int fieldlength;
+  int valuelength;
+
+private:
+  
+  dbLineData(){};
+
+  dbLineData(const dbLineData &x)
+  {
+    ENTER_FCN("dbLineData::copy-constructor");
+  };
 };
 
 } // end of namespace
