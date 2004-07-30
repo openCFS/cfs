07e6916e59a7632cec990e62b4904a6738f47a65
oweitzel <oweitzel@cfs-dev.mdmt.tuwien.ac.at>
2004-07-30 13:56:10 +0000

    Fixed memory leak at c-string operations

--- libsrc/DataInOut/Database/dbLineData.cc
+++ libsrc/DataInOut/Database/dbLineData.cc
@@ -55,44 +55,64 @@ void dbLineData::Clear()
 //  field.clear();
 }
 
-template<> void dbLineData::toString(const char *val, char* &dst)
+template<> void dbLineData::toString(const char *val, char* &dst, int &strsize)
 {
   ENTER_IFCN("dbLineData::toString(char*,char*)");
-  if (strlen(val)>ELEMENTBLOCKSIZE)
+  if (strlen(val)>=strsize)
   {
     delete[] dst;
-    dst = new char[strlen(val)+1];
+    strsize=strlen(val)+1;
+    dst = new char[strsize];
   }
   strcpy(dst,val);
 }
 
-template<> void dbLineData::toString(std::string val, char* &dst)
+template<> void dbLineData::toString(std::string val, char* &dst, int &strsize)
 {
   ENTER_IFCN("dbLineData::toString(std::string,char*)");
   Integer vallen=val.length();
-  if (vallen>=ELEMENTBLOCKSIZE)
+  if (vallen>=strsize)
   {
     delete[] dst;
-    dst = new char[vallen+1];
+    strsize=vallen+1;
+    dst = new char[strsize];
   }
   strcpy(dst,val.c_str());
 }
 
-template<> void dbLineData::toString(Double val, char* &dst)
+template<> void dbLineData::toString(Double val, char* &dst, int &strsize)
 {
   ENTER_IFCN("dbLineData::toString(Double,char*)");
+  if (strsize<20)
+  {
+    delete[] dst;
+    strsize = 25;
+    dst = new char[strsize];
+  }
   sprintf(dst,"%e",val);
 }
 
-template<> void dbLineData::toString(float val, char* &dst)
+template<> void dbLineData::toString(float val, char* &dst, int &strsize)
 {
   ENTER_IFCN("dbLineData::toString(float,char*)");
+  if (strsize<20)
+  {
+    delete[] dst;
+    strsize = 25;
+    dst = new char[strsize];
+  }
   sprintf(dst,"%e",val);
 }
 
-template<> void dbLineData::toString(Integer val, char* &dst)
+template<> void dbLineData::toString(Integer val, char* &dst, int &strsize)
 {
   ENTER_IFCN("dbLineData::toString(Integer,char*)");
+  if (strsize<20)
+  {
+    delete[] dst;
+    strsize = 25;
+    dst = new char[strsize];
+  }
   sprintf(dst,"%d",val);
 }
 
