15dc31a316ea2b632cdc2ff76e9ec8e7a7eb87ee
Andreas Hauck <mail@andreas-hauck.com>
2004-08-03 09:23:56 +0000

    Added a new class PDEMemento which stores the internal state of a PDE, that is the solution, the time derivative(s) and also an instance of the CouplingMemento (see seperate checkin). The memento class encapsulates this information. Therefore it is a friend class of PDE and vice versa. The Memento object  is needed to give the information from one multiSequence step to the next one. This generalizes the previous approach, where only the solution itself was given explicitly to the PDEs in the next multiSequence step.

---
+++ libsrc/PDE/pdememento.cc
@@ -0,0 +1,34 @@
+#include "pdememento.hh"
+
+namespace CoupledField{
+
+PDEMemento::PDEMemento()
+{
+  ENTER_IFCN( "PDEMemento::PDEMemento");
+  
+  sol_       = NULL;
+  isSet_ = FALSE;
+}
+
+PDEMemento::~PDEMemento()
+{
+  ENTER_IFCN( "PDEMemento::~PDEMemento");
+  
+  if (sol_)
+    delete sol_;
+
+  sol_       = NULL;
+}
+
+void PDEMemento::Clear()
+{
+  ENTER_FCN( "PDEMemento::Clear");
+  if (sol_)
+    delete sol_;
+
+  sol_       = NULL;
+  solDeriv1_.Clear();
+  solDeriv2_.Clear();
+}
+
+} // namespace
