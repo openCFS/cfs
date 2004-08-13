46832daf403fdf26fa2484ba7e06f9a98c0dcbc6
oweitzel <oweitzel@cfs-dev.mdmt.tuwien.ac.at>
2004-08-13 13:28:12 +0000

    Added column with filename

--- auxiliary/mysql/getresultidx.sql
+++ auxiliary/mysql/getresultidx.sql
@@ -1,5 +1,8 @@
 SELECT	DATE_FORMAT(c.stamp,"%d.%m.%y   %H:%i:%S"),
+	i.filename,
 	r.idx 
 FROM	Calculation as c,
-	Result as r
+	Result as r,
+	InputParam as i
 WHERE 	r.calculation_idx	= c.idx
+  AND	i.idx 			= c.inputparam_idx
