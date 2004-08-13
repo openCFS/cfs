96fd61abc94b366bf3a25ab5651d4322303c0be3
Oliver Weitzel <oweitzel@svn>
2004-08-13 14:39:11 +0000

    Adapted scripts to new database structure without names like "Dataset55"

--- auxiliary/mysql/getdataset781.sql
+++
@@ -1,8 +0,0 @@
-SELECT 	coord.node_label as node,
-	coord.x_coord as x,
-	coord.y_coord as y,
-	coord.z_coord as z
-FROM 	Dataset781 as ds781,
-	Dataset781_node as coord
-WHERE	ds781.result_idx	= RESULTIDX
-	AND coord.dataset781_idx=ds781.idx
