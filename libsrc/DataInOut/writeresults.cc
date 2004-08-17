818e340611c1d59b9276a5c3d96a7c89375fa004
Markus Mohr <marcus.mohr@geophysik.uni-muenchen.de>
2004-08-17 17:13:08 +0000

    Added some further comments and indentation.

--- libsrc/DataInOut/writeresults.cc
+++ libsrc/DataInOut/writeresults.cc
@@ -13,9 +13,11 @@
 
 namespace CoupledField {
 
-  WriteResults::WriteResults(const Char * const filename,
-			     FileType * const aInFile)
-  {
+  // ===============
+  //   Constructor
+  // ===============
+  WriteResults::WriteResults( const Char * const filename,
+			      FileType * const aInFile ) {
 
     ENTER_FCN( "WriteResults::WriteResults" );
 
@@ -24,239 +26,285 @@ namespace CoupledField {
     ascii_=TRUE;
     
     pt2Inputfile_ = aInFile;
-    
-}
+  }
 
 
-WriteResults::~WriteResults()
-{
-  ENTER_FCN( "WriteResults::~WriteResults" );
+  // ======================
+  //   Default Destructor
+  // ======================
+  WriteResults::~WriteResults() {
+    ENTER_FCN( "WriteResults::~WriteResults" );
   
-  for (Integer i=0; i<historyFiles_.GetSize(); i++)
-    for (Integer j=0; j<historyFiles_[i].GetSize(); j++)
-      if (historyFiles_[i][j])
-	delete historyFiles_[i][j];
-  
-}
+    for (Integer i=0; i<historyFiles_.GetSize(); i++) {
+      for (Integer j=0; j<historyFiles_[i].GetSize(); j++) {
+	if (historyFiles_[i][j]) {
+	  delete historyFiles_[i][j];
+	}
+      }
+    }
+  }
 
 
+  // ==========================
+  //   Initialisation Routine
+  // ==========================
+  void WriteResults::InitHistoryFiles() {
 
-void WriteResults::InitHistoryFiles()
-{
-  ENTER_FCN( "WriteResults::InitHistoryFiles" );
-
+    ENTER_FCN( "WriteResults::InitHistoryFiles" );
  
- StdVector<Integer> nodesTmp;
+    StdVector<Integer> nodesTmp;
 
 #ifndef XMLPARAMS
- conf->getlist(nodesTmp,"history_node");
- Warning("History files are not supported anymore for .conf-file format!");
+    conf->getlist(nodesTmp,"history_node");
+    Warning("History files are not supported anymore for .conf-file format!");
 #else
+
+    // *****************************************************************
+    //   Determine for which output quantities there are history nodes
+    //   and what their names are
+    // *****************************************************************
+    StdVector<std::string> keyVec, attrVec, valVec;
+    StdVector<std::string> nodeVec, quantVec;
+    SolutionType solType;
  
- StdVector<std::string> keyVec, attrVec, valVec;
- StdVector<std::string> nodeVec, quantVec;
- SolutionType solType;
+    attrVec = "", "";
+    valVec = "", "";
+
+    keyVec = "storeResults", "nodeHistory", "saveNodes";
+    params->GetList(keyVec, attrVec, valVec, nodeVec);
+
+    keyVec = "storeResults", "nodeHistory", "type";
+    params->GetList(keyVec, attrVec, valVec, quantVec);
+
+    // Check if there are any history nodes at all
+    // and set NeedHistory_ flag accordingly
+    NeedHistory_ = FALSE;
+    if (nodeVec.GetSize() > 0) {
+      NeedHistory_ = TRUE;
+    }
+
+    // If there is aren't any history nodes,
+    // then there is nothing to do
+    if ( NeedHistory_ == FALSE ) {
+      return;
+    }
+
+    // ***********************************************
+    //   Generate vectors holding information on the
+    //   output quantities and nodes for which the
+    //   user wants to see the history
+    //
+    //   histQuantities_ .... holds all quantities
+    //   histNodesPerPDE_ ... number of history nodes
+    //                        for each PDE
+    // ***********************************************
+
+    // Make a sanity check
+    if ( histQuantities_.GetSize() != 0 || histNodesPerPDE_.GetSize() != 0 ) {
+      Warning( "Repeated call to WriteResults::InitHistoryFiles ?!?!",
+	       __FILE__, __LINE__ );
+    }
+
+    // Define some loop counters and auxilliary variables
+    Integer iQuant, i, j;
+    Integer quantityFound;
+
+    // In the vector of quantities there may be duplicates
+    // these must be eliminated!
+    for ( iQuant = 0; iQuant < quantVec.GetSize(); iQuant++ ) {
+
+      // (re-)initialise
+      quantityFound = -1;
+
+      // Determine if current output quantity was already assigned
+      for ( j = 0; j < histQuantities_.GetSize(); j++ ) {
+	String2Enum( quantVec[iQuant], solType );
+	if ( histQuantities_[j] == solType ) {
+	  quantityFound = j;
+	  break;
+	}
+      }
+
+      // The quantity was not yet assigned, so do this now
+      // and also make room for storing the curresponding
+      // number of history nodes
+      if ( quantityFound == -1 ) {
+	String2Enum( quantVec[iQuant], solType );
+	histQuantities_.Push_back(solType);
+	histNodesPerPDE_.Push_back(StdVector<Integer>());
+	quantityFound = histNodesPerPDE_.GetSize() - 1;
+      }
+
+      // Whenever an output quantity appears in quantVec it is
+      // related to a saveNodes specifier. Add the corresponding
+      // nodes to the histNodesPerPDE_ vector
+      StdVector<Integer> tempNodes;
+      pt2Inputfile_->ReadSaveNodes( tempNodes, nodeVec[iQuant] );
+      for ( i = 0; i < tempNodes.GetSize(); i++ ) {
+	histNodesPerPDE_[quantityFound].Push_back(tempNodes[i]);
+      }
+    }
+
+
+    // ***********************
+    //   Prepare file output
+    // ***********************
+
+    // Create history directory
+    std::string S="mkdir -p history";
+    system(S.c_str());
  
- attrVec = "", "";
- valVec = "", "";
+    std::string namePrefix="history/" + namefile_ + "-";
+    std::string namePostfix = ".hist";
+    std::string totalName;
 
- keyVec = "storeResults", "nodeHistory", "saveNodes";
- params->GetList(keyVec, attrVec, valVec, nodeVec);
+    // Resize number of history files
+    historyFiles_.Resize(histQuantities_.GetSize());
 
- keyVec = "storeResults", "nodeHistory", "type";
- params->GetList(keyVec, attrVec, valVec, quantVec);
+    // Iterate over all quantities
+    std::string quantityName;
+    for ( iQuant = 0; iQuant < histQuantities_.GetSize(); iQuant++ ) {
 
+      // std::string comment = "Identifiers for history nodes of quantity '";
+      std::string comment = "Node numbers of history nodes for quantity '";
+      Enum2String( histQuantities_[iQuant], quantityName );
+      comment +=  quantityName + "'";
+      Info->PrintVec(comment.c_str(), histNodesPerPDE_[iQuant]);
 
+      historyFiles_[iQuant].Resize(histNodesPerPDE_[iQuant].GetSize());
+      for (Integer iNode=0; iNode<histNodesPerPDE_[iQuant].GetSize(); iNode++){
 
- NeedHistory_ = FALSE;
- if (nodeVec.GetSize() > 0)
-   NeedHistory_ = TRUE;
+	// Create total filename
+	std::string quantString;
+	Enum2String(histQuantities_[iQuant], quantString);
+	totalName = namePrefix + quantString;
+	totalName += "-node-";
+	totalName += Info->GenStr(histNodesPerPDE_[iQuant][iNode]);
+	totalName += namePostfix;
 
- if (NeedHistory_ == FALSE)
-   return;
-
- // Loop over all read in quantities and nodes
- for (Integer iQuant=0; iQuant<quantVec.GetSize(); iQuant++) {
-   
-   Integer quantityFound = -1;
-
-   // Look in all histQuantites, if quantity was already read in
-   for (Integer j=0; j<histQuantities_.GetSize(); j++) {
-     String2Enum(quantVec[iQuant], solType);
-     if (histQuantities_[j] == solType) {
-       quantityFound = j;
-       break;
-     }
-   }
-  
-   
-   if (quantityFound == -1) {
-     String2Enum(quantVec[iQuant], solType);
-     histQuantities_.Push_back(solType);
-     histNodesPerPDE_.Push_back(StdVector<Integer>());
-     quantityFound = histNodesPerPDE_.GetSize() -1;
-   }
-   
-   // Add new nodes
-   StdVector<Integer> tempNodes;
-   pt2Inputfile_->ReadSaveNodes(tempNodes, nodeVec[iQuant]);
-   for (Integer i=0; i<tempNodes.GetSize(); i++)
-     histNodesPerPDE_[quantityFound].Push_back(tempNodes[i]);
-    } // iQuant
-
- // Create history directory
- std::string S="mkdir -p history";
- system(S.c_str());
- 
- std::string namePrefix="history/" + namefile_ + "-";
- std::string namePostfix = ".hist";
- std::string totalName;
- 
- // Resize number of history files
- historyFiles_.Resize(histQuantities_.GetSize());
- 
- // Iterate over all quantities
- for (Integer iQuant=0; iQuant<histQuantities_.GetSize(); iQuant++) {
-   
-   std::string comment = "Nodehistory for quantity '";
-   comment += histQuantities_[iQuant] + "'";
-   Info->PrintVec(comment.c_str(), histNodesPerPDE_[iQuant]);
-
-   historyFiles_[iQuant].Resize(histNodesPerPDE_[iQuant].GetSize());
-   for (Integer iNode=0; iNode<histNodesPerPDE_[iQuant].GetSize(); iNode++) {
-
-     // Create total filename
-     std::string quantString;
-     Enum2String(histQuantities_[iQuant], quantString);
-     totalName = namePrefix + quantString;
-     totalName += "-node-";
-     totalName += Info->GenStr(histNodesPerPDE_[iQuant][iNode]);
-     totalName += namePostfix;
-
-     historyFiles_[iQuant][iNode] = NULL;
-     historyFiles_[iQuant][iNode] = new std::ofstream(totalName.c_str());
+	historyFiles_[iQuant][iNode] = NULL;
+	historyFiles_[iQuant][iNode] = new std::ofstream(totalName.c_str());
      
-     if (!historyFiles_[iQuant][iNode]) {
-       std::string errMsg = "InitHistory: Can not open history file '";
-       errMsg += totalName + "'";
-       Error (errMsg.c_str(), __FILE__, __LINE__);
-     }
-     
-   }
- }
+	if (!historyFiles_[iQuant][iNode]) {
+	  std::string errMsg = "InitHistory: Can not open history file '";
+	  errMsg += totalName + "'";
+	  Error (errMsg.c_str(), __FILE__, __LINE__);
+	}
+      }
+    }
  
-#endif
-}
+#endif // ifndef XMLPARAMS
+  }
 
-void WriteResults::WriteSolMatrix(Grid * ptgrid, const Integer level, const Vector<Double> sol, 
-				  const std::string matFileName, const Integer nrDofs)
-{
-  ENTER_FCN( "WriteResults::WriteSolMatrix" );
 
-  //get and write number of nodes on the level
-  Integer numnodes=ptgrid->GetMaxnumnodes(level);
-  Integer dim=ptgrid->GetDim();
-  Integer i;
+  void WriteResults::WriteSolMatrix(Grid * ptgrid, const Integer level,
+				    const Vector<Double> sol, 
+				    const std::string matFileName,
+				    const Integer nrDofs) {
 
-  std::ofstream * matrixOut = new std::ofstream(matFileName.c_str());
-  // use scientific output, formatting is much better
-  matrixOut->setf(std::ios::scientific, std::ios::floatfield);
+    ENTER_FCN( "WriteResults::WriteSolMatrix" );
+
+    //get and write number of nodes on the level
+    Integer numnodes=ptgrid->GetMaxnumnodes(level);
+    Integer dim=ptgrid->GetDim();
+    Integer i;
+
+    std::ofstream * matrixOut = new std::ofstream(matFileName.c_str());
+    // use scientific output, formatting is much better
+    matrixOut->setf(std::ios::scientific, std::ios::floatfield);
   
-  if (dim==2) 
-    {
+    if (dim==2) {
       Point<2> point;
       
       // write x,y,z-coordinate
-      for (i=0; i<numnodes; i++)
-	{
-	  ptgrid->GetCoordinateNode(i,level,point);
-	  (*matrixOut) << " \t" << point[0] << " \t" << point[1] << " \t" << 0 << " \t";
-	    
-	  for (Integer actDof =0; actDof < nrDofs; actDof++)
-	    (*matrixOut) << sol[i*nrDofs + actDof] << " \t";
-	  (*matrixOut) << std::endl;
+      for (i=0; i<numnodes; i++) {
+	ptgrid->GetCoordinateNode(i,level,point);
+	(*matrixOut) << " \t" << point[0] << " \t" << point[1] << " \t"
+		     << 0 << " \t";
+
+	for (Integer actDof =0; actDof < nrDofs; actDof++) {
+	  (*matrixOut) << sol[i*nrDofs + actDof] << " \t";
 	}
+	(*matrixOut) << std::endl;
+      }
     }
-  else 
-    {
+    else {
       Point<3> point;
       
       // write x,y,z-coordinate
-      for (i=0; i<numnodes; i++)
-	{
-	  ptgrid->GetCoordinateNode(i,level,point);
-	  (*matrixOut) << " \t" << point[0] << " \t" << point[1] << " \t" << point[2] << " \t";
+      for (i=0; i<numnodes; i++) {
+	ptgrid->GetCoordinateNode(i,level,point);
+	(*matrixOut) << " \t" << point[0] << " \t" << point[1] << " \t"
+		     << point[2] << " \t";
 
-	  for (Integer actDof =0; actDof < nrDofs; actDof++)
-	    (*matrixOut) << sol[i*nrDofs + actDof] << " \t";
-	  (*matrixOut) << std::endl;
-	} 
+	for (Integer actDof =0; actDof < nrDofs; actDof++) {
+	  (*matrixOut) << sol[i*nrDofs + actDof] << " \t";
+	}
+	(*matrixOut) << std::endl;
+      } 
     }
-}
-
-void WriteResults::WriteNodeHistoryTransient(const NodeStoreSol<Double>& data, 
-					     const Integer step, 
-					     const Double time)
-{
-  ENTER_FCN( "WriteResults::WriteNodeHistoryTransient" );
-
-  std::ofstream * myHist;
-  SolutionType actSolType;
-  StdVector<SolutionType> solTypes;
-  Integer iQuant, actDof;
-  std::string quantity;
-  Double val;
+  }
 
 
-  data.GetSolutionTypes(solTypes);
+  void WriteResults::WriteNodeHistoryTransient(const NodeStoreSol<Double>&data,
+					       const Integer step,
+					       const Double time) {
+
+    ENTER_FCN( "WriteResults::WriteNodeHistoryTransient" );
+
+    std::ofstream * myHist;
+    SolutionType actSolType;
+    StdVector<SolutionType> solTypes;
+    Integer iQuant, actDof;
+    std::string quantity;
+    Double val;
+
+    data.GetSolutionTypes(solTypes);
   
-  // Iterate over all solutiontypes
-  for (Integer iSol=0; iSol<solTypes.GetSize(); iSol++) {
+    // Iterate over all solutiontypes
+    for (Integer iSol=0; iSol<solTypes.GetSize(); iSol++) {
    
-    actDof = data.GetDof(solTypes[iSol]);
+      actDof = data.GetDof(solTypes[iSol]);
     
-    // Find the according quantitiy
-    iQuant = -1;
-    for (Integer i=0; i<histQuantities_.GetSize(); i++) {
-     
-      if (histQuantities_[i] == solTypes[iSol]){
-	iQuant = i;
-	break;
+      // Find the according quantitiy
+      iQuant = -1;
+      for (Integer i=0; i<histQuantities_.GetSize(); i++) {
+
+	if (histQuantities_[i] == solTypes[iSol]){
+	  iQuant = i;
+	  break;
+	}
       }
-    }
-    
-
-    if (iQuant == -1){
-      Enum2String(solTypes[iSol], quantity);
-      std::string errMsg = "Quantity '";
-      errMsg += quantity;
-      errMsg += "' for history not found!";
-      Error(errMsg.c_str(), __FILE__, __LINE__);
-    }
-    
-    // Iterate over all history nodes
-    for (Integer iNode=0; 
-	 iNode<histNodesPerPDE_[iQuant].GetSize(); 
-	 iNode++) {
-      myHist = historyFiles_[iQuant][iNode];
-      (*myHist) << time;
       
-      // Iterate over all dofs
-      for (Integer iDof=0; iDof<actDof; iDof++) {
-	data.Get(solTypes[iSol],histNodesPerPDE_[iQuant][iNode]-1, iDof, val);
-	(*myHist) << "  " << val;
-      } // iDof
-      (*myHist) << std::endl;
-    }  // iNode
-  } // iSol
-}
+      if (iQuant == -1){
+	Enum2String(solTypes[iSol], quantity);
+	std::string errMsg = "Quantity '";
+	errMsg += quantity;
+	errMsg += "' for history not found!";
+	Error(errMsg.c_str(), __FILE__, __LINE__);
+      }
+    
+      // Iterate over all history nodes
+      for (Integer iNode=0; iNode<histNodesPerPDE_[iQuant].GetSize(); iNode++){
+	myHist = historyFiles_[iQuant][iNode];
+	(*myHist) << time;
+      
+	// Iterate over all dofs
+	for (Integer iDof=0; iDof<actDof; iDof++) {
+	  data.Get(solTypes[iSol],histNodesPerPDE_[iQuant][iNode]-1, iDof,val);
+	  (*myHist) << "  " << val;
+	} // iDof
+	(*myHist) << std::endl;
+      }  // iNode
+    } // iSol
+  }
 
-void WriteResults::WriteNodeHistoryHarmonic(const NodeStoreSol<Complex>& data, 
-					    const Integer step,
-					    const Double frequency,
-					    const ComplexFormat format)
-{
-  ENTER_FCN( "WriteResults::WriteNodeHistoryHarmonic" );
-}
+
+  void WriteResults::WriteNodeHistoryHarmonic(const NodeStoreSol<Complex>&data,
+					      const Integer step,
+					      const Double frequency,
+					      const ComplexFormat format) {
+    ENTER_FCN( "WriteResults::WriteNodeHistoryHarmonic" );
+  }
 
 } // end of namespace
