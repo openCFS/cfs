1901f4b5a681576c59a8d4527e92a35615357665
bahr <bahr@cfs-dev.mdmt.tuwien.ac.at>
2004-09-10 15:18:25 +0000

    Added new file mathfunction. Contains now gammaln to compute Gamma function. Needed for fractional damping model.

---
+++ libsrc/Utils/mathfunctions.hh
@@ -0,0 +1,16 @@
+#ifndef FUNCTIONS_2001
+#define FUNCTIONS_2001
+
+#include "General/environment.hh"
+
+namespace CoupledField
+{
+  Double gammaln(Double xx);
+  //! Returns the value ln[gamma(xx)] for xx > 0
+  /*!
+    \param real value greater zero
+  */
+
+} // end of namespace CoupledField
+
+#endif  // FILE_FUNCTIONS
