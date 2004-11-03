4249da433ed8578128c80199c81cebab07dc0862
tlahmer <tlahmer@cfs-dev.mdmt.tuwien.ac.at>
2004-11-03 13:20:37 +0000

    there might be a new beer sponsor in our house ...
    well, in massInt::CalcElementMatrix a new variable factor_ gained admission. Unfortunately it did not got initialized in all constructors (there was a third constructor at the bottom of the file ... who puts a constructor there?)
    
    Actions:
    Set factor_ =1;
    Moved constructor to top of file.

--- libsrc/Forms/massInt.cc
+++ libsrc/Forms/massInt.cc
@@ -5,145 +5,147 @@
 
 namespace CoupledField {
 
-MassInt::MassInt(BaseFE * aptelem, Double aDensity, Boolean axi)
-  : BaseForm(aptelem), 
-	density_(aDensity), 
-	nrDofsPerNode_(1)
-{
-  ENTER_FCN( "MassInt::MassInt" );
+  MassInt::MassInt(BaseFE * aptelem, Double aDensity, Boolean axi)
+    : BaseForm(aptelem), 
+      density_(aDensity), 
+      nrDofsPerNode_(1)
+  {
+    ENTER_FCN( "MassInt::MassInt" );
 
-  factor_ = 1.0;
-  isaxi_ = axi;
-  dofzero_ = 0;
-}
+    factor_ = 1.0;
+    isaxi_ = axi;
+    dofzero_ = 0;
+  }
 
-MassInt::MassInt(const Double aDensity,  const Integer nrDofsPerNode, Boolean axi)
-  : BaseForm(), 
-	density_(aDensity), 
-	nrDofsPerNode_(nrDofsPerNode)
-{
-  ENTER_FCN( "MassInt::MassInt" );
+  MassInt::MassInt(const Double aDensity,  const Integer nrDofsPerNode, Boolean axi)
+    : BaseForm(), 
+      density_(aDensity), 
+      nrDofsPerNode_(nrDofsPerNode)
+  {
+    ENTER_FCN( "MassInt::MassInt" );
 
-  factor_ = 1.0;
-  isaxi_ = axi;
-  dofzero_ = 0;
-}
+    factor_ = 1.0;
+    isaxi_ = axi;
+    dofzero_ = 0;
+  }
+
+  MassInt::MassInt(const Double aDensity,  const Integer nrDofsPerNode, Integer dofzero,
+		   Boolean axi)
+    : BaseForm(), 
+      density_(aDensity), 
+      nrDofsPerNode_(nrDofsPerNode)
+  {
+    ENTER_FCN( "MassInt::MassInt" );
+
+    factor_ = 1.0;
+    isaxi_ = axi;
+    dofzero_ = dofzero;
+  }
  
-MassInt::~MassInt()
-{
-  ENTER_FCN( "MassInt::~MassInt" );
-}
-
-void MassInt::CalcElementMatrix(Matrix<Double> & ptCoord,
-								Matrix<Double> & elemMat)
-{
-  ENTER_FCN( "MassInt::CalcElemMatrix" );
-  
-  const Integer nrIntPts= ptelem->GetNumIntPoints();
-  const Integer nrNodes = ptelem->GetNumNodes();
-  const Vector<Double> & intWeights = ptelem->GetIntWeights();  
-  Double jacDet;
-
-  // derivation of shape functions after global coordinates 
-
-  Vector<Double> shapeFncAtIp;
-  Matrix<Double> partElemMat;
-  Vector<Double> CoordAtIP;
-
-  // set matrix to desired size and set all elements to zero
-  //    partElemMat.Resize(nrNodes);
-  elemMat.Resize(nrNodes);
-    
-
-  for (Integer actIntPt=1; actIntPt <= nrIntPts; actIntPt++) {
-
-	jacDet = ptelem->CalcJacobianDetAtIp(actIntPt, ptCoord);
-	
-	ptelem-> GetShFncAtIp(shapeFncAtIp, actIntPt);
-	
-	partElemMat.DyadicMult(shapeFncAtIp);
-	
-	if (isaxi_) {
-	  CoordAtIP = ptCoord * shapeFncAtIp;
-	  partElemMat *= 2 * PI * intWeights[actIntPt-1] * density_ * factor_* jacDet * CoordAtIP[0];
-	}
-	else 
-	  partElemMat *= intWeights[actIntPt-1] * density_ * factor_ * jacDet;
-	
-	elemMat += partElemMat;
+  MassInt::~MassInt()
+  {
+    ENTER_FCN( "MassInt::~MassInt" );
   }
+
+  void MassInt::CalcElementMatrix(Matrix<Double> & ptCoord,
+				  Matrix<Double> & elemMat)
+  {
+    ENTER_FCN( "MassInt::CalcElemMatrix" );
   
-  if (nrDofsPerNode_ > 1 && dofzero_ > 0 ) {
-	Matrix <Double> multDofMassZero;
-	MassMultiDofZero(multDofMassZero, elemMat);
-	elemMat = multDofMassZero;
-  }
+    const Integer nrIntPts= ptelem->GetNumIntPoints();
+    const Integer nrNodes = ptelem->GetNumNodes();
+    const Vector<Double> & intWeights = ptelem->GetIntWeights();  
+    Double jacDet;
+
+    // derivation of shape functions after global coordinates 
+
+    Vector<Double> shapeFncAtIp;
+    Matrix<Double> partElemMat;
+    Vector<Double> CoordAtIP;
+
+    // set matrix to desired size and set all elements to zero
+    //    partElemMat.Resize(nrNodes);
+    elemMat.Resize(nrNodes);
     
-  else if (nrDofsPerNode_ > 1 && dofzero_ == 0 ) {
-	Matrix <Double> multDofMass;
-	MassMultiDof(multDofMass, elemMat, nrDofsPerNode_);	
-	elemMat = multDofMass;
-  }
+
+    for (Integer actIntPt=1; actIntPt <= nrIntPts; actIntPt++) {
+
+      jacDet = ptelem->CalcJacobianDetAtIp(actIntPt, ptCoord);
+	
+      ptelem-> GetShFncAtIp(shapeFncAtIp, actIntPt);
+	
+      partElemMat.DyadicMult(shapeFncAtIp);
+	
+      if (isaxi_) {
+	CoordAtIP = ptCoord * shapeFncAtIp;
+	partElemMat *= 2 * PI * intWeights[actIntPt-1] * density_ * factor_* jacDet * CoordAtIP[0];
+      }
+      else 
+	partElemMat *= intWeights[actIntPt-1] * density_ * factor_ * jacDet;
+	
+      elemMat += partElemMat;
+    }
   
-}
-
-void MassInt::Print(std::ostream * out, const Matrix<Double> Result) const
-{
-  ENTER_FCN( "MassInt::Print" );
-}
-
-
-void MassInt::MassMultiDof(Matrix<Double>& massMultDof, 
-						   const Matrix<Double>& massMatSingleDof,  const Integer nrDofs)
-{
-  ENTER_FCN( "MassInt::MassMultiDof" );
+    if (nrDofsPerNode_ > 1 && dofzero_ > 0 ) {
+      Matrix <Double> multDofMassZero;
+      MassMultiDofZero(multDofMassZero, elemMat);
+      elemMat = multDofMassZero;
+    }
     
-  const Integer singleDofSize = massMatSingleDof.GetSizeRow();
+    else if (nrDofsPerNode_ > 1 && dofzero_ == 0 ) {
+      Matrix <Double> multDofMass;
+      MassMultiDof(multDofMass, elemMat, nrDofsPerNode_);	
+      elemMat = multDofMass;
+    }
+  
+  }
 
-  Integer i, j, actDof;
-    
-  massMultDof.Resize(nrDofsPerNode_*singleDofSize);
-  massMultDof.Init();
-    
-  for (i=0; i < singleDofSize; i++)
-	for (j=0; j < singleDofSize; j++)
-	  for (actDof=0; actDof < nrDofs; actDof++)
-		massMultDof[i*nrDofs + actDof][j*nrDofs + actDof] = massMatSingleDof[i][j]; 
-}
+  void MassInt::Print(std::ostream * out, const Matrix<Double> Result) const
+  {
+    ENTER_FCN( "MassInt::Print" );
+  }
 
-MassInt::MassInt(const Double aDensity,  const Integer nrDofsPerNode, Integer dofzero,
-				 Boolean axi)
-  : BaseForm(), 
-	density_(aDensity), 
-	nrDofsPerNode_(nrDofsPerNode)
-{
-  ENTER_FCN( "MassInt::MassInt" );
 
-  isaxi_ = axi;
-  dofzero_ = dofzero;
-}
+  void MassInt::MassMultiDof(Matrix<Double>& massMultDof, 
+			     const Matrix<Double>& massMatSingleDof,  const Integer nrDofs)
+  {
+    ENTER_FCN( "MassInt::MassMultiDof" );
+    
+    const Integer singleDofSize = massMatSingleDof.GetSizeRow();
 
-void MassInt::MassMultiDofZero(Matrix<Double>& massMultDofZero, const Matrix<Double>& massMatSingleDof)
-{
-  ENTER_FCN( "MassInt::MassMultiDofZero" );
+    Integer i, j, actDof;
     
-  Integer nrDofs = nrDofsPerNode_ -1;
-  const Integer singleDofSize = massMatSingleDof.GetSizeRow();
+    massMultDof.Resize(nrDofsPerNode_*singleDofSize);
+    massMultDof.Init();
     
-  Integer i, j, actDof;
+    for (i=0; i < singleDofSize; i++)
+      for (j=0; j < singleDofSize; j++)
+	for (actDof=0; actDof < nrDofs; actDof++)
+	  massMultDof[i*nrDofs + actDof][j*nrDofs + actDof] = massMatSingleDof[i][j]; 
+  }
+
+
+  void MassInt::MassMultiDofZero(Matrix<Double>& massMultDofZero, const Matrix<Double>& massMatSingleDof)
+  {
+    ENTER_FCN( "MassInt::MassMultiDofZero" );
     
-  massMultDofZero.Resize(nrDofsPerNode_*singleDofSize);
-  massMultDofZero.Init();
+    Integer nrDofs = nrDofsPerNode_ -1;
+    const Integer singleDofSize = massMatSingleDof.GetSizeRow();
     
-  for (i=0; i < singleDofSize; i++)
-	for (j=0; j < singleDofSize; j++)
-	  for (actDof=0; actDof < nrDofsPerNode_ ; actDof++)
-		{
-		  if (actDof+1 != dofzero_)
-			massMultDofZero[i*nrDofsPerNode_ + actDof][j*nrDofsPerNode_ + actDof] = 
-			  massMatSingleDof[i][j]; 
-		}
-}
+    Integer i, j, actDof;
+    
+    massMultDofZero.Resize(nrDofsPerNode_*singleDofSize);
+    massMultDofZero.Init();
+    
+    for (i=0; i < singleDofSize; i++)
+      for (j=0; j < singleDofSize; j++)
+	for (actDof=0; actDof < nrDofsPerNode_ ; actDof++)
+	  {
+	    if (actDof+1 != dofzero_)
+	      massMultDofZero[i*nrDofsPerNode_ + actDof][j*nrDofsPerNode_ + actDof] = 
+		massMatSingleDof[i][j]; 
+	  }
+  }
 
 } // end namespace CoupledField
 
