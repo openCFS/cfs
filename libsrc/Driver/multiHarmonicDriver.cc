b93f9f2e6028cdcf91ee139e7a7beb7ba2fb9793
tlahmer <tlahmer@cfs-dev.mdmt.tuwien.ac.at>
2005-01-24 15:23:33 +0000

    Added Skeleton for new Driver 'multiHarmonic'.

---
+++ libsrc/Driver/multiHarmonicDriver.cc
@@ -0,0 +1,187 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include "multiHarmonicDriver.hh"
+
+#include "DataInOut/GMV/outGMV.hh"
+#include "DataInOut/WriteInfo.hh"
+#include "DataInOut/ParamHandling/BaseParamHandler.hh"
+
+#include <PDE/basePDE.hh>
+
+#ifdef __sgi
+#include <stdarg.h>
+#include <stdio.h>
+#include <math.h>
+#define POW pow
+#else
+#include <cstdarg>
+#include <cstdio>
+#include <cmath>
+#define POW std::pow
+#endif
+
+
+
+
+
+namespace CoupledField
+{
+
+  // ========================================================================
+  // ========================= piezoParamIdent - Part ===========================
+  // ========================================================================
+
+  // constructor
+  // opens datafiles: measuredData.dat for input, imedCurve.dat and piezoLog.dat for output
+
+MultiHarmonicDriver::MultiHarmonicDriver(Domain * adomain,
+				   Integer stepOffset,
+				   Double timeOffset,
+				   std::string driverTag,
+				   Boolean isPartOfSequence)
+:SingleDriver(adomain, stepOffset, timeOffset, 
+		  driverTag, isPartOfSequence){
+
+    ENTER_FCN( "multiharmonic::multiharmonic" );
+
+    //  ptDomain = adomain;
+
+    StdVector<std::string> keyVec, attrVec, valVec;
+    
+    attrVec = "tag";
+    valVec = driverTag_;
+    
+    // Get time stepping information from parameter object
+    keyVec = "multiHarmonic", "startFreq";
+    params->Get(keyVec, attrVec, valVec, startFreq_);
+  
+    keyVec = "multiHarmonic", "stopFreq";
+    params->Get(keyVec, attrVec, valVec, stopFreq_);
+    
+    keyVec = "multiHarmonic", "numFreq";
+    params->Get(keyVec, attrVec, valVec, numFreq_);
+
+    keyVec = "multiHarmonic", "nrMultiHarmonics";
+    params->Get(keyVec, attrVec, valVec, nrMultHarms_);
+
+ 
+    Char* measuredData="measuredData.dat";
+    allMeasuredData = new std::ifstream(measuredData, std::basic_ios<char>::in);
+
+    if (!allMeasuredData)
+      {
+	std::cerr << "\n File measuredData.dat does not exist!" << std::endl;
+	exit(1);
+      }
+
+    std::cout<<"\n Opens impedCurve.dat and piezoLog.dat ... "<<std::endl;
+
+    std::string filename= "imped.dat";
+
+    impedCurve = new std::ofstream(filename.c_str(),std::basic_ios<char>::out);
+
+    if (!impedCurve)
+      {
+	std::cerr << "\n ImpedanceCurve.dat could not be initialized" << std::endl;
+      }
+
+    std::string filenameLog= "piezoLog.dat";
+
+    piezoLog = new std::ofstream(filenameLog.c_str(),std::basic_ios<char>::out);
+    
+    if (!piezoLog)
+      {
+	std::cerr << "\n piezoLog.dat could not be initialized" << std::endl;
+      }
+
+    std::string filenameParLog= "parLog.dat";
+    parLog = new std::ofstream(filenameParLog.c_str(),std::basic_ios<char>::out);
+
+    if (!parLog)
+      {
+	std::cerr << "\n piezoLog.dat could not be initialized" << std::endl;
+      }
+
+  } // end of constructor
+
+  // destructor
+  
+MultiHarmonicDriver::~MultiHarmonicDriver(){
+    ENTER_FCN( "MultiHarmonicDriver::~MultiHarmonicDriver" );
+    allMeasuredData->close();
+    impedCurve->close();
+    piezoLog->close();
+    parLog->close();
+  }
+
+void MultiHarmonicDriver::SolveProblem() {
+    ENTER_FCN( "MultiHarmonicDriver::SolveProblem" );
+
+
+    Integer level=0;
+  Boolean reset = TRUE;
+  
+  if (! isPartOfSequence_)
+  ptdomain_->PrintGrid(level);
+
+  if (PrintGridOnly)
+      exit(0);
+
+  // if driver is not part of multiSequence Driver, get list
+  // of pdes which have to be solved and intialize them
+  if (isPartOfSequence_ == FALSE){     
+    GetMyPDEs();
+    Info->StartProgress ("Starting to solve problem", FALSE);
+  }
+  
+  pdes_[0]->WriteGeneralPDEdefines();
+      
+  Integer fstep;
+  Double actFreq  = startFreq_;
+  Double freqIncr = (stopFreq_ - startFreq_) / numFreq_;
+  std::string errMsg;
+  
+  if (pdes_.GetSize() <= 1) {
+
+    // branch for single PDE
+    for (fstep = 1; fstep <= numFreq_; fstep++) {
+      Info->WriteHarmonicStep(pdes_[0]->GetName(), fstep, actFreq);
+      
+      //      std::cout<<"\n multiHarm: 1 " <<std::endl;
+      pdes_[0]->GetSolveStep()->PreStepHarmonic(fstep, actFreq, level, reset);
+
+      //      std::cout<<"\n multiHarm: 2"  <<std::endl;
+      pdes_[0]->GetSolveStep()->SolveStepHarmonic(fstep, actFreq, level, reset);
+
+      //      std::cout<<"\n multiHarm: 3 " <<std::endl;
+      pdes_[0]->GetSolveStep()->PostStepHarmonic(fstep, actFreq, level, reset);
+
+      //      std::cout<<"\n multiHarm: nrMultHarms_ =  " <<nrMultHarms_ <<std::endl;
+           
+      // writing results in output-file
+      pdes_[0]->PostProcess(level);
+      pdes_[0]->WriteResultsInFile();
+           
+      actFreq += freqIncr;
+    }
+  } 
+  else {
+    errMsg  = "MultiHarmonicDriver::Solve: Harmonic simulation is ";
+    errMsg += "not yet implemented for coupled PDEs, sorry!";
+    Error(errMsg.c_str(), __FILE__, __LINE__);
+  }
+
+
+
+  }
+
+
+
+  void MultiHarmonicDriver::updateMaterialData(Vector<Double> & parameter, MaterialData * ptMaterial){
+    ENTER_FCN( "multiharmonic::updateMaterialData");
+  }
+
+
+}// end of namespace coupled field
---
+++ libsrc/Driver/multiHarmonicDriver.hh
@@ -0,0 +1,46 @@
+#include "singleDriver.hh"
+
+
+namespace CoupledField 
+{
+  //! multiharmonic analysis
+class MultiHarmonicDriver : public SingleDriver
+{
+
+public:
+
+  MultiHarmonicDriver(Domain * adomain,
+		    Integer stepOffset = 0,
+		    Double timeOffset = 0.0,
+		    std::string driverTag = "anyTag",
+		    Boolean isPartOfSequence = FALSE);
+
+    //! Destructor
+  virtual ~MultiHarmonicDriver();
+  
+  std::ifstream * allMeasuredData;
+  std::ofstream * impedCurve;
+  std::ofstream * piezoLog;
+  std::ofstream * parLog;
+  
+  virtual void SolveProblem();
+
+protected:
+    //! \param parameter - new set of piezoelectric material parameters
+  void updateMaterialData(Vector<Double> & parameter, MaterialData * ptMaterial);
+  // Domain * ptDomain;
+
+private:
+
+  Double  startFreq_;
+  Double  stopFreq_;
+  Integer numFreq_;
+  Integer saveType_;
+  Integer nrMultHarms_;
+
+
+  }; // end of class piezoParamIdent
+}
+
+
+
