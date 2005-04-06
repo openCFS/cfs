a1bb89647f92dfcda14d1ae4a9ec9b4a00722f43
Markus Mohr <marcus.mohr@geophysik.uni-muenchen.de>
2005-04-06 13:30:35 +0000

    Only cosmetic changes to formatting and (missing) documentation

--- libsrc/Forms/bdbInt.hh
+++ libsrc/Forms/bdbInt.hh
@@ -9,6 +9,7 @@ namespace CoupledField {
   class BDBInt : public BaseForm {
 
   public:
+
     //! Constructor with pointer to BaseElem
     BDBInt();
     
@@ -22,46 +23,60 @@ namespace CoupledField {
     virtual ~BDBInt();
 
     //! Function for calculation bdb matrix 
-    virtual void CalcElementMatrix(Matrix<Double> &ptCoord, Matrix<Double> & elemmat);
+    virtual void CalcElementMatrix( Matrix<Double> &ptCoord,
+                                    Matrix<Double> &elemmat );
 
-    virtual void CalcComplexElementMatrix(Matrix<Double>& ptCoord, Matrix<Complex> & elemMat,Double & beta, Double & omega);
+    //! \note This memorial is dedicated to the undocumented function
+    virtual void CalcComplexElementMatrix(Matrix<Double> &ptCoord,
+                                          Matrix<Complex> &elemMat,
+                                          Double &beta, Double &omega );
 
   protected:
 
     //! returns B - matrix for BDB
     virtual void calcBMat( Matrix<Double> &bMat, Integer ip,
-			   Matrix<Double> &ptCoord ) = 0;
+                           Matrix<Double> &ptCoord ) = 0;
 
     //! returns D - matrix for BDB
     virtual void calcDMat( Matrix<Double> &dMat ) {
       Error( "BDBInt::calcDMat(Matrix<Double>&) not correctly overwritten!",
-	     __FILE__, __LINE__);
+             __FILE__, __LINE__);
     };
 
     //! returns D - matrix for BDB
-    virtual void calcDMaterialMatWithComplexDamping(Matrix<Complex> &dMat,Double &beta,Double &omega){
-      Error( "BDBInt::calcDMaterialMatWithComplexDamping(Matrix<Complex> &dMat, Double &beta, Double &omega) not correctly overwritten!",
-	     __FILE__, __LINE__);
+    virtual void calcDMaterialMatWithComplexDamping( Matrix<Complex> &dMat,
+                                                     Double &beta,
+                                                     Double &omega ) {
+      (*error) << "BDBInt::calcDMaterialMatWithComplexDamping"
+               << "(Matrix<Complex> &dMat, Double &beta, Double &omega) "
+               << "not correctly overwritten!";
+      Error( __FILE__, __LINE__ );
     };
-
 
     //! returns D - matrix for BDB, changes in every integration point
     virtual void calcDMat( Matrix<Double> &dMat, Integer ip,
-			   Matrix<Double> &ptCoord ) {
-      Error( "BDBInt::calcDMat(Matrix<Double>&, int, Matrix<Double>&) not correct overwritten!",__FILE__,__LINE__);
+                           Matrix<Double> &ptCoord ) {
+      (*error) << "BDBInt::calcDMat(Matrix<Double>&, int, Matrix<Double>&) "
+               << "not correct overwritten!";
+      Error( __FILE__, __LINE__ );
     };
 
     //! returns dimension of D matrix
-    virtual Integer getDimD()=0;
+    virtual Integer getDimD() = 0;
 
     //! returns nr. of degrees of freedom
-    virtual Integer getNrDofs()=0;
+    virtual Integer getNrDofs() = 0;
 
-    //! for nonlinear calculations, the d-matrix has to be updated in every
-    //! integration point
+    //! Boolean for signaling that D matrix is non-constant
+
+    //! In some cases, e.g. in non-linear computations, it may be
+    //! necessary to compute the D matrix for each integration point
+    //! individually. This attribute is used to signal when the latter is
+    //! required.
     Integer updateDMatInEveryIP_;
   };
 
 }
 
-#endif // FILE_MASSINT
+#endif
+
