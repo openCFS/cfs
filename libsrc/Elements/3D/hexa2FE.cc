32171ca714434e1775c915cf21a6495dfbc382f7
Manfred Kaltenbacher <manfred.kaltenbacher@tuwien.ac.at>
2005-04-07 19:22:55 +0000

    hexa2FE: second order hex-element is now working (or better I hope!!) tested against CAPA (mechanics3d and piezoelectrics3d)

--- libsrc/Elements/3D/hexa2FE.cc
+++ libsrc/Elements/3D/hexa2FE.cc
@@ -125,7 +125,7 @@ void Hexa2FE::SetCornerCoords()
   LCornerCoords_[2][18] =   0;
  
   LCornerCoords_[0][19] =  -1;
-  LCornerCoords_[1][19] =   1;
+  LCornerCoords_[1][19] =  -1;
   LCornerCoords_[2][19] =   0;
 
 }
@@ -137,41 +137,66 @@ void Hexa2FE :: CalcShapeFnc(Vector<Double> & Shape,
 {
   ENTER_IFCN( "Hexa2FE::CalcShapeFnc" );
 
+  //helping variables
+  Double  rgauss,sgauss,tgauss;
+  Double  onehalf,one,two;
+  Double  oneplusr,oneminusr,oneminusr2,two_r;
+  Double  onepluss,oneminuss,oneminuss2,two_s;
+  Double  oneplust,oneminust,oneminust2,two_t;
+
   Shape.Resize(NumNodes_);
 
- 
-  //nodes 1-8
-  for( Integer i=0; i<8; i++)
-    Shape[i] = 0.125 * (1 + LCornerCoords_[0][i] * LCoord[0])
-                     * (1 + LCornerCoords_[1][i] * LCoord[1]) 
-                     * (1 + LCornerCoords_[2][i] * LCoord[2])
-                     * (-2 + LCornerCoords_[0][i] * LCoord[0]
-                           + LCornerCoords_[1][i] * LCoord[1]
-                           + LCornerCoords_[2][i] * LCoord[2]);
-  
-  //nodes 9,11,14,18
-  Integer k =8;
-  for( Integer i=0; i<4; i++) {
-    Shape[k] = 0.25 * (1 - LCoord[0]*LCoord[0])
-                    * (1 + LCornerCoords_[1][k] * LCoord[1]) 
-                    * (1 + LCornerCoords_[2][k] * LCoord[2]);
-    k +=2;
-  }
-  
-  //nodes 10,12,14,16
-  k = 9;
-  for( Integer i=0; i<4; i++) {
-    Shape[k] = 0.25 * (1 - LCoord[1]*LCoord[1])
-                    * (1 + LCornerCoords_[0][k] * LCoord[0]) 
-                    * (1 + LCornerCoords_[2][k] * LCoord[2]);
-    k +=2;
-  }
+  //integration points
+  rgauss = LCoord[0];
+  sgauss = LCoord[1];
+  tgauss = LCoord[2];
 
-  //nodes 17, 18, 19, 20
-  for( Integer i=16; i<20; i++) 
-    Shape[i] = 0.25 * (1 - LCoord[2]*LCoord[2])
-                    * (1 + LCornerCoords_[0][i] * LCoord[0]) 
-                    * (1 + LCornerCoords_[1][i] * LCoord[1]);
+  onehalf = 0.5;
+  one     = 1.0;
+  two     = 2.0;
+
+  oneplusr   = onehalf * (one + rgauss);
+  oneminusr  = onehalf * (one - rgauss);
+  onepluss   = onehalf * (one + sgauss);
+  oneminuss  = onehalf * (one - sgauss);
+  oneplust   = onehalf * (one + tgauss);
+  oneminust  = onehalf * (one - tgauss);
+  oneminusr2 = (one + rgauss) * (one - rgauss);
+  oneminuss2 = (one + sgauss) * (one - sgauss);
+  oneminust2 = (one + tgauss) * (one - tgauss);
+
+
+  //compute the shape functions
+  Shape[0]  = oneminusr  * oneminuss * oneminust;
+  Shape[1]  = oneplusr   * oneminuss * oneminust;
+  Shape[2]  = oneplusr   * onepluss  * oneminust;
+  Shape[3]  = oneminusr  * onepluss  * oneminust;
+  Shape[4]  = oneminusr  * oneminuss * oneplust;
+  Shape[5]  = oneplusr   * oneminuss * oneplust;
+  Shape[6]  = oneplusr   * onepluss  * oneplust;
+  Shape[7]  = oneminusr  * onepluss  * oneplust;
+  Shape[8]  = oneminusr2 * oneminuss * oneminust;
+  Shape[9]  = oneminuss2 * oneplusr  * oneminust;
+  Shape[10] = oneminusr2 * onepluss  * oneminust;
+  Shape[11] = oneminuss2 * oneminusr * oneminust;
+  Shape[12] = oneminusr2 * oneminuss * oneplust;
+  Shape[13] = oneminuss2 * oneplusr  * oneplust;
+  Shape[14] = oneminusr2 * onepluss  * oneplust;
+  Shape[15] = oneminuss2 * oneminusr * oneplust;
+  Shape[16] = oneminusr  * oneminuss * oneminust2;
+  Shape[17] = oneminuss  * oneplusr  * oneminust2;
+  Shape[18] = oneplusr   * onepluss  * oneminust2;
+  Shape[19] = onepluss   * oneminusr * oneminust2;
+
+
+  Shape[0] -= onehalf * (Shape[11] + Shape[8] + Shape[16]);
+  Shape[1] -= onehalf * (Shape[8]  + Shape[9] + Shape[17]);
+  Shape[2] -= onehalf * (Shape[9]  + Shape[10] + Shape[18]);
+  Shape[3] -= onehalf * (Shape[13] + Shape[10] + Shape[19]);
+  Shape[4] -= onehalf * (Shape[15] + Shape[12] + Shape[16]);
+  Shape[5] -= onehalf * (Shape[12] + Shape[13] + Shape[19]);
+  Shape[6] -= onehalf * (Shape[13] + Shape[14] + Shape[16]);
+  Shape[7] -= onehalf * (Shape[14] + Shape[15] + Shape[19]);
 }
 
 void Hexa2FE::CalcLocalDerivShapeFnc(Matrix<Double> & LDeriv, 
@@ -179,84 +204,120 @@ void Hexa2FE::CalcLocalDerivShapeFnc(Matrix<Double> & LDeriv,
 {
   ENTER_IFCN( "Hexa2FE::CalcLocalDerivShapeFnc" );
 
+  //helper variables
+  Double  rgauss,sgauss,tgauss;
+  Double  onehalf,one,two;
+  Double  oneplusr,oneminusr,oneminusr2,two_r;
+  Double  onepluss,oneminuss,oneminuss2,two_s;
+  Double  oneplust,oneminust,oneminust2,two_t;
+
+
   LDeriv.Resize(NumNodes_,Dim_);
+  LDeriv.Init(0);
 
-  //nodes 1-8
-  for( Integer i=0; i<8; i++)
-    {
-      LDeriv[i][0] = 0.125 * LCornerCoords_[0][i] 
-	* (1 + LCornerCoords_[1][i] * LCoord[1] )* (1 + LCornerCoords_[2][i] * LCoord[2])
-        * (-1 + 2 * LCornerCoords_[0][i] * LCoord[0] 
-              + LCornerCoords_[1][i] * LCoord[1]
-              + LCornerCoords_[2][i] * LCoord[2]);
+  //integration points
+  rgauss = LCoord[0];
+  sgauss = LCoord[1];
+  tgauss = LCoord[2];
 
-      LDeriv[i][1] = 0.125 * LCornerCoords_[1][i] 
-	* (1 + LCornerCoords_[0][i] * LCoord[0] )* (1 + LCornerCoords_[2][i] * LCoord[2])
-        * (-1 + LCornerCoords_[0][i] * LCoord[0] 
-              + 2 * LCornerCoords_[1][i] * LCoord[1]
-              + LCornerCoords_[2][i] * LCoord[2]);
+  onehalf = 0.5;
+  one     = 1.0;
+  two     = 2.0;
+  two_r   = -two * rgauss;
+  two_s   = -two * sgauss;
+  two_t   = -two * tgauss;
 
-      LDeriv[i][2] = 0.125 * LCornerCoords_[2][i] 
-	* (1 + LCornerCoords_[0][i] * LCoord[0] )* (1 + LCornerCoords_[1][i] * LCoord[1])
-        * (-1 + LCornerCoords_[0][i] * LCoord[0] 
-              + LCornerCoords_[1][i] * LCoord[1]
-              + 2 * LCornerCoords_[2][i] * LCoord[2]);
+  oneplusr   = onehalf * (one + rgauss);
+  oneminusr  = onehalf * (one - rgauss);
+  onepluss   = onehalf * (one + sgauss);
+  oneminuss  = onehalf * (one - sgauss);
+  oneplust   = onehalf * (one + tgauss);
+  oneminust  = onehalf * (one - tgauss);
+  oneminusr2 = (one + rgauss) * (one - rgauss);
+  oneminuss2 = (one + sgauss) * (one - sgauss);
+  oneminust2 = (one + tgauss) * (one - tgauss);
 
-    }
 
-  //nodes 9,11,14,18
-  Integer k =8;
-  for( Integer i=0; i<4; i++) {
-    LDeriv[k][0] = -0.5  * LCoord[0]
-                         * (1 + LCornerCoords_[1][k] * LCoord[1]) 
-                         * (1 + LCornerCoords_[2][k] * LCoord[2]);
-
-    LDeriv[k][1] = 0.25 * LCornerCoords_[1][k]
-                        * (1 - LCoord[0] * LCoord[0]) 
-                        * (1 + LCornerCoords_[2][k] * LCoord[2]);
-
-    LDeriv[k][2] = 0.25 * LCornerCoords_[2][k]
-                       * (1 - LCoord[0] * LCoord[0]) 
-                       * (1 + LCornerCoords_[1][k] * LCoord[1]);
-
-    k +=2;
-  }
+  //deriavtives of shape functions
+  LDeriv[6][0] =  onehalf * onepluss  * oneplust;
+  LDeriv[5][0] =  onehalf * oneminuss * oneplust;
+  LDeriv[2][0] =  onehalf * onepluss  * oneminust;
+  LDeriv[1][0] =  onehalf * oneminuss * oneminust;
+  LDeriv[7][0] = -LDeriv[6][0];
+  LDeriv[4][0] = -LDeriv[5][0];
+  LDeriv[3][0] = -LDeriv[2][0];
+  LDeriv[0][0] = -LDeriv[1][0];
   
-
-  //nodes 10,12,14,16
-  k = 9;
-  for( Integer i=0; i<4; i++) {
-    LDeriv[k][0] = 0.25 * LCornerCoords_[0][k]
-                    * (1 - LCoord[1] * LCoord[1]) 
-                    * (1 + LCornerCoords_[2][k] * LCoord[2]);
-
-    LDeriv[k][1] = -0.5 * LCoord[1]
-                    * (1 + LCornerCoords_[0][k] * LCoord[0]) 
-                    * (1 + LCornerCoords_[2][k] * LCoord[2]);
-
-    LDeriv[k][2] = 0.25 * LCornerCoords_[2][k]
-                    * (1 + LCornerCoords_[0][k] * LCoord[0])
-                    * (1 - LCoord[1] * LCoord[1]);
-
-    k +=2;
-  }
-
-
-  //nodes 17, 18, 19, 20
-  for( Integer i=16; i<20; i++) {
-    LDeriv[i][0] = 0.25 * LCornerCoords_[0][i]
-                    * (1 + LCornerCoords_[1][i] * LCoord[1]) 
-                    * (1 - LCoord[2] * LCoord[2]);
-
-    LDeriv[i][1] = 0.25 * LCornerCoords_[1][i]
-                    * (1 + LCornerCoords_[0][i] * LCoord[0]) 
-                    * (1 - LCoord[2] * LCoord[2]);
-
-    LDeriv[i][2] = -0.5 * LCoord[2]
-                    * (1 + LCornerCoords_[0][i] * LCoord[0]) 
-                    * (1 + LCornerCoords_[1][i] * LCoord[1]);
-  }
+  LDeriv[6][1] = onehalf * oneplusr  * oneplust;
+  LDeriv[7][1] = onehalf * oneminusr * oneplust;
+  LDeriv[2][1] = onehalf * oneplusr  * oneminust;
+  LDeriv[3][1] = onehalf * oneminusr * oneminust;
+  LDeriv[4][1] = -LDeriv[7][1];
+  LDeriv[5][1] = -LDeriv[6][1];
+  LDeriv[0][1] = -LDeriv[3][1];
+  LDeriv[1][1] = -LDeriv[2][1];
   
+  LDeriv[6][2] = onehalf * oneplusr  * onepluss;
+  LDeriv[7][2] = onehalf * oneminusr * onepluss;
+  LDeriv[4][2] = onehalf * oneminusr * oneminuss;
+  LDeriv[5][2] = onehalf * oneplusr  * oneminuss;
+  LDeriv[2][2] = -LDeriv[6][2];
+  LDeriv[3][2] = -LDeriv[7][2];
+  LDeriv[0][2] = -LDeriv[4][2];
+  LDeriv[1][2] = -LDeriv[5][2];
+
+
+  LDeriv[14][0] =  two_r   * onepluss   * oneplust;
+  LDeriv[15][0] = -onehalf * oneminuss2 * oneplust;
+  LDeriv[12][0] =  two_r   * oneminuss  * oneplust;
+  LDeriv[13][0] = -LDeriv[15][0];
+  LDeriv[10][0] =  two_r   * onepluss   * oneminust;
+  LDeriv[11][0] = -onehalf * oneminuss2 * oneminust;
+  LDeriv[8][0]  =  two_r   * oneminuss  * oneminust;
+  LDeriv[9][0]  = -LDeriv[11][0];
+  LDeriv[18][0] =  onehalf * onepluss   * oneminust2;
+  LDeriv[19][0] = -LDeriv[18][0];
+  LDeriv[16][0] = -onehalf * oneminuss  * oneminust2;
+  LDeriv[17][0] = -LDeriv[16][0];
+  
+  LDeriv[14][1] =  onehalf * oneminusr2 * oneplust;
+  LDeriv[15][1] =  two_s   * oneminusr  * oneplust;
+  LDeriv[12][1] = -LDeriv[14][1];
+  LDeriv[13][1] =  two_s   * oneplusr   * oneplust;
+  LDeriv[10][1] =  onehalf * oneminusr2 * oneminust;
+  LDeriv[11][1] =  two_s   * oneminusr  * oneminust;
+  LDeriv[8][1]  = -LDeriv[10][1];
+  LDeriv[9][1]  =  two_s   * oneplusr   * oneminust;
+  LDeriv[18][1] =  onehalf * oneplusr   * oneminust2;
+  LDeriv[19][1] =  onehalf * oneminusr  * oneminust2;
+  LDeriv[16][1] = -LDeriv[19][1];
+  LDeriv[17][1] = -LDeriv[18][1];
+
+  LDeriv[14][2] =  oneminusr2 * onepluss  * onehalf;
+  LDeriv[15][2] =  oneminuss2 * oneminusr * onehalf;
+  LDeriv[12][2] =  oneminusr2 * oneminuss * onehalf;
+  LDeriv[13][2] =  oneminuss2 * oneplusr  * onehalf;
+  LDeriv[10][2] = -LDeriv[14][2];
+  LDeriv[11][2] = -LDeriv[15][2];
+  LDeriv[8][2]  = -LDeriv[12][2];
+  LDeriv[9][2]  = -LDeriv[13][2];
+  LDeriv[18][2] =  oneplusr  * onepluss  * two_t;
+  LDeriv[19][2] =  onepluss  * oneminusr * two_t;
+  LDeriv[16][2] =  oneminusr * oneminuss * two_t;
+  LDeriv[17][2] =  oneminuss * oneplusr  * two_t;
+
+
+  for (Integer i=0; i<3; i++) {
+    LDeriv[0][i] -= onehalf * (LDeriv[11][i] + LDeriv[8][i]  + LDeriv[16][i]);
+    LDeriv[1][i] -= onehalf * (LDeriv[8][i]  + LDeriv[9][i]  + LDeriv[17][i]);
+    LDeriv[2][i] -= onehalf * (LDeriv[9][i]  + LDeriv[10][i] + LDeriv[18][i]);
+    LDeriv[3][i] -= onehalf * (LDeriv[10][i] + LDeriv[11][i] + LDeriv[19][i]);
+    LDeriv[4][i] -= onehalf * (LDeriv[15][i] + LDeriv[12][i] + LDeriv[16][i]);
+    LDeriv[5][i] -= onehalf * (LDeriv[12][i] + LDeriv[13][i] + LDeriv[17][i]);
+    LDeriv[6][i] -= onehalf * (LDeriv[13][i] + LDeriv[14][i] + LDeriv[18][i]);
+    LDeriv[7][i] -= onehalf * (LDeriv[14][i] + LDeriv[15][i] + LDeriv[19][i]);
+  }
+
 }
 
 } // end of namespace
--- libsrc/Elements/3D/hexaFE.cc
+++ libsrc/Elements/3D/hexaFE.cc
@@ -14,12 +14,12 @@ HexaFE::HexaFE()
 {
   ENTER_FCN( "HexaFE::HexaFE" );
 
-    Dim_      = 3;
-    NumEdges_ = 12;
-    NumFaces_ = 6;
+    Dim_        = 3;
+    NumEdges_   = 12;
+    NumFaces_   = 6;
     NumCorners_ = 8;
-    numChilds_ = 8;
-    MidPoint_ = 0.0, 0.0, 0.0;
+    numChilds_  = 8;
+    MidPoint_   = 0.0, 0.0, 0.0;
     
     std::string integtype;
     params->Get( "type", integtype, "integRules", "hexa" );
@@ -37,7 +37,9 @@ HexaFE::~HexaFE()
 void HexaFE::SetIntPoints()
 {
   ENTER_IFCN( "HexaFE::SetIntPoints" );
- 
+  
+  Double factor = sqrt(3.0/5.0);
+
   switch(IntegType) 
     {
     case GaussOrder1:
@@ -64,55 +66,244 @@ void HexaFE::SetIntPoints()
       break;
 
     case GaussOrder2:
-	  
-      NumIntPoints_=8;
+
+      NumIntPoints_=27;
       DegreeInteg_=3;
 
+      IntWeights_.Resize(NumIntPoints_);
+      IntWeights_[0]  = 0.171467764060357;
+      IntWeights_[1]  = 0.274348422496571;
+      IntWeights_[2]  = 0.171467764060357;
+      IntWeights_[3]  = 0.274348422496571;
+      IntWeights_[4]  = 0.438957475994513;
+      IntWeights_[5]  = 0.274348422496571;
+      IntWeights_[6]  = 0.171467764060357;
+      IntWeights_[7]  = 0.274348422496571;
+      IntWeights_[8]  = 0.171467764060357;
+      IntWeights_[9]  = 0.274348422496571;
+      IntWeights_[10] = 0.438957475994513;
+      IntWeights_[11] = 0.274348422496571;
+      IntWeights_[12] = 0.438957475994513;
+      IntWeights_[13] = 0.702331961591221;
+      IntWeights_[14] = 0.438957475994513;
+      IntWeights_[15] = 0.274348422496571;
+      IntWeights_[16] = 0.438957475994513;
+      IntWeights_[17] = 0.274348422496571;
+      IntWeights_[18] = 0.171467764060357;
+      IntWeights_[19] = 0.274348422496571;
+      IntWeights_[20] = 0.171467764060357;
+      IntWeights_[21] = 0.274348422496571;
+      IntWeights_[22] = 0.438957475994513;
+      IntWeights_[23] = 0.274348422496571;
+      IntWeights_[24] = 0.171467764060357;
+      IntWeights_[25] = 0.274348422496571;
+      IntWeights_[26] = 0.171467764060357;
+  
+      if (!IntPoints_)
+	IntPoints_ = new Vector<Double>[NumIntPoints_];
 
-	IntWeights_.Resize(NumIntPoints_);
-	// all weights are 1.0
-	for(Integer i=0; i<IntWeights_.GetSize(); i++)
-	  IntWeights_[i] = 1.0;
-      
-	if (!IntPoints_)
-	  IntPoints_ = new Vector<Double>[NumIntPoints_];
+      for(Integer i=0; i<NumIntPoints_; i++)
+	IntPoints_[i].Resize(Dim_);
 
-	for(Integer i=0; i<NumIntPoints_; i++)
-	  IntPoints_[i].Resize(Dim_);
+      IntPoints_[0][0] =  -factor;
+      IntPoints_[0][1] =  -factor;
+      IntPoints_[0][2] =  -factor;
 
-      IntPoints_[0][0] = -0.57735026919;
-      IntPoints_[0][1] = -0.57735026919;
-      IntPoints_[0][2] = -0.57735026919;
-      
-      IntPoints_[1][0] = 0.57735026919;
-      IntPoints_[1][1] = -0.57735026919;
-      IntPoints_[1][2] = -0.57735026919;
+      IntPoints_[1][0] =  -factor;
+      IntPoints_[1][1] =  -factor;
+      IntPoints_[1][2] =   0;
 
-      IntPoints_[2][0] = 0.57735026919;
-      IntPoints_[2][1] = 0.57735026919;
-      IntPoints_[2][2] = -0.57735026919;
+      IntPoints_[2][0] =  -factor;
+      IntPoints_[2][1] =  -factor;
+      IntPoints_[2][2] =   factor;
 
-      IntPoints_[3][0] = -0.57735026919;
-      IntPoints_[3][1] = 0.57735026919;
-      IntPoints_[3][2] = -0.57735026919;
+      IntPoints_[3][0] =  -factor;
+      IntPoints_[3][1] =   0;
+      IntPoints_[3][2] =  -factor;
 
-      IntPoints_[4][0] = -0.57735026919;
-      IntPoints_[4][1] = -0.57735026919;
-      IntPoints_[4][2] = 0.57735026919;
-      
-      IntPoints_[5][0] = 0.57735026919;
-      IntPoints_[5][1] = -0.57735026919;
-      IntPoints_[5][2] = 0.57735026919;
+      IntPoints_[4][0] =  -factor;
+      IntPoints_[4][1] =   0;
+      IntPoints_[4][2] =   0;
 
-      IntPoints_[6][0] = 0.57735026919;
-      IntPoints_[6][1] = 0.57735026919;
-      IntPoints_[6][2] = 0.57735026919;
+      IntPoints_[5][0] =  -factor;
+      IntPoints_[5][1] =   0;
+      IntPoints_[5][2] =   factor;
 
-      IntPoints_[7][0] = -0.57735026919;
-      IntPoints_[7][1] = 0.57735026919;
-      IntPoints_[7][2] = 0.57735026919;
+      IntPoints_[6][0] =  -factor;
+      IntPoints_[6][1] =   factor;
+      IntPoints_[6][2] =  -factor;
+
+      IntPoints_[7][0] =  -factor;
+      IntPoints_[7][1] =   factor;
+      IntPoints_[7][2] =   0;
+
+      IntPoints_[8][0] =  -factor;
+      IntPoints_[8][1] =   factor;
+      IntPoints_[8][2] =   factor;
+
+      IntPoints_[9][0] =   0;
+      IntPoints_[9][1] =  -factor;
+      IntPoints_[9][2] =  -factor;
+
+      IntPoints_[10][0] =   0;
+      IntPoints_[10][1] =  -factor;
+      IntPoints_[10][2] =   0;
+
+      IntPoints_[11][0] =   0;
+      IntPoints_[11][1] =  -factor;
+      IntPoints_[11][2] =   factor;
+
+      IntPoints_[12][0] =   0;
+      IntPoints_[12][1] =   0;
+      IntPoints_[12][2] =  -factor;
+
+
+      IntPoints_[13][0] =   0;
+      IntPoints_[13][1] =   0;
+      IntPoints_[13][2] =   0;
+
+      IntPoints_[14][0] =   0;
+      IntPoints_[14][1] =   0;
+      IntPoints_[14][2] =   factor;
+
+      IntPoints_[15][0] =   0;
+      IntPoints_[15][1] =   factor;
+      IntPoints_[15][2] =  -factor;
+
+      IntPoints_[16][0] =   0;
+      IntPoints_[16][1] =   factor;
+      IntPoints_[16][2] =   0;
+
+      IntPoints_[17][0] =   0;
+      IntPoints_[17][1] =   factor;
+      IntPoints_[17][2] =   factor;
+
+      IntPoints_[18][0] =   factor;
+      IntPoints_[18][1] =  -factor;
+      IntPoints_[18][2] =  -factor;
+
+      IntPoints_[19][0] =   factor;
+      IntPoints_[19][1] =  -factor;
+      IntPoints_[19][2] =   0;
+
+      IntPoints_[20][0] =   factor;
+      IntPoints_[20][1] =  -factor;
+      IntPoints_[20][2] =   factor;
+
+      IntPoints_[21][0] =   factor;
+      IntPoints_[21][1] =   0;
+      IntPoints_[21][2] =  -factor;
+
+      IntPoints_[22][0] =   factor;
+      IntPoints_[22][1] =   0;
+      IntPoints_[22][2] =   0;
+
+      IntPoints_[23][0] =   factor;
+      IntPoints_[23][1] =   0;
+      IntPoints_[23][2] =   factor;
+
+      IntPoints_[24][0] =   factor;
+      IntPoints_[24][1] =   factor;
+      IntPoints_[24][2] =  -factor;
+
+      IntPoints_[25][0] =   factor;
+      IntPoints_[25][1] =   factor;
+      IntPoints_[25][2] =   0;
+
+      IntPoints_[26][0] =   factor;
+      IntPoints_[26][1] =   factor;
+      IntPoints_[26][2] =   factor;
 
       break;
+	  
+ //      NumIntPoints_=6;
+//       DegreeInteg_=3;
+
+
+//       IntWeights_.Resize(NumIntPoints_);
+// 	// all weights are 4/3
+//       for(Integer i=0; i<IntWeights_.GetSize(); i++)
+// 	IntWeights_[i] = 4.0/3.0;
+      
+//       if (!IntPoints_)
+// 	IntPoints_ = new Vector<Double>[NumIntPoints_];
+
+//       for(Integer i=0; i<NumIntPoints_; i++)
+// 	IntPoints_[i].Resize(Dim_);
+
+//       IntPoints_[0][0] =  1.0/sqrt(6.0);
+//       IntPoints_[0][1] =  1.0/sqrt(2.0);
+//       IntPoints_[0][2] = -1.0/sqrt(3.0);
+
+//       IntPoints_[1][0] =  1.0/sqrt(6.0);
+//       IntPoints_[1][1] = -1.0/sqrt(2.0);
+//       IntPoints_[1][2] = -1.0/sqrt(3.0);
+      
+//       IntPoints_[2][0] = -1.0/sqrt(6.0);
+//       IntPoints_[2][1] =  1.0/sqrt(2.0);
+//       IntPoints_[2][2] =  1.0/sqrt(3.0);
+
+//       IntPoints_[3][0] = -1.0/sqrt(6.0);
+//       IntPoints_[3][1] = -1.0/sqrt(2.0);
+//       IntPoints_[3][2] =  1.0/sqrt(3.0);
+
+//       IntPoints_[4][0] = -sqrt(2.0/3.0);
+//       IntPoints_[4][1] = 0.0;
+//       IntPoints_[4][2] = -1/sqrt(3.0);
+
+//       IntPoints_[5][0] = sqrt(2.0/3.0);
+//       IntPoints_[5][1] = 0.0;
+//       IntPoints_[5][2] = 1/sqrt(3.0);
+
+
+//       NumIntPoints_=8;
+//       DegreeInteg_=3;
+
+
+// 	IntWeights_.Resize(NumIntPoints_);
+// 	// all weights are 1.0
+// 	for(Integer i=0; i<IntWeights_.GetSize(); i++)
+// 	  IntWeights_[i] = 1.0;
+      
+// 	if (!IntPoints_)
+// 	  IntPoints_ = new Vector<Double>[NumIntPoints_];
+
+// 	for(Integer i=0; i<NumIntPoints_; i++)
+// 	  IntPoints_[i].Resize(Dim_);
+
+//       IntPoints_[0][0] = -0.57735026919;
+//       IntPoints_[0][1] = -0.57735026919;
+//       IntPoints_[0][2] = -0.57735026919;
+      
+//       IntPoints_[1][0] = 0.57735026919;
+//       IntPoints_[1][1] = -0.57735026919;
+//       IntPoints_[1][2] = -0.57735026919;
+
+//       IntPoints_[2][0] = 0.57735026919;
+//       IntPoints_[2][1] = 0.57735026919;
+//       IntPoints_[2][2] = -0.57735026919;
+
+//       IntPoints_[3][0] = -0.57735026919;
+//       IntPoints_[3][1] = 0.57735026919;
+//       IntPoints_[3][2] = -0.57735026919;
+
+//       IntPoints_[4][0] = -0.57735026919;
+//       IntPoints_[4][1] = -0.57735026919;
+//       IntPoints_[4][2] = 0.57735026919;
+      
+//       IntPoints_[5][0] = 0.57735026919;
+//       IntPoints_[5][1] = -0.57735026919;
+//       IntPoints_[5][2] = 0.57735026919;
+
+//       IntPoints_[6][0] = 0.57735026919;
+//       IntPoints_[6][1] = 0.57735026919;
+//       IntPoints_[6][2] = 0.57735026919;
+
+//       IntPoints_[7][0] = -0.57735026919;
+//       IntPoints_[7][1] = 0.57735026919;
+//       IntPoints_[7][2] = 0.57735026919;
+
+//      break;
 
    default:
       Error("Integration type is not implemented",__FILE__,__LINE__);
