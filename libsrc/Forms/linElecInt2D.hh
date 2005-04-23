43290c045c705b10b16bf4b3a5aa8209137a3f31
Andreas Hauck <mail@andreas-hauck.com>
2005-04-23 15:03:27 +0000

    - Added bilinear form for 2D general electrostatic field - Corrected comments

--- libsrc/Forms/linElecInt2D.hh
+++ libsrc/Forms/linElecInt2D.hh
@@ -1,15 +1,16 @@
-#ifndef FILE_LIN_ELEC_INT_3D
-#define FILE_LIN_ELEC_INT_3D
+#ifndef FILE_LIN_ELEC_INT_2D
+#define FILE_LIN_ELEC_INT_2D
 
 namespace CoupledField {
 
-
-  //! Class describing the general BDB operator for 3D electrostatic
+  //! Class describing the general BDB operator for 2D electrostatic
 
   //! The main objective of this class is to implement the pure vitual
   //! methods of the BDBInt parent class for the case of a linear 
-  //! electrostatic 3D simulation.
-  class linElecInt3D : public BDBInt {
+  //! electrostatic 2D simulation.
+  //! \Note: The class asssumes that the computational 2D area lies in the
+  //! yz-plane.
+  class linElecInt2D : public BDBInt {
     
   public:
 
@@ -20,25 +21,27 @@ namespace CoupledField {
     //@{ \name Construction and destruction
 
     //! Default constructor
-    linElecInt3D() {
-      ENTER_FCN( "linElecInt3D::linElecInt3D" );
+    linElecInt2D( Boolean isAxi) {
+      ENTER_FCN( "linElecInt2D::linElecInt2D" );
       
+      isaxi_ = isAxi;
       factor_ = 0.0;
     }
 
     //! Constructor with material data
-    linElecInt3D( MaterialData & matData)  :
-      BDBInt(matData)
+    linElecInt2D( MaterialData & matData, Boolean isAxi)  :
+      BDBInt( matData )
     {
-      ENTER_FCN( "linElecInt3D::linElecInt3D" );
+      ENTER_FCN( "linElecInt2D::linElecInt2D" );
 
       factor_ = 0.0;
+      isaxi_ = isAxi;
     }
     
 
     //! Destructor
-    ~linElecInt3D() {
-      ENTER_FCN( "linElecInt3D::~linElecInt3D" );
+    ~linElecInt2D() {
+      ENTER_FCN( "linElecInt2D::~linElecInt2D" );
 
     }
     //@}
@@ -51,12 +54,11 @@ namespace CoupledField {
 
      //! Compute the matrix \f$B\f$ of the \f$BDB\f$ operator
 
-    //! The method computes the matrix \f$B\f$ of the \f$ADB\f$ operator
-    //! which, in the case of a 3D electrostatic field,
+    //! The method computes the matrix \f$B\f$ of the \f$BDB\f$ operator
+    //! which, in the case of a 2D electrostatic field,
     //! is given  by
     //! \f[
     //! B = \left(\begin{array}{c}
-    //! \displaystyle\frac{\partial N_\Phi}{\partial x}\\[3ex]
     //! \displaystyle\frac{\partial N_\Phi}{\partial y}\\[3ex]
     //! \displaystyle\frac{\partial N_\Phi}{\partial z}
     //! \end{array}\right)
@@ -80,13 +82,13 @@ namespace CoupledField {
     void calcBMat( Matrix<Double> &bMat, Integer ip,
                    Matrix<Double> &ptCoord ) {
 
-      ENTER_FCN( "linElecInt3D::calcBMat" );
+      ENTER_FCN( "linElecInt2D::calcBMat" );
 
       // Obtain info on number of element's nodes
       const Integer numNodes = ptelem->GetNumNodes();
 
       // Set correct size of matrix B and initialise with zeros
-      bMat.Resize( 3, numNodes );
+      bMat.Resize( 2, numNodes );
 
       // Get derivatives of local shape functions with respect to global
       // coords (format: nrNodes x spaceDim)
@@ -100,23 +102,22 @@ namespace CoupledField {
       // second z.
       for( Integer actNode = 0; actNode < numNodes; actNode++ ) {
         bMat[0][actNode] = xiDx[actNode][0];
-        bMat[1][actNode] = xiDx[actNode][1];
-	bMat[2][actNode] = xiDx[actNode][2];
+	bMat[1][actNode] = xiDx[actNode][1];
       }
     }
     
      void calcDMat( Matrix<Double> &dMat ) {
-       ENTER_FCN( "linElecInt3D::calcDMat" );
-       dMat.Resize( 3, 3 );
+       ENTER_FCN( "linElecInt2D::calcDMat" );
+       dMat.Resize( 2, 2 );
        Matrix<Double> *matMatrix = ptMaterial->GetMatrix();
        
        // copy electric part of material matrix, which 
        // is the lower-right sub-diagonal block
-       // d[7-9][7-9]
-       Integer startRow = 6;
-       Integer startCol = 6;
-       for( Integer i = 0; i < 3; i++ ) {
-	 for ( Integer j = 0; j < 3; j++ ) {
+       // d[8-9][8-9]
+       Integer startRow = 7;
+       Integer startCol = 7;
+       for( Integer i = 0; i < 2; i++ ) {
+	 for ( Integer j = 0; j < 2; j++ ) {
 	   dMat[i][j] = factor_ *
 	     (*matMatrix)[startRow+i][startCol+j];
 	 }
@@ -125,13 +126,13 @@ namespace CoupledField {
     
     //! Returns dimension of D matrix
     Integer getDimD() {
-      ENTER_IFCN( "linElecInt3D::getDimD" );
-      return 3;
+      ENTER_IFCN( "linElecInt2D::getDimD" );
+      return 2;
     }
 
     //! Returns nr. of degrees of freedom
     Integer getNrDofs() {
-      ENTER_IFCN( "linElecInt3D::getNrDofs" );
+      ENTER_IFCN( "linElecInt2D::getNrDofs" );
       return 1;
     }
     
