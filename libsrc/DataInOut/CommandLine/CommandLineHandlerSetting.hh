7e0ba650d1481f15df18e99f0755990ca0944440
Markus Mohr <marcus.mohr@geophysik.uni-muenchen.de>
2005-05-09 17:26:26 +0000

    Class for handling of command line parameters that relies on the Settings library by Uwe Fabricius for parsing of command line and setting of default values.

---
+++ libsrc/DataInOut/CommandLine/CommandLineHandlerSetting.hh
@@ -0,0 +1,142 @@
+#ifndef FILE_COMMANDLINE_HANDLER_SETTING_HH
+#define FILE_COMMANDLINE_HANDLER_SETTING_HH
+
+
+#include "Settings.hh"
+#include "BaseCommandLineHandler.hh"
+
+
+namespace CoupledField {
+
+
+  //! Command line handler built on Settings library by Uwe Fabricius
+  class CommandLineHandlerSetting : public BaseCommandLineHandler {
+
+
+  public:
+
+    // =======================================================================
+    // CONSTRUCTIION and DESTRUCTION
+    // =======================================================================
+
+    //@{
+    //! \name Methods for construction, destruction and setup
+
+    //! The constructor requires passing of the command line arguments
+    CommandLineHandlerSetting( Integer argc, const char **argv );
+
+    //! Destructor
+    ~CommandLineHandlerSetting() {
+      ENTER_FCN( "CommandLineHandlerSetting::~CommandLineHandlerSetting" );
+    };
+
+    //@}
+
+
+    // =======================================================================
+    // QUERY METHODS FOR PARAMETERS
+    // =======================================================================
+
+    //@{
+    //! \name Query methods for command line parameters
+    //! For each command line parameter there exists an associated query
+    //! method. If no command line value was specified than the query method
+    //! will return the corresponding default, if it exists.
+
+    //! Return base name of simulation run
+
+    //! This method can be used to query the name of the current simulation
+    //! run, which may be something like e.g. plate3D. This name is used by
+    //! CFS++ internally as basename for the generation of several (default)
+    //! names for input and output files.
+    std::string GetSimName() const {
+      ENTER_FCN( "CommandLineHandlerSetting::GetSimName" );
+      return simName_;
+    };
+
+    //! Return name of XML parameter file
+
+    //! This method can be used to query the name of the parameter file in
+    //! XML format that contains the steering parameters for the simulation.
+    std::string GetParamFile() const {
+      ENTER_FCN( "CommandLineHandlerSetting::GetParamFile" );
+      Setting *aux = commandLine_.getSetting( markerParamFile_.c_str() );
+      std::string paramFile = aux->getString();
+      return paramFile;
+    };
+
+    //! Return name of mesh file
+
+    //! This method can be used to query the name of the mesh file containing
+    //! the description of the FEM mesh for the simulation.
+    std::string GetMeshFile() const {
+      ENTER_FCN( "CommandLineHandlerSetting::GetMeshFile" );
+      Setting *aux = commandLine_.getSetting( markerMeshFile_.c_str() );
+      std::string meshFile = aux->getString();
+      return meshFile;
+    };
+
+    //! Return printGrid flag
+
+    //! This method can be used to query the status of the printGrid flag.
+    //! By specifying this flag one instructs the executable to do not
+    //! perform an actual simulation, but to only import the grid and
+    //! re-export it to an output file in the format specified in the XML
+    //! parameter file.
+    Boolean GetPrintGrid() const {
+      ENTER_FCN( "CommandLineHandlerSetting::GetPrintGrid" );
+      Setting *aux = commandLine_.getSetting( markerPrintGrid_.c_str() );
+      return aux->getBool();
+    };
+
+    //! Return writeSkeleton flag
+
+    //! This method can be used to query the status of the writeSkeleton flag.
+    //! As a convenience for the CFS++ user it is possible to let the
+    //! executable write a skeleton XML parameter file that must then be
+    //! filled out by the user for a subsequent simulation run.
+    Boolean GetWriteSkeleton() const {
+      ENTER_FCN( "CommandLineHandlerSetting::GetWriteSkeleton" );
+      Setting *aux = commandLine_.getSetting( markerWriteSkeleton_.c_str() );
+      return aux->getBool();
+    };
+
+    //! Return depth of function tracing
+
+    //! This method can be used to query the depth desired for generating
+    //! function trace information.
+    //! \note Function tracing is a time-consuming process that can easily
+    //!       lead to the generation of a tremendously large output file
+    //!       containing the trace information. Thus, tracing must explicitely
+    //!       be compiled into the executable by turning on the respective
+    //!       option in <em>Makefile.option</em>. Specifying a trace depth of
+    //!       zero avoids generation of the trace file, but does not remove
+    //!       the work associated with function tracing.
+    Integer GetTraceDepth() const {
+      ENTER_FCN( "CommandLineHandlerSetting::GetTraceDepth" );
+      Setting *aux = commandLine_.getSetting( markerTraceDepth_.c_str() );
+      return (Integer)(aux->getInt());
+    };
+
+    //@}
+
+  private:
+
+    //! The SettingDataBase object that does the actual command line handling
+
+    //! The %CommandLineHandlerSettings class does not directly handle the
+    //! command line. Instead this work is delegated to an instance of the
+    //! SettingDataBase class from the Settings library by Uwe Fabricius.
+    SettingDataBase commandLine_;
+
+    //! This string stores the name of the current simulation run
+
+    //! This attribute stores as string the name of the current simulation
+    //! run as it was specified on the command line.
+    std::string simName_;
+
+  };
+
+}
+
+#endif
