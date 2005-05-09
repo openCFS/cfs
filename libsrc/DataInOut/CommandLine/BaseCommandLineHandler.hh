9730279484452739610f30e4ec49bc2c2fdfd0dc
mmohr <mmohr@cfs-dev.mdmt.tuwien.ac.at>
2005-05-09 18:13:49 +0000

    Moved to definition of the static members to a .cc file in order to avoid problems with multiple definitions, while keeping the instantiation in the CommandLine module.

--- libsrc/DataInOut/CommandLine/BaseCommandLineHandler.hh
+++ libsrc/DataInOut/CommandLine/BaseCommandLineHandler.hh
@@ -201,49 +201,6 @@ namespace CoupledField
 
   };
 
-
-  // ------------------------------------------------
-  //  Definition of static data members of the class
-  // ------------------------------------------------
-
-  // Help strings
-  const std::string BaseCommandLineHandler::helpSimName_       =
-  "name of simulation run";
-
-  const std::string BaseCommandLineHandler::helpParamFile_     =
-  "name of XML parameter file for the simulation";
-
-  const std::string BaseCommandLineHandler::helpMeshFile_      =
-  "name of mesh file for the simulation";  
-
-  const std::string BaseCommandLineHandler::helpTraceDepth_    =
-  "depth of function tracing";
-
-  const std::string BaseCommandLineHandler::helpWriteSkeleton_ =
-  "write skeleton of XML file for subsequent simulation";
-
-  const std::string BaseCommandLineHandler::helpPrintGrid_     =
-  "only read grid from mesh-file and write it to output file";
-
-  // Short forms of markers
-  const std::string BaseCommandLineHandler::markerParamFile_     = "-p";
-  const std::string BaseCommandLineHandler::markerMeshFile_      = "-m";
-  const std::string BaseCommandLineHandler::markerTraceDepth_    = "-t";
-  const std::string BaseCommandLineHandler::markerWriteSkeleton_ = "-s";
-  const std::string BaseCommandLineHandler::markerPrintGrid_     = "-g";
-
-  // Long forms of markers
-  const std::string BaseCommandLineHandler::markerLongParamFile_     =
-  "--paramFile";
-  const std::string BaseCommandLineHandler::markerLongMeshFile_      =
-  "--meshFile";
-  const std::string BaseCommandLineHandler::markerLongTraceDepth_    =
-  "--traceDepth";
-  const std::string BaseCommandLineHandler::markerLongWriteSkeleton_ =
-  "--writeSkeleton";
-  const std::string BaseCommandLineHandler::markerLongPrintGrid_     =
-  "--PrintGrid";
-
 }
 
 #endif
