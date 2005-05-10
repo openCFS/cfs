839c7d344997e6b440c1a3c63721c94d3555084b
Markus Mohr <marcus.mohr@geophysik.uni-muenchen.de>
2005-05-10 13:17:29 +0000

    Added two new methods:
    - PrintUsage() ..... outputs usage information on available command line
                         parameters, if -h / --help is specified or in the
                         case of parse errors
    
    - PrintParams() .... output all (specified and default) parameter values to
                         a given stream. Used in debugging.

--- libsrc/DataInOut/CommandLine/BaseCommandLineHandler.cc
+++ libsrc/DataInOut/CommandLine/BaseCommandLineHandler.cc
@@ -1,5 +1,15 @@
 #include "DataInOut/CommandLine/BaseCommandLineHandler.hh"
 
+// Be a little bit "colorful"
+#ifndef SUPPRESS_COLORED_OUTPUT
+// #define COLOR_INIT "\033[1m"
+#define COLOR_INIT "\033[34m"
+#define COLOR_STOP "\033[0m"
+#else
+#define COLOR_INIT ""
+#define COLOR_STOP ""
+#endif
+
 namespace CoupledField {
 
   // ------------------------------------------------
@@ -25,12 +35,16 @@ namespace CoupledField {
   const std::string BaseCommandLineHandler::helpPrintGrid_     =
   "only read grid from mesh-file and write it to output file";
 
+  const std::string BaseCommandLineHandler::helpHelp_          =
+  "print this usage information";
+
   // Short forms of markers
   const std::string BaseCommandLineHandler::markerParamFile_     = "-p";
   const std::string BaseCommandLineHandler::markerMeshFile_      = "-m";
   const std::string BaseCommandLineHandler::markerTraceDepth_    = "-t";
   const std::string BaseCommandLineHandler::markerWriteSkeleton_ = "-s";
   const std::string BaseCommandLineHandler::markerPrintGrid_     = "-g";
+  const std::string BaseCommandLineHandler::markerHelp_          = "-h";
 
   // Long forms of markers
   const std::string BaseCommandLineHandler::markerLongParamFile_     =
@@ -42,6 +56,115 @@ namespace CoupledField {
   const std::string BaseCommandLineHandler::markerLongWriteSkeleton_ =
   "--writeSkeleton";
   const std::string BaseCommandLineHandler::markerLongPrintGrid_     =
-  "--PrintGrid";
+  "--printGrid";
+  const std::string BaseCommandLineHandler::markerLongHelp_          =
+  "--help";
+
+
+  // ---------------------------------------------------------------
+  //  Implementation of some class methods too large for the header
+  // ---------------------------------------------------------------
+
+
+  // **************
+  //   PrintUsage
+  // **************
+  void BaseCommandLineHandler::PrintUsage() {
+
+    ENTER_FCN( "BaseCommandLineHandler::PrintUsage" );
+
+    std::ostream &os = std::cout;
+
+    os << COLOR_INIT << "\n Usage: " << COLOR_STOP
+
+       << "./cfs [param] <name of simulation run>\n\n"
+       << " where [param] is one or more of the following parameters\n\n"
+
+      // --paramFile
+       << COLOR_INIT
+       << " " << markerParamFile_ << ", " << markerLongParamFile_
+       << COLOR_STOP
+       << " = <string>\n"
+       << " " << helpParamFile_ << "\n\n"
+
+      // --meshFile
+       << COLOR_INIT
+       << " " << markerMeshFile_ << ", " << markerLongMeshFile_
+       << COLOR_STOP
+       << " = <string>\n"
+       << " " << helpMeshFile_ << "\n\n"
+
+      // --traceDepth
+       << " " << COLOR_INIT
+       << markerTraceDepth_ << ", " << markerLongTraceDepth_
+       << COLOR_STOP
+       << " = <non-negative integer>\n"
+       << " " << helpTraceDepth_ << "\n\n"
+
+      // --printGrid
+       << " " << COLOR_INIT
+       << markerPrintGrid_ << ", " << markerLongPrintGrid_
+       << COLOR_STOP
+       << '\n'
+       << " " << helpPrintGrid_ << "\n\n"
+
+      // --WriteSkeleton
+       << " " << COLOR_INIT
+       << markerWriteSkeleton_ << ", " << markerLongWriteSkeleton_
+       << COLOR_STOP
+       << '\n'
+       << " " << helpWriteSkeleton_ << "\n\n"
+
+      // --help
+       << " " << COLOR_INIT
+       << markerHelp_ << ", " << markerLongHelp_
+       << COLOR_STOP
+       << '\n'
+       << " " << helpHelp_ << "\n\n";
+
+  };
+
+
+  // ***************
+  //   PrintParams
+  // ***************
+  void BaseCommandLineHandler::PrintParams( std::ostream &out ) {
+
+    ENTER_FCN( "BaseCommandLineHandler::PrintParams" );
+
+    out << COLOR_INIT
+        << "\n Status of command line parameters (including defaults):\n\n"
+        << COLOR_STOP
+
+        << " name of simulation run = "
+        << COLOR_INIT
+        << GetSimName()
+        << COLOR_STOP << '\n'
+
+        << ' ' << markerLongParamFile_ << " = "
+        << COLOR_INIT
+        << GetParamFile()
+        << COLOR_STOP << '\n'
+
+        << ' ' << markerLongMeshFile_ << " = "
+        << COLOR_INIT
+        << GetMeshFile()
+        << COLOR_STOP << '\n'
+
+        << ' ' << markerLongTraceDepth_ << " = "
+        << COLOR_INIT
+        << GetTraceDepth()
+        << COLOR_STOP << '\n'
+
+        << ' ' << markerLongPrintGrid_ << " = "
+        << COLOR_INIT
+        << GetPrintGrid()
+        << COLOR_STOP << '\n'
+
+        << ' ' << markerLongWriteSkeleton_ << " = "
+        << COLOR_INIT
+        << GetWriteSkeleton()
+        << COLOR_STOP << "\n\n";
+  };
 
 }
