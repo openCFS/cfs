b34993a9ebde1771acae31d0a1266f21b48e5ddb
Andreas Hauck <mail@andreas-hauck.com>
2005-05-30 13:56:49 +0000

    Added ipmlementation of the pairwise direct mechanics-acoustic coupling in  via surface elements.

---
+++ libsrc/CoupledPDE/AcouMechCoupling.cc
@@ -0,0 +1,76 @@
+#include "AcouMechCoupling.hh"
+
+
+#include "Driver/assemble.hh"
+#include "DataInOut/MaterialData.hh"
+#include "DataInOut/LoadMaterialData.hh"
+#include "DataInOut/ParamHandling/BaseParamHandler.hh"
+#include "PDE/SinglePDE.hh"
+
+
+// integrator (bi-)linear forms
+#include "Forms/acouMechInt.hh"
+
+
+namespace CoupledField
+{
+
+  AcouMechCoupling::AcouMechCoupling( SinglePDE *pde1, SinglePDE *pde2 )
+    : BasePairCoupling( pde1, pde2 ) {
+    ENTER_FCN( "AcouMechCoupling::AcouMechCoupling" );
+
+    couplingName_ = "acouMechDirect";
+    materialClass_ = "fluid";
+  }
+
+
+  AcouMechCoupling::~AcouMechCoupling() {
+    ENTER_FCN( "AcouMechCoupling::~AcouMechCoupling" )
+      }
+
+  void AcouMechCoupling::PostProcess() {
+    ENTER_FCN( "AcouMechCoupling::PostProcess" );
+  }
+  
+
+  void AcouMechCoupling::DefineIntegrators() {
+    ENTER_FCN( "AcouMechCoupling::DefineIntegrators" );
+    
+
+    UInt dofs = ptGrid_->GetDim();
+    Boolean isAxi = params->HasValue( "type", "axi", "geometry" );
+    
+
+    // iterate over all subdomains
+    for ( UInt actSD = 0; actSD < surfRegions_.GetSize(); actSD++ ) {
+       
+      
+
+      // ==============  add mass ========================================
+      SurfForm * massInt = new AcouMechInt(dofs,isAxi);
+
+      // Set information about two neighbouring volume regions
+      massInt->SetFirstVoluInfo(pde1_->GetName(), pde1_->getPDE_subdoms(), 
+                                pde1_->getPDEMaterialData());
+      massInt->SetSecondVoluInfo(pde2_->GetName(), pde2_->getPDE_subdoms(), 
+                                 pde2_->getPDEMaterialData());
+
+      assemble_->AddSurfIntegrator(massInt, surfRegions_[actSD], DAMPING, FALSE);
+      
+    }
+
+  }
+  
+  void AcouMechCoupling::ReadStoreResults() {
+    ENTER_FCN( "AcouMechCoupling::ReadStoreResults" );
+    
+  }
+
+  void AcouMechCoupling::ReadMaterialData() {
+    ENTER_FCN( "AcouMechCoupling::ReadMaterialData" );
+    
+  }
+  
+
+
+} // end of namespace
