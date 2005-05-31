b7a19567cb1c8f7e5d31832ba0ef7475a622896d
Andreas Hauck <mail@andreas-hauck.com>
2005-05-31 15:29:57 +0000

    Removed some debugging statements.

--- libsrc/Forms/acouMechInt.cc
+++ libsrc/Forms/acouMechInt.cc
@@ -36,14 +36,6 @@ namespace CoupledField {
     const StdVector<RegionIdType> * acouRegionIds;
     const MaterialData * acouMaterials;
     
-    std::cerr << std::endl << "normal befor=\n" << normal_ << std::endl;
-    std::cerr << "SurfElem Nr. " << actElem_->elemNum 
-              << " has volume neiggbours " << actElem_->ptVolElem1->regionId
-              << " and " << actElem_->ptVolElem2->regionId << std::endl;
-
-     std::cerr << "First PDE-name = " << firstPDEName_ << " and second " 
-               << secondPDEName_ << std::endl;
-    
     // determine correct material list and regionIds
     if ( firstPDEName_ == "acoustic" ) {
       acouRegionIds = &firstRegionIds_; 
@@ -52,7 +44,6 @@ namespace CoupledField {
       acouRegionIds = &secondRegionIds_; 
       acouMaterials = secondMaterials_;
     }
-    std::cerr << "acouREgions=\n" << *acouRegionIds << std::endl;
     
     index = acouRegionIds->Find(actElem_->ptVolElem1->regionId);
     if ( index == -1 ) {
@@ -61,10 +52,8 @@ namespace CoupledField {
       normal_ *= -1.0;
     }
 
-    std::cerr << "normal after:\n" << normal_ << std::endl;
     density = acouMaterials[index].GetDensity();
 
-//     std::cerr << "using density: " << density << std::endl;
 
     // 2) Calculate a normal mass matrix
     helpMat.Resize(nrNodes);
@@ -91,7 +80,6 @@ namespace CoupledField {
       helpMat += partHelpMat;
     }
 
-//     std::cerr << "helpMat =\n" << helpMat << std::endl;
 
     // 3) Create a multi-dof matrix, multiplied by normal vector
     elemMat.Resize( nrNodes*dofs_, nrNodes );
@@ -104,7 +92,6 @@ namespace CoupledField {
         }
       }
     }
-//     std::cerr << "elemMat = \n" << elemMat << std::endl;
   }
   
 
