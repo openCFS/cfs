03431df7a320ffc0ba6c21e555e5a39c5593d143
Andreas Hauck <mail@andreas-hauck.com>
2005-06-22 12:15:56 +0000

    Added a new macro SETPROFILE(name) which triggers sampling of the current memory consumption and some other quantities (in the future).

--- libsrc/General/defs.hh
+++ libsrc/General/defs.hh
@@ -5,6 +5,10 @@
 #include "Utils/tracing.hh"
 #endif
 
+#ifdef PROFILING
+#include "Utils/profiler.hh"
+#endif
+
 #include <typeinfo>
 
 namespace CoupledField{
@@ -26,6 +30,16 @@ OutInfo::FcnTraceObjLocal fcn(name);
 #else//no tracing
 #define ENTER_FCN(name) 
 #define ENTER_IFCN(name)        
+#endif
+
+  // ****************************************************************************
+  //   This block deals with profiling information
+  // ****************************************************************************
+#ifdef PROFILING
+#define SETPROFILE(name)\
+profiler->Trace(name)
+#else
+#define SETPROFILE(name)
 #endif
 
 } // end of namespace
