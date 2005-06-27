c2b929bb29abef43153127fcc637704bd4fab5f1
mmohr <mmohr@cfs-dev.mdmt.tuwien.ac.at>
2005-06-27 08:24:42 +0000

    Dynamically changing the modification date in footer.html lead to constant merging conflicts. Therefore I moved footer.html to footerTemplate.html which does not contain a date. changeDate.sh now uses footerTemplate.html to generate a footer.html containing the current date.

--- Doc/makedoc.sh
+++ Doc/makedoc.sh
@@ -7,7 +7,8 @@ echo " "
 echo " Generating Doxygen Documentation for CFS++"
 echo " "
 # Change date in footer.html
-DoxygenConfig/changeDate.sh DoxygenConfig/footer.html
+DoxygenConfig/changeDate.sh DoxygenConfig/footerTemplate.html \
+DoxygenConfig/footer.html 
 doxygen DoxygenConfig/doxy-config
 echo " "
 echo " See `pwd`/html/index.html for doc"
