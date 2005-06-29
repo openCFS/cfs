f056f59aa7666f429b47f3ab1f05da7aa017b72e
Andreas Hauck <mail@andreas-hauck.com>
2005-06-29 17:54:01 +0000

    The new class DefaultCoordSys represents the global cartesian coordinate system. Since we do not need here a 'real' transformation of coordinates, only the GetVecComponent and GetDofName methods are implemented.

---
+++ libsrc/Utils/defaultCoordSys.cc
@@ -0,0 +1,91 @@
+#include "defaultCoordSys.hh"
+#include <cmath>
+
+namespace CoupledField{
+
+  DefaultCoordSystem::DefaultCoordSystem(Grid * ptGrid) 
+    : CoordSystem(std::string("default") , ptGrid) {
+    
+    ENTER_FCN("DefaultCoordSystem::DefaultCoordSystem");
+   
+  }
+  
+  DefaultCoordSystem::~DefaultCoordSystem(){
+    ENTER_FCN("DefaultCoordSystem::~DefaultCoordSystem");
+  }
+
+  void DefaultCoordSystem::Local2GlobalCoord( Vector<Double> & glob, 
+                                          const Vector<Double> & loc ) const {
+    ENTER_FCN("DefaultCoordSystem::Local2GlobalCoord");
+    glob = loc;
+}
+  
+  void DefaultCoordSystem::Global2LocalCoord( Vector<Double> & loc, 
+                                          const Vector<Double> & glob ) const {
+    ENTER_FCN("DefaultCoordSystem:: Global2LocalCoord");
+    loc = glob;
+  }
+  
+  void DefaultCoordSystem::
+  Local2GlobalVector( Vector<Double> & globVec, 
+                      const Vector<Double> & locVec, 
+                      const Vector<Double> & globModelPoint ) const { 
+    ENTER_FCN("DefaultCoordSystem::Local2GlobalVector");
+
+    globVec = locVec;
+  }
+
+  UInt DefaultCoordSystem::GetVecComponent( const std::string & dof )  const {
+    ENTER_FCN( "DefaultCoordSystem::GetVecComponent" );
+
+    
+    UInt component = 0;
+    
+    if ( dof == "ux" )
+      component = 1;
+    if ( dof == "uy" )
+      component = 2;
+    if ( dof == "uz" )
+      component = 3;
+
+    // --- DOF for piezo - HARD CODED -
+    if ( dof == "ep" )
+      component = ptGrid_->GetDim() + 1;
+    
+    if ( component == 0 ) {
+      (*error) << "DefaultCoordSystem:GetVecComponent:\n"
+               << "The component with name '" << dof 
+               << "' is not known in the global cartesian coordinate system";
+      Error( __FILE__, __LINE__ );
+    }
+
+    return component;
+  }
+
+  
+  const std::string DefaultCoordSystem::GetDofName( const UInt dof ) const {
+    ENTER_FCN( "DefaultCoordSystem::GetDofName" );
+    
+    std::string ret = "";
+    
+    switch (dof) {
+    case 1:
+      ret = "ux";
+      break;
+    case 2:
+      ret = "uy";
+      break;
+    case 3:
+      ret = "uz";
+      break;
+    default:
+      (*error) << "DefaultCoordSystem::GetDofName:\n"
+               << "The component number " << dof << " does not exist in a "
+               << "global cartesian coordinate system!";
+      Error( __FILE__, __LINE__ );
+    }
+
+    return ret;
+  }
+
+} // end of namespace
---
+++ libsrc/Utils/defaultCoordSys.hh
@@ -0,0 +1,110 @@
+#ifndef DEFAULT_COORD_SYSTEM_HH
+#define DEFAULT_COORD_SYSTEM_HH
+
+#include "coordSystem.hh"
+
+namespace CoupledField {
+
+
+  //! Describes the global cartesian coordinate system
+  class DefaultCoordSystem : public CoordSystem{
+
+  public:
+
+    //! Default constructor
+    //! \param name (in) name of the coordinate system which is used to 
+    //!                  find the additional parameters in the .xml-file
+    DefaultCoordSystem(Grid * ptGrid);
+    
+    //! Destructor
+    virtual ~DefaultCoordSystem();
+
+    //! Transform local into global coordinate
+
+    //! This method transforms a point given in local coordinates into a
+    //! point w.r.t to the global cartesian x,y,z coordinates.
+    //! \param glob (out) point w.r.t. to global cartesian coordinate system
+    //! \param loc (in) point w.r.t. to local coordinate system
+    void Local2GlobalCoord( Vector<Double> & glob, 
+                            const Vector<Double> & loc ) const;
+    
+    //! Transform global into local coordinate
+
+    //! This method transforms a point given in global cartesian coordinates
+    //! into a representation w.r.t to the local coordinate system.
+    //! \param loc (out) point w.r.t. to local coordinate system
+    //! \param glob (in) point w.r.t. to global cartesian coordinate system
+    void Global2LocalCoord( Vector<Double> &loc,  
+                            const Vector<Double> & glob ) const;
+    
+    //! Transform local vector into global one for a given global model point
+    
+    //! This method transforms a vector with a local coordinate representation
+    //! and a given global model point (german: "Aufpunkg") into a vector with 
+    //! a representation in global cartesian coordinates.
+    //! This method can be used for example to get for a global element 
+    //! mid-point and a given local load vector the global representation of
+    //! the load vector.
+    //! \param globVec (out) vector w.r.t. to global cartesian coordinate 
+    //!                      system
+    //! \param locVec (in) vector w.r.t. to local coordinate system
+    //! \param globModelPoint (in) global model point (german: "Aufpunkt") of
+    //!                            of the local/global vector
+    void Local2GlobalVector( Vector<Double> & globVec, 
+                             const Vector<Double> & locVec, 
+                             const Vector<Double> & globModelPoint ) const;
+    
+    //! Returns for a given coordinate name the according index
+
+    //! This method returns for a given coordinate name (rad,phi,ax)
+    //! the according index in the local vector representation.
+    //! \param dof (in) name of a coordinate component
+    //! \return index of the coordinate component
+    UInt GetVecComponent( const std::string & dof ) const;
+
+    //! Returns for a given coordinate index the according name
+
+    //! This method returns for a given coordinate index (1,2,3)
+    //! the according name (rad,phi,ax).
+    //! \param dof (in) index of the coordinate component
+    //! \return name of the coordinate component
+    const std::string GetDofName( const UInt dof ) const;
+
+
+  protected:
+
+    //! Calculate rotation matrix
+    void CalcRotationMatrix(){};
+    
+  };
+
+
+#ifdef DOXYGEN_DETAILED_DOC
+
+  // =========================================================================
+  //     Detailed description of the class 
+  // =========================================================================
+
+  //! \class DefaultCoordSystem
+  //! 
+  //! \purpose 
+  //! This class mainly a dummy class, since it represents the global
+  //! cartesian coordinate system, so no real transformation is applied.
+  //! Its main purpose is to convert dofs and the according indices.
+  //! 
+  //! \collab 
+  //! 
+  //! \implement 
+  //! 
+  //! \status In use
+  //! 
+  //! \unused 
+  //! 
+  //! \improve
+  //! 
+
+#endif
+
+} // end of namespace
+
+#endif
