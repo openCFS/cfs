56437a2b906e8e8317aadcdb34ab25cc57884d97
Andreas Hauck <mail@andreas-hauck.com>
2005-06-29 18:34:46 +0000

    Re-implemented this class. Now this class does not write out any information anymore. It has only three methods: - Start() - GetTime() - Reset() , where GetTime delivers both, wall clock time and cpu time.

--- libsrc/Utils/myclock.cc
+++ libsrc/Utils/myclock.cc
@@ -1,75 +1,39 @@
-#include <string>
-#include <fstream>
-#include <iostream>
- 
 #include "myclock.hh"
 
 namespace CoupledField
 {
 
-  MyClock::MyClock (Char * title)
-  {
- 
-    if (title)
-      {
-        Char * namefile=new Char[20];
-        strcpy(namefile,title);
- 
-        filetime.open(strcat(namefile,".time"));
- 
-        if (!filetime) std::cerr<< "ERROR ("<<__FILE__<<" "<<__LINE__<<"): Cann't create  file :" 
-                                << namefile << std::endl;
- 
-        delete namefile;
-        InFile=TRUE;
- 
-      }
-    else InFile=FALSE;
+  MyClock::MyClock () {
+    ENTER_FCN( "MyClock::MyClock" );
   }
  
-  MyClock::~MyClock()
-  {
-
-    if (InFile) filetime.close();
- 
+  MyClock::~MyClock() {
+    ENTER_FCN( "MyClock::~MyClock" );
   }
 
-  void MyClock::ClockCount(enum status n, const std::string title)
-  {
-    switch(n)
-      { 
-      case beg:
-        tm = time(0);
-        ck = clock();
- 
-        break;
- 
-      case end:
-        tm_tmp=time(0);
-        ck_tmp=clock();
- 
-        if (InFile) 
-          {
-            if (title.empty()) filetime << "You don't specified title !" << std::endl;
- 
-            filetime << title << "::\t" << "Wall time: " <<
-              difftime(tm_tmp, tm) << " c." << "\t" << "CPU time: " <<
-              Double(ck_tmp - ck)/CLOCKS_PER_SEC << std::endl;
- 
-            tm=tm_tmp;
-            ck=ck_tmp;
-          }
-        else 
-          {  
-            std::cout << " " << title << "::\t" << "Wall time: " <<
-              difftime(tm_tmp, tm) << " c." << "\t" << "CPU time: " <<
-              Double(ck_tmp - ck)/CLOCKS_PER_SEC << std::endl << std::endl;
- 
-            tm=tm_tmp;
-            ck=ck_tmp; 
-          }
- 
-        break;
-      }
+  void MyClock::Start() {
+    ENTER_FCN( "MyClock::Start" );
+
+    wTimeStart_ = std::time(NULL);
+    cTimeStart_ = std::clock();
   }
+
+  void MyClock::Reset() {
+    ENTER_FCN( "MyClock::Reset" );
+    wTimeStart_ = std::time(NULL);
+    cTimeStart_ = std::clock();
+  }
+
+  void MyClock::GetTime(Double & wallTime, Double & userTime) {
+    
+    std::time_t wTimeCur, cTimeCur;
+
+    // get current time and clock counts
+    wTimeCur = std::time(NULL);
+    cTimeCur = std::clock();
+    
+    wallTime = (Double) std::difftime(wTimeCur, wTimeStart_);
+    userTime = ( (Double) (cTimeCur - cTimeStart_)) / CLOCKS_PER_SEC;
+  }
+  
 } // end of namespace  
