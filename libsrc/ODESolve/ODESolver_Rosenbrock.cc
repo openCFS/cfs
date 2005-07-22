05a675eb8404684634e845cb9b416bb1a6323357
Nina Bretz <nbretz@svn>
2005-07-22 09:37:07 +0000

    New ODE-Solver, based on the semi-implicit Rosenbrock-Method. Solver needs user given computation of the rhs of dy/dt = f(y,t) and of the jacobian of f(y,t).

---
+++ libsrc/ODESolve/ODESolver_Rosenbrock.cc
@@ -0,0 +1,380 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include <cmath>     
+
+
+#include "ODESolver_Rosenbrock.hh"
+#include "ODEDescr/Gilmore.hh"
+#include "ODEDescr/Gilmoredimlos.hh"
+#include "ODEDescr/KellerMiksis.hh"
+
+namespace CoupledField
+{
+  void ODESolver_Rosenbrock::Solve( const Double tInit,
+				    const Double tStop,
+				    StdVector<Double> &yInitOut,
+				    BaseODEProblem &myODE,
+				    Double &hInit,
+				    Double hMin,
+				    Double hMax ){
+    ENTER_FCN( " ODESolver_Rosenbrock::Solve " );
+    const Double tiny = 1.0e-30;
+    UInt i;
+    UInt nstp;
+    Double t;
+    Double h;
+    Double hNext;
+    Double hDid;
+
+    UInt nvar = yInitOut.GetSize();
+    StdVector<Double> yScal(nvar);
+    StdVector<Double> C(nvar);
+    StdVector<Double> y(nvar);
+    StdVector<Double> dydt(nvar);
+
+
+
+    t = tInit;
+    //Achtung bei nachfolgender Abfrage Problem durch default wert?
+    h = ((tStop-tInit) >= 0 ? 
+	 (hInit>= 0 ? hInit :-hInit) : (hInit>= 0 ? -hInit : hInit));
+
+    // Initialise status parameters
+    numStepsLastSolve_    = 0;
+    numBadStepsLastSolve_ = 0;
+    successLastSolve_     = 0;
+
+    for ( i = 0; i < nvar; i++ ){
+      y[i] = yInitOut[i]; 
+    }
+
+
+    // *** Test ************************************
+    if (t+h-tStop > 0.0) {
+      h = tStop-t;
+    }
+    // *********************************************
+
+    for (nstp=0; nstp<maxSteps_; nstp++){
+      myODE.CompDeriv(t,y,dydt);
+      //Scaling to monitor accuracy.
+      //Can be modified if different yscal is needed.
+      //Initialise the vector for the error control
+      //Choice of C only good for dimensionless case
+      for ( i = 0; i < nvar; i++ ){
+	C[i] = 1; 
+	yScal[i] = std::max(C[i],std::abs(y[i]));
+      }
+
+      // *** Test ************************************
+      // //If stepsize can overshoot, decrease
+      //if ((t+h-tStop)*(t+h-tInit) > 0.0){
+      //  h = tStop-t;
+      //}
+      // *********************************************
+      RosenbAdaptiveStepsize(y,dydt,t,h,yScal,hDid,hNext,myODE);
+
+
+      if (hDid == h){
+	++numStepsLastSolve_;
+      }
+      else{
+	++numBadStepsLastSolve_;
+      }	
+      //Did we get the final value for tStop?
+      // *** MODIFIED testing case, added second condition********
+      if (((t-tStop)*(tStop-tInit) >= 0.0) || (t+hNext-tStop > 0.0)){
+	for (i=0; i<nvar; i++){
+	  yInitOut[i] = y[i];
+	}
+	hInit = hNext; //added compared to nr
+	successLastSolve_ = true;
+	return;      // Normal exit
+      }
+      if (fabs(hNext) <= hMin){
+	//hNext=hMin;
+	Error("Step size too small",__FILE__,__LINE__);
+	successLastSolve_ = false;
+      }
+      h = hNext;
+    }
+
+    Error( "Too many steps", __FILE__, __LINE__ );
+
+    successLastSolve_ = false;
+
+	    
+  }
+
+
+
+  void ODESolver_Rosenbrock::RosenbAdaptiveStepsize (StdVector<Double> &y,
+						     StdVector<Double> &dydt,
+						     Double &t,
+						     const Double hTry,
+						     const StdVector<Double> &yScal,
+						     Double &hDid,
+						     Double &hNext,
+						     BaseODEProblem &myODE){
+
+    ENTER_FCN( "ODESolver_Rosenbrock::RosenbAdaptiveStepsize" );
+    // powerGrow and powerShrink are exponents in computation of step size
+    Double  powerGrow = -0.25;
+    Double  powerShrink = -1.0/3.0;
+
+    Double  Grow   = 1.5;
+    Double  Shrink = 0.5;
+
+    // errCon = (Grow/safetyFac_) rasied to the power (1/powergrow)
+    Double errCon =  0.012345679;//0.1296;
+    UInt maxTry = 40;
+
+    Double GAM, A21, A31, A32, C21, C31, C32, C41, C42, C43, B1, B2, B3, B4;
+    Double  E1, E2, E3, E4, A2X, A3X;
+
+    GAM = 1.0 / 2.0;
+    A21 = 2.0;
+    A31 = 48.0 / 25.0;
+    A32 = 6.0 / 25.0;
+    C21 = -8.0;
+    C31 = 372.0 / 25.0;
+    C32 = 12.0 / 5.0;
+    C41 = -112.0 / 125.0;
+    C42 = -54.0 / 125.0;
+    C43 = -2.0/5.0;
+    B1  = 19.0 / 9.0;
+    B2  = 1.0 / 2.0;
+    B3  = 25.0 / 108.0;
+    B4  = 125.0 / 108.0;
+    E1  = 17.0 / 54.0;
+    E2  = 7.0 / 36.0;
+    E3  = 0.0;
+    E4  = 125.0 / 108.0;
+    A2X = 1.0;
+    A3X = 3.0 / 5.0;
+
+    UInt i, j, jtry;
+
+    UInt n = y.GetSize();
+
+    StdVector<UInt> indx(n);
+
+    Double d;
+
+    Double  errMax;
+    Double  h;
+    Double  xsav;
+
+    Matrix<Double> a(n,n);
+    Matrix<Double> dfdy(n,n);
+
+
+    StdVector<Double> dysav(n);     
+    StdVector<Double> err(n);
+    StdVector<Double> g1(n);
+    StdVector<Double> g2(n);
+    StdVector<Double> g3(n);
+    StdVector<Double> g4(n);
+    StdVector<Double> ysav(n);
+
+    xsav = t;
+    for(i=0; i<n; i++){
+      ysav[i]   = y[i];
+      dysav[i] = dydt[i];
+    }
+
+    myODE.Jacobi(ysav,dfdy,xsav);
+
+    h = hTry;               //Set stepsize to inital guess
+
+    for( jtry=0; jtry<maxTry; jtry++){
+      for(i=0; i<n; i++){
+	for(j=0; j<n; j++){
+	  a(i,j)= -dfdy(i,j);
+	}
+	a(i,i) += 1.0/(GAM*h);
+      }
+      LUDecomposition(a,n,indx,d);
+ 
+      for(i=0; i<n; i++){
+	g1[i]= dysav[i];
+      }
+
+      LUBackSub(a,n,indx,g1);
+    
+      for(i=0; i<n; i++){
+	y[i]=ysav[i]+A21*g1[i];
+      }
+      t=xsav+A2X*h;
+      myODE.CompDeriv(t,y,dydt);
+
+      for(i=0; i<n; i++){
+	g2[i]= dydt[i]+C21*g1[i]/h;
+      }
+
+      LUBackSub(a,n,indx,g2); 
+      for(i=0; i<n; i++){
+	y[i]=ysav[i]+A31*g1[i]+A32*g2[i];
+      }
+      t=xsav+A3X*h;
+      myODE.CompDeriv(t,y,dydt);
+      for(i=0; i<n; i++){
+	g3[i]= dydt[i]+(C31*g1[i]+C32*g2[i])/h;
+      }
+      LUBackSub(a,n,indx,g3);
+      for(i=0; i<n; i++){
+	g4[i]= dydt[i]+(C41*g1[i]+C42*g2[i]+C43*g3[i])/h;
+      }
+  
+      LUBackSub(a,n,indx,g4);
+      for(i=0; i<n; i++){
+	y[i]=ysav[i]+B1*g1[i]+B2*g2[i]+B3*g3[i]+B4*g4[i];
+	err[i]=E1*g1[i]+E2*g2[i]+E3*g3[i]+E4*g4[i];
+      }
+
+      t= xsav+h;
+      if (t == xsav){
+	Error("Stepsize not significant in stiff",__FILE__,__LINE__);
+      }
+      errMax = 0.0;
+      for(i=0; i<n; i++){
+	errMax =( (fabs(err[i]/yScal[i])) > errMax 
+		  ? (fabs(err[i]/yScal[i])): (errMax));
+	//	std::cerr << err[i] << "   " << yScal[i] << "   " <<errMax<<std::endl;
+      }
+      errMax /= eps_;
+      if ( errMax <= 1.0){
+	hDid=h;
+	hNext=(errMax >errCon? safetyFac_ * h* std::pow(errMax, powerGrow) : Grow*h);
+
+	return;
+      }
+      else{
+	hNext = safetyFac_ *h * std::pow(errMax,powerShrink);
+	h = ( h >= 0.0 ? 
+	      ((Shrink*h) > hNext ? (Shrink*h) : (hNext)):((Shrink*h) < hNext ? (Shrink*h) : (hNext) ));
+      }
+    }
+    Error("Exceeded maxTry in stiff",__FILE__,__LINE__);
+  }
+
+
+
+
+
+  // LU-Decomposition 
+  void ODESolver_Rosenbrock::LUDecomposition(Matrix<Double> &a,
+					     UInt n,
+					     StdVector<UInt> &indx,
+					     Double &d){
+
+    ENTER_FCN( "ODESolver_Rosenbrock::LUDecomposition" );
+    const Double tiny = 1.0e-20;
+    UInt i, imax, j, k;
+    Double big, dum, sum, temp;
+    StdVector<Double> v(n);
+    d = 1.0;
+
+    for (i=0; i<n ; i++){
+      big=0.0;
+      for (j=0; j<n ; j++){
+	if ( (temp=fabs(a(i,j))) > big ) big=temp;
+      }
+      if ( big == 0){
+	Error( "Singular matrix in routine LUDecomposition", __FILE__, __LINE__ );
+      }
+      v[i]= 1.0 / big;
+    }
+    
+    for (j=0; j<n ; j++){
+      for (i=0; i<j ; i++){
+	sum = a(i,j);
+	for (k=0; k<i ; k++){
+	  sum -= a(i,k) * a(k,j);
+	}
+	a(i,j) = sum;
+      }
+      big = 0.0;
+      for (i=j; i<n ; i++){
+	sum = a(i,j);
+	for (k=0; k<j ; k++){
+	  sum -= a(i,k) * a(k,j);
+	}
+	a(i,j) = sum;
+	if ( (dum = v[i] * fabs(sum)) >= big){
+	  big = dum;
+	  imax = i;
+	}
+      }
+      if( j != imax){
+	for (k=0; k< n ; k++){
+	  dum = a(imax,k);
+	  a(imax,k) = a(j,k);
+	  a(j,k) = dum;
+	}
+	d = -d;
+	v[imax] = v[j];
+      }
+      indx[j]= imax;
+      if ( a(j,j) == 0.0){
+	a(j,j)=tiny;
+      }
+      if (j!=n-1){
+	dum = 1.0/(a(j,j));
+	for (i=j+1; i<n; i++){
+	  a(i,j) *= dum;
+	}
+      }
+    }
+
+  }
+
+  // LU-BAcksubstitution 
+  void ODESolver_Rosenbrock::LUBackSub(Matrix<Double> &a,
+				       UInt n,
+				       StdVector<UInt> &indx,
+				       StdVector<Double> &b){
+    ENTER_FCN( " ODESolver_Rosenbrock::LUBackSub" );
+    Integer i,ii, ip,j;
+    Double sum;
+    ii =0;
+
+    for(i=0; i<n; i++){
+
+      ip = indx[i];
+      sum = b[ip];
+      b[ip]= b[i];
+      if( ii!=0) {
+	for( j = ii-1; j<i; j++){
+	  sum -= a(i,j)*b[j];
+	}
+      }
+      else{
+	if ( sum!=0.0 ){
+	  ii = i+1;
+	}
+      }
+      b[i] = sum;
+    }
+
+    for( i= n-1; i >= 0; i--){
+      sum = b[i];
+      for( j= i+1; j< n; j++){
+	sum -= a(i,j) *b[j];
+      }
+      b[i] = sum / a(i,i);
+    }
+
+  }
+
+  
+
+
+
+  
+
+
+} // end of namespace
+
---
+++ libsrc/ODESolve/ODESolver_Rosenbrock.hh
@@ -0,0 +1,127 @@
+#ifndef ODE_SOLVER_ROSENBROCK_HH
+#define ODE_SOLVER_ROSENBROCK_HH
+
+#include "General/environment.hh"
+#include "Utils/StdVector.hh"
+#include "Utils/tools.hh"
+#include "BaseODESolver.hh"  
+#include "Matrix/matrix.hh"
+
+namespace CoupledField {
+
+  //! Implicit Rosenbrock solver for systems of first order ode's.
+  class ODESolver_Rosenbrock : public BaseODESolver {
+
+  public:
+
+    //! Default Constructor
+    ODESolver_Rosenbrock()  {
+      ENTER_FCN( "ODESolver_Rosenbrock::ODESolver_Rosenbrock" );
+
+
+    }
+
+    //! Default Destructor
+    ~ODESolver_Rosenbrock() {
+      ENTER_FCN( "ODESolver_Rosenbrock::~ODESolver_Rosenbrock" );
+    }
+
+    //! Compute the solution of the initial value problem
+    //! \param tInit     initial time
+    //! \param tStop     final time
+    //! \param yIintOut  containing on input the initial values and on output
+    //!                  the solution
+    //! \param myODE     object containing information on the right hand side
+    //!                  function of the ODE
+    //! \param hInit     Suggestion for size of first time step
+    //! \param hMin      Minimal allowed size for time step
+    //! \param hMax      Maximal allowed size for time step
+    void Solve( const Double tInit,
+		const Double tStop,
+		StdVector<Double> &yInitOut,
+		BaseODEProblem &myODE,
+		Double &hInit,
+		Double hMin = -1.0,
+		Double hMax = -1.0 );
+
+
+
+
+
+  private:
+
+
+    //! Computes and controlls Rosenbrock step by monitoring local 
+    //! truncation error and adjusting stepsize
+    //! \param y         containing on input the initial values and on output
+    //!                  the solution
+    //! \param dydt      containing the known derivatives of y at time t
+    //! \param t         current time
+    //! \param htry      Suggested step size
+    //! \param vector    yscal: vector against which the error is scaled 
+    //! \param hdid      Stepsize that was actually accomplished
+    //! \param hnext     Estimation for next time step
+    //! \param myODE     object containing information on the right hand side
+    //!                  function of the ODE 
+    void RosenbAdaptiveStepsize (StdVector<Double> &y,
+				 StdVector<Double> &dydt,
+				 Double &t,
+				 const Double hTry,
+				 const StdVector<Double> &yScal,
+				 Double &hDid,
+				 Double &hNext,
+				 BaseODEProblem &myODE);
+
+
+    // LU-Decomposition
+    void LUDecomposition(Matrix<Double> &a,
+			 UInt n,
+			 StdVector<UInt> &indx,
+			 Double &d);
+                     
+
+
+    // LU-BAcksubstitution
+    void LUBackSub(Matrix<Double> &a,
+		   UInt n,
+		   StdVector<UInt> &indx,
+		   StdVector<Double> &b);
+    
+  };
+
+#ifdef DOXYGEN_DETAILED_DOC
+
+  // =========================================================================
+  //     Detailed description of the class 
+  // =========================================================================
+
+  //! \class ODESolver_Rosenbrock
+  //! 
+  //! \purpose
+  //! Integrator for solution of an initial value problem using a
+  //! semi-implicit Rosenbrock method of 4th order with a step size control of
+  //! 5th order. See Numerical Recipes.
+  //! 
+  //! \collab
+  //! Needs a user provided routine to compute the right hand side
+  //! of the ode dy/dt = f(t,y) and computation of the Jacobian Matrix; 
+  //! here this is done via the pointer
+  //! to the bubble-class ( Gilmoredimlos, Gilmore, Keller-Miksis).
+  //! 
+  //! \implement 
+  //! 
+  //! \status In use
+  //! 
+  //! \unused 
+  //! 
+  //! \improve
+
+
+#endif
+
+
+
+}
+
+
+#endif
