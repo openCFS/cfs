9743eec44426d2101717ab7f887c92f26c547901
Andreas Hauck <mail@andreas-hauck.com>
2005-08-08 09:15:59 +0000

    The method CalcLocalGlobalMapping() now checks if the PDE is defined on all volume regions. If this is the case, then a simple one-to-one mapping of global/local node numbers is done. Otherwise the local nodes are renumbered. This might help to obtain potentially better reordering results when using Sloan (assuming that the initial ANSYS node numbering was not too bad at all ;-)

--- libsrc/PDE/nodeEQN.cc
+++ libsrc/PDE/nodeEQN.cc
@@ -67,49 +67,80 @@ namespace CoupledField
     mesh2PDEElem.Init(-1);
     pde2MeshElem.Resize(ptGrid_->GetNumElems(subdoms_));
     pde2MeshElem.Init(0);
-    //std::cerr << "After init of pde2MeshElem" << std::endl;
-    //std::cerr << "Size of pde2MeshEl
-
+ 
 
     UInt nodeCounter = 0;
     UInt elemCounter = 1;
+     StdVector<Elem*> subdom;
  
-    StdVector<Elem*> subdom;
- 
+    // First, check, if PDE is defined on all regions of domain
+    StdVector<RegionIdType> allRegions;
+    Boolean allFound = TRUE;
+    Integer index = -1;
 
-    // iterate over all subdomains
-    for (UInt iSD=0; iSD<subdoms_.GetSize(); iSD++)
-      {
-        ptGrid_->GetElems(subdom,subdoms_[iSD]); 
-
-        // iterate over all elems in subdomain
-        for (UInt iElem=0; iElem<subdom.GetSize(); iElem++)
-          {
-            //std::cerr << "before mapping of elements" << std::endl;
-            // *** Mapping of Elements ***
-            mesh2PDEElem[subdom[iElem]->elemNum - 1 ] = elemCounter;
-            pde2MeshElem[elemCounter-1] = subdom[iElem]->elemNum;
-            elemCounter++;
-            //std::cerr << "after mapping of elements" << std::endl;
-
-          
-            // *** Mapping of Nodes ***
-            // iterate over all nodes in elem
-            for (UInt iNode=0; iNode<subdom[iElem]->connect.GetSize(); iNode++)
-              // Check if node was already assigned
-              if (mesh2PDENode[subdom[iElem]->connect[iNode]-1] == -1)
-                {
-                  // std::cerr << "mesh2PDENode[" << subdom[iElem]->connect[iNode]-1 << "] = ";
-                  //              std::cerr << nodeCounter +1 << std::endl;
-                  mesh2PDENode[subdom[iElem]->connect[iNode]-1] = ++nodeCounter;
-                  pde2MeshNode.Push_back(subdom[iElem]->connect[iNode]);
-                }
-          }
+    ptGrid_->GetVolRegionIds( allRegions );
+    for ( UInt i = 0; i < allRegions.GetSize(); i++ ) {
+      index = subdoms_.Find(allRegions[i]);
+      if ( index == -1 ) {
+        allFound = FALSE;
+        break;
       }
-
+    }
+    
+    // Case 1: This pde is defined on all volume regions of the grid
+    //         --> Do a simple 1-to-1 mapping of global node number
+    //             and local one
+    if ( allFound ) {
+      pde2MeshNode.Resize(mesh2PDENode.GetSize());
+      for (UInt i = 0; i<mesh2PDENode.GetSize(); i++) {
+        mesh2PDENode[i] = i+1;
+        pde2MeshNode[i] = i+1;
+    }
+      
+      pde2MeshElem.Resize(ptGrid_->GetNumElems());
+      for (UInt i = 0; i<mesh2PDEElem.GetSize(); i++) {
+        pde2MeshElem[i] = i+1;
+        mesh2PDEElem[i] = i+1;
+      }
+      
+      
+      nodeCounter=mesh2PDENode.GetSize();
+    } else {
+      // Case 2: This pde is defined on a subset of all regions
+      //         --> Perform normal node renumbering
+      
+      //  // iterate over all subdomains
+      for (UInt iSD=0; iSD<subdoms_.GetSize(); iSD++) {
+        ptGrid_->GetElems(subdom,subdoms_[iSD]); 
+        
+        // iterate over all elems in subdomain
+        for (UInt iElem=0; iElem<subdom.GetSize(); iElem++) {
+          //std::cerr << "before mapping of elements" << std::endl;
+          // *** Mapping of Elements ***
+          mesh2PDEElem[subdom[iElem]->elemNum - 1 ] = elemCounter;
+          pde2MeshElem[elemCounter-1] = subdom[iElem]->elemNum;
+          elemCounter++;
+          //std::cerr << "after mapping of elements" << std::endl;
+          
+          
+          // *** Mapping of Nodes ***
+          // iterate over all nodes in elem
+          for (UInt iNode=0; iNode<subdom[iElem]->connect.GetSize(); iNode++)
+            // Check if node was already assigned
+            if (mesh2PDENode[subdom[iElem]->connect[iNode]-1] == -1) {
+              // std::cerr << "mesh2PDENode[" 
+              //               << subdom[iElem]->connect[iNode]-1 << "] = ";
+              //              std::cerr << nodeCounter +1 << std::endl;
+              mesh2PDENode[subdom[iElem]->connect[iNode]-1] = ++nodeCounter;
+              pde2MeshNode.Push_back(subdom[iElem]->connect[iNode]);
+            }
+        }
+      }
+    }
+    
     numPDENodes_ = nodeCounter;
   }
-
-
-
+  
+  
+  
 } // end of namespace
