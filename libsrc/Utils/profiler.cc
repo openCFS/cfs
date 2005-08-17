6f9271aafd33f0b86a4edf1480c76f9657a13b97
Markus Mohr <marcus.mohr@geophysik.uni-muenchen.de>
2005-08-17 14:52:16 +0000

    Introduced another attribute doProfiling_ that is used to detect whether we should actually do profiling or just show a dummy behaviour.

--- libsrc/Utils/profiler.cc
+++ libsrc/Utils/profiler.cc
@@ -16,29 +16,41 @@ namespace CoupledField{
   // ***********************
   Profiler::Profiler() {
 
-    // get process id
-    myPid_ = getpid();
+    // Determine whether profiling should be performed
+    doProfiling_ = !commandLine->GetNoProfile();
 
-    // intiialize profile file
-    fileName_ = commandLine->GetSimName() + ".profile";
-    memOut_ = new std::ofstream(fileName_.c_str());
+    if ( doProfiling_ == true ) {
 
-    (*memOut_).setf(std::ios::fixed,std::ios::floatfield);
-    (*memOut_).precision(2);
-      
-    (*memOut_) << "# Profiling output\n"
-               << "# Memory | Wall time | User Time "
-               << "| Rel. wall time | Rel. user time | Name\n";
-    (*memOut_) << std::setw(80) << std::setfill('=') << "" 
-               << std::setfill(' ') << std::endl;
+      // get process id
+      myPid_ = getpid();
 
-    // Initialise attributes
-    wTimeLast_ = 0.0;
-    cTimeLast_ = 0.0;
+      // intialize profile file
+      fileName_ = commandLine->GetSimName() + ".profile";
+      memOut_ = new std::ofstream(fileName_.c_str());
 
-    // Start timing
-    clock_ = new MyClock();
-    clock_->Start();
+      (*memOut_).setf(std::ios::fixed,std::ios::floatfield);
+      (*memOut_).precision(2);
+
+      (*memOut_) << "# Profiling output\n"
+                 << "# Memory | Wall time | User Time "
+                 << "| Rel. wall time | Rel. user time | Name\n";
+      (*memOut_) << std::setw(80) << std::setfill('=') << "" 
+                 << std::setfill(' ') << std::endl;
+
+      // Initialise attributes
+      wTimeLast_ = 0.0;
+      cTimeLast_ = 0.0;
+
+      // Start timing
+      clock_ = new MyClock();
+      clock_->Start();
+    }
+
+    // We do not do profiling
+    else {
+      memOut_ = NULL;
+      clock_  = NULL;
+    }
   }
 
 
@@ -56,39 +68,41 @@ namespace CoupledField{
       delete clock_;
       clock_ = NULL;
     }
-    
   }
   
   
   // *********
   //   Trace
   // *********
-  void Profiler::Trace(std::string name) {
-    std::ostringstream command;
-    Double wTime, cTime;
-    
-    // Get memory consumption
-    command << "ps -F " << myPid_ 
-            << " | grep " << myPid_ 
-            << " | gawk '{printf \"% 5d MB\",$6/1024}' >> " << fileName_;
-    memOut_->close();
-    std::system( (command.str()).c_str() );
-    memOut_->open(fileName_.c_str(), std::ofstream::out | std::ofstream::app);
+  void Profiler::Trace( std::string name ) {
 
-    // Get time
-    clock_->GetTime(wTime, cTime);
-    (*memOut_) << "\t" << wTime << "\t" << cTime;
-    
-    // Write relative time
-    (*memOut_) << "\t" <<  wTime-wTimeLast_
-               << "\t" <<  cTime-cTimeLast_;
-    wTimeLast_ = wTime;
-    cTimeLast_ = cTime;
-     
+    if ( doProfiling_ == TRUE ) {
 
-    // Write name
-    (*memOut_)  << '\t' << name << std::endl;
+      std::ostringstream command;
+      Double wTime, cTime;
+
+      // Get memory consumption
+      command << "ps -F " << myPid_ 
+              << " | grep " << myPid_ 
+              << " | gawk '{printf \"% 5d MB\",$6/1024}' >> " << fileName_;
+      memOut_->close();
+      std::system( (command.str()).c_str() );
+      memOut_->open( fileName_.c_str(), std::ofstream::out |
+                     std::ofstream::app);
+
+      // Get time
+      clock_->GetTime(wTime, cTime);
+      (*memOut_) << "\t" << wTime << "\t" << cTime;
+
+      // Write relative time
+      (*memOut_) << "\t" <<  wTime-wTimeLast_
+                 << "\t" <<  cTime-cTimeLast_;
+      wTimeLast_ = wTime;
+      cTimeLast_ = cTime;
+
+      // Write name
+      (*memOut_)  << '\t' << name << std::endl;
+    }
   }
-  
-  
-} // end of namespace
+
+}
