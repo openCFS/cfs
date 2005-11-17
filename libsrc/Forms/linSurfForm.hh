4dc0231c03c1884865978051164189f866769f91
Ludwig Bahr <bahr@svn>
2005-11-17 18:16:08 +0000

    For implementation of Neumann BCs in acoustics added class LinearSurfForm which is derived from LinearForm. From LinearSurfForm we derive AcouNeumannInt. We had to take this step, because up to now every integrator in acoustics has to be multiplied with density. AcouNeumannInt gets for every surface element the density of the neighboring volume element. For a revised class structure of BaseForm and LinearForm Andi will make up his mind.

---
+++ libsrc/Forms/linSurfForm.hh
@@ -0,0 +1,53 @@
+#ifndef FILE_LIN_SURF_FORM
+#define FILE_LIN_SURF_FORM
+
+#include "Forms/linearForm.hh"
+
+namespace CoupledField 
+{
+
+  //! Base class for all type of right-hand-side surface integrators
+  class LinearSurfForm : public LinearForm {
+
+  public:
+
+    //! Standard constructor
+    LinearSurfForm();
+
+    //! Destructor
+    ~LinearSurfForm();
+
+    //! Set pointer to surface element
+    void SetSurfElem( SurfElem * ptSurfElem);
+
+    //! Set normal pointing out of first volume element
+    void SetVoluNormal( Vector<Double> & n );
+    
+    //! Set information of related volume region
+    void SetVoluInfo( const StdVector<RegionIdType> & regionIds,
+                      const MaterialData* materials );
+    
+    //! set additional multiplicative factor for matrix
+    void SetFactor(Double factor); 
+    
+  protected:
+
+    //! Current surface element
+    SurfElem * actElem_;
+
+    //! Normal pointing out of first volume element
+    Vector<Double> normal_;
+
+    //! Region Ids of associated volume regions
+    StdVector<RegionIdType> regionIds_;
+
+    //! Materials of volume regions
+    const MaterialData * materials_;
+
+    //! Multiplicative factor for vector
+    Double factor_;
+
+  };
+
+} // end of namespace
+#endif
