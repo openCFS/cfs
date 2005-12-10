5cacf22c69bd388351fd7a1b094052306cacb647
Andreas Hauck <mail@andreas-hauck.com>
2005-12-10 19:18:59 +0000

    This header file contains only some definitions for type promotion / traits. These are used to allow mixed double-complex expressions within the Vector- and Matrix-class.

---
+++ libsrc/Utils/promote.hh
@@ -0,0 +1,22 @@
+#ifndef PROMOTE_HH
+#define PROMOTE_HH
+
+#include "General/environment.hh"
+using namespace CoupledField;
+template<typename T1, typename T2> 
+struct Trait2 {
+};
+
+#define DECLARE_TRAIT2(A,B,C)                    \
+  template<> struct Trait2<A,B> {                 \
+    typedef C T_PROMOTE;\
+};
+DECLARE_TRAIT2(Complex,Double,Complex);
+DECLARE_TRAIT2(Double,Complex,Complex);
+DECLARE_TRAIT2(Double,Double,Double);
+DECLARE_TRAIT2(Complex,Complex,Complex);
+
+// DEFINE for type promotion
+#define PROMOTE(P,P2) typename Trait2<P,P2>::T_PROMOTE
+
+#endif
