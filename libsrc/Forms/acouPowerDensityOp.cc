66afd150c0b08456ec6f903bbd573559e3f15498
Ludwig Bahr <bahr@svn>
2006-03-03 09:39:44 +0000

    This class calculates source term in acoustic to be used in heat conduction. Now works also for the transientHarmonic analysis.

--- libsrc/Forms/acouPowerDensityOp.cc
+++ libsrc/Forms/acouPowerDensityOp.cc
@@ -12,30 +12,31 @@
 #include <PDE/StdPDE.hh>
 
 namespace CoupledField {
-  
-  AcouPowerDensityOp::AcouPowerDensityOp(Grid * ptGrid, 
-                                         StdPDE * ptPDE,
-                                         NodeEQN * ptEQN,
-                                         Boolean isaxi)
+
+  template<class TYPE>  
+  AcouPowerDensityOp<TYPE>::AcouPowerDensityOp(Grid * ptGrid, 
+                                               StdPDE * ptPDE,
+                                               NodeEQN * ptEQN,
+                                               Boolean isaxi)
     : BaseOperator(ptGrid, ptPDE, ptEQN, isaxi)
   {
     ENTER_FCN( "AcouPowerDensityOp::AcouPowerDensityOp" );
     
     isaxi_ = isaxi;
 
-    //this->sol_ = & sol;
-    //this->solDeriv1_ = & solDeriv1;
   }
 
-
-  AcouPowerDensityOp::~AcouPowerDensityOp()
+  template<class TYPE>
+  AcouPowerDensityOp<TYPE>::~AcouPowerDensityOp()
   {
     ENTER_FCN( "AcouPowerDensityOp::~AcouPowerDensityOp" );
   }
 
-  void AcouPowerDensityOp::CalcElemPD(Vector<Double> & elemPD,
-                                          const Elem * ptElement,
-                                          const Double density)
+
+  template<class TYPE>
+  void AcouPowerDensityOp<TYPE>::CalcElemPD(Vector<Double> & elemPD,
+                                            const Elem * ptElement,
+                                            const Double density)
   {
     ENTER_FCN( "AcouPowerDensityOp::CalcElemEnergy" );
 
@@ -50,23 +51,29 @@ namespace CoupledField {
     Matrix<Double> ptCoord;
     ptPDE_->GetElemCoords(connect, ptCoord);
 
-
-    Vector<Double> elemSol, elemSolDeriv1;
-    ptPDE_->GetSolVecOfElement(elemSol,connect);
-    ptPDE_->GetDerivSolVecOfElement(elemSolDeriv1,connect);
-
     // derivation of shape functions after global coordinates 
     Matrix<Double> xiDx, xiDxTransp;
 
     Vector<Double> ShpFncAtIp;
     Vector<Double> CoordAtIp;
 
-    Vector<Double> solGradAtIp, solDeriv1GradAtIp;
-    Double solDeriv1AtIp;
+    Vector<TYPE> elemSol, elemSolDeriv1;
+    ptPDE_->GetSolVecOfElement(elemSol,connect);
+    ptPDE_->GetDerivSolVecOfElement(elemSolDeriv1,connect);
+
+//     std::cout << "Number Nodes:\n" << nrNodes << std::endl;
+//     std::cout << "Number IntPoints:\n" << nrIntPts << std::endl;
+//     std::cout << "elemSol:\n" << elemSol << std::endl;
+//     std::cout << "elemSolDeriv1:\n" << elemSolDeriv1 << std::endl;
+
+    Vector<TYPE> solGradAtIp, solDeriv1GradAtIp;
+    TYPE solDeriv1AtIp;
 
     Double N1;
     Vector<Double> N2;
     Double jacDet;
+    Double Jdet=0.0;
+
     for (UInt actIntPt=1; actIntPt <= nrIntPts; actIntPt++) {  
 
       jacDet = 0;
@@ -82,6 +89,7 @@ namespace CoupledField {
       }
         
       xiDx.Transpose(xiDxTransp);
+      dimensions_ = xiDx.GetSizeCol();
         
       // compute gradient of solution + 1st derivative at integration point
       solGradAtIp       = xiDxTransp * elemSol;
@@ -91,33 +99,126 @@ namespace CoupledField {
       solDeriv1AtIp     = ShpFncAtIp * elemSolDeriv1;
 
         
-      N1 = 0;
-      for (UInt j=0; j<xiDx.GetSizeCol(); j++)
-        N1 += solGradAtIp[j] * solDeriv1GradAtIp[j];
+      N1 = ComputeN1( solGradAtIp, solDeriv1GradAtIp );
+      N2 = ComputeN2(solGradAtIp, solDeriv1AtIp);
 
-      N2 = solGradAtIp * solDeriv1AtIp;
-
+      Jdet += jacDet;
       for (UInt i=0; i< nrNodes; i++) {
 
-        elemPD[i] += ShpFncAtIp[i] * N1;
+        elemPD[i] += ShpFncAtIp[i] * N1 * jacDet;
 
         for (UInt j=0; j<xiDx.GetSizeCol(); j++)
-          elemPD[i] -= xiDx[i][j] * N2[j];
+          elemPD[i] -= xiDx[i][j] * N2[j] * jacDet;
       }
-      elemPD *= jacDet * density;  
     }
+//     std::cout << "Jdet=" << Jdet << std::endl;
+
+    elemPD *= density;
+
+#ifdef DEBUG
+    (*debug) << "Coupling vector acoustic-heat conduction is: " 
+             << std::endl << elemPD << std::endl << std::endl;
+#endif
     
   }
 
-  
-  void AcouPowerDensityOp::CalcSubdomPD(Vector<Double> & elemPD,
-                                        const StdVector<RegionIdType> & SD,
-                                        const Double density)
+  template<class TYPE>  
+  Double AcouPowerDensityOp<TYPE>::ComputeN1( Vector<TYPE> solGrtIp,
+                                              Vector<TYPE> solD1GrAtIp )
   {
-    ENTER_FCN( "AcouPowerDensityOp::CalcSDEnergy" );
+    ENTER_FCN( "AcouPowerDensityOp::ComputeN1" );
 
-    (*error) << "AcouPowerDensityOp::CalcSDElecField: Not working yet"; 
+    (*error) << "AcouPowerDensityOp::ComputeN1 "
+             << "only defined for TYPE=Complex/Double "; 
     Error( __FILE__, __LINE__ );
-
+    return TYPE();
   }
+
+  template<>
+  Double AcouPowerDensityOp<Double>::ComputeN1( Vector<Double> solGrAtIp,
+                                                Vector<Double> solD1GrAtIp )
+  {
+    ENTER_FCN( "AcouPowerDensityOp::ComputeN1" );
+
+    Double N1 = 0;
+    for (UInt k=0; k<dimensions_; k++)
+      N1 += solGrAtIp[k] * solD1GrAtIp[k];
+    return N1;
+  }
+
+  template<>
+  Double AcouPowerDensityOp<Complex>::ComputeN1( Vector<Complex> solGrAtIp,
+                                                 Vector<Complex> solD1GrAtIp )
+  {
+    ENTER_FCN( "AcouPowerDensityOp::ComputeN1" );
+
+    Double N1 = 0;
+    for (UInt k=0; k<dimensions_; k++)
+      N1 += 0.5 * std::real( std::conj(solGrAtIp[k]) * solD1GrAtIp[k] );
+    return N1;
+  }
+
+
+  template<class TYPE>  
+  Vector<Double> AcouPowerDensityOp<TYPE>::ComputeN2( Vector<TYPE> solGrAtIp,
+                                                      TYPE solD1AtIp )
+  {
+    ENTER_FCN( "AcouPowerDensityOp::ComputeN2" );
+
+    (*error) << "AcouPowerDensityOp::ComputeN2 "
+             << "only defined for TYPE=Complex/Double "; 
+    Error( __FILE__, __LINE__ );
+    return TYPE();
+  }
+
+  template<>
+  Vector<Double> AcouPowerDensityOp<Double>::ComputeN2( Vector<Double> solGrAtIp,
+                                                        Double solD1AtIp )
+  {
+    ENTER_FCN( "AcouPowerDensityOp::ComputeN2" );
+
+    Vector<Double> N2;
+    N2 = solGrAtIp * solD1AtIp;
+    return N2;
+  }
+
+  template<>
+  Vector<Double> AcouPowerDensityOp<Complex>::ComputeN2( Vector<Complex> solGrAtIp,
+                                                         Complex solD1AtIp )
+  {
+    ENTER_FCN( "AcouPowerDensityOp::ComputeN2" );
+
+    Vector<Double> N2;
+    N2.Resize( solGrAtIp.GetSize() );
+
+    for (UInt k=0; k<solGrAtIp.GetSize(); k++) {
+      N2[k] = 0.5 * std::real( std::conj(solGrAtIp[k]) * solD1AtIp );
+    }   
+    return N2;
+  }
+
+//   void AcouPowerDensityOp::CalcSubdomPD(Vector<Double> & elemPD,
+//                                         const StdVector<RegionIdType> & SD,
+//                                         const Double density)
+//   {
+//     ENTER_FCN( "AcouPowerDensityOp::CalcSDEnergy" );
+//     (*error) << "AcouPowerDensityOp::CalcSDElecField: Not implemented yet"; 
+//     Error( __FILE__, __LINE__ );
+//   }
+
+
+
+  // explicit teplate instantiation for gcc compiler
+#ifdef __GNUC__
+template class AcouPowerDensityOp<Double>;
+template class AcouPowerDensityOp<Complex>;
+#endif
+
+  // explicit template instantiation for SGI compiler
+#ifdef __sgi
+#pragma instantiate AcouPowerDensityOp<Double>
+#pragma instantiate AcouPowerDensityOp<Complex>
+#endif
+
+
 } // namespace CoupledField
