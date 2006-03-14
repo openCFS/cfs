adc99b12ef88819cdb67c6265e813eaf0cfdd723
tlahmer <tlahmer@cfs-dev.mdmt.tuwien.ac.at>
2006-03-14 14:25:51 +0000

    tiny changes in output to std::cout for clearer reading

--- libsrc/ParamIdent/leastSquare.cc
+++ libsrc/ParamIdent/leastSquare.cc
@@ -10,7 +10,6 @@ namespace CoupledField
 
   void piezoParamIdent::leastSquare(){ 
     ENTER_FCN("piezoParamIdent::leastSquare");    
-    std::cout<<" Entering least square ..."<<std::endl;
 
     MaterialData * ptMaterial=ptMyPDE_->getPDEMaterialData();   // Pointer to MaterialData
 
@@ -18,21 +17,12 @@ namespace CoupledField
     Vector<Double> freqs5;
     freqs5.Resize(20);
 
-    //         freqs5[0]=2.5e+06;
-    //         freqs5[1]=3.5e+06;
-    //         freqs5[2]=4.5e+06;
-    //         freqs5[3]=5.5e+06;
-    //         freqs5[4]=6.0e+06;
-    //         freqs5[5]=6.2e+06;
-    //         freqs5[6]=6.4e+06;
-    //         freqs5[7]=6.95e+06;
-    //         freqs5[8]=7.1e+06;
-    //         freqs5[9]=7.4e+06;
-    //         freqs5[10]=7.75e+06;
-    //         freqs5[11]=8.0e+06;
-    //  freqs5[12]=9.5e+06;
-    //    freqs5[13]=14.0e+06;
-    if (FALSE){
+   
+    if (TRUE){
+
+      nrMeasuredData=15;
+      freqs5.Resize(15);
+
       freqs5[0]=0.2e+05;
       freqs5[1]=0.35e+05;
       freqs5[2]=0.45e+05;
@@ -73,36 +63,74 @@ namespace CoupledField
       freqs5[19]=2.0e+05;
     } 
     // 100V
-    if (TRUE){
-      nrMeasuredData=15;
-      freqs5.Resize(15);
+    if (FALSE){
+      nrMeasuredData=23;
+      freqs5.Resize(23);
 
-
+      nrMeasuredData=23;
+      freqs5.Resize(23);
       freqs5[0]=0.2e+05;
       freqs5[1]=0.35e+05;
       freqs5[2]=0.45e+05;
       freqs5[3]=0.55e+05;
       freqs5[4]=0.6e+05;
-      freqs5[5]=0.69e+05;
-      freqs5[6]=0.72e+05;
-      freqs5[7]=0.75e+05;
-//       freqs5[8]=0.82e+05;
-//       freqs5[9]=0.78e+05;
-//       freqs5[10]=0.85e+05;
-//       freqs5[11]=0.9e+05;
-//       freqs5[12]=1.0e+05;
-      freqs5[8]=1.1e+05;
-      freqs5[9]=1.2e+05;
-      freqs5[10]=1.3e+05;
-      freqs5[11]=1.4e+05;
-      freqs5[12]=1.6e+05;
-      freqs5[13]=1.8e+05;
-      freqs5[14]=2.0e+05;
-
+      freqs5[5]=0.65e+05;
+      freqs5[6]=0.70e+05;
+      freqs5[7]=0.72e+05;
+      freqs5[9]=0.75e+05;
+      freqs5[10]=0.76e+05;
+      freqs5[11]=0.78e+05;
+      freqs5[12]=0.79e+05;
+      freqs5[13]=0.8e+05;
+      freqs5[14]=1.09e+05;
+      freqs5[15]=1.1e+05;
+      freqs5[16]=1.2e+05;
+      freqs5[17]=1.3e+05;
+      freqs5[18]=1.4e+05;
+      freqs5[19]=1.5e+05;
+      freqs5[20]=1.6e+05;
+      freqs5[21]=1.8e+05;
+      freqs5[22]=2.0e+05;
     }
 
+
+  // 150V
+    if (FALSE){
+      nrMeasuredData=22;
+      freqs5.Resize(22);
+
+
+      freqs5[0]=0.2e+05;
+      freqs5[1]=0.3e+05;
+      freqs5[2]=0.35e+05;
+      freqs5[3]=0.4e+05;
+      freqs5[4]=0.45e+05;
+      freqs5[5]=0.5e+05;
+      freqs5[6]=0.55e+05;
+      freqs5[7]=0.60e+05;
+      freqs5[8]=0.65e+05;
+      freqs5[9]=0.7e+05;
+      freqs5[10]=0.72e+05;
+  //     freqs5[11]=0.74e+05;
+//       freqs5[12]=0.75e+05;
+      //    freqs5[13]=1.1e+05;
+      // freqs5[14]=1.15e+05;
+      freqs5[11]=1.2e+05;
+      freqs5[12]=1.25e+05;
+      freqs5[13]=1.3e+05;
+      freqs5[14]=1.35e+05;
+      freqs5[15]=1.4e+05;
+      freqs5[16]=1.45e+05;
+      freqs5[17]=1.5e+05;
+      freqs5[18]=1.6e+05;
+      freqs5[19]=1.7e+05;
+      freqs5[20]=1.8e+05;
+      freqs5[21]=2.0e+05;
+
+    }
+   
     if(FALSE){
-      Integer numMeasPoints=35;
+      Integer numMeasPoints=30;
       Double fmin=0.2e+5;
       Double fmax=2.0e+5;
       Double fincr=(fmax-fmin)/numMeasPoints;
@@ -128,11 +156,11 @@ namespace CoupledField
     Double lambda=1.0;
     Boolean negFlag=FALSE;      
 
-    std::cout<<"Before read in Measuerment"<<std::endl;
+    //    std::cout<<"Before read in Measuerment"<<std::endl;
   
     readInMeasurement(newFreqs);
 
-    std::cout<<"newFreqs:"<<std::endl;
+    std::cout<<"++ Selected frequencies for fitting:"<<std::endl;
     std::cout<<newFreqs<<std::endl;
 
     calc_measuredCharge(freqs, real, imag, y_hat); // out of new measurements
@@ -143,6 +171,28 @@ namespace CoupledField
       updateComplexMaterialData(parameterC,ptMaterial);
 
       createF(ptMaterial, F_hat, FALSE);
+
+      if ((iterIndex+1)%10==0){
+        Integer nrfreqTemp=100;
+        Vector<Double> freqsTemp = freqs;
+        freqs.Resize(nrfreqTemp);
+        Double startFreqTemp;
+        startFreqTemp=startfreq;
+        Double freqincr=(stopfreq-startfreq)/nrfreqTemp;
+        for(UInt i=0;i<nrfreqTemp;i++){
+          startFreqTemp+=freqincr;
+          freqs[i]=startFreqTemp;
+        }
+        if (impedCurve)
+          impedCurve->close();
+        std::string filename= "imped.dat";
+        impedCurve = new std::ofstream(filename.c_str(),std::basic_ios<char>::out);
+        if (!impedCurve){
+          std::cerr << "\n ImpedanceCurve.dat could not be initialized" << std::endl;
+        }
+        calcImpedanceCurve();
+        freqs = freqsTemp;
+      }
       
       for (UInt i=0;i<nrMeasuredData;i++)
         F_y[i]=F_hat[i]-y_hat[i];
@@ -156,7 +206,7 @@ namespace CoupledField
 
       norm(F_y,normFy0,maxres,y_hat);
       
-      std::cout<<"normF_y0 = " << normFy0<<std::endl;
+      std::cout<<"Residue (||F(p)-y||) = " << normFy0<<std::endl;
       std::cout<< "--------------------------------\n"<<std::endl;
 
       *parLog<<normFy0<<"  ";
@@ -245,8 +295,8 @@ namespace CoupledField
       //     for (UInt par=0;par<nrParameter;par++)
       //       gradP[par]=gradP[par]/scaling[par];
       
-      std::cout<<"gradP: "<<std::endl;
-      std::cout<<gradP<<std::endl;
+      //      std::cout<<"gradP: "<<std::endl;
+      // std::cout<<gradP<<std::endl;
 
       indPar=0;
       lambda=100*lambda;
@@ -266,7 +316,7 @@ namespace CoupledField
       for (UInt par=0;par<nrParameter;par++)
         if (parameter[par]<0.0&&par!=6){
           //  parameter[par]=parameter_old[par];
-          std::cout<<"parameter( " << par << " ) was negative " <<std::endl;
+          std::cout<<"parameter( " << par << " ) is negative " <<std::endl;
           negFlag=TRUE;
         }  
 
@@ -280,7 +330,7 @@ namespace CoupledField
 
       while (normFy1>normFy0||negFlag==TRUE){
         if (negFlag==TRUE)
-          std::cout<< "reduce lambda due to negative parameters"<<std::endl;
+          std::cout<< "reduce lambda due to a negative parameter or a nondecreasing residue"<<std::endl;
         parameter=parameter_old;
         parameterC=parameter_oldC;
         lambda=0.1*lambda;
@@ -318,7 +368,7 @@ namespace CoupledField
       normFy0=normFy1;
       
 
-      std::cout<<"parameter:"<<std::endl;
+      std::cout<<"New parameters:"<<std::endl;
 
       //std::cout<<parameter<<std::endl;
       for (UInt i=0; i<parameter.GetSize(); i++){
