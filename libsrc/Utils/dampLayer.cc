12a837dd130b959bf275b5ca873a892ad9b6755e
Manfred Kaltenbacher <manfred.kaltenbacher@tuwien.ac.at>
2006-07-17 23:30:15 +0000

    computation of damping coefficient for sponge layer

---
+++ libsrc/Utils/dampLayer.cc
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <fstream>
+#include "Domain/domain.hh"
+#include "Domain/grid.hh"
+
+#include "dampLayer.hh"
+
+namespace CoupledField
+{
+
+  DampLayer::DampLayer(std::string type) {
+    ENTER_FCN( "DampLayer::DampLayer" );
+
+    //check correct damping type
+    if ( type == "linear" ) {
+      dampFncType_ = type;
+    }
+    else if ( type == "quadDist" ) {
+      dampFncType_ = type;
+    }
+    else if ( type == "inverseDist" ) {
+      dampFncType_ = type;
+    }
+    else if ( type == "exponential" ) {
+      dampFncType_ = type;
+    }
+    else {
+      Error("Damping type for Sponge Layer not known", __FILE__, __LINE__);
+    }
+  }
+
+
+ 
+  DampLayer::~DampLayer()
+  {
+    ENTER_FCN( "DampLayer::~DampLayer" );
+  }
+
+
+  void DampLayer::CalcDampFactor( Double& factor, EntityIterator& ent)
+  {
+    ENTER_FCN( "DampLayer ::ComputeDampFactor"); 
+
+    domain->GetGrid()->GetElemNodesCoord( ptCoord_, 
+                                          ent.GetElem()->connect,
+                                          false );
+
+    UInt numVals = ptCoord_.GetSizeRow();
+    Vector<Double> pos;
+    pos.Resize(numVals);
+    pos.Init(0);
+    for (UInt i=0; i<ptCoord_.GetSizeRow(); i++) {
+      for (UInt j=0; j<ptCoord_.GetSizeCol(); j++) {
+	pos[i] += ptCoord_[i][j];
+      }
+      pos[i] /= (Double) ptCoord_.GetSizeCol();
+    }
+
+    //    std::cout << "pos:\n" << pos << std::endl;
+
+    factor = EvalDampFnc(pos);
+    //    std::cout << "DampFactor: " << factor << "\n" << std::endl;
+  }
+
+
+  Double DampLayer::EvalDampFnc(Vector<Double>& pos)
+  {
+    ENTER_FCN( "DampLayer :: EvalDampFnc"); 
+
+    Double factor;
+
+    if ( dampFncType_ == "linear" ) {
+      factor = dampingFactor_;
+    }
+
+    else if ( dampFncType_ == "quadDist" ) {
+      factor = dampingFactor_;
+    }
+
+    else if ( dampFncType_  == "inverseDist" ) {
+      Error("PML damping inverseDist divides by factor smaller 1E-12",
+	    __FILE__,__LINE__);
+    }
+    else if ( dampFncType_ == "exponential" ) {
+      UInt dim = pos.GetSize(); 
+      Vector<Double> diff(dim);
+
+      diff =  pos - midPoint_;
+      Double dist = diff.NormL2() - startRadius_;
+
+      //      std::cout << "dist= " << dist << std::endl;
+
+      if ( dist > 0 ) {
+	factor = std::exp( dampingFactor_*dist / layerThickness_ );
+	if ( factor > dampingFactorMax_ ) 
+	  factor = dampingFactorMax_;
+      }
+      else {
+	factor = 1.0;
+      }
+    }
+
+    return factor;
+  }
+
+
+  void DampLayer::SetDampingParams(Vector<Double> point, 
+				   Double& dampFactor, 
+				   Double& dampFactorMax, 
+				   Double& startRadius, 
+				   Double& endRadius) {
+    ENTER_FCN( "DampLayer::DampingParams"); 
+
+    // center point
+    midPoint_ = point;
+
+    startRadius_ = startRadius;
+    endRadius_   = endRadius;
+
+    dampingFactor_   = dampFactor;
+    dampingFactorMax_ = dampFactorMax;
+    layerThickness_ =  endRadius - startRadius;
+    if ( layerThickness_ < 0 ) {
+      Error("Start-Radius has to be larger then End-Radius",__FILE__,__LINE__);
+    }
+  }
+
+
+} // end namespace CoupledField
