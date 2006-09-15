33dc6e6987571db67ccdc805fc7be327d4e14d6c
Nina Bretz <nbretz@svn>
2006-09-15 15:54:03 +0000

    Added two new form classes. These are necessary if the acoustic is coupled with the bubble computation. They belong to the new model.
    Attention: They are not working, yet.

---
+++ libsrc/Forms/bubbleDampInt.hh
@@ -0,0 +1,75 @@
+#ifndef FILE_BUBBLEDAMP_INT
+#define FILE_BUBBLEDAMP_INT
+
+#include "baseForm.hh"
+
+namespace CoupledField
+{
+
+  //! Class for calculation  element mass matrix
+  class BubbleDampInt : public BaseForm
+  {
+  public:
+
+    // Constructor
+    BubbleDampInt(const Double aDensity,
+		  Double densityforbubble,
+		  Double sonicVel, 
+		  Double viscosity,
+		  Double bubbleDensity,
+		  Double frequency,
+		  bool axi=false );
+
+    // Destructor
+    virtual ~BubbleDampInt();
+
+    // Calculation of element matrix
+    void CalcElementMatrix( Matrix<Double>& elemMat,
+                            EntityIterator& ent1, 
+                            EntityIterator& ent2 );
+      
+    //! set additional multiplicative factor of mass matrix
+    void SetFactor(Double aFactor) 
+    {factor_ = aFactor;};
+   
+    //! Set element Numbers and radius values
+    void SetValues( StdVector<UInt> elemNumbers, Vector<Double> * radius,
+		    Vector<Double> * radiusDeriv );
+ 
+
+  protected:
+    
+
+  
+  private:
+
+    Double density_;          //!< multiplicative value for mass integrator
+    Double factor_;           //!< yet another multiplicative value for mass integrator
+ 
+    //! vector for element number
+    StdVector<UInt> elemNumbers_;
+
+    //! pointer to vector with element radius values
+    Vector<Double> * radius_;
+
+    //! pointer to vector with element radius values derivatives
+    Vector<Double> * radiusDeriv_;
+    
+    //! map from global element number to index position in elemNumbers/radius
+    std::map<UInt, UInt> indexMap_;
+
+    //! parameters to compute bubble-dependend coefficients
+    Double sonicVel_, densityforbubble_, viscosity_, bubbleDensity_;
+
+    //! parameters to determine when bubbleterms should be added
+    Double frequency_;
+  
+  };
+
+
+
+
+
+}
+
+#endif // FILE_MASSINT
