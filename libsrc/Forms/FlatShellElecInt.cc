8e27b053735e507ee9a58ab3f30573efdb3df121
Gerhard Link <glink@svn>
2006-12-07 09:55:25 +0000

    reorderd some mathematical expression such that overloaded operators can be found.

--- libsrc/Forms/FlatShellElecInt.cc
+++ libsrc/Forms/FlatShellElecInt.cc
@@ -36,7 +36,7 @@ namespace CoupledField {
     
     Omega = ptelem->CalcVolume(ShellCoord,FALSE);    
     //See equation 4.65 Pietfort
-    elemMat = eMat * (- Omega);
+    elemMat = (- Omega)*eMat ;
 
     // Do the back-Rotation and return
     // Back Rotation is not needed, as this matrix does not depend on 
