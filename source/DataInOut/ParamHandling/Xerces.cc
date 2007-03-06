3c4b28d22c92a2461126c8c35628267a68e3f46d
Andreas Hauck <mail@andreas-hauck.com>
2007-03-06 12:06:37 +0000

    The methods of boosts' filesystem library expect as arguments normall the canonical (platform-independent) representation of files and paths.
    If we want have a native (platform-dependent) representation, we have to create a new boost::filsystem::path object, with the additional argument 'native' to indicate, that this is the native represenatioon.
    
    Therefore the call to the method boost::filesystem::exixts within the constructor of Xerces now takes as argument a previously created path-object. 
    
    This fixes the problem, that previously an error was thrown, if the schema or the xml file were located in a directory containing the literals 'CFS++', as this represents no valid canonical path, but instead a native representation.

--- source/DataInOut/ParamHandling/Xerces.cc
+++ source/DataInOut/ParamHandling/Xerces.cc
@@ -13,8 +13,10 @@
 #include "DataInOut/ParamHandling/ParamNode.hh"
 #include "DataInOut/CommandLine/BaseCommandLineHandler.hh"
 
+
 // we want to use the Xerces-C++ namespace
 using namespace xercesc;
+namespace fs = boost::filesystem;
 
 extern BaseCommandLineHandler* commandLine;
 
@@ -26,10 +28,15 @@ namespace CoupledField
     parser_  = NULL;
     root_    = NULL;
  
-    if(!boost::filesystem::exists(file)) 
+    // create canonical path from native-representation of the 
+    // file and the schema path
+    fs::path filePath = fs::path( file, fs::native );
+    fs::path schemaPath = fs::path( schema, fs::native );
+
+    if(!fs::exists(filePath)) 
         EXCEPTION("xml file " << file << " doesn't exist"); 
 
-    if(!boost::filesystem::exists(schema)) 
+    if(!fs::exists(schemaPath)) 
         EXCEPTION("schema file " << file << " doesn't exist");
     
     this->file_   = file;
