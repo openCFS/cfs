0eda7bf977f9fbdfe2f60d2b258f3d4a4b624979
Simon Triebenbacher <strieben@svn>
2007-05-11 14:05:03 +0000

    - Added a starting point for the CFS converter tool.

---
+++ source/cfstool/cfstool.cc
@@ -0,0 +1,12 @@
+// -*- mode: c++; coding: utf-8; indent-tabs-mode: nil; -*-
+// kate: space-indent on; indent-width 2; encoding utf-8;
+// kate: auto-brackets on; mixedindent off; indent-mode cstyle;
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+  std::cout << "CFS TOOL 1.0" << std::endl;
+  
+  return 0;
+}
