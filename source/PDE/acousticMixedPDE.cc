16d596cb70df51c16f58b06b5a29a2708c7ed1d7
Manfred Kaltenbacher <manfred.kaltenbacher@tuwien.ac.at>
2007-07-17 14:05:47 +0000

    Taylor Hood for acoustics in mixed formulation

---
+++ source/PDE/acousticMixedPDE.cc
@@ -0,0 +1,326 @@
+#include "acousticMixedPDE.hh"
+#include <sstream>
+#include <iomanip>
+
+#include "Forms/forms_header.hh"
+#include "Forms/linElastInt.hh"
+#include "Forms/massInt.hh"
+#include "Forms/linPressureInt.hh"
+#include "Driver/assemble.hh"
+#include "newmark.hh"
+#include "DataInOut/ParamHandling/ParamNode.hh"
+#include "CoupledPDE/pdecoupling.hh"
+#include "Domain/domain.hh"
+#include "Utils/coordSystem.hh"
+#include "Driver/assemble.hh"
+#include "trapezoidal.hh"
+#include "StdPDE.hh"
+#include "Driver/stdSolveStep.hh"
+
+#ifdef USE_SCRIPTING
+#include "DataInOut/Scripting/cfsmessenger.hh" 
+#endif
+
+namespace CoupledField
+{
+
+  AcousticMixedPDE::AcousticMixedPDE( Grid* aGrid, ParamNode* paramNode )
+    :SinglePDE(aGrid, paramNode )
+
+  {
+    ENTER_FCN( "AcousticMixedPDE::AcousticMixedPDE" );
+
+    pdename_          = "acousticMixed";
+    pdematerialclass_ = FLUID;
+    maxTimeDerivOrder_ = 1;
+
+    // ****************************
+    // DETERMINE GEOMETRY
+    // ****************************
+
+    // Get problem geometry and PDE subtype
+    myParam_->Get( "subType", subType_ );
+    std::string probGeo;
+    param->Get("domain")->Get("geometryType", probGeo );
+
+
+    // Set number of degrees of freedom and
+    // ensure that subtype fits to problem geometry
+    UInt dofspernode = 0;
+    if ( subType_ == "3d" && probGeo == "3d" ) {
+      dofspernode = 4;
+      Info->PrintF("", "=== 3D PROBLEM\n");
+    }
+    else if ( subType_ == "axi" && probGeo == "axi" ) {
+      isaxi_ = true;
+      dofspernode = 3;
+      Info->PrintF("", "=== AXISYSMMETRIC PROBLEM\n");
+    }
+    else if ( subType_ == "plane" && probGeo == "plane" ) {
+      dofspernode = 3;
+      Info->PrintF("", "=== PLANE PROBLEM\n");
+    }
+    else
+      {
+        std::string errmsg = "Subtype '" + subType_;
+        errmsg += "' of PDE '" + pdename_ + "' does not fit to problem ";
+        errmsg += "geometry '" + probGeo + "'\n";
+        Info->Error( errmsg, __FILE__, __LINE__ );
+      }
+
+
+    // timestepping formulation
+    std::string str = "";
+    myParam_->Get( "timeSteppingFormulation", str,  false );
+    //myParam_->Get( "timeSteppingFormulation", str,  true );
+    //str = "effMassMatrix";
+    if ( str == "effMassMatrix" ) {
+      effectiveMass_ = true;
+      Info->PrintF( pdename_, 
+                    "      * effective mass matrix timestepping\n");
+    } 
+    else if ( str == "diagMassMatrix" ) {
+      diagMass_      = true;
+      effectiveMass_ = true;
+      Info->PrintF( pdename_, 
+                    "      * diagonal mass matrix in explicit timestepping\n");
+    } 
+    else {
+      effectiveMass_ = false;
+      Info->PrintF( pdename_, 
+                    "      * effective stiffness matrix timestepping\n");
+    }
+   }
+
+  AcousticMixedPDE::~AcousticMixedPDE()
+  {
+    ENTER_FCN( "AcousticMixedPDE::~AcousticMixedPDE" );
+  }
+
+  void AcousticMixedPDE::DefineIntegrators()
+  {
+    ENTER_FCN( "AcousticMixedPDE::DefineIntegerators" );
+
+    // help variables for parameter checking
+    StdVector<std::string> keyVec;
+    StdVector<std::string> attrVec;
+    StdVector<std::string> valVec;
+    
+    Double density, bulkModulus, c0;
+
+    for (UInt actSD = 0; actSD < subdoms_.GetSize(); actSD++) {
+
+      // create new entity list
+      shared_ptr<ElemList> actSDList( new ElemList(ptgrid_ ) );
+      actSDList->SetRegion( subdoms_[actSD] );
+      
+      BaseMaterial * actMat = materials_[subdoms_[actSD]];
+      actMat->GetScalar(density, DENSITY,REAL);
+      actMat->GetScalar(bulkModulus, ACOU_BULK_MODULUS,REAL);
+      c0 = sqrt(bulkModulus/density);
+      
+      Info->PrintF( pdename_, "density = %e\n", density);
+      Info->PrintF( pdename_, "bulk modulus = %e\n", bulkModulus);
+      
+      // ==============  add stiffness ======================================
+      // ==============  add KVP ======================================
+      BaseForm *bilinearStiff_KVP = new StiffMixedInt_KVP( 1.0, isaxi_);
+      BiLinFormContext * stiffContext_KVP =
+        new BiLinFormContext(bilinearStiff_KVP, STIFFNESS );
+
+      stiffContext_KVP->SetPtPdes(this, this);
+      stiffContext_KVP->SetResults( results_[0], results_[1],
+				    actSDList, actSDList );
+
+      assemble_->AddBiLinearForm( stiffContext_KVP );
+
+       // ==============  add KPV ======================================
+      BaseForm *bilinearStiff_KPV = new StiffMixedInt_KPV( 1.0, isaxi_);
+      BiLinFormContext * stiffContext_KPV =
+        new BiLinFormContext(bilinearStiff_KPV, STIFFNESS );
+
+      stiffContext_KPV->SetPtPdes(this, this);
+      stiffContext_KPV->SetResults( results_[1], results_[0],
+				actSDList, actSDList );
+
+      assemble_->AddBiLinearForm( stiffContext_KPV );
+
+      //==============  add MPP ======================================
+      Double coeffMass = 1.0;
+      BaseForm * bilinearMass_PP = new MassMixedInt_PP(coeffMass, isaxi_);
+
+      BiLinFormContext * massContext_PP =  
+	new BiLinFormContext( bilinearMass_PP, MASS );
+
+      massContext_PP->SetPtPdes(this, this);
+      massContext_PP->SetResults( results_[0], results_[0],
+			       actSDList, actSDList );
+      assemble_->AddBiLinearForm( massContext_PP );
+
+
+      //==============  add MVV ======================================	
+      BaseForm * bilinearMass_VV = new MassMixedInt_VV(coeffMass, isaxi_);
+
+      BiLinFormContext * massContext_VV =  
+	new BiLinFormContext( bilinearMass_VV, MASS );
+
+      massContext_VV->SetPtPdes(this, this);
+      massContext_VV->SetResults( results_[1], results_[1],
+			       actSDList, actSDList );
+      assemble_->AddBiLinearForm( massContext_VV );
+
+      // Give result to equation numbering class
+      eqnMap_->AddResult( *results_[0], actSDList );
+      eqnMap_->AddResult( *results_[1], actSDList );
+    }
+  }
+
+
+  void AcousticMixedPDE::DefineSolveStep()
+  {
+    ENTER_FCN( "AcousticMixedPDE::DefineSolveStep" );
+
+    solveStep_ = new  StdSolveStep(*this);
+  }
+
+
+  // ======================================================
+  // TIME STEPPING SECTION
+  // ======================================================
+
+
+  void AcousticMixedPDE :: InitTimeStepping()
+  {
+    ENTER_FCN( "AcousticMixedPDE::InitTimeStepping" );
+    if ( effectiveMass_ == true ) {
+      TS_alg_ = new TrapezoidalEffMass( algsys_ );
+    }
+    else {
+      TS_alg_ = new Trapezoidal( algsys_ );
+    }
+  }
+
+
+
+  // ======================================================
+  // POSTPROCESSING SECTION
+  // ======================================================
+
+
+  // ***********************************************************************
+  //   Obtain information on desired output quantities from parameter file
+  // ***********************************************************************
+  void AcousticMixedPDE::DefineAvailResults() {
+    ENTER_FCN( "AcousticMixedPDE::DefineAvailResults" );
+
+    // =====================================================================
+    // set solution information
+    // =====================================================================
+
+    // === PRESSURE ===
+    shared_ptr<ResultInfo> res1(new ResultInfo);
+    res1->resultType = ACOU_PRESSURE;
+    res1->dofNames = "";
+    res1->unit = "Pa";
+    res1->entryType = ResultInfo::SCALAR;
+
+    // check if problem is lagrange or legendre
+    std::string approxType = myParam_->Get("type")->AsString();
+    if ( approxType == "lagrange" ) {
+      shared_ptr<AnsatzFct> fct(new LagrangeFct);
+      res1->fctType = fct;
+      res1->definedOn = ResultInfo::NODE;
+    } 
+    else if( approxType == "taylorHood" ) {
+      std::cerr << "Using taylorHood!\n";
+      // define Legendre type
+      shared_ptr<LegendreFct> fct(new LegendreFct);
+      fct->SetIsoOrder( 1 );
+      res1->fctType = fct;
+      res1->definedOn = ResultInfo::PFEM;
+    } else {
+     EXCEPTION( "approximation type '" << approxType << "' not allowd");
+    }
+      
+    results_.Push_back( res1 );
+    availResults_.insert( res1 );
+
+    // === VELOCITY ===
+    shared_ptr<ResultInfo> res2(new ResultInfo); res2->resultType = ACOU_VELOCITY;
+    if( subType_ == "3d" ) {
+      res2->dofNames = "x", "y", "z";
+    } 
+    else if ( subType_ == "axi" ) {
+      res2->dofNames = "r", "z";
+    } 
+    else if( subType_ == "plane") {
+      res2->dofNames = "x", "y";
+    } 
+    else {
+      EXCEPTION( "AcousticMixedPDE: subType '" << subType_ 
+                   << "' not known'" );
+    }
+
+    res2->unit = "m/s";
+    res2->entryType = ResultInfo::VECTOR;
+
+    // check if problem is lagrange or legendre
+    std::string approxTypeV = myParam_->Get("type")->AsString();
+    if ( approxTypeV == "lagrange" ) {
+      shared_ptr<AnsatzFct> fctV(new LagrangeFct);
+      res2->fctType = fctV;
+      res2->definedOn = ResultInfo::NODE;
+    } else if( approxType == "taylorHood" ) {
+     std::cerr << "Using taylorHood!\n";
+      // define Legendre type
+      shared_ptr<LegendreFct> fctV(new LegendreFct);
+      fctV->SetIsoOrder( 2 );
+      res2->fctType = fctV;
+      res2->definedOn = ResultInfo::PFEM;
+    } else {
+       EXCEPTION( "approximation type '" << approxType << "' not allowd");
+    }
+
+    results_.Push_back( res2 );
+    availResults_.insert( res2 );
+
+  }
+
+
+  void  AcousticMixedPDE::CalcResults( shared_ptr<BaseResult> result ) {
+    ENTER_FCN( " AcousticMixedPDE::CalcResults" );
+    
+    switch (result->GetResultInfo()->resultType ) {
+      
+    case ACOU_PRESSURE:
+      if( isComplex_ ) {
+        ExtractResult<Complex>( result, sol_ );
+      } else {
+        ExtractResult<Double>( result, sol_ );
+      }
+      break;
+
+    case ACOU_VELOCITY:
+      if( isComplex_ ) {
+        ExtractResult<Complex>( result, sol_ );
+      } else {
+        ExtractResult<Double>( result, sol_ );
+      }
+      break;
+
+    default:
+      Warning( "Resulttype not computable by mechanic PDE",
+               __FILE__, __LINE__ );
+    }
+  }
+
+  // ************************************************************
+  //   PostProcess
+  // ************************************************************
+
+  void AcousticMixedPDE::PostProcess() {
+    ENTER_FCN( "AcousticMixedPDE::PostProcess" );
+  }
+} // end namespace CoupledField
+
+
---
+++ source/PDE/acousticMixedPDE.hh
@@ -0,0 +1,78 @@
+#ifndef FILE_NEWBASEACOUSTICMIXEDPDE
+#define FILE_NEWBASEACOUSTICMIXEDPDE
+
+#include "SinglePDE.hh"
+ 
+namespace CoupledField
+{
+
+  //! Forward class declarations
+  class BaseForm;
+
+
+  //! Class for acoustic PDE using velocity and pressure
+  class AcousticMixedPDE: public SinglePDE
+  {
+
+  public:
+
+    //!  Constructor. here we read integration parameters
+    /*!
+      \param aGrid pointer to grid
+      \param aOutFile  pointer to class WriteResults. output data.
+      \param aTimeFunc pointer to class TimeFunc
+    */
+//     Grid *aGrid; //pointer to grid
+//     TimeFunc *aTimeFunc; //pointer to class WriteResults. output data.
+//     WriteResults *aOutFile; //pointer to class TimeFunc
+
+
+    AcousticMixedPDE(Grid* aGrid, ParamNode* paramNode );
+
+    //!  Deconstructor
+    virtual ~AcousticMixedPDE();
+
+   //! Define availabe result types
+    void DefineAvailResults();
+
+    //! define all (bilinearform) integrators needed for this pde
+    virtual void DefineIntegrators( );
+
+    //! define the SoltionStep-Driver
+    virtual void DefineSolveStep();
+
+    // ======================================================
+    // POSTPROC SECTION
+    // ======================================================
+
+    //! Calculate result for given result class
+    void CalcResults( shared_ptr<BaseResult> results );
+
+   //! do PostProcessing step
+    virtual void PostProcess( );
+
+    //! initalize PDE coupling
+    virtual void InitCoupling(PDECoupling * Coupling) {;};
+
+    //! Fill in input coupling terms
+    virtual void CalcInputCoupling() {;};
+  
+    //! calculate coupling terms
+    virtual void CalcOutputCoupling() {;};
+
+  protected:
+ 
+    //! Init the time stepping
+    void InitTimeStepping();
+
+  private:
+
+    //! true, if solution should be written to result file
+    bool saveSolVel_;
+    bool saveSolPres_;
+
+  };
+
+} // end of namespace
+#endif
+
