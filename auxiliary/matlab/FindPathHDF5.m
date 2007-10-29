242d59ad5eb20a5c76621cdaed2cdaed27e5a4cb
Jens Grabinger <jens.grabinger@simetris.de>
2007-10-29 17:41:36 +0000

    - updated the Time2Freq Matlab script to support the current HDF5 file format - grid.cc: fixed a bug that prevented building on x86 machines

--- auxiliary/matlab/FindPathHDF5.m
+++ auxiliary/matlab/FindPathHDF5.m
@@ -1,7 +1,10 @@
 % ==============================================================
 %
+%    FindPathHDF5
+%
+%
 %    GENERAL
-%    Time data filtering tool for CFS++ HDF5 data.
+%    Searches a HDF5 file for the path to a given result.
 %
 %    INPUT/S
 %      toplevel  - Top level node (/) of a HDF5 file
@@ -11,21 +14,44 @@
 %      region    - on what region
 %        
 %    OUTPUT/S
-%      found     - found the dataset or any path between...
-%      stepname  - 
-%      msgroup   - node of the multistep group
+%      found     - integer that indicates at which node level the search
+%                  was stopped.
+%
+%                  toplevel                             found=0
+%                  |
+%                  `-Results                            found=1
+%                    |
+%                    `-Mesh                             found=2
+%                      |
+%                      `-MultiStep_<n>                  found=3
+%                        |
+%                        `-Step_<t>                     found=4
+%                          |
+%                          `-<quantity>                 found=5
+%                            |
+%                            `-<region>                 found=6
+%                              |
+%                              `-[ Nodes | Elements ]   found=7
+%                                |
+%                                `-Real                 found=8
+%
+%      resgroup  - group node where the search ended (according to output
+%                  parameter 'found', if found >= 3). Does NOT point to
+%                  dataset 'Real' even if found == 8.
+%      msgroup   - node of the multistep group (valid if found >= 3)
 %
 %      
 %    ABOUT
 %
 %      -Created:     Jun 2007
-%      -Last update: Jun 2007
-%      -Revision:    0.1
-%      -Author:      Simon Triebenbacher
+%      -Last update: 26 Oct 2007
+%      -Revision:    0.2
+%      -Authors:     Simon Triebenbacher, Jens Grabinger
+%
 % ==============================================================
 
 
-function [found stepname msgroup] =  FindPathHDF5(toplevel, multistep, step, quantity, region)
+function [found resgroup restype msgroup] =  FindPathHDF5(toplevel, multistep, step, quantity, region)
 
 found = 0;
 
@@ -34,7 +60,7 @@ for group=1:number_of_groups
   actgroup = toplevel.Groups(group);
   group_name = actgroup.Name;
 
-  if strcmp(group_name, '/Data')
+  if strcmp(group_name, '/Results')
 %    sprintf('We have found the data group')
     ndatagroups = length(actgroup.Groups);
 
@@ -43,7 +69,7 @@ for group=1:number_of_groups
     for datagroup=1:ndatagroups
       actgroup2 = actgroup.Groups(datagroup);
       group_name = actgroup2.Name;
-      if strcmp(group_name, '/Data/Volume')
+      if strcmp(group_name, '/Results/Mesh')
 %        sprintf('We have found the volume data group')
         nvoldatagroups = length(actgroup2.Groups);
 
@@ -53,7 +79,7 @@ for group=1:number_of_groups
           actgroup3 = actgroup2.Groups(voldatagroup);
           group_name = actgroup3.Name;
 
-          cmpstr = sprintf('/Data/Volume/Multistep_%d', multistep);
+          cmpstr = sprintf('/Results/Mesh/MultiStep_%d', multistep);
           if strcmp(group_name, cmpstr)
 %            sprintf('We have found the multistep %d group', multistep)
             basepath = cmpstr;
@@ -61,7 +87,7 @@ for group=1:number_of_groups
             break;
           end
 
-          cmpstr = sprintf('/Data/Volume/Multistep %d', multistep);
+          cmpstr = sprintf('/Results/Mesh/MultiStep %d', multistep);
           if strcmp(group_name, cmpstr)
 %            sprintf('We have found the multistep %d group', multistep)
             basepath = cmpstr;
@@ -76,7 +102,23 @@ for group=1:number_of_groups
   end
 end
 
+if found < 3
+  return;
+end
 msgroup = actgroup3;
+resgroup = msgroup;
+
+%% commented out, because we look for the 'Real' dataset only
+%cmpstr = sprintf('%s/AnalysisType', basepath);
+%nmstepattrs = length(actgroup3.Attributes);
+%for mstepattr=1:nmstepattrs
+%  actattr = actgroup3.Attributes(mstepattr);
+%  attr_name = actattr.Name;
+%  if strcmp(attrname, cmpstr)
+%    ana_type = hdf5read(actattr.Filename, attr_name);
+%    break;
+%  end
+%end
 
 nmstepgroups = length(actgroup3.Groups);
 for mstepgroup=1:nmstepgroups
@@ -101,12 +143,17 @@ for mstepgroup=1:nmstepgroups
 
 end
 
+if found < 4
+  return;
+end
+resgroup = actgroup;
+
+cmpstr = sprintf('%s/%s/%s', basepath, stepname, quantity);
 nstepgroups = length(actgroup.Groups);
 for stepgroup=1:nstepgroups
   actgroup2 = actgroup.Groups(stepgroup);
   group_name = actgroup2.Name;
 
-  cmpstr = sprintf('%s/%s/%s', basepath, stepname, quantity);
   if strcmp(group_name, cmpstr)
 %    sprintf('We have found the quantity %s group', quantity)
     found = 5;
@@ -115,17 +162,111 @@ for stepgroup=1:nstepgroups
 
 end
 
+if found < 5
+  return;
+end
+resgroup = actgroup2;
+
+cmpstr = sprintf('%s/%s/%s/%s', basepath, stepname, quantity, region);
+nqgroups = length(actgroup2.Groups);
+for qgroup=1:nqgroups
+  actgroup = actgroup2.Groups(qgroup);
+  group_name = actgroup.Name;
+
+  if strcmp(group_name, cmpstr)
+%    sprintf('We have found the region group %s', region)
+    found = 6;
+    break;
+  end
+end
+
+if found < 6
+  return;
+end
+resgroup = actgroup;
+
+restype = 0;
+cmpstr = sprintf('%s/ResultDescription', basepath);
+nmstepgroups = length(msgroup.Groups);
+for mstepgroup=1:nmstepgroups
+  actgroup3 = msgroup.Groups(mstepgroup);
+  group_name = actgroup3.Name;
+
+  if strcmp(group_name, cmpstr)
+%    sprintf('We have found the ResultDescription group')
+    cmpstr = sprintf('%s/%s', group_name, quantity);
+
+    nrdgroups = length(actgroup3.Groups);
+    for rdgroup=1:nrdgroups
+      actrdgroup = actgroup3.Groups(rdgroup);
+      group_name = actrdgroup.Name;
+
+      if strcmp(group_name, cmpstr)
+%        sprintf('We have found the ResultDescription/%s group', quantity)
+        cmpstr = sprintf('%s/DefinedOn', group_name);
+
+        ndatasets = length(actrdgroup.Datasets);
+        for dataset=1:ndatasets
+          ds = actrdgroup.Datasets(dataset);
+          ds_name = ds.Name;
+
+          if strcmp(ds_name, cmpstr)
+            restype = hdf5read(ds.Filename, ds_name);
+            break;
+          end
+        end
+
+        break;
+      end
+    end
+
+    break;
+  end
+end
+
+switch restype
+case 0 % not found
+%  errorstr = sprintf('result type of quantity %s not found.', quantity);
+%  error(errorstr);
+  return;
+case 1 % nodes
+  path_suffix = 'Nodes';
+case 4 % elements
+  path_suffix = 'Elements';
+otherwise
+  errorstr = sprintf('result type %d not supported.', restype);
+  error(errorstr);
+  return;
+end
+
+cmpstr = sprintf('%s/%s/%s/%s/%s', basepath, stepname, quantity, region, path_suffix);
+nreggroups = length(actgroup.Groups);
+for reggroup=1:nreggroups
+  actgroup2 = actgroup.Groups(reggroup);
+  group_name = actgroup2.Name;
+
+  if strcmp(group_name, cmpstr)
+%    sprintf('We have found the %s group', path_suffix)
+    found = 7;
+    break;
+  end
+end
+
+if found < 7
+  return;
+end
+resgroup = actgroup2;
+
 ndatasets = length(actgroup2.Datasets);
 for dataset=1:ndatasets
   ds = actgroup2.Datasets(dataset);
   ds_name = ds.Name;
 
-  cmpstr = sprintf('%s/%s/%s/%s', basepath, stepname, quantity, region);
+  cmpstr = sprintf('%s/%s/%s/%s/%s/%s', basepath, stepname, quantity, region, path_suffix, 'Real');
   if strcmp(ds_name, cmpstr)
-%    sprintf('We have found the dataset %s', region)
-    found = 6;
+%    sprintf('We have found the dataset')
+    found = 8;
     break;
   end
 
 end
-
---
+++ auxiliary/matlab/exec.m
@@ -0,0 +1,35 @@
+% ==============================================================
+%
+%    exec
+%
+%
+%    GENERAL
+%    Executes a shell command like MATLAB's system function, but uses a
+%    clean LD_LIBRARY_PATH.
+%
+%    INPUT/S
+%      cmd       - shell command to be executed
+%        
+%    OUTPUT/S
+%      status    - the shell's exit status
+%      
+%    ABOUT
+%
+%      -Created:     29 Oct 2007
+%      -Revision:    0.1
+%      -Author:      Jens Grabinger
+%
+% ==============================================================
+
+function status = exec(cmd)
+
+tmpfile = sprintf('.exec%d.sh', ceil(666*rand));
+
+fid = fopen(tmpfile, 'w');
+fprintf(fid, '#!/bin/sh\nunset LD_LIBRARY_PATH\n');
+fprintf(fid, 'source /home/data/libraries/CFSDEPS/import_cfsdeps.sh\n');
+fprintf(fid, '%s\n', cmd);
+
+status = system(sprintf('%s %s', getenv('SHELL'), tmpfile));
+
+delete(tmpfile);
