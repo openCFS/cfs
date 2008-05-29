e74af531e089f0258d0e6e4545338296b4a302d8
Simon Triebenbacher <strieben@svn>
2008-05-29 07:53:21 +0000

    - cplreader now uses the C++ HDF5 API for writing out data. - The old way of writing HDF5 files (using command line tools)  still exists in this revision but will be removed in the next revision.

--- source/cplreader/MpCCIexch.hh
+++ source/cplreader/MpCCIexch.hh
@@ -1,11 +1,17 @@
 #ifndef FILE_MPCCIEXCH_2006
 #define FILE_MPCCIEXCH_2006
 
+#include "H5Cpp.h"
+
 #include <cplreaderdefs.hh>
 #include "filereader.hh"
 
 namespace CoupledField
 {
+
+//! define CFS-HDF5 file format version
+#define CFS_HDF5_FORMAT_MAJOR 0
+#define CFS_HDF5_FORMAT_MINOR 9
 
   //! Class for mesh coupling
   /*! 
@@ -51,6 +57,30 @@ namespace CoupledField
 
     int ElemTypes2MpCCI(FEType et);
 
+    // HDF5 specific member functions
+    void CheckOpenObjects();
+    void WriteFileInfoHeader();
+    void InitHDF5();
+    void WriteRegion(const H5::Group& meshGroup,
+                     const std::vector<UInt>& nodes,
+                     const std::vector<UInt>& elems,
+                     const UInt dim,
+                     const std::string name);
+    void InitResultsGroup();
+    void WriteResultDescriptions(UInt numSteps,
+                                 const std::vector<std::string>& outputFields,
+                                 const std::vector<UInt> stepNumbers,
+                                 const std::vector<Double> stepValues,
+                                 const std::vector<std::string> regions);
+    void WriteResults( H5::Group& resultGroup,
+                       std::vector<Double>& resultVals,
+                       const UInt numDOFs,
+                       const bool isImag );
+    void CreateExternalFile(UInt timeStep);
+    
+
+
+
     FileReader *  ptFileReader_;
     std::map<UInt, std::vector<Double> > NodalCoords_;
     std::map<UInt, std::vector<UInt> > Topology_;
@@ -59,6 +89,34 @@ namespace CoupledField
     std::map<UInt, std::vector<UInt> > elemTypes_;
 
     std::map<UInt, std::map<UInt, UInt> > nodesInPartition_;
+
+    // HDF5 specific members
+    //! Dataset property list (used for chunking and compression )
+    H5::DSetCreatPropList dPropList_;
+    
+    //! Main file containing grid and meta information
+    H5::H5File mainFile_;
+
+    //! In case we use
+    H5::H5File currStepFile_;
+
+    //! Main / Root Group
+    H5::Group mainGroup_;
+
+    //! Mesh Group
+    H5::Group meshGroup_;
+
+    //! Group for results
+    H5::Group resultsGroup_;
+
+    //! Group for user data
+    H5::Group userDataGroup_;
+
+    //! Group for mesh results
+    H5::Group meshResultsGroup_;
+    
+    //! Group for current analysis step for mesh results
+    H5::Group currMeshStepGroup_;
 
     //!MpCCI
     int meshId_;
