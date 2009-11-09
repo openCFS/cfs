34d452367326c039057a1cfb503aa0eafb366c0c
Andreas Hauck <mail@andreas-hauck.com>
2009-11-09 21:02:59 +0000

    - Adjusted the lsepaper style to include the CFS++ logo
    - Now also the \xml{} elements are highlighted in the html-documentation
    
    - Adjusted the algorithm for indentation in dopygment.py

--- share/doc/user/xmlFile/dopygment.py
+++ share/doc/user/xmlFile/dopygment.py
@@ -29,12 +29,11 @@ cmdNames = ["xml"]
 # substitution for html (as the html code will be embedded 
 # in TeX again
 htmlSubst = { 
-  "{" : "\{", 
-  "}" : "\}",   
   "&#39;" : "&prime;",
   "}" : "\}", 
   "\n": "<br>\n", 
-  "%": "\%"
+  "%": "\%",
+  "#": "\#",
 }
 
 
@@ -73,20 +72,34 @@ def codeReplaceHtml( txt ):
   # in addition we have to replace all literal spaces
   # by &nbsp;, as the \HCode{} statement removes all
   # trailing whitespaces, which woud ruin the indentation
+  n = 0
   for i in result.split("\n"):
+    n = n+1
     ret = ret + "\HCode{"
+    
+    # if we are in the first line, we omit everything, until
+    # the <pre> element and just take 
+    beginPos = 0
+    if n == 1:
+       beginPos = i.find("<pre>",0)
+       if beginPos > 0:
+         ret = ret + i[:beginPos+5] + "&nbsp;"
+         i = i[beginPos+5:]
+    
     temp = ""
     index = 0
-    while index < len(i)  and i[index] == " ":
+    while index < len(i) and i[index] == " ":
       ret = ret + "&nbsp;"
       index = index+1
     ret=  ret + i + "}\n"
+    
   return ret
 
 # callback function for latex output
 def codeReplaceTeX( txt ):
   result = highlight(txt.group(2), lexer, formatter)
   return result
+
 
 # pygments stuff:
 # ----------------
@@ -97,13 +110,18 @@ callback = lambda x: x
 # and syntax highglighter depending on format html / tex
 # and define callback function
 formatter = LatexFormatter()
+inlineFormatter = LatexFormatter()
 if parserType == "tex":
-  formatter = LatexFormatter()  
+  formatter = LatexFormatter()
+  inlineFormatter = LatexFormatter()
   callback = codeReplaceTeX
   formatFile ="style.tex"
 elif parserType == "html" :
   formatter = HtmlFormatter(style="default", linenos=False, 
                             cssclass="xmlcode", cssFilefull="False")
+  inlineFormatter = HtmlFormatter(style="default", linenos=False, nowrap=True, nobackground=True,
+                                  noclasses=True)
+                            
   callback = codeReplaceHtml
   formatFile = "style.css"
 else:
@@ -133,26 +151,46 @@ for actEnv in envNames:
     beginPos = code.find("\\begin{"+actEnv+"}", endPos)
     endPos = code.find("\\end{"+actEnv+"}", beginPos)
     #check if environment was found
-    if not(endPos == -1 or beginPos == -1):
+    if not(endPos == -1 and beginPos == -1):
       endPos = endPos + 6 + len(actEnv)
       
       # perform syntax highlightning and replace original code
       temp = re.sub(myexp, callback, code[beginPos:endPos])
       code = code.replace(code[beginPos:endPos], temp)
 
-# iterate over all commands and do syntax highlightning
-# ... to be programmed ;-)
-
-# write highlighted code to file
-f = open(newFileName, 'w')
-f.write(code)
-f.close()
-
-# in the end, write html-css / tex-header to format File
+# write html-css / tex-header to style file, before treating
+# the inline case, which requires no external style definition
 f = open(formatFile, 'w')
 f.write(formatter.get_style_defs() )
 f.close()
 
 
+# perform inline syntax coloring hjus
+if parserType == "html":
+  # iterate over all commands and do syntax highlightning
+  # but first we have to use the inlineFormatter
+  formatter = inlineFormatter
+  for actCmd in cmdNames:
+    actRegEx = "(\\\\"+actCmd+"\{)(.*)(?<!%.)(})"
+    myexp = re.compile(actRegEx,re.DOTALL)
+    beginPos = 0
+    endPos = 0
+    while endPos < len(code) and endPos != -1:
+      #print "looking for ","\\"+actCmd+"{"
+      beginPos = code.find("\\"+actCmd+"{", endPos)
+      endPos = code.find("}", beginPos)
+   
+      # return substrings with content between 
+      # \cmdName{ ... and }
+      if not(endPos == -1 and beginPos == -1):
+        endPos = endPos + 1
+        
+        # perform syntax highlightning and replace original code
+        temp = re.sub(myexp, callback, code[beginPos:endPos])
+        code = code.replace(code[beginPos:endPos], temp)
 
+# write highlighted code to file
+f = open(newFileName, 'w')
+f.write(code)
+f.close()
 
