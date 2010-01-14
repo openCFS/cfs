ce1248ae72bf25e0b90d21dbd092afa0c2b179d1
Fabian Wein <fabian.wein@fau.de>
2010-01-14 08:48:25 +0000

    minor: remove hardcoded 3.6.1 revision number when building paraview

--- cmake_modules/FindParaView.cmake
+++ cmake_modules/FindParaView.cmake
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------------
 # Look for ParaView directory.
 #-------------------------------------------------------------------------------
-BUILD_EXTLIB("ParaView 3.6.1 with HDF5 support"
+BUILD_EXTLIB("ParaView with HDF5 support"
   "${CFS_BINARY_DIR}/paraview"
   "${CFS_DEPS_ROOT}/paraview/build_paraview.pl"
   "build_paraview.log")
