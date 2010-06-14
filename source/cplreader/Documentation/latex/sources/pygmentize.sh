9fb2313078c4c482601af9584d977889af515e59
Simon Triebenbacher <strieben@svn>
2010-06-14 14:40:16 +0000

    - Cleaned up layout of cplreader documention and adapted pygmentize.sh for   newer versions of Python Pygments.

--- source/cplreader/Documentation/latex/sources/pygmentize.sh
+++ source/cplreader/Documentation/latex/sources/pygmentize.sh
@@ -2,22 +2,33 @@
 
 SOURCES_DIR=$1
 DEST_DIR=$2
-STYLE=borland
+STYLE=tango
 OPTIONS="-f latex -O linenos=True,linenostep=5,style=$STYLE"
 PYG=pygmentize
 
 mkdir -p $DEST_DIR
 
 $PYG -f latex -O full,style=$STYLE $SOURCES_DIR/cfs_interpolation.xml > $DEST_DIR/dummy.tex
+# Grep the definitions for the fancy verbatim environments from dummy.tex (for older pygmentize)
 fgrep newcommand $DEST_DIR/dummy.tex > $DEST_DIR/pygmentize.tex
+# Extract the definitions (for newer pygmentize)
+cat $DEST_DIR/dummy.tex | sed -e '/./{H;$!d;}' -e 'x;/def/!d;' >> $DEST_DIR/pygmentize.tex
 #echo $SOURCES_DIR
 #echo $DEST_DIR
 
-$PYG $OPTIONS $SOURCES_DIR/cfs_interpolation.xml > $DEST_DIR/cfs_interpolation.tex
-$PYG $OPTIONS $SOURCES_DIR/gengrids_spherical_shells.xml > $DEST_DIR/gengrids_spherical_shells.tex
-$PYG $OPTIONS $SOURCES_DIR/gengrids_rectlin.xml > $DEST_DIR/gengrids_rectlin.tex
-$PYG $OPTIONS $SOURCES_DIR/gengrids_refelems.xml > $DEST_DIR/gengrids_refelems.tex
-$PYG $OPTIONS $SOURCES_DIR/gengrids_refelems.xml > $DEST_DIR/gengrids_refelems.tex
-$PYG $OPTIONS $SOURCES_DIR/gengrids_fields.xml > $DEST_DIR/gengrids_fields.tex
-$PYG $OPTIONS $SOURCES_DIR/check_cons_interp.m > $DEST_DIR/check_cons_interp.tex
-$PYG $OPTIONS $SOURCES_DIR/sumup.m > $DEST_DIR/sumup.tex
+function Pygmentize {
+  INPUT=$1
+  OUTPUT=$2
+ 
+  $PYG $OPTIONS $SOURCES_DIR/$INPUT > $DEST_DIR/dummy.tex
+  cat $DEST_DIR/dummy.tex | sed 's/commandchars/frame=lines,framesep=2mm,fontsize=\\\small,commandchars/' > $DEST_DIR/$OUTPUT
+}
+
+Pygmentize cfs_interpolation.xml cfs_interpolation.tex
+Pygmentize gengrids_spherical_shells.xml gengrids_spherical_shells.tex
+Pygmentize gengrids_rectlin.xml gengrids_rectlin.tex
+Pygmentize gengrids_refelems.xml gengrids_refelems.tex
+Pygmentize gengrids_refelems.xml gengrids_refelems.tex
+Pygmentize gengrids_fields.xml gengrids_fields.tex
+Pygmentize check_cons_interp.m check_cons_interp.tex
+Pygmentize sumup.m sumup.tex
