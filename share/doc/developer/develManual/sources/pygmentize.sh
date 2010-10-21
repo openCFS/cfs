0e24265119bcfe9bf3ef08766fe079db7d23c89b
Simon Triebenbacher <strieben@svn>
2010-10-21 13:06:45 +0000

    - Added a section about texture mapping in ParaView.

--- share/doc/developer/develManual/sources/pygmentize.sh
+++ share/doc/developer/develManual/sources/pygmentize.sh
@@ -43,6 +43,7 @@ Pygmentize gid_pw_server.sh gid_pw_server.tex
 Pygmentize pv_analytical_sol.py pv_analytical_sol.tex
 Pygmentize pv_scipy_hankel1.py pv_scipy_hankel1.tex
 Pygmentize pv_matlab_besselh.m pv_matlab_besselh.tex
+Pygmentize pv_texture_map.py pv_texture_map.tex
 Pygmentize icem_to_ansys.in.txt icem_to_ansys.in.tex
 Pygmentize Pfeifenfussmodell_modificated_swept.in.txt Pfeifenfussmodell_modificated_swept.in.tex
 Pygmentize mkmesh.txt mkmesh.tex
