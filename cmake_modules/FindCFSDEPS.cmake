ed4e748cc1b5189a39d65de0ecf942e13bcca3ad
Simon Triebenbacher <strieben@svn>
2010-12-15 12:52:20 +0000

    - cfs-hdf5 has to be built after HDF5 and Boost, since it depends on   both libraries. - Wrote down, why Pardiso is shit!

--- cmake_modules/FindCFSDEPS.cmake
+++ cmake_modules/FindCFSDEPS.cmake
@@ -90,7 +90,6 @@ INCLUDE("${CFS_SOURCE_DIR}/cmake_modules/FindBzip2.cmake")
 #-------------------------------------------------------------------------------
 IF(USE_HDF5)
   INCLUDE("${CFS_SOURCE_DIR}/cmake_modules/FindHDF5.cmake")
-  INCLUDE("${CFS_SOURCE_DIR}/cmake_modules/FindCfs-hdf5.cmake")
 ENDIF(USE_HDF5)
 
 #-------------------------------------------------------------------------------
@@ -199,6 +198,13 @@ ENDIF(USE_BLAS OR USE_LAPACK)
 # Find Boost
 #-------------------------------------------------------------------------------
 INCLUDE("${CFS_SOURCE_DIR}/cmake_modules/FindBoostForCFS.cmake")
+
+#-------------------------------------------------------------------------------
+# Our cfs-hdf5 I/O library depends on Boost, that is why we check for it here.
+#-------------------------------------------------------------------------------
+IF(USE_HDF5)
+  INCLUDE("${CFS_SOURCE_DIR}/cmake_modules/FindCfs-hdf5.cmake")
+ENDIF(USE_HDF5)
 
 #-------------------------------------------------------------------------------
 # If USE_PYTHON option is defined find Python library
