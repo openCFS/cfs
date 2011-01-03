17e26fae5d0655621c822e815edfd9746b652aab
Simon Triebenbacher <strieben@svn>
2011-01-03 19:36:16 +0000

    - Some Subversion users (like students) do not have full access to the   closed source libraries SCPIP and ILUPACK. To avoid inconvenient configuration errors if the libs are missing from CFSDEPS, the USE_ILUPACK and USE_SCPIP options are forced off for these users.

--- cmake_modules/FindSCPIP.cmake
+++ cmake_modules/FindSCPIP.cmake
@@ -13,19 +13,24 @@
 
 SET(SCPIP_FOUND 0)
 
-BUILD_EXTLIB("SCPIP"
-  "${CFS_BINARY_DIR}/${LIB_SUFFIX}/${CFS_ARCH_STR}/libscpip.a"
-  "${CFS_DEPS_ROOT}/scpip/build_scpip.pl"
-  "build_scpip.log")
+IF(EXISTS "${CFS_DEPS_ROOT}/scpip/build_scpip.pl")
+  BUILD_EXTLIB("SCPIP"
+    "${CFS_BINARY_DIR}/${LIB_SUFFIX}/${CFS_ARCH_STR}/libscpip.a"
+    "${CFS_DEPS_ROOT}/scpip/build_scpip.pl"
+    "build_scpip.log")
 
-#-------------------------------------------------------------------------------
-# Determine paths of SCPIP libraries.
-#-------------------------------------------------------------------------------
-SET(LD "${CFS_BINARY_DIR}/${LIB_SUFFIX}/${CFS_ARCH_STR}")
-SET(SCPIP_LIBRARY
-  "${LD}/libscpip.a"
-  CACHE FILEPATH "SCPIP library.")
+  #-----------------------------------------------------------------------------
+  # Determine paths of SCPIP libraries.
+  #-----------------------------------------------------------------------------
+  SET(LD "${CFS_BINARY_DIR}/${LIB_SUFFIX}/${CFS_ARCH_STR}")
+  SET(SCPIP_LIBRARY
+    "${LD}/libscpip.a"
+    CACHE FILEPATH "SCPIP library.")
 
-MARK_AS_ADVANCED(SCPIP_LIBRARY)
+  MARK_AS_ADVANCED(SCPIP_LIBRARY)
 
-SET(SCPIP_FOUND 1)
+  SET(SCPIP_FOUND 1)
+ELSE(EXISTS "${CFS_DEPS_ROOT}/scpip/build_scpip.pl")
+  SET(USE_SCPIP OFF CACHE BOOL "Enable SCPIP" FORCE)
+  MESSAGE(STATUS "SCPIP not found in CFSDEPS. Forcing it off!")
+ENDIF(EXISTS "${CFS_DEPS_ROOT}/scpip/build_scpip.pl")
