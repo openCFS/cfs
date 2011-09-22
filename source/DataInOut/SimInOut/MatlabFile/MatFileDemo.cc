498d17870b05f41f2d6734e07e21de24e1f73ec6
Simon Triebenbacher <strieben@svn>
2011-09-22 12:34:56 +0000

    - Added a small sample program, which demonstrates the usage of the   MatFile class for writing HDF5 based Matlab 7.3 .mat files. Using .mat   files for Matlab is little bit more convenient than using text files.

---
+++ source/DataInOut/SimInOut/MatlabFile/MatFileDemo.cc
@@ -0,0 +1,30 @@
+#include "MatFile.hh"
+
+// This sample program shows how to  use the MatFile class to write Matlab 7.3
+// .mat files, which are HDF5 based.
+
+// The  .mat files  are normal  HDF5 files,  which have  a prepended  512 byte
+// header.  To view  them  in hdfview  one  can remove  the  header using  the
+// following command:
+
+// dd if=test.mat bs=512 skip=1 of=out.h5
+
+int main(int argc, char** argv) 
+{
+  // Create new MatFile object.
+  MatFile mf("test.mat");
+
+  // Prepare some data
+  double data[6];
+  for(unsigned i=0; i<6; i++) 
+  {
+    data[i] = i+1;
+  }  
+  
+  // Write a matrix and a vector to the data file.
+  mf.WriteMatrix("A", 3, 2, data);
+  mf.WriteVector("b", 3, data+2);
+  
+  // Close .mat file and automatically prepend a header.
+  mf.Close();
+}
