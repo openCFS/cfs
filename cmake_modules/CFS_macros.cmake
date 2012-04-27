cc9ec454a121e520ed60a96ec03c7d94e8089e55
Simon Triebenbacher <strieben@cfs-dev.mdmt.tuwien.ac.at>
2012-04-27 17:42:01 +0000

    - Reimplemented build of documentation in pure CMake. This should considerably improve maintainability, since only one set of CMake files have to be managed on Linux and Windows (cf. NACS 2.0) - There are now separate targets for all manuals (doc-develmanual, doc-doxygen, doc-xmlmat, doc-xmlref, doc-advprepost) - The targets doc-devel and doc-user depend and build the corresponding manuals in the subdirs. - The target doc builds everything.

--- cmake_modules/CFS_macros.cmake
+++ cmake_modules/CFS_macros.cmake
@@ -151,12 +151,12 @@ ENDMACRO(CFS_CHECK_CXX_SOURCE_RUNS)
 MACRO (TODAY RESULT)
     IF (WIN32)
         EXECUTE_PROCESS(COMMAND "date" "/T" OUTPUT_VARIABLE ${RESULT})
-        string(REGEX REPLACE "(..)/(..)/..(..).*" "\\3\\2\\1"
-${RESULT} ${${RESULT}})
+#        string(REGEX REPLACE "(..)/(..)/..(..).*" "\\3\\2\\1"
+#	  ${RESULT} ${${RESULT}})
     ELSEIF(UNIX)
         EXECUTE_PROCESS(COMMAND "date" "+%d/%m/%Y" OUTPUT_VARIABLE ${RESULT})
-        string(REGEX REPLACE "(..)/(..)/..(..).*" "\\3\\2\\1"
-${RESULT} ${${RESULT}})
+#        string(REGEX REPLACE "(..)/(..)/..(..).*" "\\3\\2\\1"
+#	  ${RESULT} ${${RESULT}})
     ELSE (WIN32)
         MESSAGE(SEND_ERROR "date not implemented")
         SET(${RESULT} 000000)
--- share/doc/developer/DoxygenConfig/changeDate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ACTDATE=`date '+%B%_e, %G'`
-sed "s/Last.*/Last modified: $ACTDATE/g" $1 > $2
