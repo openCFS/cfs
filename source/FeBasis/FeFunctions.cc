3745cc8ce153797198a42f031efb007d0f494470
Simon Triebenbacher <strieben@cfs-dev.mdmt.tuwien.ac.at>
2012-06-14 21:32:09 +0000

    - The destructor of FeSpace was never called! Therefore 11 MB (!!!) of memory were leaking in my two-element example. Now, I decrease the reference counter of feSpace_ in the destructor of BaseFeFunction which has the desired effect of the FeSpace destructor getting actually called. Since feSpace_ is a shared_ptr<FeSpace> this should work automatically, but obviously it does not. Andis, could you please have a look at this issue?

--- source/FeBasis/FeFunctions.cc
+++ source/FeBasis/FeFunctions.cc
@@ -30,6 +30,8 @@ DECLARE_LOG(fefunc)
   }
   
   BaseFeFunction::~BaseFeFunction(){
+    // Manually decrease the reference counter of our shared_ptr<FeSpace>
+    feSpace_.~shared_ptr();
   }
   
   
