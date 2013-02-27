7d804abc8475618a941e0938f0301b57610d2b2f
Jens Grabinger <jens.grabinger@simetris.de>
2013-02-27 15:42:56 +0000

    Copied method UpdateIntersection from Grid class (branch jens_rotate) to NcInterface class.

--- source/Domain/Mesh/NcInterface.cc
+++ source/Domain/Mesh/NcInterface.cc
@@ -113,4 +113,110 @@ namespace CoupledField {
 
   }
 
+  void NcInterface::UpdateIntersection() {
+    StdVector<SurfElem*> masterElems;
+    StdVector<SurfElem*> slaveElems;
+    StdVector<NCElem*> ncElems;
+    StdVector<SurfElem*> ncElemsHelper;
+    StdVector<UInt> masterNodes;
+    StdVector<UInt> ncElemIds;
+    StdVector<UInt> newNodes;
+    StdVector<std::string> listNodeNames;
+    std::string newNodesName = ncIf.name + "_nodes";
+
+    ClearRegion(ncIf.region);
+    GetListNodeNames(listNodeNames);
+    if ( listNodeNames.Find(newNodesName) != -1 )
+    {
+      DeleteNamedNodes(newNodesName);
+    }
+
+    GetSurfElems(masterElems, ncIf.masterSurfRegion);
+    GetSurfElems(slaveElems, ncIf.slaveSurfRegion);
+
+    switch (ncIf.intersectAlgo) {
+      case LINE_INTERSECT:
+        LOG_DBG3(grid) << "\ncomputing integration grid of ncInterfaces...";
+        for (UInt i = 0; i < masterElems.GetSize(); ++i) {
+          for (UInt j = 0; j < slaveElems.GetSize(); ++j) {
+            SideOnSide(masterElems[i], slaveElems[j], ncIf.coplanar,
+                        ncIf.needsUpdate, ncElems, newNodes );
+          }
+        }
+        break;
+      case RECT_INTERSECT:
+        if (!ncIf.coplanar) {
+          EXCEPTION("Only coplanar interfaces are supported with coaxial "
+                    << "rectangle algorithm.");
+        }
+        for (UInt i = 0; i < masterElems.GetSize(); ++i) {
+          for(UInt j = 0; j < slaveElems.GetSize(); ++j) {
+            SurfElem* m_el = masterElems[i];
+            SurfElem* s_el = slaveElems[j];
+            if( (m_el->type != Elem::ET_QUAD4 )
+              || s_el->type != Elem::ET_QUAD4 )
+              {
+              EXCEPTION("Only quadrilaterals can be intersected with coaxial "
+                        << "rectangle algorithm.");
+            }
+
+            if(RectangleOnRectangle(masterElems[i], slaveElems[j],
+                                    ncIf.needsUpdate, ncElems))
+            {
+              LOG_DBG3(grid) << "Intersection between "
+                             << masterElems[i]->elemNum << " and "
+                             << slaveElems[j]->elemNum << std::endl;
+            }
+          }
+        }
+        break;
+      case POLYGON_INTERSECT:
+        for (UInt i = 0; i < masterElems.GetSize(); ++i) {
+          for (UInt j = 0; j < slaveElems.GetSize(); ++j) {
+            SurfElem* m_el = masterElems[i];
+            SurfElem* s_el = slaveElems[j];
+            if( (m_el->type != Elem::ET_QUAD4 )
+                          || s_el->type != Elem::ET_QUAD4 )
+            {
+              EXCEPTION("Only triangles and quadrilaterals can be intersected"
+                        << " with polygon algorithm.");
+            }
+
+            if (PolygonOnPolygon(masterElems[i], slaveElems[j], ncIf.coplanar,
+                                 ncIf.needsUpdate, ncElems,
+                                 ncIf.tolAbs, ncIf.tolRel))
+            {
+              LOG_DBG3(grid) << "Intersection between "
+                << masterElems[i]->elemNum << " and "
+                << slaveElems[j]->elemNum << std::endl;
+            }
+          }
+        }
+    }
+
+    if(ncElems.GetSize() > 0)
+    {
+      UInt numElems = ncElems.GetSize();
+
+      ncElemsHelper.Resize(numElems);
+
+      for(UInt i=0; i<numElems; ++i)
+      {
+        ncElemsHelper[i] = ncElems[i];
+      }
+
+      AddSurfaceElems(ncIf.region, ncElemsHelper, ncElemIds);
+      
+      if ( newNodes.GetSize() > 0 )
+      {
+        AddNamedNodes(newNodesName, newNodes);
+      }
+    }
+    else {
+      EXCEPTION("No intersection elements were computed for non-conforming"
+                << " interface '" << ncIf.name
+                << "'. Please check your mesh file.");
+    }
+  }
+  
 } // namespace CoupledField
