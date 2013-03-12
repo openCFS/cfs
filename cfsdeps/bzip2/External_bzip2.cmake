742e915c3e33c86a4ed8571dce2d5568cca0c3f1
Simon Triebenbacher <strieben@cfs-dev.mdmt.tuwien.ac.at>
2013-03-12 18:20:39 +0000

    - bzip2 gets built using CMake now, which makes porting to new compilers much easier.

--- cfsdeps/bzip2/External_bzip2.cmake
+++ cfsdeps/bzip2/External_bzip2.cmake
@@ -1,22 +1,48 @@
 #-------------------------------------------------------------------------------
-# Set prefix path and path to bzip2 sources according to ExternalProject.cmake 
+# Set paths to bzip2 sources according to ExternalProject.cmake 
 #-------------------------------------------------------------------------------
 set(bzip2_prefix  "${CMAKE_CURRENT_BINARY_DIR}/cfsdeps/bzip2")
-set(bzip2_source  "${bzip2_prefix}/src/bzip2-static")
+set(bzip2-static_source  "${bzip2_prefix}/src/bzip2-static")
+set(bzip2-shared_source  "${bzip2_prefix}/src/bzip2-shared")
+set(bzip2_install  "${CMAKE_CURRENT_BINARY_DIR}")
+
+SET(CMAKE_ARGS
+  -DCMAKE_INSTALL_PREFIX:PATH=${bzip2_install}
+  -DCMAKE_COLOR_MAKEFILE:BOOL=${CMAKE_COLOR_MAKEFILE}
+  -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
+  -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+  -DCMAKE_RANLIB:FILEPATH=${CMAKE_RANLIB}
+  -DCFS_ARCH_STR:STRING=${CFS_ARCH_STR}
+  -DLIB_SUFFIX:STRING=${LIB_SUFFIX}
+  )
+
+IF(CFS_DISTRO STREQUAL "MACOSX")
+  SET(CMAKE_ARGS
+    ${CMAKE_ARGS}
+    -DCMAKE_C_FLAGS:FILEPATH=${CFLAGS}
+    -DCMAKE_OSX_ARCHITECTURES:STRING=${CMAKE_OSX_ARCHITECTURES}
+    -DCMAKE_OSX_SYSROOT:PATH=${CMAKE_OSX_SYSROOT}
+    )
+ENDIF(CFS_DISTRO STREQUAL "MACOSX")
+
+IF(CMAKE_TOOLCHAIN_FILE)
+  LIST(APPEND CMAKE_ARGS
+    -DCMAKE_TOOLCHAIN_FILE:FILEPATH=${CMAKE_TOOLCHAIN_FILE}
+  )
+ENDIF()
 
 #-------------------------------------------------------------------------------
-# The bzip2-static external project
+# The bzip2 external project
 #-------------------------------------------------------------------------------
 ExternalProject_Add(bzip2-static
   PREFIX "${bzip2_prefix}"
   DOWNLOAD_DIR ${CFS_DEPS_CACHE_DIR}/sources/bzip2
   URL ${BZIP2_URL}/${BZIP2_GZ}
   URL_MD5 ${BZIP2_MD5}
-  CONFIGURE_COMMAND ""
-  BUILD_IN_SOURCE 1
-  BUILD_COMMAND ${CMAKE_MAKE_PROGRAM} bzip2 bzip2recover
-  INSTALL_COMMAND ${CMAKE_MAKE_PROGRAM} install
-  )
+  CMAKE_ARGS
+    ${CMAKE_ARGS}
+    -DBUILD_SHARED_LIBS:BOOL=OFF
+)
 
 #-------------------------------------------------------------------------------
 # Set names of patch file and template file.
@@ -41,24 +67,21 @@ ExternalProject_Add_Step(bzip2-static custom_patch
    DEPENDEES download
    DEPENDERS configure
    DEPENDS "${PFN}"
-   WORKING_DIRECTORY ${bzip2_source}
+   WORKING_DIRECTORY ${bzip2-static_source}
 )
 
 #-------------------------------------------------------------------------------
-# The bzip2-static external project
+# The bzip2 external project
 #-------------------------------------------------------------------------------
-set(bzip2_source  "${bzip2_prefix}/src/bzip2-shared")
-
 ExternalProject_Add(bzip2-shared
   PREFIX "${bzip2_prefix}"
   DOWNLOAD_DIR ${CFS_DEPS_CACHE_DIR}/sources/bzip2
   URL ${BZIP2_URL}/${BZIP2_GZ}
   URL_MD5 ${BZIP2_MD5}
-  CONFIGURE_COMMAND ""
-  BUILD_IN_SOURCE 1
-  BUILD_COMMAND ${CMAKE_MAKE_PROGRAM} -f Makefile-libbz2_so
-  INSTALL_COMMAND ""
-  )
+  CMAKE_ARGS
+    ${CMAKE_ARGS}
+    -DBUILD_SHARED_LIBS:BOOL=ON
+)
 
 #-------------------------------------------------------------------------------
 # Set names of patch file and template file.
@@ -83,7 +106,7 @@ ExternalProject_Add_Step(bzip2-shared custom_patch
    DEPENDEES download
    DEPENDERS configure
    DEPENDS "${PFN}"
-   WORKING_DIRECTORY ${bzip2_source}
+   WORKING_DIRECTORY ${bzip2-shared_source}
 )
 
 #-------------------------------------------------------------------------------
@@ -95,8 +118,15 @@ SET(CFSDEPS
   bzip2-shared
 )
 
-SET(BZIP2_LIBRARY ${CFS_BINARY_DIR}/${LIB_SUFFIX}/${CFS_ARCH_STR}/${CMAKE_STATIC_LIBRARY_PREFIX}bz2${CMAKE_STATIC_LIBRARY_SUFFIX} CACHE FILEPATH "bzip2 library")
-SET(BZIP2_INCLUDE_DIR ${CFS_BINARY_DIR}/include CACHE PATH "bzip2 include directory")
+SET(BZIP2_INCLUDE_DIR "${CFS_BINARY_DIR}/include")
 
-MARK_AS_ADVANCED(BZIP2_LIBRARY)
+#-------------------------------------------------------------------------------
+# Determine paths of BZIP2 libraries.
+#-------------------------------------------------------------------------------
+SET(LD "${CFS_BINARY_DIR}/${LIB_SUFFIX}/${CFS_ARCH_STR}")
+SET(BZIP2_LIBRARY
+  "${LD}/${CMAKE_STATIC_LIBRARY_PREFIX}bz2${CMAKE_STATIC_LIBRARY_SUFFIX}"
+  CACHE FILEPATH "BZIP2 library.")
+
 MARK_AS_ADVANCED(BZIP2_INCLUDE_DIR)
+MARK_AS_ADVANCED(BZIP2_LIBRARY)
