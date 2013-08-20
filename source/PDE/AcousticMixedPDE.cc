06494c011aee294c7eabe1f2636d9c6aa3f1ee79
pploss <pploss@cfs-dev.mdmt.tuwien.ac.at>
2013-08-20 06:12:01 +0000

    Fix small typo in file's indendation guideluines that was introduced in revision 12643.

--- source/PDE/AcousticMixedPDE.cc
+++ source/PDE/AcousticMixedPDE.cc
@@ -1,4 +1,4 @@
-// -*- mode: c++; cod"ing: utf-8; indent-tabs-mode: nil; -*-
+// -*- mode: c++; coding: utf-8; indent-tabs-mode: nil; -*-
 // vim: set ts=2 sw=2 et nu ai ft=cpp cindent !:
 // kate: space-indent on; indent-width 2; encoding utf-8;
 // kate: auto-brackets on; mixedindent off; indent-mode cstyle;
