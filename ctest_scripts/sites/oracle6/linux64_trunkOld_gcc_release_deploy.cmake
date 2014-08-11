8310740576ff56a752234defa79a35cb3ecb0fd1
Andreas Hüppe <andreas_hueppe@web.de>
2014-08-11 15:33:27 +0000

    Renamed and edited remaining files. CDash should be back on running now.

--- ctest_scripts/sites/oracle6/linux64_trunkOld_gcc_release_deploy.cmake
+++ ctest_scripts/sites/oracle6/linux64_trunkOld_gcc_release_deploy.cmake
@@ -3,20 +3,22 @@
 #-----------------------------------------------------------------------------
 SET(CTEST_BINARY_DIRECTORY "$ENV{HOME}/Documents/dev/CFS_BUILD_NIGHTLY")
 
-# Determine arch string
 INCLUDE("${CTEST_BINARY_DIRECTORY}/CMakeFiles/distro_out.cmake")
 
 message("  Searching for cfs and cfstool executables...")
 SET(CFS_BIN_DIR
-  "${CTEST_BINARY_DIRECTORY}/bin/${DIST}_${REV}_${ARCH}")
+  "${CTEST_BINARY_DIRECTORY}/bin/${DIST_FAMILY}_${MAJOR_REV}_${ARCH}")
 SET(CFS_BINARY "${CFS_BIN_DIR}/cfsbin")
 SET(CFSTOOL_BINARY "${CFS_BIN_DIR}/cfstoolbin")
+SET(CPLREADER_BINARY "${CFS_BIN_DIR}/cplreader")
 
 MESSAGE("CFS_BINARY ${CFS_BINARY}")
 MESSAGE("CFSTOOL_BINARY ${CFSTOOL_BINARY}")
+MESSAGE("CPLREADER_BINARY ${CPLREADER_BINARY}")
 
 IF(EXISTS "${CFS_BINARY}" AND
-   EXISTS "${CFSTOOL_BINARY}")
+   EXISTS "${CFSTOOL_BINARY}" AND
+   EXISTS "${CPLREADER_BINARY}")
 
   message("  Cleaning up binary directory...")
 
@@ -25,9 +27,10 @@ IF(EXISTS "${CFS_BINARY}" AND
     "${CTEST_BINARY_DIRECTORY}/cfsdeps"
     "${CTEST_BINARY_DIRECTORY}/include"
     "${CTEST_BINARY_DIRECTORY}/tmp"
-    "${CTEST_BINARY_DIRECTORY}/testsuite"
+    "${CTEST_BINARY_DIRECTORY}/CFS_TESTSUITE_TRUNK"
     "${CTEST_BINARY_DIRECTORY}/source"
     "${CTEST_BINARY_DIRECTORY}/CMakeFiles"
+    "${CTEST_BINARY_DIRECTORY}/vtk"
     "${CTEST_BINARY_DIRECTORY}/Testing"
   )
 
