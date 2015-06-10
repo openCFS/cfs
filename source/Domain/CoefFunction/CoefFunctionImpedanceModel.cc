7cd1008c6ba09f888842c52720309af8a7a63bc0
Stefan Zörner <stefan.zoerner@tuwien.ac.at>
2015-06-10 19:40:44 +0000

    clean up code, removed dbg outputs --

--- source/Domain/CoefFunction/CoefFunctionImpedanceModel.cc
+++ source/Domain/CoefFunction/CoefFunctionImpedanceModel.cc
@@ -30,48 +30,41 @@ namespace CoupledField{
                                                                  Double innerR, Double outerR) {
     LocPointMapped lp_dummy;
     Double tmpBlkMod;
+    // density_, DENSITY
     PtrCoefFct tmpPtFc = material->GetScalCoefFnc( DENSITY, Global::REAL );
     tmpPtFc->GetScalar(density_, lp_dummy);
-    //material->GetScalar(density_, DENSITY, Global::REAL);
+    // nu_, KINEMATIC_VISCOSITY
     tmpPtFc = material->GetScalCoefFnc( KINEMATIC_VISCOSITY, Global::REAL );
     tmpPtFc->GetScalar(nu_, lp_dummy);
-    //material->GetScalar(nu_, KINEMATIC_VISCOSITY, Global::REAL);
+    // slitWidth_, SLITWIDTH
     tmpPtFc = material->GetScalCoefFnc( SLITWIDTH, Global::REAL );
     tmpPtFc->GetScalar(slitWidth_, lp_dummy);
-    //material->GetScalar(slitWidth_, SLITWIDTH, Global::REAL);
+    // mppThick_, MPP_THICKNESS
     tmpPtFc = material->GetScalCoefFnc( MPP_THICKNESS, Global::REAL );
     tmpPtFc->GetScalar(mppThick_, lp_dummy);
-    //material->GetScalar(mppThick_, MPP_THICKNESS, Global::REAL);
+    // sigma_, POROSITY
     tmpPtFc = material->GetScalCoefFnc( POROSITY, Global::REAL );
     tmpPtFc->GetScalar(sigma_, lp_dummy);
-    //material->GetScalar(sigma_, POROSITY, Global::REAL);
+    // flowMachNr_, FLOW_MACH_NUMBER
     tmpPtFc = material->GetScalCoefFnc( FLOW_MACH_NUMBER, Global::REAL );
     tmpPtFc->GetScalar(flowMachNr_, lp_dummy);
-    //material->GetScalar(flowMachNr_, FLOW_MACH_NUMBER, Global::REAL);
+    // beta_, BETA
     tmpPtFc = material->GetScalCoefFnc( BETA, Global::REAL );
     tmpPtFc->GetScalar(beta_, lp_dummy);
-    //material->GetScalar(beta_, BETA, Global::REAL);
+    // tmpBlkMod, ACOU_BULK_MODULUS
     tmpPtFc = material->GetScalCoefFnc( ACOU_BULK_MODULUS, Global::REAL );
     tmpPtFc->GetScalar(tmpBlkMod, lp_dummy);
-    //material->GetScalar(tmpBlkMod, ACOU_BULK_MODULUS, Global::REAL);
+    // calc speed of sound
     c0_ = sqrt(tmpBlkMod/density_);
     innerR_ = innerR;
     outerR_ = outerR;
   }
 
   void CoefFunctionImpedanceModel<Complex>::Recalculate() {
-//std::cerr << "Recalculate Impedance!!!!!!!!!" << std::endl;
     Complex Z_mpp, Z_cav, Z_wall;
     Complex tmp1, tmp2, tmp3;
     Double waveNum, shearWaveNum;
     const Complex i(0,1);
-    /* const Double nu = 0.00001511;    // TODO: hard coded kinematic viscosity (air)
-    const Double slitWidth = 0.000095; // TODO: hard coded slit width
-    const Double thick = 0.001;
-    const Double sigma = 0.043;
-    const Double rho = 1.21;
-    const Double machNr = 0.05;
-    const Double beta = 0.1; */
     const Double mu = nu_*density_;
     Double R_s; // surface resistance
     Double maxSchnelle; // maximal particle velocity
@@ -84,7 +77,6 @@ namespace CoupledField{
       //calc Z_mpp
       //    i*2*pi*f*1/(1-tanh(2*pi*f/c*sqrt(i))/(2*pi*f/c*sqrt(i)))
       MathParser::HandleType h = mp_->GetNewHandle();
-      //StdVector<std::string> varNames;
       const Double pi = mp_->GetExprVars(h, "_pi");
       Double f = mp_->GetExprVars(h, "f");
       mp_->ReleaseHandle(h);
@@ -107,7 +99,7 @@ namespace CoupledField{
 
       tmp1 = i;
       tmp1 *= waveNum;
-      tmp1 *= mppThick_/sigma_; // TODO: sigma is missing, the porosity of the perforated surface. And thickness is missing
+      tmp1 *= mppThick_/sigma_;
       tmp2 = shearWaveNum*sqrt(i);
       tmp3 = -tanh(tmp2);
       tmp3 /= tmp2;
@@ -118,8 +110,6 @@ namespace CoupledField{
       Z_mpp += 4*R_s/(sigma_*density_*c0_);
       Z_mpp += maxSchnelle/(sigma_ * c0_);
       Z_mpp += beta_*flowMachNr_/sigma_;
-
-      // TODO: Z_mpp + velocity terms + Mach number term
 
       // calc Z_cav
       tmp1 = boost::math::cyl_hankel_1(1,waveNum*outerR_);
@@ -140,17 +130,6 @@ namespace CoupledField{
       constCoefScalar_ = 1;
       constCoefScalar_ *= -i;
       constCoefScalar_ /= (Z_wall*density_*c0_);
-std::cerr << std::endl;
-std::cerr << __LINE__ << " Z_mpp " <<Z_mpp <<std::endl;
-std::cerr << __LINE__ << " Z_cav " <<Z_cav <<std::endl;
-std::cerr << __LINE__ << " constCoefScalar " <<constCoefScalar_ <<std::endl;
-std::cerr << __LINE__ << " Resistance " <<Z_wall.real() <<std::endl;
-std::cerr << __LINE__ << " Reactance " <<Z_wall.imag() <<std::endl;
-std::cerr << std::endl;
-std::cerr << __LINE__ << " sigma_*c0_ " <<sigma_*c0_ <<std::endl;
-std::cerr << __LINE__ << " maxSchnelle " <<maxSchnelle <<std::endl;
-
-
     } else {
       EXCEPTION("CoefFunctionImpedanceMode only implemented for scalar")
     }
