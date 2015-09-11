a1309c0659c2dc700943be9a337702e0be133562
Thomas Guess <thomas.guess@fau.de>
2015-09-11 12:52:59 +0000

    merged with master_to_fespace
    RELEASE
    98% tests passed, 3 tests failed out of 128
    
    Total Test time (real) =  43.30 sec
    
    The following tests FAILED:
            109 - Optimization_Dynamic_eigenfrequency (Failed)
            110 - Optimization_Dynamic_bloch2d (Failed)
            113 - Optimization_FMO_mech_coef_ev (Failed)
    
    
    DEBUG
    93% tests passed, 9 tests failed out of 128
    
    Total Test time (real) = 153.86 sec
    
    The following tests FAILED:
             87 - Optimization_Transform_rot_compl_single (Failed)
             89 - Optimization_Technical_multiload (Failed)
             90 - Optimization_Technical_multiload_explicit (Failed)
             97 - Optimization_Homogenization_max_ortho_robust (Failed)
             98 - Optimization_Transform_rot_compl_slack (Failed) (per Hand getestet geht er)
            109 - Optimization_Dynamic_eigenfrequency (Failed)
            110 - Optimization_Dynamic_bloch2d (Failed)
            113 - Optimization_FMO_mech_coef_ev (Failed)
            114 - Optimization_ParamMat_TopMat_DensityTimesRotatedTransversalIsotropic (Failed)

--- share/python/hdf5_tools.py
+++ share/python/hdf5_tools.py
@@ -3,6 +3,7 @@ import h5py
 import numpy
 import operator
 
+
 # open a hdf5 file as
 # f = h5py.File("/home/fwein/project/simp/piezo_fmo.h5")
 
@@ -17,22 +18,36 @@ def validate_region(hdf5_file, region):
 def element_dimensions(elem_id, all_elements, all_nodes):
   node_coords = []
   for n in range(len(all_elements[elem_id])):
-    node_coords.append(all_nodes[all_elements[elem_id][n]-1]) # numbers are one-based
-  ma = numpy.array([max(node_coords,key=operator.itemgetter(0))[0], max(node_coords,key=operator.itemgetter(1))[1],  max(node_coords,key=operator.itemgetter(2))[2]])
-  mi = numpy.array([min(node_coords,key=operator.itemgetter(0))[0], min(node_coords,key=operator.itemgetter(1))[1],  min(node_coords,key=operator.itemgetter(2))[2]])
+    node_coords.append(all_nodes[all_elements[elem_id][n] - 1])  # numbers are one-based
+  ma = numpy.array([max(node_coords, key=operator.itemgetter(0))[0], max(node_coords, key=operator.itemgetter(1))[1], max(node_coords, key=operator.itemgetter(2))[2]])
+  mi = numpy.array([min(node_coords, key=operator.itemgetter(0))[0], min(node_coords, key=operator.itemgetter(1))[1], min(node_coords, key=operator.itemgetter(2))[2]])
   elem_dim = ma - mi
   return elem_dim
 
+def num_nodes_by_type(type_id):
+  if type_id == 6:
+    return 4
+  if type_id == 16:
+    return 6
+  if type_id == 11:
+    return 8
+  if type_id == 9:
+    return 4
+  assert(False)
 
-## give back elements with barycenters
+# # give back elements with barycenters
 # works 2D and 3D
 # @return list barycenter tuple ordered by elements and min and max node coordinates and region element dimensions (first or all)
-def centered_elements(hdf5_file, region, all_elem_dim = False):
-  all_elements = hdf5_file['/Mesh/Elements/Connectivity'].value # for all regions
+def centered_elements(hdf5_file, region, all_elem_dim=False, region_force=None, region_support=None):
+  all_elements = hdf5_file['/Mesh/Elements/Connectivity'].value  # for all regions
   reg_elements = hdf5_file['/Mesh/Regions/' + region + '/Elements'].value
   types = hdf5_file['/Mesh/Elements/Types'].value
   all_nodes = hdf5_file['/Mesh/Nodes/Coordinates'].value
   reg_nodes = hdf5_file['/Mesh/Regions/' + region + '/Nodes']
+  if region_force <> None:
+    reg_force_nodes = hdf5_file['/Mesh/Groups/' + region_force + '/Nodes']
+  if region_support <> None:
+    reg_support_nodes = hdf5_file['/Mesh/Groups/' + region_support + '/Nodes']
   
   # determine elem_dim from first region element dimensions or from all
   elem_dim = None
@@ -46,32 +61,50 @@ def centered_elements(hdf5_file, region, all_elem_dim = False):
   # determine region dimensions, we need to resort for the desired region! Due to 1 to zero based conversion we need to do it manually :(
   nodes = numpy.zeros((len(reg_nodes), 3))
   for e in range(len(reg_nodes)):
-    nodes[e] = all_nodes[reg_nodes[e] - 1]  
-  min_dim = min(nodes[:,0]), min(nodes[:,1]), min(nodes[:,2])  
-  max_dim = max(nodes[:,0]), max(nodes[:,1]), max(nodes[:,2])   
+    nodes[e] = all_nodes[reg_nodes[e] - 1]
+  # extract boundary force nodes from region_force if available
+  if region_force <> None:
+    nodes_force = numpy.zeros((len(reg_force_nodes), 3))
+    for e in range(len(reg_force_nodes)):
+      nodes_force[e] = all_nodes[reg_force_nodes[e] - 1]
+  else:
+    nodes_force = None
+  
+  # extract boundary support nodes from region_support if available
+  if region_support <> None:
+    # determine region dimensions, we need to resort for the desired region! Due to 1 to zero based conversion we need to do it manually :(
+    nodes_support = numpy.zeros((len(reg_support_nodes), 3))
+    for e in range(len(reg_support_nodes)):
+      nodes_support[e] = all_nodes[reg_support_nodes[e] - 1]
+  else:
+    nodes_support = None
+    
+  min_dim = min(nodes[:, 0]), min(nodes[:, 1]), min(nodes[:, 2])  
+  max_dim = max(nodes[:, 0]), max(nodes[:, 1]), max(nodes[:, 2])   
     
   result = []
   for e in range(len(reg_elements)):
-    idx = reg_elements[e] - 1 # cfs writes one based
+    idx = reg_elements[e] - 1  # cfs writes one based
     nod = all_elements[idx]
     center = numpy.array([0.0, 0.0, 0.0])
-    for n in range(len(nod)):
-      center += all_nodes[nod[n]-1] # numbers are one-based
+    len_nod = num_nodes_by_type(types[idx])
+    for n in range(len_nod):
+      center += all_nodes[nod[n] - 1]  # numbers are one-based
       # print "el=" + str(e) + " n=" + str(n) + " node=" + str(nod[n]) + "->" + str(nodes[nod[n]-1]) + " center=" + str(center) 
-    center *= 1.0/len(nod)
+    center *= 1.0 / len_nod
     result.append(center)
-    # print "e=" + str(e) + " idx=" + str(idx) + " nod=" + str(nod) + " center=" + str(center) 
-  return result, min_dim, max_dim, elem_dim     
+    # print "e=" + str(e) + " idx=" + str(idx) + " nod=" + str(nod) + " center=" + str(center)
+  return result, min_dim, max_dim, elem_dim, nodes_force, nodes_support 
                 
-## find minimal and maximal coordinate
+# # find minimal and maximal coordinate
 # @param coordinates as from centered_elements
 def find_corners(centers):
   min = [1e30, 1e30, 1e30]
   max = [-1e30, -1e30, -1e30]
   
   if len(centers) == 1:
-    min = [0.0,0.0,0.0]
-    max = [1.0,1.0,0.0]
+    min = [0.0, 0.0, 0.0]
+    max = [1.0, 1.0, 0.0]
     
   
   for e in range(len(centers)):
@@ -103,6 +136,13 @@ def last_h5_step(hdf5_file):
 
   return int(last[5:])
           
+def read_displacement(hdf5_file,nr):
+  u = hdf5_file['/Results/Mesh/MultiStep_1/Step_'+str(nr)+'/mechDisplacement/mech/Nodes/Real'].value
+  return u
+
+def read_density(hdf5_file):
+  dens = hdf5_file['/Results/Mesh/MultiStep_1/Step_0/physicalPseudoDensity/mech/Elements/Real'].value
+  return dens
     
 # dumps meta data    
 def dump_h5_meta(hdf5_file):   
@@ -130,7 +170,7 @@ def dump_h5_meta(hdf5_file):
     
     des = None
           
-## Test for result
+# # Test for result
 def has_element(hdf5_file, name, given_step=99999):
   try:   
     step = min((given_step, last_h5_step(hdf5_file)))
@@ -151,20 +191,4 @@ def get_element(hdf5_file, name, region, given_step=99999):
     return hdf5_file[key].value
   except:
     raise Exception("cannot access '" + key + "' in " + str(hdf5_file.filename))
-  
 
-#f = h5py.File("/home/fwein/project/simp/hook.h5")
-#s1 = get_element(f, "design_stiff1_plain", "mech")
-#s2 = get_element(f, "design_stiff2_plain", "mech") 
-#angle = get_element(f, "design_rotAngle_plain", "mech")
- 
-#t = to_mech_tensor(eval("[2.607069, 1.484705, 0.1626158, 0,0, 0.3030707]")) 
- 
-#f = h5py.File("/home/fwein/project/simp/piezo_fmo.h5")
-# f = h5py.File("/home/fwein/tmp/l_sl-m_20-g_al-p_0.1-gamma_1e-07-tau_0.01.h5")
-#r  = centered_elements(f)
-#tensor = get_element(f, "mechTensor", "piezo")
-# tensor = get_element(f, "mechTensor", "mech")
-#rot = perform_rotations(tensor, 10)
-# rot = perform_rotations(tensor, t21=True)
-# orientational_stiffness(r, rot, 1500, 2.5).show()
