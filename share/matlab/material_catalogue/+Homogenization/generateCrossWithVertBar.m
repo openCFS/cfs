d61a76a3970248f2a4ab9f2c538a7a7b538057c4
Daniel Hübner <daniel.huebner@fau.de>
2015-12-04 17:36:42 +0000

    Removed matviz function show_sheared_cross (obsolete due to show_sheared_rot_cross).
    Added matviz function to visualize a rotated cross with an additional vertical bar.
    
    Changed Matlab script to generate a rotated cross with an additional
    vertical bar instead of two vertical frame parts (microstructures are
    identical in terms of homogenization).

--- share/matlab/material_catalogue/+Homogenization/generateCrossWithVertBar.m
+++ share/matlab/material_catalogue/+Homogenization/generateCrossWithVertBar.m
@@ -1,40 +1,39 @@
 function [ file, volume ] = generateCrossWithVertBar(point,filepath,nx)
 % GENERATECROSSWITHVERTBAR  -  Generates an orthogonal cross, which is rotated
-% by 45 degrees overlayed with vertical bars at the left and right edge.
+% by 45 degrees overlayed with a vertical bar.
 %
 % @param:
-%       point(1)  thickness of horizontal frame parts
-%       point(2)  thickness of vertical frame parts
-%       point(3)  thickness of the bar from the upper left corner to the
+%       point(1)  thickness of vertical bar
+%       point(2)  thickness of the bar from the upper left corner to the
 %                 lower right corner in normal direction
-%       point(4)  thickness of the bar from the upper right corner to the
+%       point(3)  thickness of the bar from the upper right corner to the
 %                 lower left corner in normal direction
 %       filepath  path to generated mesh file (optional)
 %       nx        resolution of mesh (optional)
 % 
 % 
-%       s2 
-%      xxxx                                 xxxx
-%      xxxx                               xxxxxx
-%      xxxxxx                           xxxxxxxx
-%      xxxx xxx                       xxxxxxxxxx
-%      xxxx   xxx                   xxxxxxx xxxx
-%      xxxx     xxx               xxxxxxx   xxxx
-%      xxxx        s3             s4        xxxx
-%      xxxx         xxx       xxxxxxx       xxxx
-%      xxxx           xxx   xxxxxxx         xxxx
-%      xxxx             xxxxxxxxx           xxxx
-%      xxxx             xxxoxxx             xxxx
-%      xxxx           xxxxxxxxx             xxxx
-%      xxxx         xxxxxxx   xxx           xxxx
-%      xxxx       xxxxxxx       xxx         xxxx
-%      xxxx     xxxxxxx           xxx       xxxx
-%      xxxx   xxxxxxx               xxx     xxxx
-%      xxxx xxxxxxx                   xxx   xxxx
-%      xxxxxxxxxx                       xxx xxxx
-%      xxxxxxxx                           xxxxxx
-%      xxxx                                 xxxx
-%      xxxx                                 xxxx
+%                          s2 
+%      xx                xxxxx              xxxx
+%       xxx              xxxxx            xxxxxx
+%         xxx            xxxxx          xxxxxxx
+%           xxx          xxxxx        xxxxxxx
+%             xxx        xxxxx      xxxxxxx 
+%               xxx      xxxxx    xxxxxxx   
+%                  s3    xxxxx    s4        
+%                   xxx  xxxxxxxxxxxx       
+%                     xxxxxxxxxxxxx         
+%                       xxxxxxxxx           
+%                       xxxoxxx             
+%                     xxxxxxxxx             
+%                   xxxxxxxxxxxxx           
+%                 xxxxxxxxxxxx  xxx         
+%               xxxxxxx  xxxxx    xxx       
+%             xxxxxxx    xxxxx      xxx     
+%           xxxxxxx      xxxxx        xxx   
+%         xxxxxxx        xxxxx          xxx 
+%       xxxxxxx          xxxxx            xxx
+%      xxxxxx            xxxxx              xxxx
+%      xxxx              xxxxx                xx
 % 
 
 if nargin < 3
@@ -55,9 +54,12 @@ s2 = point(1)*nx;
 s3 = point(2)*nx;
 s4 = point(3)*nx;
 
+% For periodic boundaries we generate the structure for a smaller grid
+% first and copy the first row and column later.
+nx = nx - 1;
+
 % Frame
-% horzBar = [1:round(s1/2),nx-round(s1/2)+1:nx];
-vertBar = [1:round(s2/2),nx-round(s2/2)+1:nx];
+vertBar = round((nx-s2)/2)+1:round((nx+s2)/2);
 density(:,vertBar) = 1;
 
 % Cross
@@ -67,6 +69,10 @@ for i = 1:nx
     bar2 = mod( (i-round(s4/2)+1 : i+round(s4/2)-1) - 1, nx ) + 1;
     density(i,nx-bar2+1) = 1;
 end
+
+% Copy first row and column to the opposite boundary -> periodicity!
+density(nx+1,:) = density(1,:);
+density(:,nx+1) = density(:,1);
 
 volume = sum(sum(density))/nx^2;
 
