7be3e9f3dc19651e5b7f7dd426f468fbf3af2f79
Fabian Wein <fabian.wein@fau.de>
2015-12-06 10:19:42 +0000

    work on nightly builds on eamc061 * skip site_specifiy_init, shall not be needed * rename trunk and activate now

--- ctest_scripts/sites/eamc061/generate_test_name_list.cmake
+++ ctest_scripts/sites/eamc061/generate_test_name_list.cmake
@@ -6,6 +6,7 @@ MACRO(GENERATE_TEST_NAME_LIST TEST_NAME_LIST)
     linux64_shared_opt_intel_release
     linux64_shared_opt_gcc_debug
     linux64_shared_opt_no_prec_gcc_release
+    linux64_trunk_intel_release
   )
 ENDMACRO()
 
