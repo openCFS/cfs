2f279fda43ec1dd40c49faa3372da0d5c6e312b4
Miriam Polzer <miriam.polzer@gmail.com>
2016-01-08 15:03:33 +0000

    ctest nightly script

--- ctest_scripts/sites/eamc067/generate_test_name_list.cmake
+++ ctest_scripts/sites/eamc067/generate_test_name_list.cmake
@@ -1,7 +1,7 @@
 # Manually set list of test names for the current site.
 MACRO(GENERATE_TEST_NAME_LIST TEST_NAME_LIST)
   SET(${TEST_NAME_LIST}
-    linux64_shared_opt_intel_debug
+    linux64_mpolzer_gcc_debug
   )
 ENDMACRO()
 
