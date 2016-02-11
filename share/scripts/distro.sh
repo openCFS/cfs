e48a8e8698581a1c5db3fa81a985361b62846666
Florian Toth <florian.toth@tuwien.ac.at>
2016-02-11 18:38:49 +0000

    bigfix to handle multiple minor revisions in distro.sh

--- share/scripts/distro.sh
+++ share/scripts/distro.sh
@@ -285,10 +285,10 @@ elif [ ${OS} = "Darwin" ]; then
 fi
 
 case "$(echo $DIST | sed 'y/'$LOWER'/'$UPPER'/')" in
-    "SCIENTIFIC") DIST_FAMILY="RHEL"; MAJOR_REV=$(echo ${REV} | sed -e 's/\.[0-9]$//') ;;
-    "CENTOS") DIST_FAMILY="RHEL"; MAJOR_REV=$(echo ${REV} | sed -e 's/\.[0-9]$//') ;;
-    "ORACLE") DIST_FAMILY="RHEL"; MAJOR_REV=$(echo ${REV} | sed -e 's/\.[0-9]$//') ;;
-    "RHEL") DIST_FAMILY="RHEL"; MAJOR_REV=$(echo ${REV} | sed -e 's/\.[0-9]$//') ;;
+    "SCIENTIFIC") DIST_FAMILY="RHEL"; MAJOR_REV=$(echo ${REV} | sed -e 's/\.[0-9.]*$//') ;;
+    "CENTOS") DIST_FAMILY="RHEL"; MAJOR_REV=$(echo ${REV} | sed -e 's/\.[0-9.]*$//');;
+    "ORACLE") DIST_FAMILY="RHEL"; MAJOR_REV=$(echo ${REV} | sed -e 's/\.[0-9.]*$//') ;;
+    "RHEL") DIST_FAMILY="RHEL"; MAJOR_REV=$(echo ${REV} | sed -e 's/\.[0-9.]*$//') ;;
     "SLE") DIST_FAMILY="SLE"; MAJOR_REV=${REV} ;;
     *) break ;;
 esac
