3cc473baf4c8f267ee5a383b2df469e39b177772
Daniel Hübner <daniel.huebner@fau.de>
2016-04-22 12:31:57 +0000

    Added xml file for 3D catalogue. Updated grid generation scripts for new version of sgpp.

--- share/matlab/material_catalogue/writeHeader.m
+++ share/matlab/material_catalogue/writeHeader.m
@@ -5,7 +5,7 @@ data = load(file);
 
 if any(data(:,1) > 1) % -> point given by level and index
     givenByLevelAndIndex = 1;
-    dim = ( size(data,2)-7 ) / 2;
+    dim = ( find( abs( data(2,:) - round(data(2,:)) ), 1)-1 ) / 2;
     level = max(data(:,1));
     m = size(data,2)-2*dim;
 else
@@ -18,26 +18,25 @@ else
     m = size(data,2)-dim;
 end
 
-size(data)
 % Write header and data
 fid = fopen(file,'wt');
 if givenByLevelAndIndex
     % sparsegrid N d m voigt 0 0 0...
-    format = [ 'sparsegrid\t%d\t%d\t%d\t%s\t%s\n'];
+    format = 'sparsegrid\t%d\t%d\t%d\t%s\t%s\n';
     fprintf(fid,format,size(data,1),dim,m,'nonhierarchized','voigt');
     % L1 L2 L3 I1 I2 I3 E11 E12 ...
-    format = [ repmat('%d\t',1,2*dim), repmat('%e\t',1,6), '%e\n'];
+    format = [ repmat('%d\t',1,2*dim), repmat('%e\t',1,m-1), '%e\n'];
     fprintf(fid,format,data');
 else
     if data(1,1) == .5
         % 3D L5 N voigt 0 0 0...
-        format = [ '%dD\tL%d\t%d\t%s', repmat('\t%e',1,7-4+dim) , '\n'];
+        format = [ '%dD\tL%d\t%d\t%s', repmat('\t%e',1,m-4+dim) , '\n'];
     else
         % 3D 1 N voigt 0 0 0...
-        format = [ '%dD\t%d\t%d\t%s', repmat('\t%e',1,7-4+dim) , '\n'];
+        format = [ '%dD\t%d\t%d\t%s', repmat('\t%e',1,m-4+dim) , '\n'];
     end
-    fprintf(fid,format,dim,level,size(data,1),'voigt',zeros(1,7-4+dim));
-    format = [ repmat('%.10f\t',1,dim) , repmat('%e\t',1,6), '%e\n'];
+    fprintf(fid,format,dim,level,size(data,1),'voigt',zeros(1,m-4+dim));
+    format = [ repmat('%.10f\t',1,dim) , repmat('%e\t',1,m-1), '%e\n'];
     fprintf(fid,format,data');
 end
 fclose(fid);
