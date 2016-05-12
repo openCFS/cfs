96870c49dbc121dbfa0b610f5812f5d0ecb77e5e
Fabian Wein <fabian.wein@fau.de>
2016-05-12 13:20:25 +0000

    Fixed bug for periodic local shape map constraints

--- share/python/find_bloch_band_gaps.py
+++ share/python/find_bloch_band_gaps.py
@@ -79,6 +79,7 @@ parser.add_argument('--gnuplot', help='create gnuplot output, specify the type',
 #parser.add_argument('--show',  help='use gnuplot to create a png and shows it', action='store_true',)
 parser.add_argument('--nolines', action='store_true', help='gnuplot: do not concatenate points by lines')
 parser.add_argument('--commonsymbol', action='store_true', help='gnuplot: use the same line symbol for all lines')
+parser.add_argument('--notitle', action='store_true', help="gnuplot: suppress line lables")
 parser.add_argument('--nicelabel', action='store_true', help='gnuplot: use nice labels')
 parser.add_argument('--horizontal', action='store_true', help='display only the horizontal part of the wavevector (G->X)')
 args = parser.parse_args()
@@ -150,8 +151,8 @@ gaps = None if args.xml is None else etree.SubElement(root, "gaps")
 if args.gnuplot:
   if args.gnuplot == "eps":
     print 'set size ratio 1.0'
-    print 'set terminal postscript eps enhanced "Helvetica, 20" color' #  change to monochrome for papers
-    print 'set output "' + args.bloch[:-len(".bloch.dat")] + '.eps"'
+    print 'set terminal postscript eps enhanced "Helvetica, 12" color' #  change to monochrome for papers
+    print 'set output "' + args.bloch[:-len(".dat")] + '.eps"'
   if args.gnuplot == "png":
     print 'set size ratio 1.0'
     print 'set terminal png font Helvetica size 1000,1000'
@@ -212,10 +213,10 @@ if args.gnuplot:
 
   
 
-  wl =   '' if args.nolines else ' with linespoints '
+  wl =   '' if args.nolines else ' with lines '
   lc = ' lc 7 lt 1 ' if args.commonsymbol else ''
   for i in range(offset,  max_mode): # 1-based
-    title = ' notitle ' if args.commonsymbol else ' t "' + str(i-offset+1) + '. mode" ' 
+    title = ' notitle ' if args.commonsymbol or args.notitle else ' t "' + str(i-offset+1) + '. mode" ' 
     print ('plot' if i <= offset else '    ') + '"' + args.bloch + '" u ' + str(i+1) + title + wl + lc + (' ,\\' if i < max_mode -1  else '')
  
  
