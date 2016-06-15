593831b793cb62a0f8dbccc327ac81d241564911
Florian Toth <florian.toth@tuwien.ac.at>
2016-06-15 17:31:39 +0000

    added some nicghtly tests

---
+++ ctest_scripts/sites/test-ubuntu14/generate_test_name_list.cmake
@@ -0,0 +1,8 @@
+# Manually set list of test names for the current site.
+MACRO(GENERATE_TEST_NAME_LIST TEST_NAME_LIST)
+  SET(${TEST_NAME_LIST}
+    linux64_trunk_gcc_release
+#    linux64_trunk_gcc_debug
+#    linux64_trunk_no-opt_gcc_release
+  )
+ENDMACRO()
--- ctest_scripts/sites/test-ubuntu14/site_specific_vars.cmake
+++ ctest_scripts/sites/test-ubuntu14/site_specific_vars.cmake
@@ -2,21 +2,25 @@
 # it is called by nightly_test.cmake and should be used in
 # all the tests on this site
 
+# these variables must be set:
 set(NIGHTLY_TESTSUITE_DIR "$ENV{HOME}/cfs/testsuite") # source of the testsuite
 set(NIGHTLY_CACHE_DIR "$ENV{HOME}/cfs/build/cache") # cache dir for builds
 set(NIGHTLY_SOURCE_DIR "$ENV{HOME}/cfs/trunk") # source of cfs
 set(NIGHTLY_BUILD_DIR "$ENV{HOME}/cfs/build") # where the build should be made (a sub-path should be appended for each test)
 
-# set the name of the machine
+# set the name of the machine (Site)
 if(NOT CFS_BUILD_HOST)
   site_name(CFS_BUILD_HOST) # sets CFS_BUILD_HOST to the name of the computer
   string(REPLACE "." ";" CFS_BUILD_HOST_LIST "${CFS_BUILD_HOST}") # puts it into list
   list(GET CFS_BUILD_HOST_LIST 0 CFS_BUILD_HOST) # gets first element
 endif(NOT CFS_BUILD_HOST)
-set(CTEST_SITE "${CFS_BUILD_HOST}")
+set(CTEST_SITE "mdmt.${CFS_BUILD_HOST}")
 
 # use git to update
 find_program(CTEST_GIT_COMMAND NAMES git)
 set(CTEST_UPDATE_TYPE "git") # use git
 set(CTEST_UPDATE_COMMAND "${CTEST_GIT_COMMAND}")
 set(CTEST_GIT_UPDATE_CUSTOM "${CTEST_GIT_COMMAND};svn;rebase") # use git-svn
+
+# load data on development server
+include("${NIGHTLY_SOURCE_DIR}/cmake_modules/DevelopmentServer.cmake")
