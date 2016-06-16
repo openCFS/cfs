a0223c859562157cdb156455dcaada85ba957834
Florian Toth <florian.toth@tuwien.ac.at>
2016-06-16 08:16:24 +0000

    more nightly tests again

--- ctest_scripts/sites/test-ubuntu14/generate_test_name_list.cmake
+++ ctest_scripts/sites/test-ubuntu14/generate_test_name_list.cmake
@@ -1,8 +1,12 @@
 # Manually set list of test names for the current site.
 MACRO(GENERATE_TEST_NAME_LIST TEST_NAME_LIST)
   SET(${TEST_NAME_LIST}
+    update_cfs_trunk
+    update_testsuite_trunk
     linux64_trunk_gcc_release
-#    linux64_trunk_gcc_debug
-#    linux64_trunk_no-opt_gcc_release
+    linux64_trunk_gcc_debug
+    linux64_trunk_no-opt_gcc_release
+    linux64_trunk_icc_release
+    linux64_trunk_icc_debug
   )
 ENDMACRO()
