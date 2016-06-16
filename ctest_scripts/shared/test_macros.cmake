18d57c4be72fddd85ac161f9c3a3308ef74b6598
Florian Toth <florian.toth@tuwien.ac.at>
2016-06-16 15:38:14 +0000

    sortened nighlty tests by replacing copy&past parts by macros

--- ctest_scripts/shared/test_macros.cmake
+++ ctest_scripts/shared/test_macros.cmake
@@ -381,6 +381,7 @@ MACRO(SITE_SPECIFIC_FINISH)
 #  MESSAGE("Leaving SITE_SPECIFIC_FINISH...")
 ENDMACRO()
 
+
 MACRO(INIT_CACHE CACHE_VAR)
   # =========================================================================
   #
@@ -396,118 +397,6 @@ MACRO(INIT_CACHE CACHE_VAR)
     "
   )
 ENDMACRO()
-
-
-macro(SET_COMPILER_ENV COMPILER_TYPE)
-  message("Setting environment for ${COMPILER_TYPE}")
-  # =========================================================================
-  #
-  #  Set evironment for different compilers (gcc and intel)
-  #
-  # =========================================================================
-  if(${COMPILER_TYPE} STREQUAL "GCC")
-    #-----------------------------------------------------------------------------
-    # Set the following environment variables for the test run. This can be used
-    # to specifiy the compilers and that all messages should be output in English
-    # language, so that CTest may properly parse them.
-    #-----------------------------------------------------------------------------
-    SET(ENV{CC} "/usr/bin/gcc")
-    SET(ENV{CXX} "/usr/bin/g++")
-    SET(ENV{FC} "/usr/bin/gfortran")
-    SET(ENV{LC_MESSAGES} "C")
-    SET(ENV{LC_ALL} "C")
-    SET(ENV{LANG} "C")
-    SET(ENV{LANGUAGE} "C")
-    # determine compiler info
-    SET(IDCOMP_TEMPL "${CTEST_SOURCE_DIRECTORY}/share/scripts/identify_compiler.cmake.in")
-    SET(COMPILER_ID_FILE "${CTEST_BINARY_DIRECTORY}/CMakeFiles/out.cmake")
-    SET(IDENTIFY_COMPILER_SRC "${CTEST_SOURCE_DIRECTORY}/share/scripts/IdentifyCXXCompiler.cpp")
-    SET(COMPILER "$ENV{CXX}")
-    SET(ID_CXX "${CTEST_BINARY_DIRECTORY}/share/scripts/identify_cxx.cmake")
-    CONFIGURE_FILE("${IDCOMP_TEMPL}" "${ID_CXX}" @ONLY)
-    # create build/tmp directory
-    EXECUTE_PROCESS(COMMAND "${CMAKE_COMMAND}" -E make_directory "${CTEST_BINARY_DIRECTORY}/tmp" WORKING_DIRECTORY "${CTEST_BINARY_DIRECTORY}" RESULT_VARIABLE RETVAL)
-    # read ID_CXX
-    EXECUTE_PROCESS(COMMAND "${CMAKE_COMMAND}" -P "${ID_CXX}" WORKING_DIRECTORY "${CTEST_BINARY_DIRECTORY}/tmp" RESULT_VARIABLE RETVAL)
-    INCLUDE("${COMPILER_ID_FILE}")
-
-  elseif(${COMPILER_TYPE} STREQUAL "ICC")
-
-    if(NOT INTEL_COMPILER_PATH)
-      set(INTEL_COMPILER_PATH "/share/programs/intel/composer_xe_2015.2.164")
-      message("INTEL_COMPILER_PATH not defined, guessing ${INTEL_COMPILER_PATH}")
-    endif(NOT INTEL_COMPILER_PATH)
-
-    SET(INTEL_COMPVARS_SH "${CTEST_BINARY_DIRECTORY}/CMakeFiles/out.sh")
-    SET(INTEL_COMPVARS_CMAKE "${CTEST_BINARY_DIRECTORY}/CMakeFiles/out.cmake")
-
-    FILE(WRITE "${INTEL_COMPVARS_SH}"
-    "
-    source ${INTEL_COMPILER_PATH}/bin/compilervars.sh intel64
-    ${CTEST_CMAKE_COMMAND} -E environment
-    ")
-
-    SET(SHCMD
-      sh
-      "${INTEL_COMPVARS_SH}"
-    )
-    EXECUTE_PROCESS(
-      COMMAND ${SHCMD}
-      OUTPUT_VARIABLE INTEL_ENV_SH
-    )
-    STRING(REPLACE "\n" ";" INTEL_ENV_SH ${INTEL_ENV_SH})
-    #message("XXX ${INTEL_ENV_SH}")
-
-    SET(INTEL_ENV_CMAKE "")
-    FOREACH(LINE IN ITEMS ${INTEL_ENV_SH})
-      #message("-> ${LINE}")
-      STRING(REPLACE "=" ";" LINE_TOKENS ${LINE}INTEL_ENV_SH)
-      LIST(GET LINE_TOKENS 0 VAR_NAME)
-      LIST(GET LINE_TOKENS 1 VAR_VALUE)
-      IF(NOT VAR_NAME STREQUAL "INTEL_LICENSE_FILE" AND
-         NOT VAR_NAME STREQUAL "PWD" AND
-         NOT VAR_NAME STREQUAL "SHLVL" AND
-         NOT VAR_NAME STREQUAL "_")
-        SET(INTEL_ENV_CMAKE "${INTEL_ENV_CMAKE}\nSET(ENV{${VAR_NAME}} \"${VAR_VALUE}\")")
-        #message("${INTEL_ENV_CMAKE}\nSET(ENV{${VAR_NAME}} \"${VAR_VALUE}\")")
-      ENDIF()
-    ENDFOREACH()
-
-    #message("INTEL_ENV_CMAKE XXX ${INTEL_ENV_CMAKE} XXX INTEL_ENV_CMAKE")
-    FILE(WRITE "${INTEL_COMPVARS_CMAKE}" "${INTEL_ENV_CMAKE}")
-    INCLUDE("${INTEL_COMPVARS_CMAKE}")
-
-    SET(ENV{LM_LICENSE_FILE} "/share/programs/intel/licenses/pmklicserv.lic")
-    SET(ENV{INTEL_LICENSE_FILE} "$ENV{LM_LICENSE_FILE}")
-    SET(ENV{CC} "icc")
-    SET(ENV{CXX} "icpc")
-    SET(ENV{FC} "ifort")
-    SET(ENV{LC_MESSAGES} "C")
-    SET(ENV{LC_ALL} "C")
-    SET(ENV{LANG} "C")
-    SET(ENV{LANGUAGE} "C")
-
-    SET(IDCOMP_TEMPL "${CTEST_SOURCE_DIRECTORY}/share/scripts/identify_compiler.cmake.in")
-    SET(COMPILER_ID_FILE "${CTEST_BINARY_DIRECTORY}/CMakeFiles/out.cmake")
-    SET(IDENTIFY_COMPILER_SRC "${CTEST_SOURCE_DIRECTORY}/share/scripts/IdentifyCXXCompiler.cpp")
-
-    # determine compiler info
-    SET(COMPILER "$ENV{CXX}")
-    SET(ID_CXX "${CTEST_BINARY_DIRECTORY}/share/scripts/identify_cxx.cmake")
-    CONFIGURE_FILE("${IDCOMP_TEMPL}" "${ID_CXX}" @ONLY)
-    # create build/tmp directory
-    EXECUTE_PROCESS(COMMAND "${CMAKE_COMMAND}" -E make_directory "${CTEST_BINARY_DIRECTORY}/tmp" WORKING_DIRECTORY "${CTEST_BINARY_DIRECTORY}" RESULT_VARIABLE RETVAL)
-    # read ID_CXX
-    EXECUTE_PROCESS(COMMAND "${CMAKE_COMMAND}" -P "${ID_CXX}" WORKING_DIRECTORY "${CTEST_BINARY_DIRECTORY}/tmp" RESULT_VARIABLE RETVAL)
-    # include compiler info
-    INCLUDE("${COMPILER_ID_FILE}")
-
-  else(${COMPILER_TYPE} STREQUAL "GCC")
-
-    message("can only set compiler environment for GCC or ICC, not for ${COMPILER}!")
-
-  endif(${COMPILER_TYPE} STREQUAL "GCC")
-endmacro()
 
 
 macro(IDENTIFY_DISTRO)
--- ctest_scripts/sites/test-ubuntu14/site_specific_vars.cmake
+++ ctest_scripts/sites/test-ubuntu14/site_specific_vars.cmake
@@ -1,12 +1,12 @@
-# this script sets varaibles appropriate to this site.
-# it is called by nightly_test.cmake and should be used in
-# all the tests on this site
+# This script sets varaibles appropriate to this site.
+# It is called by nightly_test.cmake and is also used
+# in all the tests on this site.
 
 # these variables must be set:
 set(NIGHTLY_TESTSUITE_DIR "$ENV{HOME}/cfs/testsuite") # source of the testsuite
 set(NIGHTLY_CACHE_DIR "$ENV{HOME}/cfs/build/cache") # cache dir for builds
 set(NIGHTLY_SOURCE_DIR "$ENV{HOME}/cfs/trunk") # source of cfs
-set(NIGHTLY_BUILD_DIR "$ENV{HOME}/cfs/build") # where the build should be made (a sub-path should be appended for each test)
+set(NIGHTLY_BUILD_DIR "$ENV{HOME}/cfs/build") # where the builds should be made (a sub-path is appended for each test)
 
 # set the name of the machine (Site)
 if(NOT CFS_BUILD_HOST)
@@ -14,7 +14,7 @@ if(NOT CFS_BUILD_HOST)
   string(REPLACE "." ";" CFS_BUILD_HOST_LIST "${CFS_BUILD_HOST}") # puts it into list
   list(GET CFS_BUILD_HOST_LIST 0 CFS_BUILD_HOST) # gets first element
 endif(NOT CFS_BUILD_HOST)
-set(CTEST_SITE "mdmt.${CFS_BUILD_HOST}")
+set(CTEST_SITE "mdmt.${CFS_BUILD_HOST}") # use mdmt in front to allow for sorting in cdash
 
 # use git to update
 find_program(CTEST_GIT_COMMAND NAMES git)
