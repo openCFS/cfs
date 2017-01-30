e186a0131ce1bcc1c816b671bc83fd1ae4a21566
Bich Ngoc Vu <bich.ngoc.vu@fau.de>
2017-01-30 10:25:10 +0000

    Reverted parallelization

--- share/python/basecell.py
+++ share/python/basecell.py
@@ -162,7 +162,6 @@ parser.add_argument('--export', help="export different stuff", choices=['radius_
 parser.add_argument('--force_bisec', help="take given bisec curve", choices=['bicubic','bspline','linear','heaviside'], required=False)
 parser.add_argument('--beta', help="steepness of heaviside function", type=float, default=10)
 parser.add_argument('--eta', help="midpoint heaviside function", type=float, default=0.5)
-parser.add_argument('--j', help="number of parallel threads", type=int, default=1)
 parser.add_argument('--logging',help="print logging while fixing surface gaps to log_fix_surface_gaps.txt", action='store_true',default=False,required=False)
 
 args = parser.parse_args()
