4babe5e90409dc64299150c013bbbc56837fe98c
Fabian Wein <fwein@fau.de>
2017-05-02 11:02:14 +0000

    Prepare eamc080 nightly builds
    It should work with a crontab like this
    
    # DO NOT EDIT THIS FILE - edit the master and reinstall.
    # (/tmp/crontab.1my5uQ installed on Sun Apr  2 12:00:18 2017)
    # (Cronie version 4.2)
    05  01  *   *   * source /home/eamnightly/.bashrc; /usr/bin/ctest -S /home/eamnightly/code/shared/ctest_scripts/nightly_test.cmake -V > /home/eamnightly/code/cronjobs.log 2>&1
    30  03  *   *   * source /home/eamnightly/.bashrc; /usr/bin/ctest -S /home/eamnightly/code/trunk/ctest_scripts/nightly_test.cmake -V > /home/eamnightly/code/cronjobs.trunk.log 2>&1

--- ctest_scripts/sites/eamc080/generate_test_name_list.cmake
+++ ctest_scripts/sites/eamc080/generate_test_name_list.cmake
@@ -3,15 +3,10 @@ MACRO(GENERATE_TEST_NAME_LIST TEST_NAME_LIST)
   IF(SITE_DIR MATCHES "trunk")
     SET(${TEST_NAME_LIST} 
      linux64_trunk_gcc_release
-     linux64_trunk_gcc_debug
-     linux64_trunk_intel_release
-     linux64_trunk_clang_release)
+     linux64_trunk_gcc_debug)
   ELSE()
     SET(${TEST_NAME_LIST}
      linux64_shared_opt_gcc_release
-     linux64_shared_opt_clang_release
-     linux64_shared_opt_intel_release
-     linux64_shared_opt_gcc_debug
-     linux64_shared_opt_no_prec_gcc_release)
+     linux64_shared_opt_gcc_debug)
   ENDIF()  
 ENDMACRO()
