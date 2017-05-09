9fa190696864140d41e9dc6607090fed22b5f52f
Florian Toth <florian.toth@tuwien.ac.at>
2017-05-09 08:02:00 +0000

    fix for nightly tests @ TU-Wien
     - init functions must be run as a macro in the test file
       to generate the correct binary path
     - hopefully this was the last fix now ...

--- ctest_scripts/sites/test-ubuntu14/init_test.cmake
+++ ctest_scripts/sites/test-ubuntu14/init_test.cmake
@@ -8,6 +8,8 @@ include("${CMAKE_CURRENT_LIST_DIR}/site_specific_vars.cmake")
 # load data on development server
 include("${NIGHTLY_SOURCE_DIR}/cmake_modules/DevelopmentServer.cmake")
 
+# This initializes the test and must be invoked in the actual test file
+macro(INIT_TEST)
 # set source and binary dirs
 SET(CTEST_SOURCE_DIRECTORY "${NIGHTLY_SOURCE_DIR}")
 get_filename_component(FNAME "${CMAKE_CURRENT_LIST_FILE}" NAME_WE)
@@ -29,4 +31,4 @@ WRITE_CTEST_CONFIG()
 # set ctest & cmake commands to the executables corresponding to the one used to run this script.
 SET(CTEST_COMMAND  "\"${CTEST_EXECUTABLE_NAME}\"")
 SET(CTEST_CMAKE_COMMAND  "\"${CMAKE_EXECUTABLE_NAME}\"")
-
+endmacro(INIT_TEST)
