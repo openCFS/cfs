60de89b8716d99c77f6b52c5da255713e58341e3
Fabian Wein <fwein@fau.de>
2017-10-25 23:48:48 +0000

    remove Open64 compiler support. Develoment ceased 2011, it won't be of practical usage for cfs

--- cmake_modules/compiler.cmake
+++ cmake_modules/compiler.cmake
@@ -313,20 +313,7 @@ ELSEIF(CFS_CXX_COMPILER_NAME STREQUAL "ICC") # strange, as the c-compiler is icc
     SET(CFS_SUPPRESSIONS "${CFS_SUPPRESSIONS} -fno-builtin-std::basic_istream::get")
     SET(CFS_SUPPRESSIONS "${CFS_SUPPRESSIONS} -fno-builtin-std::max")
   ENDIF(CFS_CXX_COMPILER_VER MATCHES "11\\.")
-  
-  # The  intel  compiler might  not  know  the  function __builtin_isnan  (and
-  #  isinf), so redirect that to isnan
-  # comment it out as it breaks with icc 2016 and gcc 6 libs
-  #SET(CFS_CXX_FLAGS "${CFS_CXX_FLAGS} -D__builtin_isnan=::isnan -D__builtin_isinf=::isinf")
-  # end icc section
-ELSEIF(CFS_CXX_COMPILER_NAME STREQUAL "OPEN64")
-  IF(NOT USE_CGAL)
-    SET(CFS_C_FLAGS "-pedantic ${CFS_C_FLAGS}")
-    SET(CFS_CXX_FLAGS "-pedantic ${CFS_CXX_FLAGS}")
-  ELSE(NOT USE_CGAL)
-    SET(CFS_C_FLAGS "-mieee-fp -fp-accuracy=strict -DCGAL_DISABLE_ROUNDING_MATH_CHECK ${CFS_C_FLAGS}")
-    SET(CFS_CXX_FLAGS "-mieee-fp -fp-accuracy=strict -DCGAL_DISABLE_ROUNDING_MATH_CHECK ${CFS_CXX_FLAGS}")
-  ENDIF()
+  # Open64 compiler support removed. See svn version 15997  
 ENDIF() # close all CXX compiler specific blocks
 
 # common for all compilers
