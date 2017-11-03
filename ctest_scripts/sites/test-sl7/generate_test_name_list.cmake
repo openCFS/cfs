eaffa64b415840072f5b60e98a49076672420f4a
Florian Toth <florian.toth@tuwien.ac.at>
2017-11-03 15:06:22 +0000

    disable ICC tests @ TU WIEN
     - MKL2018 needs ICC >= 16

--- ctest_scripts/sites/test-sl7/generate_test_name_list.cmake
+++ ctest_scripts/sites/test-sl7/generate_test_name_list.cmake
@@ -9,6 +9,5 @@ MACRO(GENERATE_TEST_NAME_LIST TEST_NAME_LIST)
     linux64_trunk_gcc_release
     linux64_trunk_gcc_debug
     linux64_trunk_no-opt_gcc_release
-    linux64_trunk_icc_release
   )
 ENDMACRO()
