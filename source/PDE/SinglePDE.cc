d46886ed8ba423a188121c3327e286e1f90126c3
Fabian Wein <fwein@fau.de>
2018-01-15 14:49:43 +0000

    intermediate commit. Works for non-sym bulk2d_20

--- source/PDE/SinglePDE.cc
+++ source/PDE/SinglePDE.cc
@@ -1238,6 +1238,9 @@ namespace CoupledField {
         << ": WriteResultsInFile() kstep: " <<  kstep
         << " actTimeFreq: " << actTimeFreq;
     
+    shared_ptr<Timer> timer = infoNode_->Get(ParamNode::SUMMARY)->Get("writeResults/timer")->AsTimer();
+    timer->Start();
+
     // ===================================================
     //  Trigger calculation of interpolated field results 
     // ===================================================
@@ -1415,8 +1418,8 @@ namespace CoupledField {
       }
 
       out.close();
-      
     }
+    timer->Stop();
   }
   
   
