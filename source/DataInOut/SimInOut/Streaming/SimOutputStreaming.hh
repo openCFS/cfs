c5af2e7a141988d8e017f3c5ba9deed7ebaca046
Simon Michalke <simon.michalke@fau.de>
2018-01-31 15:44:27 +0000

    finish streaming with guaranteed last file being sent; using self-deleting clients

--- source/DataInOut/SimInOut/Streaming/SimOutputStreaming.hh
+++ source/DataInOut/SimInOut/Streaming/SimOutputStreaming.hh
@@ -5,13 +5,14 @@
 #include <string>
 #include <thread>
 #include <mutex>
-#include <condition_variable>
+#include <atomic>
 
 #include "DataInOut/ParamHandling/ParamNode.hh"
 #include "DataInOut/SimOutput.hh"
 #include "Utils/StdVector.hh"
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
+#include <boost/thread.hpp>
 
 namespace CoupledField {
 class BaseResult;
@@ -47,8 +48,11 @@ namespace CoupledField
     //! Begin single analysis step
     void BeginStep( UInt stepNum, Double stepVal );
 
-    /** This transmits the data */
+    /** This calls data transmit */
     void FinishStep();
+
+    /** This transmits the data */
+    void TransmitData(bool force);
 
     /** Mesh results are node and cell results */
     void AddResult( shared_ptr<BaseResult> sol );
@@ -70,8 +74,9 @@ namespace CoupledField
     class Client
     {
     public:
-      Client(boost::asio::io_service& io_service,
-          const std::string& server, const std::string& port, const std::string& path, SimOutputStreaming* base);
+      Client(boost::asio::io_service& io_service, SimOutputStreaming* base);
+
+      void Send(const std::string& server, const std::string& port, const std::string& path);
 
     private:
       void handle_resolve(const boost::system::error_code& err,
@@ -87,6 +92,8 @@ namespace CoupledField
       void handle_read_headers(const boost::system::error_code& err);
 
       void handle_read_content(const boost::system::error_code& err);
+
+      SimOutputStreaming* base_;
 
       tcp::resolver resolver_;
       tcp::socket socket_;
@@ -106,7 +113,11 @@ namespace CoupledField
     /** this is the according threading object **/
     std::thread io_service_thread;
 
+    /** this pseudowork hinders the io_service.run() to exit immediately after sending first data **/
     boost::shared_ptr<boost::asio::io_service::work> io_service_work;
+
+    /** the current sending client, used to determine whether stuff is being sent at the moment or not **/
+    Client* current_client;
 
     /** do http streaming or file? */
     bool http_;
