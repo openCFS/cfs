a0d4917acc4dd6c129770648b4bd4990c8cb99f2
Simon Michalke <simon.michalke@fau.de>
2018-01-31 15:44:27 +0000

    finish streaming with guaranteed last file being sent; using self-deleting clients

--- source/DataInOut/SimInOut/Streaming/SimOutputStreaming.cc
+++ source/DataInOut/SimInOut/Streaming/SimOutputStreaming.cc
@@ -8,10 +8,17 @@
 #include "Domain/Mesh/Grid.hh"
 #include "Domain/Results/ResultInfo.hh"
 #include "SimOutputStreaming.hh"
+#include "DataInOut/Logging/LogConfigurator.hh"
+#include "DataInOut/Logging/log.hpp"
 
 using boost::asio::ip::tcp;
 
 using namespace CoupledField;
+
+
+DECLARE_LOG(SOS)
+DEFINE_LOG(SOS, "streaming")
+
 
 SimOutputStreaming::SimOutputStreaming(PtrParamNode outputNode, PtrParamNode infoNode, bool isRestart) :
   SimOutput("", outputNode, infoNode, isRestart),
@@ -31,22 +38,23 @@ SimOutputStreaming::SimOutputStreaming(PtrParamNode outputNode, PtrParamNode inf
   content_ = PtrParamNode(new ParamNode(ParamNode::INSERT));
   content_->SetName("cfsStreaming");
 
+  //wait_var = new std::condition_variable();
+  current_client = NULL;
+
   // start the sending thread
   io_service_thread.detach();
-
-  //wait_var = new std::condition_variable();
 }
 
 SimOutputStreaming::~SimOutputStreaming()
 {
   // send last .info.xml to receive status="finished" and memory data
-  FinishStep();
+  // set force to true
+  TransmitData(true);
 
-  io_service_work.reset();
+  io_service_work.reset(); // tell the dummy work to stop
 
-  sleep(1);
-
-  //io_service_thread.join();
+  io_service.run(); // joining the C++ thread is buggy for some reason
+  // this will do it as well
 }
 
 void SimOutputStreaming::Init(Grid * ptGrid, bool printGridOnly)
@@ -87,19 +95,39 @@ void SimOutputStreaming::AddResult( shared_ptr<BaseResult> sol)
   results_.Push_back(sol);
 }
 
-void SimOutputStreaming::FinishStep()
-{
-  if(http_)
-  {
-    /// TODO: make suicidal clients that delete themself
-    new Client(io_service, host_, port_, path_, this);
-    results_.Clear();
-  }
-  else
-  {
-     std::ofstream out(path_.c_str());
-     Transmit(out);
-     out.close();
+void SimOutputStreaming::FinishStep() {
+  TransmitData(false);
+}
+
+void SimOutputStreaming::TransmitData(bool force) {
+  if (http_) {
+    /** we do not want to send more than one thing at a time,
+     * otherwise client suicide will mess everything up **/
+    if (force) {
+      //int i=0;
+      while(current_client != NULL) {
+        sleep(0.01); // kindof busy sleep because this will only be executed at the very end
+        /*i++;
+        if (i>2000) {
+          // if this takes longer than 20 seconds, orphan the running client:
+          current_client = NULL; // because we still want to try sending data
+          // (to avoid deadlock)
+          break;
+        }*/
+      }
+    }
+    if ( current_client == NULL ) {
+      // this client will destroy itself upon finishing
+      // is the requests fails, it will lock up and skip sending data
+      // (except for the final data which will still be tried)
+      current_client = new Client(io_service, this);
+      current_client->Send(host_, port_, path_);
+      results_.Clear();
+    }
+  } else {
+    std::ofstream out(path_.c_str());
+    Transmit(out);
+    out.close();
   }
 }
 
@@ -196,24 +224,27 @@ void SimOutputStreaming::Transmit(std::ostream& out)
 }
 
 void SimOutputStreaming::io_service_runner(void) {
-  std::cout << "starting io_service thread ..." << std::endl;
+  LOG_DBG(SOS) << "++ starting io_service thread ..." << std::endl;
   io_service.run();
-  std::cout << "io_service thread ended" << std::endl;
+  LOG_DBG(SOS) << "++ io_service thread ended" << std::endl;
 }
 
 void SimOutputStreaming::io_service_runner_wrapper(SimOutputStreaming* this_) {
   this_->io_service_runner();
 }
 
-SimOutputStreaming::Client::Client(boost::asio::io_service& io_service,
-      const std::string& server, const std::string& port, const std::string& path, SimOutputStreaming* base)
-    : resolver_(io_service),
-      socket_(io_service)
+SimOutputStreaming::Client::Client(boost::asio::io_service& io_service, SimOutputStreaming* base)
+: resolver_(io_service),
+  socket_(io_service) {
+  base_ = base;}
+
+void SimOutputStreaming::Client::Send(const std::string& server, const std::string& port, const std::string& path)
 {
     // it is more robust known the content length a priori, therefore we use a memory stream
     std::stringstream mem;
     // fill with all our data
-    base->Transmit(mem);
+    base_->Transmit(mem);
+
 
     std::ostream request_stream(&request_);
     request_stream << "POST " << path << " HTTP/1.1\r\n";
@@ -226,8 +257,7 @@ SimOutputStreaming::Client::Client(boost::asio::io_service& io_service,
     // copy data
     request_stream << mem.rdbuf() << "\r\n\r\n";
 
-    if(!base->silent_)
-      std::cout << "try to connect " << server << " port " << port << " to transmit " << mem.tellp() << " bytes" << std::endl;
+    LOG_DBG(SOS) << "try to connect " << server << " port " << port << " to transmit " << mem.tellp() << " bytes" << std::endl;
 
     // Start an asynchronous resolve to translate the server and service names
     // into a list of endpoints.
@@ -343,8 +373,9 @@ void SimOutputStreaming::Client::handle_read_headers(const boost::system::error_
       //std::cout << "\n";
 
       // Write whatever content we already have to output.
-      if (response_.size() > 0)
-        std::cout << &response_;
+      if (response_.size() > 0) {
+        LOG_DBG(SOS) << "S:C:hrh: " << &response_;
+      }
 
       // Start reading remaining data until EOF.
       boost::asio::async_read(socket_, response_,
@@ -363,7 +394,7 @@ void SimOutputStreaming::Client::handle_read_content(const boost::system::error_
     if (!err)
     {
       // Write all of the data that has been read so far.
-      std::cout << &response_;
+      LOG_DBG(SOS) << "S:C:hrc: " << &response_;
 
       // Continue reading remaining data until EOF.
       boost::asio::async_read(socket_, response_,
@@ -371,9 +402,18 @@ void SimOutputStreaming::Client::handle_read_content(const boost::system::error_
           boost::bind(&Client::handle_read_content, this,
             boost::asio::placeholders::error));
     }
-    else if (err != boost::asio::error::eof)
-    {
-      std::cout << "Error: " << err << "\n";
+    else {
+      if (err != boost::asio::error::eof) {
+        std::cout << "Error: " << err << "\n";
+      } else {
+        SimOutputStreaming* base_copy = base_;
+
+        // commit suicide here
+        // DO NOT ACCESS ANY MEMBER VARIABLES AFTER DELETE!
+        delete base_->current_client;
+
+        base_copy->current_client = NULL; // tell streaming that the client comitted suicide
+      }
     }
 }
 
