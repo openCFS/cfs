fa2e785d9d000bde04d2ea9103d924fa9f7a2a86
Fabian Wein <fwein@fau.de>
2018-02-14 14:09:32 +0000

    LOG_*() ist not thread save. The dirty fix is to log only for thread 0. The clean way would be to upgrade to current boost which provides multithreaded logging. The drawback the necessary a -Wno-dangling-else
    guard Grid::CalcGridBoundingBox() as critical section. It could be that his fix solved other strange segfaults for parallel running?!

--- source/DataInOut/Logging/LogConfigurator.hh
+++ source/DataInOut/Logging/LogConfigurator.hh
@@ -7,6 +7,14 @@
 #include "DataInOut/Logging/functions.hpp"
 #include <boost/limits.hpp>
 
+// this is a dirty hack to prevent issues with parallel loggin.
+// We only log for thread 0. It would probably better to move to the current
+// boost logging were a multithreaded version is provided: _mt
+#include "def_use_openmp.hh"
+#ifdef USE_OPENMP
+  #include <omp.h>
+#endif
+
 using namespace boost::logging;
 
 // Create customized levels
@@ -32,12 +40,17 @@ BOOST_LOG_DEFINE_LEVEL(trace2, 1450)
 #define LOG(log_name) BOOST_LOG(log_name)
 
 
+// see the comments at #include "def_use_openmp.hh"
+// we have to set -Wno-dangling-else. Remove it in compiler.cmake if this construct is removed
+#define THREAD_0_LOGL(log_name, lvl) if(LogConfigurator::GetThreadNum() == 0) BOOST_LOGL(log_name,lvl)
+
+
 #ifndef NDEBUG
-#define LOG_DBG3(log_name) BOOST_LOGL(log_name,dbg3)
-#define LOG_DBG2(log_name) BOOST_LOGL(log_name,dbg2)
-#define LOG_DBG(log_name) BOOST_LOGL(log_name,dbg)
-#define LOG_TRACE2(log_name) BOOST_LOGL(log_name,trace2)
-#define LOG_TRACE(log_name) BOOST_LOGL(log_name,trace)
+#define LOG_DBG3(log_name) THREAD_0_LOGL(log_name,dbg3)
+#define LOG_DBG2(log_name) THREAD_0_LOGL(log_name,dbg2)
+#define LOG_DBG(log_name) THREAD_0_LOGL(log_name,dbg)
+#define LOG_TRACE2(log_name) THREAD_0_LOGL(log_name,trace2)
+#define LOG_TRACE(log_name) THREAD_0_LOGL(log_name,trace)
 #else
 #define LOG_DBG3(log_name) if(false) std::cout
 #define LOG_DBG2(log_name) if(false) std::cout
@@ -70,6 +83,15 @@ namespace CoupledField {
     //! Set logging level
     void SetLogLevel( const std::string& logStream,
                       const std::string& level );
+
+    static int GetThreadNum()
+    {
+    #ifdef USE_OPENMP
+      return 2; //omp_get_thread_num();
+    #else
+      return 0;
+    #endif
+    }
     
   protected:
     
