05b581742673c7bda6c8f0836b675a84e4f32656
Florian Toth <florian.toth@tuwien.ac.at>
2018-03-25 17:35:17 +0000

    added structure for averaged region results
    We use ResultFunctorIntegrate to obtain the integral of an
    arbitrary CoefFunction over one region. When setting `average_`
    to true, the integral is devided by the region volume.
    
    Thic can be used for computing
     - homogenised results
     - center of mass
     - center of volume
     - centroid of areas
     - reaction moment
     - ...
    
    Currently implemnted in MechPDE for stress and strain, i.e.
    the new region results 'mechStressAverage' and 'mechStrainAverage'.
    
    One limitation is that we cannot combine two regions, because
    we lack the necessary structure, however, this is easily done
    in a postprocessing step.
    
    C++ Tipp: When you want to dynamic_cast a shared_pointer ptr you need ptr.get() as the argument of the cast.

--- source/General/Environment.cc
+++ source/General/Environment.cc
@@ -1322,6 +1322,9 @@ namespace CoupledField {
     SolutionTypeEnum.Add(MECH_DYADIC_STRAIN_SUM, "mechDyadicStrainSum");
     SolutionTypeEnum.Add(MECH_QUAD_DISP, "mechQuadDisplacement");
     SolutionTypeEnum.Add(MECH_QUAD_DISP_SUM, "mechQuadDisplacementSum");
+    // averages
+    SolutionTypeEnum.Add(MECH_STRAIN_AVERAGE, "mechStrainAverage");
+    SolutionTypeEnum.Add(MECH_STRESS_AVERAGE, "mechStressAverage");
 
 
     SolutionTypeEnum.Add(MECH_PSEUDO_DENSITY, "mechPseudoDensity");
