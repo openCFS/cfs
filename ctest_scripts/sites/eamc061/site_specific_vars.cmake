a837cde87b33aba53b1d07d745a9a1aef66c9317
Fabian Wein <fabian.wein@fau.de>
2018-07-07 15:33:17 +0200

    one more attempt to run master testcases

--- ctest_scripts/sites/eamc061/site_specific_vars.cmake
+++ ctest_scripts/sites/eamc061/site_specific_vars.cmake
@@ -1,15 +1,19 @@
 # shall be called on every test.
 # loaded by SET_SITE_SPECIFIC_VARS() which is called by nightly_test.cmake
+# and also be each master testcase. 
 message("execute site_specific_vars.cmake")
-message("SITE_DIR=${SITE_DIR}")
+
+# shall be already set in the test to be able to find this file!
+if(NOT CTEST_SCRIPTS_DIR)
+  GET_FILENAME_COMPONENT(CTEST_SCRIPTS_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+endif()  
 
 # for the master we test the master at TU-Wien and the copy at group_stingl
-if(SITE_DIR MATCHES "master_wien")
+if(CTEST_SCRIPTS_DIR MATCHES "master_wien")
   set(MASTER "master_wien")
-elseif(SITE_DIR MATCHES "master_wien")
+elseif(CTEST_SCRIPTS_DIR MATCHES "master_wien")
   set(MASTER "master_stingl")
 endif()
 
-message("find and set git")
 find_program(CTEST_GIT_COMMAND NAMES git) 
 set(CTEST_UPDATE_TYPE "git")
