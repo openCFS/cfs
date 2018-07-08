440186e8bf9fa7673d32ffb7f59030d4afcca8a3
Fabian Wein <fabian.wein@fau.de>
2018-07-08 13:51:17 +0200

    towards testsuite

--- ctest_scripts/sites/eamc061/site_specific_vars.cmake
+++ ctest_scripts/sites/eamc061/site_specific_vars.cmake
@@ -3,9 +3,12 @@
 # and also be each master testcase. 
 message("execute site_specific_vars.cmake")
 
+message("CTEST_SCRIPTS_DIR = ${CTEST_SCRIPTS_DIR}")
+
 # shall be already set in the test to be able to find this file!
 if(NOT CTEST_SCRIPTS_DIR)
   GET_FILENAME_COMPONENT(CTEST_SCRIPTS_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+  message("set CTEST_SCRIPTS_DIR = ${CTEST_SCRIPTS_DIR}")
 endif()  
 
 # for the master we test the master at TU-Wien and the copy at group_stingl
@@ -13,6 +16,8 @@ if(CTEST_SCRIPTS_DIR MATCHES "master_wien")
   set(MASTER "master_wien")
 elseif(CTEST_SCRIPTS_DIR MATCHES "master_wien")
   set(MASTER "master_stingl")
+else()
+  message("MASTER not set, CTEST_SCRIPTS_DIR = ${CTEST_SCRIPTS_DIR}")  
 endif()
 
 find_program(CTEST_GIT_COMMAND NAMES git) 
