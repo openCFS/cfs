ea6015fa43090621af27fa78f34c4429f323675a
Simon Michalke <simon.michalke@fau.de>
2018-08-20 15:39:04 +0200

    fixed mingw

--- cfsdeps/boost/External_Boost.cmake
+++ cfsdeps/boost/External_Boost.cmake
@@ -92,9 +92,46 @@ IF(MINGW)
   SET(BOOST_BOOTSTRAP_PARAMS --with-toolset=gcc --without-libraries=context --without-libraries=coroutine)
   SET(BOOST_JAM_PATCH_COMMAND echo "using gcc : mingw : x86_64-w64-mingw32-gcc $<SEMICOLON>" > user-config.jam)
   SET(BOOST_JAM_PARAMS --user-config=user-config.jam toolset=gcc-mingw )
+
+  SET(EXT_ZLIB_zlib_prefix "${BOOST_prefix}/ext_zlib" )
+
+  SET(EXT_ZLIB_MIRRORS
+    "http://zlib.net/${ZLIB_GZ}"
+    "http://fossies.org/linux/misc/${ZLIB_GZ}"
+    "${ZLIB_URL}/${ZLIB_GZ}"
+  )
+  SET(EXT_ZLIB_LOCAL_FILE "${CFS_DEPS_CACHE_DIR}/sources/zlib/${ZLIB_GZ}")
+  SET(EXT_ZLIB_MD5_SUM ${ZLIB_MD5})
+
+  SET(EXT_ZLIB_DLFN "${zlib_prefix}/zlib-download.cmake")
+  CONFIGURE_FILE(
+    "${CFS_SOURCE_DIR}/cmake_modules/cfsdeps_download.cmake.in"
+    "${EXT_ZLIB_DLFN}"
+    @ONLY
+  )
+
+  ExternalProject_Add(boost_download_zlib
+    PREFIX ${EXT_ZLIB_zlib_prefix}
+    SOURCE_DIR ${EXT_ZLIB_zlib_source}
+    URL ${EXT_ZLIB_LOCAL_FILE}
+    URL_MD5 ${ZLIB_MD5}
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+  )
+
+  ExternalProject_Add_Step(boost_download_zlib cfsdeps_download
+    COMMAND ${CMAKE_COMMAND} -P "${EXT_ZLIB_DLFN}"
+    DEPENDERS download
+    DEPENDS "${EXT_ZLIB_DLFN}"
+    WORKING_DIRECTORY ${zlib_prefix}
+  )
+
+  SET(BOOST_ZLIB_SOURCE ${EXT_ZLIB_zlib_prefix}/src/boost_download_zlib)
+  SET(BOOST_JAM_PARAMS ${BOOST_JAM_PARAMS} -s ZLIB_SOURCE="${BOOST_ZLIB_SOURCE}" -s ZLIB_INCLUDE="${BOOST_ZLIB_SOURCE}" -s NO_ZLIB=0 ) #-s NO_BZIP2=1)
 ELSEIF(CFS_CXX_COMPILER_NAME STREQUAL "ICC")
   SET(BOOST_BOOTSTRAP_PARAMS --with-toolset=intel-linux)
-  SET(BOOST_JAM_PATCH_COMMAND echo "using intel : linux : icc : <compileflags>${CFS_C_FLAGS} $<SEMICOLON> using intel : linux : icpc : <compileflags>${CFS_CXX_FLAGS} $<SEMICOLON> " > user-config.jam)
+  SET(BOOST_JAM_PATCH_COMMAND echo "using : : : <compileflags>${CFS_C_FLAGS} $<SEMICOLON>" > user-config.jam) # using intel : linux : c++ : <compileflags>${CFS_CXX_FLAGS} $<SEMICOLON> " > user-config.jam)
 ENDIF()
 
 
@@ -149,6 +186,10 @@ ELSE()
     BUILD_COMMAND ./bootstrap.sh --without-libraries=python --prefix=${BOOST_install} ${BOOST_BOOTSTRAP_PARAMS} ${BOOST_EXTRA_PARAMS}
     INSTALL_COMMAND ./b2 ${BOOST_EXTRA_PARAMS} ${BOOST_JAM_PARAMS} threading=multi install
   )
+
+  IF(MINGW)
+    ADD_DEPENDENCIES(boost boost_download_zlib)
+  ENDIF()
 
   #-------------------------------------------------------------------------------
   # Add custom download step to be able to download from a list of mirrors
