1e1a6d0ec8c0eeb7e928df85bcbf03881547d5ab
Srikrishna <srikrishna.jaganathan@fau.de>
2018-09-07 15:28:33 +0200

    Revert back to old ilupack - temp commit

--- cfsdeps/metis/External_METIS.cmake
+++ cfsdeps/metis/External_METIS.cmake
@@ -107,9 +107,9 @@ IF("${CFS_DEPS_PRECOMPILED}" STREQUAL "ON" AND EXISTS "${PRECOMPILED_PCKG_FILE}"
     BUILD_COMMAND ""
     INSTALL_COMMAND ""
   )
-  IF(USE_ILUPACK)
-    add_dependencies(metis ilupack)
-  ENDIF()
+#  IF(USE_ILUPACK)
+#    add_dependencies(metis ilupack)
+#  ENDIF()
 ELSE("${CFS_DEPS_PRECOMPILED}" STREQUAL "ON" AND EXISTS "${PRECOMPILED_PCKG_FILE}")
   #-------------------------------------------------------------------------------
   # If precompiled package does not exist build external project
@@ -125,9 +125,9 @@ ELSE("${CFS_DEPS_PRECOMPILED}" STREQUAL "ON" AND EXISTS "${PRECOMPILED_PCKG_FILE
   )
   # The ilupack has a metis.h from a newer version of metis which is not compatible with CFS, so building it in this way 
   # replaces the ilupack metis.h with the older metis library's metis.h.
-  IF(USE_ILUPACK)
-    add_dependencies(metis ilupack)
-  ENDIF()
+#  IF(USE_ILUPACK)
+#    add_dependencies(metis ilupack)
+#  ENDIF()
 
   #-------------------------------------------------------------------------------
   # Add custom download step to be able to download from a list of mirrors
