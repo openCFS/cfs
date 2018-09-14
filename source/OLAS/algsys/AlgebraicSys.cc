f487c43ef22d710f11132f3f254d2af1a9ed5568
Florian Toth <florian.toth@tuwien.ac.at>
2018-09-14 18:59:29 +0200

    commented out because it produces a segfault - only for small problems ???

--- source/OLAS/algsys/AlgebraicSys.cc
+++ source/OLAS/algsys/AlgebraicSys.cc
@@ -312,7 +312,7 @@ namespace CoupledField {
     }
 
     // Log what we will do
-    PrintFeMatrixInfo();
+    //PrintFeMatrixInfo();
 
     // --------------------------------------------
     //  Treatment of Dirichlet Boundary Conditions
