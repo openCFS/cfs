eaa366788464f099f6cf8d699405f6a183cff8f1
Klaus Roppert <klaus.roppert@tuwien.ac.at>
2018-11-27 15:32:42 +0100

    Adapted cfstool for multiharmonic analysis

--- source/cfstool/cfstool.cc
+++ source/cfstool/cfstool.cc
@@ -1185,7 +1185,7 @@ namespace CFSTool {
                       std::cout << "\t\tAverage: " << diffAvg << " (difference), "<< refAvg <<" (reference)\n";
                       //std::string    maxDiffResultName = inResults_fut[iRes]->GetResultInfo()->resultName;
 
-                  } else if( types[actMsStep] == BasePDE::EIGENFREQUENCY || types[actMsStep] == BasePDE::HARMONIC ) {
+                  } else if( types[actMsStep] == BasePDE::EIGENFREQUENCY || types[actMsStep] == BasePDE::HARMONIC || types[actMsStep] == BasePDE::MULTIHARMONIC ) {
                       Vector<Complex> & inVec_fut = dynamic_cast<Result<Complex>& >(*inResults_fut[iRes]).GetVector();
                       Vector<Complex> & inVec_ref = dynamic_cast<Result<Complex>& >(*inResults_ref[iRes]).GetVector();
                       Vector<Complex> diffVec;// dynamic_cast<Result<Complex>& >(*outResults[iRes]).GetVector();
