f8317c94294d9c1a0f98a6a99856a8056393e652
Daniel Huebner <daniel.huebner@fau.de>
2020-04-03 17:04:40 +0200

    Add functions extrude, swap and coarser to process_density

--- share/python/process_density.py
+++ share/python/process_density.py
@@ -18,6 +18,9 @@ parser.add_argument("--lower", help="scale input range to new lower range", type
 parser.add_argument('--show', help="show output as image", action='store_true')
 parser.add_argument('--attribute', help="what to read from input ('design' or 'physical')", default="design")
 parser.add_argument('--set', help="specifiy set to read if you don't want the last one")
+parser.add_argument('--extrude', help="extrude 2d density to 3d. give number of slices", type=int)
+parser.add_argument('--swap', help="swap dimensions of density field. useful for trelis element numbering 'xzy'", choices=['yx', 'xzy', 'yxz', 'yzx', 'zxy', 'zyx'], type=str.lower)
+parser.add_argument('--coarser', help="coarsen the density X times. useful in combination with extrude", type=int, default=0)
 parser.add_argument('--output', help="optional output file name, for single input only")
 args = parser.parse_args()
 
@@ -41,7 +44,7 @@ for input in args.input:
   
   nr = read_density(input, "nr", set=args.set) if dens.ndim == 1 else None
   if args.attribute != 'design':
-    print("original 'design' min=" + str(numpy.amin(des)) + " max=" + str(numpy.amax(des)) + " vol=" + str(numpy.sum(des) / dens.size))
+    print("original 'design' min=" + str(numpy.amin(des)) + " max=" + str(numpy.amax(des)) + " vol=" + str(numpy.sum(des) / des.size))
   print("original '" + args.attribute + "' min=" + str(numpy.amin(dens)) + " max=" + str(numpy.amax(dens)) + " vol=" + str(numpy.sum(dens) / dens.size))
   
   lower = args.lower if args.lower else numpy.amin(des)
@@ -57,17 +60,39 @@ for input in args.input:
     org_lower = numpy.amin(dens)
     assert(False)
     #print "scale data from lower " + str(org_lower) + " to lower " + str(args.lower)
+  elif args.extrude:
+#    out = numpy.zeros((dens.shape[0], dens.shape[1], args.extrude))
+    mx, my, _ = read_mesh_info(input, silent=True)
+    print("original resolution: " + str(mx) + " x " + str(my))
+    out = extrude(dens, args.extrude)
+    print("new resolution: " + str(mx) + " x " + str(my) + " x " + str(args.extrude))
+    if nr:
+      nr = (nr + (mx*my) * numpy.arange(args.extrude)[:,None]).ravel()
+  elif args.coarser:
+    mx, my, _ = read_mesh_info(input, silent=True)
+    print("original resolution: " + str(mx) + " x " + str(my))
+  elif args.swap:
+    print("swaping axes")
+    out = des
   else:
     print("no action selected")
     sys.exit(0)    
   
   output = args.output if args.output and len(args.input) == 1 else input[:input.find('.density.xml')] + '-out.density.xml'
   
-  print("write '" + output + "' min=" + str(numpy.amin(out)) + " max=" + str(numpy.amax(out)) + " vol=" + str(numpy.sum(out) / dens.size))
+  print("write '" + output + "' min=" + str(numpy.amin(out)) + " max=" + str(numpy.amax(out)) + " vol=" + str(numpy.sum(out) / out.size))
   if dens.ndim == 1:
+    if args.swap:
+      out = swap(out, args.swap)
     write_density_file(output, out, elemnr=nr)
   else:
+    if args.swap:
+      out = swap(out, args.swap)
     write_density_file(output, out)  
+    for _ in range(args.coarser):
+      coarsen_density(output, output)
+      mx, my, mz = read_mesh_info(output, silent=True)
+      print("new resolution: " + str(mx) + " x " + str(my) + " x " + str(mz))
     
   if args.show and dens.ndim == 2:
     x, y = dens.shape
