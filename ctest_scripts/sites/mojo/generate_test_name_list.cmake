48cf84078653177fe725e8e5bf85e3c035d73add
Fabian Wein <fabian.wein@fau.de>
2020-04-03 23:36:25 +0200

    clean up nightly tests for mojo shared_opt and make mosim's test serial

--- ctest_scripts/sites/mojo/generate_test_name_list.cmake
+++ ctest_scripts/sites/mojo/generate_test_name_list.cmake
@@ -13,16 +13,17 @@ MACRO(GENERATE_TEST_NAME_LIST TEST_NAME_LIST)
   elseif(SITE_DIR MATCHES "master_stingl")     
     set(${TEST_NAME_LIST} 
      linux64_trunk_gcc_release
-     linux64_trunk_gcc_debug
-     linux64_trunk_intel_release)
+     linux64_trunk_gcc_debug)
+     #linux64_trunk_intel_release)
   else()
-    SET(${TEST_NAME_LIST}
+    set(${TEST_NAME_LIST}
      linux64_shared_opt_gcc_release
-     linux64_shared_opt_clang_release
-     linux64_shared_opt_intel_release
      linux64_shared_opt_gcc_debug
-     linux64_shared_opt_gcc_serial_debug  
      linux64_shared_opt_no_prec_gcc_release
-     linux64_shared_opt_openblas_release)
+     linux64_shared_opt_openblas_release
+
+     linux64_shared_opt_gcc8_release
+     # linux64_shared_opt_intel_release
+     linux64_shared_opt_clang_release)
   endif()  
 ENDMACRO()
