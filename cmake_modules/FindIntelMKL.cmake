5a20829a0ee29b607102c28f3020aba802bb78d1
Florian Toth <florian.toth@tuwien.ac.at>
2020-10-29 22:43:37 +0100

    removed compicated MKL test-compile during configure
    * use the same strategy as APPLE
    * info from the [Link Line Adviser](https://software.intel.com/content/www/us/en/develop/articles/intel-mkl-link-line-advisor.html)
    * correct Fortran interface lib
    * support dor sequential (USE_OPENMP=OFF, untested)

--- cmake_modules/FindIntelMKL.cmake
+++ cmake_modules/FindIntelMKL.cmake
@@ -132,6 +132,7 @@ elseif(APPLE) # END MSVC
   if(NOT MKL_ROOT_DIR)
     set(MKL_POSSIBLE_ROOT_DIRS 
        "/opt/intel/mkl"
+       "/share/programs/intel-mkl/latest/mkl"       
        $ENV{MKLROOT}) # set by compilervars.sh intel64 
 
     find_file(MKL_H
@@ -155,8 +156,12 @@ elseif(APPLE) # END MSVC
   #---------------------------------------------------------------------------
   MKL_VERSION_FROM_HEADER()
   set(MKL_INCLUDE_DIR "${MKL_ROOT_DIR}/include")
-  
-  set(MKL_LIB_DIR "${MKL_ROOT_DIR}/lib")
+
+  if(UNIX)
+    set(MKL_LIB_DIR "${MKL_ROOT_DIR}/lib/intel64_lin")
+  else()
+    set(MKL_LIB_DIR "${MKL_ROOT_DIR}/lib")
+  endif()
 
   # https://software.intel.com/content/www/us/en/develop/articles/intel-mkl-link-line-advisor.html
   if(USE_OPENMP)
@@ -172,11 +177,12 @@ elseif(APPLE) # END MSVC
     ${MKL_LIB_DIR}/libmkl_intel_lp64.a
     ${MKL_THREAD_LIB}
     ${MKL_LIB_DIR}/libmkl_core.a
-    -liomp5 
+    -liomp5
     -lpthread 
     -lm 
     -ldl)
 
+  if(APPLE)
   # for macOS and clang and MKL we need to link -lgcc_s.1.dylib from the libgfortran.a location
   # to prevent undefined references ___divtf3, ___eqtf2, ___gttf2, ..., ___subtf3, ___unordtf2
   # according to nm there is no static lib to provide this (used by libgfortran.a)
@@ -189,11 +195,22 @@ elseif(APPLE) # END MSVC
     ${MKL_BLAS_LIB}
     -L${GFORTRAN_LIB_DIR}
     -lgcc_s.1)
+  endif()
 
   # the path for libimp5 is not set by defeault: LD_LIBRARY_PATH=$MKLROOT/../compiler/lib/ works, 
   # but it is easier to copy the file to the lib-dir.
-  file(COPY ${MKL_ROOT_DIR}/../compiler/lib/libiomp5${CMAKE_SHARED_LIBRARY_SUFFIX} DESTINATION "${CFS_BINARY_DIR}/${LIB_SUFFIX}/${CFS_ARCH_STR}")
-     
+  if(UNIX)
+    set(MKL_OMP_LIB "${MKL_ROOT_DIR}/../compiler/lib/intel64_lin/libiomp5.so")
+  else()
+   set(MKL_OMP_LIB "${MKL_ROOT_DIR}/../compiler/lib/libiomp5${CMAKE_SHARED_LIBRARY_SUFFIX}")
+  endif()
+  # add a target for MKL copy
+  ADD_CUSTOM_TARGET(mkl ALL
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${MKL_ROOT_DIR}/../compiler/lib/intel64_lin/libiomp5.so ${LIBRARY_OUTPUT_PATH}/libiomp5.so
+    BYPRODUCTS ${LIBRARY_OUTPUT_PATH}/libiomp5.so ${MKL_LIB_DIR}/libmkl_intel_lp64.a ${MKL_THREAD_LIB} ${MKL_LIB_DIR}/libmkl_core.a
+    COMMENT "Copying libiomp5.so to ${LIBRARY_OUTPUT_PATH} folder..."
+    USES_TERMINAL)
+
   set(MKL_LAPACK_LIB ${MKL_BLAS_LIB})
 
   set(MKL_ROOT_DIR ${MKL_ROOT_DIR} CACHE PATH "Directory of MKL." FORCE)
@@ -202,234 +219,68 @@ elseif(APPLE) # END MSVC
   mark_as_advanced(MKL_H)
   mark_as_advanced(MKL_ROOT_DIR)
 
-else() # neither MSVC nor APPLE  #-------------------------------------------------------------------------------
-  # The idea behind the algorithm implemented for	 finding MKL, is to let the make
-  # files in the MKL  example directories tell us which linker  flags we need.  We
-  # therefore tell  the MKLROOT  (which contains a  include/mkl.h) to  an external
-  # shell script	(compile_mkl_test.sh) which  will then copy  one of  the example
-  # make files to	 a temporary directory, make some necessary  modifications to it
-  # and compile a simple test program  which will afterwards print out the version
-  # of MKL.  All outputs	of the	shell script  and test	executable are	in CMake
-  # syntax and get written to CMakeFiles/mkl.cmake  which will in turn be included
-  # from the current script.
-  # 
-  # This	somewhat complicated  procedure has  been chosen,  since Intel	seems to
-  # change the file and directory layout of their compiler and MKL suite even with
-  # every minor version. Therefore Intel should know best, what linker switches to
-  # use for what version and we happily copy that information.
-  # 
-  # ATTENTION: The path to MKL should not contain any spaces!
-  #-------------------------------------------------------------------------------
-  
-  SET (MKL_POSSIBLE_PATHS
-    # Path the user may have specified via -DMKL_ROOT_DIR:PATH=...
-    ${MKL_ROOT_DIR}
-    # Path the user may have specified as environment variable
-    $ENV{MKL_ROOT_DIR}
-    $ENV{MKL_BASE}
-    # Path which may have been given in a platform_defaults_*.cmake file 
-    ${MKL_ROOT_DIR_DEFAULT}
-    # Path set by ifortvars.sh resp. iccvars.sh
-    # This happens automatically when loading mkl 
-    # modules at Erlangens RRZE HPC cluster
-    $ENV{MKLROOT}
-    # Local paths
-    /opt/intel/composerxe-2011.4.191
-    /opt/intel/Compiler/11.1/069/mkl
-    /opt/intel/Compiler/11.0/059/Frameworks/mkl
-    /opt/intel/Compiler/11.0/081/mkl
-    /opt/intel/Compiler/11.0/074/mkl
-    /opt/intel/mkl/10.0.5.025
-    /opt/intel/composer_xe_2015.2.164/mkl
-    # path for mdmt
-    /share/programs/intel-mkl/latest/mkl
-    # Paths on Lima
-    /apps/intel/ComposerXE2013/composer_xe_2013.5.192/mkl
-    # intel 2016 tools
-    /opt/intel/compilers_and_libraries/linux/mkl
-   )
-  
-  #-------------------------------------------------------------------------------
-  # Find the first directory containing include/mkl.h
-  #-------------------------------------------------------------------------------
-  FIND_FILE(MKL_ROOT_DIR
-    NAMES include/mkl.h
-    PATHS ${MKL_POSSIBLE_PATHS}
-    NO_DEFAULT_PATH
-    NO_CMAKE_ENVIRONMENT_PATH
-    NO_CMAKE_PATH
-    NO_SYSTEM_ENVIRONMENT_PATH
-    NO_CMAKE_SYSTEM_PATH
-    )
-  
-  #-------------------------------------------------------------------------------
-  # Replace include/mkl.h with nothing to get the MKL root dir.
-  #-------------------------------------------------------------------------------
-  STRING(REPLACE "/include/mkl.h" "" MKL_ROOT_DIR "${MKL_ROOT_DIR}")
-  
-  #-------------------------------------------------------------------------------
-  # Make sure that there are no spaces in MKLROOT.
-  #-------------------------------------------------------------------------------
-  STRING(REGEX MATCH " " SPACES "${MKL_ROOT_DIR}")
-  STRING(LENGTH "${SPACES}" LEN)
-  IF(LEN GREATER 0)
-    MESSAGE(SEND_ERROR "No spaces are allowed in MKL root directory: ${MKL_ROOT_DIR}")
-  ENDIF(LEN GREATER 0)
-  
-  IF(NOT MKL_ROOT_DIR)
-    MESSAGE(FATAL_ERROR "Could not find Intel MKL library. This script just "
-      "checks explicitly for MKL installations in certain locactions. "
-      "If you have installed MKL into a different directory pleas do one of the following things: "
-      " 1. Set an environment variable MKL_ROOT_DIR or MKLROOT " 
-      " 2. Set '-DMKL_ROOT_DIR:PATH=...' on the CMake command line "
-      " 3. Or adapt the MKL_POSSIBLE_PATHS at the beginning of cmake_modules/FindIntelMKL.cmake. "
-      "You may also just copy MKL from LSE /home/shareAll/linux_bin/intel/mkl. "
-      "Another option is to change USE_BLAS_LAPACK and CFS_PARDISO to different "
-      "implementations. OpenBLAS and should provide similar performance to MKL.")
-  ELSE(NOT MKL_ROOT_DIR)
-    SET(MKL_FOUND 1)
-  ENDIF(NOT MKL_ROOT_DIR)
-  
-  SET(MKL_ROOT_DIR ${MKL_ROOT_DIR} CACHE PATH "Directory of MKL." FORCE)
-  SET(MKL_INCLUDE_DIR "${MKL_ROOT_DIR}/include")
-  
-  #-------------------------------------------------------------------------------
-  # Determine version of MKL and linker flags by compiling a little test program
-  # and   parsing  the   command   line   generated  by   a   makefile  from 
-  # MKLROOT/examples/solver
-  #-------------------------------------------------------------------------------
-  
-    #---------------------------------------------------------------------------
-    # Read mkl version from header files
-    #---------------------------------------------------------------------------
-    MKL_VERSION_FROM_HEADER()
-  
-    #-----------------------------------------------------------------------------
-    # Specify desired architecture and output type for make file.
-    #-----------------------------------------------------------------------------
-  
-    IF(CFS_ARCH STREQUAL "X86_64")
-      SET(MKL_ARCH_ID "intel64")
-    ENDIF()
-  
-    #-----------------------------------------------------------------------------
-    # Specify compiler type for make file.
-    #-----------------------------------------------------------------------------
-    IF(CFS_CXX_COMPILER_NAME STREQUAL "ICC")
-      SET(MKL_COMPILER_ID "intel")
-    ELSE()
-      SET(MKL_COMPILER_ID "gnu")
-    ENDIF() 
-  
-    #-----------------------------------------------------------------------------
-    # We always want to link against the parallel version of MKL.
-    #-----------------------------------------------------------------------------
-    IF(${MKL_MAJOR_VERSION}.${MKL_MINOR_VERSION}.${MKL_UPDATE} VERSION_LESS 11.3.3)
-      SET(MKL_THREADING_ID "parallel")
-    ELSE()
-      SET(MKL_THREADING_ID "omp")
-    ENDIF()
-  
-    #-----------------------------------------------------------------------------
-    # Make a directory for the test compile
-    #-----------------------------------------------------------------------------
-    SET(COMPILE_MKL_TEST_DIR "${CFS_BINARY_DIR}/tmp/mkl_test")
-    FILE(MAKE_DIRECTORY "${COMPILE_MKL_TEST_DIR}")
-  
-    #-----------------------------------------------------------------------------
-    # Search for the proper $MKLROOT/examples/solver[c]/makefile.
-    #-----------------------------------------------------------------------------
-    foreach(fname "solver" "solverc") # search for example directly 
-      file(GLOB_RECURSE MKL_SOLVERMAKEFILE FOLLOW_SYMLINKS "${MKL_ROOT_DIR}/*/examples/${fname}/[Mm]akefile")
-        if(MKL_SOLVERMAKEFILE)
-          break()
-        endif(MKL_SOLVERMAKEFILE)
-    endforeach(fname)
-    
-    if(NOT MKL_SOLVERMAKEFILE) # serach for archive and extract it if found
-      foreach(fname "examples_core.tgz" "examples_core_c.tgz")
-        file(GLOB_RECURSE MKL_SOLVERMAKEFILE_TGZ FOLLOW_SYMLINKS "${MKL_ROOT_DIR}/*/${fname}")
-        #message("${fname}:${MKL_SOLVERMAKEFILE_TGZ}")
-        if(MKL_SOLVERMAKEFILE_TGZ)
-          execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf ${MKL_SOLVERMAKEFILE_TGZ}
-            WORKING_DIRECTORY "${COMPILE_MKL_TEST_DIR}"
-          )
-          file(GLOB_RECURSE MKL_SOLVERMAKEFILE FOLLOW_SYMLINKS "${COMPILE_MKL_TEST_DIR}/solverc/makefile")
-          break()
-        endif(MKL_SOLVERMAKEFILE_TGZ)
-      endforeach(fname)
-    endif(NOT MKL_SOLVERMAKEFILE)
-  
-    if(NOT MKL_SOLVERMAKEFILE)
-      message(FATAL_ERROR "Did not find example solver makefile. Have you sourced MKL in bashrc?")
-    endif(NOT MKL_SOLVERMAKEFILE)
-  
-    #-----------------------------------------------------------------------------
-    # Configure our little build script using the previously defined variables.
-    #-----------------------------------------------------------------------------
-    CONFIGURE_FILE(
-      "${CFS_SOURCE_DIR}/share/scripts/compile_mkl_test.sh.in"
-      "${COMPILE_MKL_TEST_DIR}/compile_mkl_test.sh" @ONLY)
-   
-    # Make sure we can execute the script
-    EXECUTE_PROCESS(
-      COMMAND chmod 755 "${COMPILE_MKL_TEST_DIR}/compile_mkl_test.sh"
-      WORKING_DIRECTORY "${COMPILE_MKL_TEST_DIR}")
-    
-    #-----------------------------------------------------------------------------
-    # Execute the build script and put the generated CMake code into mkl.cmake.
-    #-----------------------------------------------------------------------------
-    EXECUTE_PROCESS(
-      COMMAND sh "${COMPILE_MKL_TEST_DIR}/compile_mkl_test.sh"
-      OUTPUT_VARIABLE MKL_INFO
-      WORKING_DIRECTORY "${COMPILE_MKL_TEST_DIR}"
-      RESULT_VARIABLE RETVAL)
-  
-    IF(RETVAL EQUAL 0)
-      # replace the intel-version with the fortran version needed for ARPACK
-      # see: https://software.intel.com/en-us/forums/intel-math-kernel-library/topic/560573
-      IF(NOT CFS_CXX_COMPILER_NAME STREQUAL "ICC")
-        string(REPLACE "libmkl_intel_lp64" "libmkl_gf_lp64" MKL_INFO "${MKL_INFO}")
-      ENDIF()
-      FILE(WRITE "${CFS_BINARY_DIR}/CMakeFiles/mkl.cmake" ${MKL_INFO})
-      FILE(REMOVE_RECURSE "${COMPILE_MKL_TEST_DIR}")
-  
-      #---------------------------------------------------------------------------
-      # Finally just include the linker flags and version info.
-      #---------------------------------------------------------------------------
-      INCLUDE(${CFS_BINARY_DIR}/CMakeFiles/mkl.cmake)
-      #---------------------------------------------------------------------------
-      # Create a custom target to copy over libiomp5.so
-      # here one could include the option USE_OMP=OFF which does not need to not copy libomp5
-      #---------------------------------------------------------------------------
-      ADD_CUSTOM_TARGET(mkl_libomp5 ALL
-        COMMAND ${CMAKE_COMMAND} -E copy_if_different ${MKL_OMP_DIR}/libiomp5.so ${LIBRARY_OUTPUT_PATH}/libiomp5.so
-        BYPRODUCTS ${LIBRARY_OUTPUT_PATH}/libiomp5.so
-        COMMENT "Copying libiomp5.so to ${LIBRARY_OUTPUT_PATH} folder..."
-        USES_TERMINAL)
-  
-    ELSE()
-      MESSAGE("executing ${COMPILE_MKL_TEST_DIR}/compile_mkl_test.sh in ${COMPILE_MKL_TEST_DIR} failed")
-      MESSAGE("RETVAL=${RETVAL} and MKL_INFO=${MKL_INFO}")
-      MESSAGE(SEND_ERROR "A problem occurred during determination of MKL linker
-                          flags. Please run ${CFS_BINARY_DIR}/tmp/mkl_test/compile_mkl_test.sh
-                          by hand to investigate the problem. Script output was\n ${MKL_INFO}")
-    ENDIF()
-  
-  SET(CFS_MKL_VERSION ${MKL_MAJOR_VERSION})
-  
-  #-------------------------------------------------------------------------------
-  # Mark libraries as advanced variables.
-  #-------------------------------------------------------------------------------
-  MARK_AS_ADVANCED(MKL_BLAS_LIB)
-  MARK_AS_ADVANCED(MKL_LAPACK_LIB)
-  MARK_AS_ADVANCED(MKL_PARDISO_LIB)
-  MARK_AS_ADVANCED(MKL_ROOT_DIR)
-  MARK_AS_ADVANCED(MKL_OMP_DIR)
-  
-  # TODO: libguide und libiomp durch die von MKL ersetzen.
-  # TODO: fuer libmkl_intel_lp64.a libmkl_gf_lp64.a fuer gnu einsetzen.
+elseif(UNIX)
+  if(NOT MKL_ROOT_DIR)
+    set(MKL_POSSIBLE_ROOT_DIRS
+       "/opt/intel/mkl"
+       "/share/programs/intel-mkl/latest/mkl"
+       $ENV{MKLROOT}) # set by compilervars.sh intel64
 
+    find_file(MKL_H
+      "include/mkl.h"
+      PATHS ${MKL_POSSIBLE_ROOT_DIRS}
+      DOC "MKL root dir"
+      NO_DEFAULT_PATH
+      NO_CMAKE_ENVIRONMENT_PATH
+      NO_CMAKE_PATH
+      NO_SYSTEM_ENVIRONMENT_PATH
+      NO_CMAKE_SYSTEM_PATH
+      NO_CMAKE_FIND_ROOT_PATH)
+    mark_as_advanced(MKL_H)
+
+    string(REPLACE "/include/mkl.h" "" MKL_ROOT_DIR "${MKL_H}")
+    message(STATUS "MKL_ROOT_DIR set from include/mkl.h: ${MKL_ROOT_DIR}")
+  endif()
+  set(MKL_ROOT_DIR ${MKL_ROOT_DIR} CACHE PATH "Directory of MKL.")
+  mark_as_advanced(MKL_ROOT_DIR)
+
+  #---------------------------------------------------------------------------
+  # Read mkl version from header files
+  #---------------------------------------------------------------------------
+  MKL_VERSION_FROM_HEADER()
+
+  # set include dir
+  set(MKL_INCLUDE_DIR "${MKL_ROOT_DIR}/include")
+
+  if(USE_OPENMP)
+    set(MKL_OMP_LIB "${MKL_ROOT_DIR}/../compiler/lib/intel64_lin/libiomp5.so")
+    # copy over
+    file(COPY ${MKL_OMP_LIB} DESTINATION ${LIBRARY_OUTPUT_PATH})
+    set(MKL_OMP_LIB_LINE "-L${LIBRARY_OUTPUT_PATH} -liomp5")
+    set(MKL_THREADING_LIB "${MKL_ROOT_DIR}/lib/intel64/libmkl_intel_thread.a")
+  else()
+    set(MKL_THREADING_LIB "${MKL_ROOT_DIR}/lib/intel64/libmkl_sequential.a")
+  endif()
+
+  # set the link line, essentailly from
+  # see https://software.intel.com/content/www/us/en/develop/articles/intel-mkl-link-line-advisor.html
+  # regarting the correct Fortratn infterface library (libmkl_gf_lp64):
+  # see: https://software.intel.com/en-us/forums/intel-math-kernel-library/topic/560573
+  set(MKL_BLAS_LIB 
+     -Wl,--start-group
+     ${MKL_ROOT_DIR}/lib/intel64/libmkl_gf_lp64.a
+     ${MKL_THREADING_LIB}
+     ${MKL_ROOT_DIR}/lib/intel64/libmkl_core.a
+     -Wl,--end-group
+     ${MKL_OMP_LIB_LINE}
+     -lpthread
+     -lm
+     -ldl)
+
+  set(MKL_LAPACK_LIB ${MKL_BLAS_LIB})
+
+else() # neither MSVC, APPLE or UNIX
+  message("what system?")
 ENDIF() # end of UNIX
 
 #-------------------------------------------------------------------------------
@@ -451,10 +302,9 @@ SET(PARDISO_LIBRARY "${MKL_PARDISO_LIB}")
 # Status message of found MKL
 #-------------------------------------------------------------------------------
 IF(DEFINED MKL_UPDATE)
-  MESSAGE(STATUS "Found Intel MKL version ${MKL_MAJOR_VERSION}.${MKL_MINOR_VERSION}.${MKL_UPDATE}.")
+  MESSAGE(STATUS "Using Intel MKL version ${MKL_MAJOR_VERSION}.${MKL_MINOR_VERSION}.${MKL_UPDATE}.")
 ELSE()
-  MESSAGE(STATUS "Found Intel MKL version ${MKL_MAJOR_VERSION}.${MKL_MINOR_VERSION}.")
+  MESSAGE(STATUS "Using Intel MKL version ${MKL_MAJOR_VERSION}.${MKL_MINOR_VERSION}.")
 ENDIF()
 
-
-
+message(STATUS "defining MKL link-line via MKL_BLAS_LIB=${MKL_BLAS_LIB}")
