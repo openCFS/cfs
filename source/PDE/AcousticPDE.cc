d723fed4f20fa8eda71fb737ff0d0e750807f9f6
pheidegger <patrick.heidegger@tugraz.at>
2023-02-23 10:38:38 +0100

    switched includes into header file, as usually includes should be set there

--- source/PDE/AcousticPDE.cc
+++ source/PDE/AcousticPDE.cc
@@ -1,50 +1,5 @@
 #include "AcousticPDE.hh"
 
-#include "General/defs.hh"
-
-#include "DataInOut/ParamHandling/ParamTools.hh"
-#include "DataInOut/Logging/LogConfigurator.hh"
-
-//new integrator concept
-#include "Forms/BiLinForms/BDBInt.hh"
-#include "Forms/BiLinForms/BBInt.hh"
-#include "Forms/BiLinForms/ABInt.hh"
-#include "Forms/BiLinForms/ADBInt.hh"
-#include "Forms/LinForms/BUInt.hh"
-#include "Forms/LinForms/BDUInt.hh"
-#include "Forms/LinForms/KXInt.hh"
-#include "Forms/Operators/GradientOperator.hh"
-#include "Forms/Operators/IdentityOperator.hh"
-#include "Forms/Operators/IdentityOperatorNormal.hh"
-#include "Forms/Operators/ConvectiveOperator.hh"
-#include "Forms/Operators/SurfaceOperators.hh"
-#include "Forms/Operators/DivOperator.hh"
-
-#include "FeBasis/FeFunctions.hh"
-#include "Utils/StdVector.hh"
-#include "FeBasis/H1/FeSpaceH1Nodal.hh"
-
-#include "Domain/Results/ResultFunctor.hh"
-
-#include "Driver/Assemble.hh"
-#include "Domain/CoefFunction/CoefXpr.hh"
-#include "Domain/CoefFunction/CoefFunctionCompound.hh"
-#include "Domain/CoefFunction/CoefFunctionConst.hh"
-#include "Domain/CoefFunction/CoefFunctionMulti.hh"
-#include "Domain/CoefFunction/CoefFunctionPML.hh"
-#include "Domain/CoefFunction/CoefFunctionFormBased.hh"
-#include "Domain/CoefFunction/CoefFunctionSurf.hh"
-#include "Domain/CoefFunction/CoefFunctionImpedanceModel.hh"
-#include "Domain/Mesh/NcInterfaces/BaseNcInterface.hh"
-
-#include <boost/lexical_cast.hpp>
-#include <cmath>
-
-
-#include "Driver/SolveSteps/StdSolveStep.hh"
-#include "Driver/TimeSchemes/TimeSchemeGLM.hh"
-#include "Materials/AcousticMaterial.hh"
-
 namespace CoupledField{
 
    DEFINE_LOG(acousticpde, "pde.acoustic")
@@ -209,7 +164,7 @@ namespace CoupledField{
     //variables used for PML
     bool harmonicPML = false;    // flag that states if we are in a PML region while performing a harmonic analysis
     std::string pmlDampId = "";  // dampId of a PML region
-    std::string pmlFormul = "";  // formulation of the PML region ("classic" or "curvilinear")
+    std::string pmlFormul = "";  // formulation of the PML region ("classic", "shifted" or "curvilinear")
 
     //type of geometry
     isaxi_ = ptGrid_->IsAxi();
