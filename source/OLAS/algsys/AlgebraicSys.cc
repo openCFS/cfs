591e841cc44725ad15cb618745961c3173e8922b
dmayrhof <dominik.mayrhofer@tuwien.ac.at>
2023-08-18 16:03:14 +0200

    Fixed a counting bug and added zero comparison

--- source/OLAS/algsys/AlgebraicSys.cc
+++ source/OLAS/algsys/AlgebraicSys.cc
@@ -3601,7 +3601,12 @@ namespace CoupledField {
     auto NumRows = scrsMat.GetNumRows();
     auto counter = 0;
     for (UInt i = 0; i < size; i++) {
-      if (DataVec[i] < abs(1e-14)) counter++;
+      if (DataVec[i] > abs(1e-14)) counter++;
+    }
+
+    auto counterZero = 0;
+    for (UInt i = 0; i < size; i++) {
+      if (DataVec[i] != 0.0) counterZero++;
     }
 
     //StdVector<Double> DataVecNew[counter];
