e472d99b65b643a94aba1ed4f99d96afa4311e93
dmayrhof <dominik.mayrhofer@tuwien.ac.at>
2023-08-26 12:05:28 +0200

    Cleanup. If this pipeline passes, everything should be ready for the UI adaption

--- source/OLAS/solver/BaseSolver.hh
+++ source/OLAS/solver/BaseSolver.hh
@@ -61,7 +61,7 @@ namespace CoupledField {
     virtual void SetNewMatrixPattern() = 0;
     
     //! \see BasePrecond::Setup
-    //virtual void Setup(BaseMatrix& sysmat) = 0;
+    virtual void Setup(BaseMatrix& sysmat) = 0;
     
   
     //! Solve the linear system sysmat*sol=rhs for sol
