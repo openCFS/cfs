028fe217d970fe9e95552d1543222ded8329b5cd
LastGentleman <candussi@student.tugraz.at>
2023-10-03 11:26:04 +0200

    Adding NiHu to CMake-Workflow
    clean up

---
+++ External_NiHu.cmake
@@ -0,0 +1,32 @@
+include(ExternalProject)
+
+# Builds external third party projects.
+# The parent script CMakeLists.txt defines the "GLOBAL_OUTPUT_PATH" variable,
+# which will be used as output directory for all *.lib, *.dll, *.a, *.so, *.pdb files.
+
+###############################################################################
+# NiHu
+###############################################################################
+
+###link_directories(${GLOBAL_OUTPUT_PATH}) # Where else to put!?
+###file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/external/NIHU/)
+
+message("STARTING EXTERNAL_NIHU!!")
+include(ExternalProject)
+
+ExternalProject_Add(
+  NiHu
+  
+  PREFIX ${CMAKE_BINARY_DIR}/cfsdeps/nihu    # external project's base path
+  GIT_REPOSITORY "git://last.hit.bme.hu/toolbox/nihu.git"
+  GIT_TAG "nightly"
+
+  UPDATE_COMMAND "" # specify if needed
+  PATCH_COMMAND ""  # specify if needed
+
+  ## path to NiHu's CMakeLists
+  CMAKE_ARGS -H${CMAKE_BINARY_DIR}/cfsdeps/nihu/src/NiHu/src
+
+  # Here we could utilize an alias e.g.
+  CMAKE_ARGS -DNIHU_INSTALL_DIR=${CMAKE_BINARY_DIR}/cfsdeps/nihu/src/NiHu-build/install_dir
+)
