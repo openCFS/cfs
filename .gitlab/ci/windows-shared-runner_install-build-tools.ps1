6ba24e4775bc3f3d7a134ff52fabdce2bf6f06a6
Florian Toth <florian.toth@tuwien.ac.at>
2024-01-10 20:22:01 +0100

    update to oneAPI 2024 in windows pipeline
    * BaseKit and HPCKit must be at the same major version to work together

--- .gitlab/ci/windows-shared-runner_install-build-tools.ps1
+++ .gitlab/ci/windows-shared-runner_install-build-tools.ps1
@@ -1,7 +1,7 @@
 # this should be run from the CFS root directory
 
 # install Fortran compiler
-.gitlab/ci/install_windows.bat https://registrationcenter-download.intel.com/akdlm/irc_nas/18976/w_HPCKit_p_2022.3.1.19755_offline.exe intel.oneapi.win.ifort-compiler
+.gitlab/ci/install_windows.bat https://registrationcenter-download.intel.com/akdlm/IRC_NAS/7a6db8a1-a8b9-4043-8e8e-ca54b56c34e4/w_HPCKit_p_2024.0.1.35_offline.exe intel.oneapi.win.ifort-compiler
 # remove unnecessary stuff
 $LATEST_VERSION=Get-ChildItem -Path "$env:INTEL_INSTALL_DIR\compiler\" -Name | Select-String -NotMatch latest | %{$_.Line} | Sort-Object | Select-Object -Last 1
 
@@ -13,9 +13,9 @@ Remove-Item "$env:INTEL_INSTALL_DIR\compiler\$LATEST_VERSION\windows\lib\ocloc" 
 Remove-Item "$env:INTEL_INSTALL_DIR\compiler\$LATEST_VERSION\windows\lib\x86" -Force  -Recurse -ErrorAction SilentlyContinue
 
 # install MKL
-.gitlab/ci/install_windows.bat https://registrationcenter-download.intel.com/akdlm/irc_nas/18969/w_BaseKit_p_2022.3.1.19798_offline.exe intel.oneapi.win.mkl.devel
+.gitlab/ci/install_windows.bat https://registrationcenter-download.intel.com/akdlm/IRC_NAS/5cb30fb9-21e9-47e8-82da-a91e00191670/w_BaseKit_p_2024.0.1.45_offline.exe intel.oneapi.win.mkl.devel
 
 # install git
 wget.exe --quiet https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/PortableGit-2.43.0-64-bit.7z.exe
-./PortableGit-2.43.0-64-bit.7z.exe -o"${pwd}/cache/git" -y
+./PortableGit-2.43.0-64-bit.7z.exe -o"$env:CI_PROJECT_DIR/cache/git" -y
 Remove-Item PortableGit-2.43.0-64-bit.7z.exe -Force -Recurse -ErrorAction SilentlyContinue
