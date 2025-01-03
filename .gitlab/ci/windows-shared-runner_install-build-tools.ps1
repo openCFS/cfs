bef0fab989150deddbf2254af6dfe28018237767
Florian Toth <florian.toth@tuwien.ac.at>
2025-01-03 08:23:05 +0100

    remove all qutation marks and retype all dashes

--- .gitlab/ci/windows-shared-runner_install-build-tools.ps1
+++ .gitlab/ci/windows-shared-runner_install-build-tools.ps1
@@ -2,10 +2,10 @@
 
 # install ccache (https://ccache.dev/download.html)
 wget.exe --quiet https://github.com/ccache/ccache/releases/download/v4.10.2/ccache-4.10.2-windows-x86_64.zip
-Expand-Archive -LiteralPath "ccache-4.10.2-windows-x86_64.zip" -DestinationPath "ccache-extract"
-New-Item  -ItemType "directory" -Path "cache/ccache"
-Get-Childitem –Path "./ccache-extract"  -Include "ccache.exe" -File -Recurse | Copy-Item  -Destination "./cache/ccache/"
+Expand-Archive -Path ccache-4.10.2-windows-x86_64.zip -DestinationPath ccache-extract
+New-Item -ItemType directory -Path cache/ccache
+Get-Childitem -Path ccache-extract -Include "ccache.exe" -File -Recurse | Copy-Item  -Destination cache/ccache
 # cleanup
-Remove-Item "ccache-4.10.2-windows-x86_64.zip" -Force -Recurse -ErrorAction SilentlyContinue
-Remove-Item "ccache-extract" -Force -Recurse -ErrorAction SilentlyContinue
+Remove-Item ccache-4.10.2-windows-x86_64.zip -Force -Recurse -ErrorAction SilentlyContinue
+Remove-Item ccache-extract -Force -Recurse -ErrorAction SilentlyContinue
 
