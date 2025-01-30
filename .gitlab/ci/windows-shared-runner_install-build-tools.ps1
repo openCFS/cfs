c06b95f58bd62caecbcb21f6ebc45cee6a5a0847
Fabian Wein <fabian.wein@fau.de>
2025-01-30 17:53:11 +0100

    Switch Windows to Intel oneAPI 2025.0 and ifx
    * use ifx instead deprecated ifort
    * manually shrink ifx/mkl from 9 GB to 2.7 GB
    * activate parallel msvc tests
    * be robust when we have modified working copy packed

--- .gitlab/ci/windows-shared-runner_install-build-tools.ps1
+++ .gitlab/ci/windows-shared-runner_install-build-tools.ps1
@@ -9,20 +9,34 @@ Get-Childitem -Path ccache-extract -Include "ccache.exe" -File -Recurse | Copy-I
 Remove-Item ccache-4.10.2-windows-x86_64.zip -Force -Recurse -ErrorAction SilentlyContinue
 Remove-Item ccache-extract -Force -Recurse -ErrorAction SilentlyContinue
 
-# install Fortran compiler
-.gitlab/ci/install_windows.bat https://registrationcenter-download.intel.com/akdlm/IRC_NAS/7a6db8a1-a8b9-4043-8e8e-ca54b56c34e4/w_HPCKit_p_2024.0.1.35_offline.exe intel.oneapi.win.ifort-compiler
-# remove unnecessary stuff
-$LATEST_VERSION=Get-ChildItem -Path "$env:INTEL_INSTALL_DIR\compiler" -Name | Select-String -NotMatch latest | %{$_.Line} | Sort-Object | Select-Object -Last 1
+# we install ifx and mkl from the same source
+# we get and install Intel oneAPI based on https://www.intel.com/content/www/us/en/developer/tools/oneapi/hpc-toolkit-download.html  
+wget.exe --quiet https://registrationcenter-download.intel.com/akdlm/IRC_NAS/a37c30c3-a846-4371-a85d-603e9a9eb94c/intel-oneapi-hpc-toolkit-2025.0.1.48_offline.exe -O oneapi_offline.exe
 
-Remove-Item "$env:INTEL_INSTALL_DIR\compiler\$LATEST_VERSION\windows\compiler\lib\ia32_win" -Force  -Recurse -ErrorAction SilentlyContinue
-Remove-Item "$env:INTEL_INSTALL_DIR\compiler\$LATEST_VERSION\windows\bin\intel64_ia32" -Force  -Recurse -ErrorAction SilentlyContinue
-Remove-Item "$env:INTEL_INSTALL_DIR\compiler\$LATEST_VERSION\windows\lib\emu" -Force  -Recurse -ErrorAction SilentlyContinue
-Remove-Item "$env:INTEL_INSTALL_DIR\compiler\$LATEST_VERSION\windows\lib\oclfpga" -Force  -Recurse -ErrorAction SilentlyContinue
-Remove-Item "$env:INTEL_INSTALL_DIR\compiler\$LATEST_VERSION\windows\lib\ocloc" -Force  -Recurse -ErrorAction SilentlyContinue
-Remove-Item "$env:INTEL_INSTALL_DIR\compiler\$LATEST_VERSION\windows\lib\x86" -Force  -Recurse -ErrorAction SilentlyContinue
+# install in two steps. We are Administrator, see https://learn.microsoft.com/de-de/archive/blogs/virtual_pc_guy/a-self-elevating-powershell-script
+# inspired by https://github.com/oneapi-src/oneapi-ci/blob/master/scripts/install_windows.bat
+# note that $CI_PROJECT_DIR is the current dir C:\GitLab-Runner\builds\openCFS\cfs
+Start-Process -FilePath "./oneapi_offline.exe" -ArgumentList "-s -x -f oneapi_extracted --log extract.log" -NoNewWindow -Wait
+Start-Process -FilePath "./oneapi_extracted/bootstrapper.exe" -ArgumentList "-s --action install --install-dir=$CI_PROJECT_DIR\cache\oneAPI --log-dir=$CI_PROJECT_DIR\oneapilog  --components=intel.oneapi.win.ifort-compiler:intel.oneapi.win.mkl.devel --eula=accept -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=0 " -NoNewWindow -Wait
 
-# install MKL
-.gitlab/ci/install_windows.bat https://registrationcenter-download.intel.com/akdlm/IRC_NAS/5cb30fb9-21e9-47e8-82da-a91e00191670/w_BaseKit_p_2024.0.1.45_offline.exe intel.oneapi.win.mkl.devel
+# clean up temporary files
+Remove-Item oneapi_offline.exe -Force 
+Remove-Item oneapi_extracted -Force -Recurse 
+Remove-Item extract.log -Force
+Remove-Item oneapilog -Force -Recurse 
+
+# remove unnecessary stuff. From originally 9 GB keep 3 GB
+Remove-Item cache/oneAPI/ocloc -Force  -Recurse
+Remove-Item cache/oneAPI/debugger -Force  -Recurse
+Remove-Item cache/oneAPI/tcm -Force  -Recurse
+Remove-Item cache/oneAPI/umf -Force  -Recurse
+Remove-Item cache/oneAPI/mpi -Force  -Recurse
+Remove-Item cache/oneAPI/compiler_ide -Force  -Recurse
+Remove-Item cache/oneAPI/tbb -Force  -Recurse 
+Remove-Item cache/oneAPI/pti -Force  -Recurse 
+# sycl is actually the biggest part (about 5 GB)
+Remove-Item cache/oneAPI/mkl/latest/lib/*sycl* -Force  -Recurse
+Remove-Item cache/oneAPI/mkl/latest/bin/*sycl* -Force  -Recurse
 
 # install git
 wget.exe --quiet https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/PortableGit-2.43.0-64-bit.7z.exe
