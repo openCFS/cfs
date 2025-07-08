0364d510d1ffd954a2b0d126ce754f04664782cd
LastGentleman <candussi@student.tugraz.at>
2025-07-08 12:28:55 +0200

    re-added testcase(s)

---
+++ Testsuite/TESTSUIT/Singlefield/AcousticsBEM/testcase_03/testcase_03_NiHu/testcase_03_NiHu.cc
@@ -0,0 +1,15 @@
+// #include "core/weighted_residual.hpp"
+// #include "tmp/algorithm.hpp"
+
+#include "/home/candussi/Devel_RK1/FEM_BEM_20240109/BIN/build23/cfsdeps/nihu/src/NiHu-install/include/tmp/algorithm.hpp"
+#include "/home/candussi/Devel_RK1/FEM_BEM_20240109/BIN/build23/cfsdeps/nihu/src/NiHu-install/include/core/weighted_residual.hpp"
+
+#include <iostream>
+
+int main(void)
+{
+  // ...
+  std::cout << "testcase_03" << std::endl;
+
+  return 0;
+}
