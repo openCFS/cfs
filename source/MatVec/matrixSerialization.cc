5752e097372d80cf3221474d2e7b6c41d62f14eb
Fabian Wein <fabian.wein@fau.de>
2026-07-08 00:37:55 +0200

    remove unused matrix/vectorSerialization
    * as discussed in openCFS-Meeting July 2026
    * spoiles Windows compile with warnings

--- source/MatVec/matrixSerialization.cc
+++
@@ -1,7 +0,0 @@
-#include "matrixSerialization.hh"
-#include <boost/serialization/export.hpp>
-
-BOOST_CLASS_EXPORT_GUID(CoupledField::Matrix<CoupledField::Double>, "CoupledField_Matrix_Double")
-BOOST_CLASS_EXPORT_GUID(CoupledField::Matrix<CoupledField::Complex>, "CoupledField_Matrix_Complex")
-BOOST_CLASS_EXPORT_GUID(CoupledField::Matrix<CoupledField::Integer>, "CoupledField_Matrix_Integer")
-BOOST_CLASS_EXPORT_GUID(CoupledField::Matrix<CoupledField::UInt>,    "CoupledField_Matrix_UInt" )
