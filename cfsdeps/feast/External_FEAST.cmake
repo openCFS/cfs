a4c29c930bfe8056ec4f723e6bc9225eedfe1ac1
Fabian Wein <fabian.wein@fau.de>
2026-08-11 09:42:00 +0200

    prepare FEAST for macOS

--- cfsdeps/feast/External_FEAST.cmake
+++ cfsdeps/feast/External_FEAST.cmake
@@ -10,7 +10,7 @@ set(FEAST_VER ${PACKAGE_VER}) # for --version
 set(PACKAGE_FILE "feast_${PACKAGE_VER}.tgz")
 set(PACKAGE_MD5 "e4e6b47de276c203de2c0e9e7d9e5a65")
 set(PACKAGE_MIRRORS "https://gitlab.com/api/v4/projects/12930334/packages/generic/cfsdeps/sources/${PACKAGE_FILE}")  
-set(DEPS_VER "-d") # set to "-a", "-b", when dependency changed with same PACKAGE_VER. Reset to "" with new PACKAGE_VER.
+set(DEPS_VER "-e") # set to "-a", "-b", when dependency changed with same PACKAGE_VER. Reset to "" with new PACKAGE_VER.
 
 # we cannot link a parallel compiled suitesparse with debug without openmp
 # we need to set DEPS_ID before calling set_precompiled_pckg_file()
@@ -62,7 +62,11 @@ else()
 endif()
 if(USE_BLAS_LAPACK STREQUAL "MKL")
   set(ADDITIONAL_FORTRAN_ARGS "${ADDITIONAL_FORTRAN_ARGS} -DMKL")
-endif() 
+elseif(USE_BLAS_LAPACK STREQUAL "ACCELERATE")
+  # Apple's legacy BLAS is f2c based, a REAL function returns a double (SCNRM2 in libnum.f90).
+  # -ff2c implies -fsecond-underscore which would rename zfeast_scsrgvx_ -> zfeast_scsrgvx__
+  set(ADDITIONAL_FORTRAN_ARGS "${ADDITIONAL_FORTRAN_ARGS} -ff2c -fno-second-underscore")
+endif()
 list(APPEND DEPS_ARGS "-DCMAKE_Fortran_FLAGS:STRING=${CFSDEPS_Fortran_FLAGS} ${ADDITIONAL_FORTRAN_ARGS}") 
 
 # copy "static" license as we configure this dependency. Check if license is still valid!
